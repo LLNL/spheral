b1ff9187fb90d51de50bfb4e44007df385d3d1bd
Mike Owen <mikeowen@llnl.gov>
2011-12-29 21:38:20 -0800

    Different compilers complain about how we resolve min and max.

--- src/NodeGenerators/fillFacetedVolume.cc
+++ src/NodeGenerators/fillFacetedVolume.cc
@@ -3,9 +3,6 @@
 namespace Spheral {
 
 using namespace std;
-using std::abs;
-using std::min;
-using std::max;
 
 //------------------------------------------------------------------------------
 // Fill an outer bounding volume.
