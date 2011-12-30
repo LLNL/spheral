a232202a3b099f88fa8ea235b7ed3b499804dbbe
Mike Owen <mikeowen@llnl.gov>
2011-12-29 23:54:56 -0800

    Taking advantage of parallelism in the fillBoundary generator helper.

--- src/NodeGenerators/fillFacetedVolume.cc
+++ src/NodeGenerators/fillFacetedVolume.cc
@@ -9,13 +9,18 @@ using namespace std;
 //------------------------------------------------------------------------------
 vector<Dim<3>::Vector>
 fillFacetedVolume(const Dim<3>::FacetedVolume& outerBoundary,
-                  const unsigned n1d) {
+                  const unsigned n1d,
+                  const unsigned domain,
+                  const unsigned numDomains) {
+  VERIFY(n1d > 0);
+  VERIFY(numDomains >= 1);
+  VERIFY(domain < numDomains);
+
   typedef Dim<3>::Vector Vector;
   typedef Dim<3>::FacetedVolume FacetedVolume;
 
   vector<Vector> result;
-  unsigned ix, iy, iz;
-  double x, y, z;
+  unsigned i, ix, iy, iz;
   const Vector& xmin = outerBoundary.xmin();
   const Vector& xmax = outerBoundary.xmax();
   const double dx = (xmax - xmin).maxElement()/n1d;
@@ -26,20 +31,29 @@ fillFacetedVolume(const Dim<3>::FacetedVolume& outerBoundary,
   CHECK(nx > 0 and nx <= n1d);
   CHECK(ny > 0 and ny <= n1d);
   CHECK(nz > 0 and nz <= n1d);
-  result.reserve(nx*ny*nz);
+
+  // Figure out the range of global IDs for this domain.
+  const unsigned nxy = nx*ny;
+  const unsigned nxyz = nx*ny*nz;
+  const unsigned ndomain0 = nxyz/numDomains;
+  const unsigned remainder = nxyz - ndomain0*numDomains;
+  CHECK(remainder < numDomains);
+  const unsigned ndomain = nxyz/numDomains + (domain < remainder ? 1 : 0);
+  const unsigned imin = domain*ndomain0 + min(domain, remainder);
+  const unsigned imax = imin + ndomain;
+  CHECK(domain < numDomains - 1 or imax == nxyz);
+  result.reserve(ndomain);
 
   if (outerBoundary.convex()) {
 
-    for (iz = 0; iz != nz; ++iz) {
-      z = xmin.z() + (iz + 0.5)*dx;
-      for (iy = 0; iy != ny; ++iy) {
-        y = xmin.y() + (iy + 0.5)*dx;
-        for (ix = 0; ix != nx; ++ix) {
-          x = xmin.x() + (ix + 0.5)*dx;
-          Vector pos(x, y, z);
-          if (outerBoundary.contains(pos)) result.push_back(pos);
-        }
-      }
+    for (i = imin; i != imax; ++i) {
+      ix = i % nx;
+      iy = (i / nx) % ny;
+      iz = i / nxy;
+      const Vector pos(xmin.x() + (ix + 0.5)*dx,
+                       xmin.y() + (iy + 0.5)*dx,
+                       xmin.z() + (iz + 0.5)*dx);
+      if (outerBoundary.contains(pos)) result.push_back(pos);
     }
 
   } else {
@@ -47,16 +61,14 @@ fillFacetedVolume(const Dim<3>::FacetedVolume& outerBoundary,
     // We'll try using the much faster convex contains method as a pre-filter
     // before using the expensive general contains.
     const FacetedVolume convexOuterBoundary(outerBoundary.vertices());
-    for (iz = 0; iz != nz; ++iz) {
-      z = xmin.z() + (iz + 0.5)*dx;
-      for (iy = 0; iy != ny; ++iy) {
-        y = xmin.y() + (iy + 0.5)*dx;
-        for (ix = 0; ix != nx; ++ix) {
-          x = xmin.x() + (ix + 0.5)*dx;
-          Vector pos(x, y, z);
-          if (convexOuterBoundary.convexContains(pos) and outerBoundary.contains(pos)) result.push_back(pos);
-        }
-      }
+    for (i = imin; i != imax; ++i) {
+      ix = i % nx;
+      iy = (i / nx) % ny;
+      iz = i / nxy;
+      const Vector pos(xmin.x() + (ix + 0.5)*dx,
+                       xmin.y() + (iy + 0.5)*dx,
+                       xmin.z() + (iz + 0.5)*dx);
+      if (convexOuterBoundary.convexContains(pos) and outerBoundary.contains(pos)) result.push_back(pos);
     }
 
   }
@@ -69,13 +81,14 @@ fillFacetedVolume(const Dim<3>::FacetedVolume& outerBoundary,
 vector<Dim<3>::Vector>
 fillFacetedVolume(const Dim<3>::FacetedVolume& innerBoundary,
                   const Dim<3>::FacetedVolume& outerBoundary,
-                  const unsigned n1d) {
+                  const unsigned n1d,
+                  const unsigned domain,
+                  const unsigned numDomains) {
   typedef Dim<3>::Vector Vector;
   typedef Dim<3>::FacetedVolume FacetedVolume;
 
   vector<Vector> result;
-  unsigned ix, iy, iz;
-  double x, y, z;
+  unsigned i, ix, iy, iz;
   const Vector& xmin = outerBoundary.xmin();
   const Vector& xmax = outerBoundary.xmax();
   const double dx = (xmax - xmin).maxElement()/n1d;
@@ -86,7 +99,18 @@ fillFacetedVolume(const Dim<3>::FacetedVolume& innerBoundary,
   CHECK(nx > 0 and nx <= n1d);
   CHECK(ny > 0 and ny <= n1d);
   CHECK(nz > 0 and nz <= n1d);
-  result.reserve(nx*ny*nz);
+
+  // Figure out the range of global IDs for this domain.
+  const unsigned nxy = nx*ny;
+  const unsigned nxyz = nx*ny*nz;
+  const unsigned ndomain0 = nxyz/numDomains;
+  const unsigned remainder = nxyz - ndomain0*numDomains;
+  CHECK(remainder < numDomains);
+  const unsigned ndomain = nxyz/numDomains + (domain < remainder ? 1 : 0);
+  const unsigned imin = domain*ndomain0 + min(domain, remainder);
+  const unsigned imax = imin + ndomain;
+  CHECK(domain < numDomains - 1 or imax == nxyz);
+  result.reserve(ndomain);
 
   const bool innerConvex = innerBoundary.convex();
   const bool outerConvex = outerBoundary.convex();
@@ -94,24 +118,22 @@ fillFacetedVolume(const Dim<3>::FacetedVolume& innerBoundary,
   const FacetedVolume convexOuterBoundary = outerConvex ? outerBoundary : FacetedVolume(outerBoundary.vertices());
 
   bool outerTest, innerTest;
-  for (iz = 0; iz != nz; ++iz) {
-    z = xmin.z() + (iz + 0.5)*dx;
-    for (iy = 0; iy != ny; ++iy) {
-      y = xmin.y() + (iy + 0.5)*dx;
-      for (ix = 0; ix != nx; ++ix) {
-        x = xmin.x() + (ix + 0.5)*dx;
-        Vector pos(x, y, z);
-        outerTest = convexOuterBoundary.contains(pos);
-        if (outerTest and not outerConvex) outerTest = outerBoundary.contains(pos);
-        if (outerTest) {
-          innerTest = convexInnerBoundary.contains(pos);
-          if (not innerTest) {
-            result.push_back(pos);
-          } else {
-            if (not innerConvex) innerTest = innerBoundary.contains(pos);
-            if (not innerTest) result.push_back(pos);
-          }
-        }
+  for (i = imin; i != imax; ++i) {
+    ix = i % nx;
+    iy = (i / nx) % ny;
+    iz = i / nxy;
+    const Vector pos(xmin.x() + (ix + 0.5)*dx,
+                     xmin.y() + (iy + 0.5)*dx,
+                     xmin.z() + (iz + 0.5)*dx);
+    outerTest = convexOuterBoundary.contains(pos);
+    if (outerTest and not outerConvex) outerTest = outerBoundary.contains(pos);
+    if (outerTest) {
+      innerTest = convexInnerBoundary.contains(pos);
+      if (not innerTest) {
+        result.push_back(pos);
+      } else {
+        if (not innerConvex) innerTest = innerBoundary.contains(pos);
+        if (not innerTest) result.push_back(pos);
       }
     }
   }
