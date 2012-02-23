3f2f77e5553b94654f050ad0043b0079c96c8c30
Mike Owen <mikeowen@llnl.gov>
2012-02-22 22:28:38 -0800

    Figured out how to handle the mismatch in namespace between Array and fftw++ in the FractalGravity/libs.h file rather than patching the Array.h header.

--- src/FractalGravity/libs.hh
+++ src/FractalGravity/libs.hh
@@ -9,6 +9,14 @@
 #include <complex>
 #include <ctime>
 #include "Array.h"
+
+// These shouldn't be necessary, but there's some inconsistency between Array and fftw++.
+namespace Array {
+  void free0(void *p) { ::free0(p); }
+  template<class T> void newAlign(T *&v, size_t len, size_t align) { ::newAlign(v, len, align); }
+  template<class T> void deleteAlign(T *v, size_t len) { ::deleteAlign(v, len); }
+}
+
 #include "fftw++.h"
 using namespace std;
 using namespace Array;
