908ca6a2aeba43150278decbfab1adc3f903425e
Mike Owen <mikeowen@llnl.gov>
2012-08-18 21:40:43 -0700

    Missed putting the Inst files in the makefile for Distributed.

--- src/Distributed/SortAndDivideRedistributeNodesInst.cc
+++ src/Distributed/SortAndDivideRedistributeNodesInst.cc
@@ -4,6 +4,6 @@
 #include "Geometry/Dimension.hh"
 #include "SortAndDivideRedistributeNodes.cc"
 
-template class Spheral::PartitionSpace::SortAndDivideRedistributeNodes< Dim<1> >;
-template class Spheral::PartitionSpace::SortAndDivideRedistributeNodes< Dim<2> >;
-template class Spheral::PartitionSpace::SortAndDivideRedistributeNodes< Dim<3> >;
+template class Spheral::PartitionSpace::SortAndDivideRedistributeNodes< Spheral::Dim<1> >;
+template class Spheral::PartitionSpace::SortAndDivideRedistributeNodes< Spheral::Dim<2> >;
+template class Spheral::PartitionSpace::SortAndDivideRedistributeNodes< Spheral::Dim<3> >;
