7884a8edc923fc95c344822783ebc1efc54d550e
Mike Owen <mikeowen@llnl.gov>
2013-05-02 15:10:22 -0700

    Updating reference for tensile rod.

--- tests/Damage/TensileRod/TensileRod-GradyKipp-1d.py
+++ tests/Damage/TensileRod/TensileRod-GradyKipp-1d.py
@@ -140,7 +140,7 @@ commandLine(length = 3.0,
 
             testtol = 1.0e-3,
             clearDirectories = False,
-            referenceFile = "Reference/TensileRod-GradyKipp-1d-1proc-reproducing-20120920.txt",
+            referenceFile = "Reference/TensileRod-GradyKipp-1d-1proc-reproducing-20130502.txt",
             dataDirBase = "dumps-TensileRod-1d",
             outputFile = "None",
             comparisonFile = "None",
