679298caab0e110c206810ee76886d80e328ecd6
Mike Owen <mikeowen@llnl.gov>
2013-05-03 15:57:38 -0700

    Mostly debugged out ANEOS hookup I think.
    1.  Found we were assuming the wrong units for ANEOS internally.  It looks like they use CGS, with temperatures in eV.
    
    2.  Modified the fortran output file for ANEOS to overwrite automatically, which seems to be working.
    
    3.  Spiffed up the ANEOS test a bit as I used it, which could probably use some ATS'ing.

--- src/SolidMaterial/tests/plotANEOS.py
+++ src/SolidMaterial/tests/plotANEOS.py
@@ -1,55 +1,57 @@
 from SolidSpheral3d import *
 import Gnuplot
 
-# We'll work in MKS units.
-units = PhysicalConstants(1.0,   # Unit length in meters
-                          1.0,   # Unit mass in kg
+# We'll work in CGS units.
+units = PhysicalConstants(0.01,  # Unit length in meters
+                          0.001, # Unit mass in kg
                           1.0)   # Unit time in sec
 
 #-------------------------------------------------------------------------------
-# Build a Tillotson for Granite like materials.
+# Build a Gruneisen for SiO2 like materials.
 #-------------------------------------------------------------------------------
 mconv = 1.0
 lconv = 1.0
 tconv = 1.0
-MPa = mconv/(lconv*tconv**2) * 1e6  # Convert MPa
-GPa = mconv/(lconv*tconv**2) * 1e9  # Convert GPa
-MJperKg = (mconv/tconv)**2 * 1e6    # Convert MJ/Kg
-GJperKg = (mconv/tconv)**2 * 1e9    # Convert GJ/Kg
 
-rho0Granite = 2.65e3
-etaMinGranite = 0.1
-etaMaxGranite = 10.0
+rho0 = 2.65               # g/cc
+C0 = 0.36839e6            # cm/s
+S1 = 1.8954               # dimensionless
+S2 = 0.0                  # dimensionless
+S3 = 0.0                  # dimensionless
+gamma0 = 0.9              # dimensionless
+b = 1.0                   # dimensionless
+etaMin = 0.2
+etaMax = 5.0
 
-eosGranite = TillotsonEquationOfState(rho0Granite,     # ref density (kg/m^3)
-                                      etaMinGranite,   # etamin             
-                                      etaMaxGranite,   # etamax             
-                                      0.5,             # a      (dimensionless)
-                                      1.5,             # b      (dimensionless)
-                                      60.0 * GPa,      # A      (Pa)
-                                      40.0 * GPa,      # B      (Pa)
-                                      5.0,             # alpha  (dimensionless)
-                                      5.0,             # beta   (dimensionless)
-                                      10.0  * MJperKg, # eps0   (J/Kg) -- energies for Dolomite
-                                      250.0 * MJperKg, # epsLiq (J/Kg) -- energies for Dolomite
-                                      1.4   * GJperKg, # epsVap (J/Kg) -- energies for Dolomite
-                                      55.350,          # atomic weight -- complete punt
-                                      units)
+eosSiO2 = GruneisenEquationOfState(rho0,            # ref density (g/cc)
+                                   etaMin,          # etamin             
+                                   etaMax,          # etamax
+                                   C0,
+                                   S1,
+                                   S2,
+                                   S3,
+                                   gamma0,
+                                   b,
+                                   60.0843,         # atomic weight
+                                   units)
 
 #-------------------------------------------------------------------------------
 # Build an ANEOS SiO2 like thing.
 #-------------------------------------------------------------------------------
 izetl = vector_of_int(1, -1)
 initializeANEOS("ANEOS.INPUT", "ANEOS.barf", izetl)
-rhoMin, rhoMax = 0.9*etaMinGranite*rho0Granite, 1.1*etaMaxGranite*rho0Granite
+rhoMin, rhoMax = 0.9*etaMin*rho0, 1.1*etaMax*rho0
+Tmin, Tmax = 1.0, 1.0e8
 eosANEOS = ANEOSEquationOfState(0,                 # Material number
                                 1000,              # num rho vals
                                 1000,              # num T vals
                                 rhoMin,            # minimum density (kg/m^3)
                                 rhoMax,            # maximum density (kg/m^3)
-                                1.0,               # minimum temperature (K)
-                                1e8,               # maximum temperature (K)
+                                Tmin,              # minimum temperature (K)
+                                Tmax,              # maximum temperature (K)
                                 units)
+eps0ANEOS = eosANEOS.specificThermalEnergy(rho0, 1.0)  # Specific energy at 1K, reference density
+print "eps0ANEOS = ", eps0ANEOS
 
 #-------------------------------------------------------------------------------
 # Plot the pressure as a function of (rho, eps)
@@ -58,71 +60,55 @@ n = 50
 drho = (rhoMax - rhoMin)/n
 rho = [rhoMin + i*drho for i in xrange(n + 1)]
 
-epsMin, epsMax = 0.0, 1.1*eosGranite.epsVapor
+epsMin = eosANEOS.specificThermalEnergy(rho0, 0.1*Tmin)
+epsMax = eosANEOS.specificThermalEnergy(rho0, 1.1*Tmax)
 deps = (epsMax - epsMin)/n
 eps = [epsMin + i*deps for i in xrange(n + 1)]
 
 # Write the (rho, eps, P, cs) set to a file.
-f = open("Granite_TillotsonEOS.txt", "w")
+f = open("SiOS_ANEOS.txt", "w")
 f.write("""
-# Tillotson EOS dump for a granite like material (all units MKS).
-# rho0 = %g kg/m^3
-# a = %g (dimensionless)
-# b = %g (dimensionless)
-# A = %g (Pa)
-# B = %g (Pa)
-# alpha = %g (dimensionless)
-# beta = %g (dimensionless)
-# eps0 = %g (J/kg)
-# epsLiq = %g (J/kg)
-# epsVap = %g (J/kg)
+# ANEOS vs. Gruneisen EOS dump for SiO2 like material (all units CGS).
 #
-""" % (eosGranite.referenceDensity,
-       eosGranite.a,
-       eosGranite.b,
-       eosGranite.A,
-       eosGranite.B,
-       eosGranite.alpha,
-       eosGranite.beta,
-       eosGranite.eps0,
-       eosGranite.epsLiquid,
-       eosGranite.epsVapor))
-f.write((6*'"%20s "' + "\n") % ("rho (kg/m^3)", "eps (J/kg)", 
-                                "P Till (Pa)", "cs Till (m/sec)", 
-                                "P ANEOS (Pa)", "cs ANEOS (m/sec)"))
+# ANEOS eps(1K) = %g
+#
+""" % (eps0ANEOS))
+f.write((6*'"%20s "' + "\n") % ("rho (g/cm^3)", "eps (erg/g)", 
+                                "P Grun (dyne)", "cs Grun (cm/sec)", 
+                                "P ANEOS (dyne)", "cs ANEOS (cm/sec)"))
 
-PT, csT, PA, csA = [], [], [], []
+PG, csG, PA, csA = [], [], [], []
 for rhoi in rho:
     for epsi in eps:
-        PT.append((rhoi/rho0Granite, epsi/eosGranite.eps0, eosGranite.pressure(rhoi, epsi)))
-        csT.append((rhoi/rho0Granite, epsi/eosGranite.eps0, eosGranite.soundSpeed(rhoi, epsi)))
-        PA.append((rhoi/rho0Granite, epsi/eosGranite.eps0, eosANEOS.pressure(rhoi, epsi)))
-        csA.append((rhoi/rho0Granite, epsi/eosGranite.eps0, eosANEOS.soundSpeed(rhoi, epsi)))
-        f.write((6*"%20g " + "\n") % (rhoi, epsi, PT[-1][-1], csT[-1][-1], PA[-1][-1], csA[-1][-1]))
+        PG.append((rhoi, epsi, eosSiO2.pressure(rhoi, epsi - epsMin)))
+        csG.append((rhoi, epsi, eosSiO2.soundSpeed(rhoi, epsi - epsMin)))
+        PA.append((rhoi, epsi, eosANEOS.pressure(rhoi, epsi)))
+        csA.append((rhoi, epsi, eosANEOS.soundSpeed(rhoi, epsi)))
+        f.write((6*"%20g " + "\n") % (rhoi, epsi, PG[-1][-1], csG[-1][-1], PA[-1][-1], csA[-1][-1]))
 f.close()
 
-PTplot = Gnuplot.Gnuplot()
-PTplot.xlabel("rho/rho0")
-PTplot.ylabel("eps (J/kg)")
-PTdata = Gnuplot.Data(PT)
-PTplot.splot(PTdata, title="Pressure (Tillotson)")
+PGplot = Gnuplot.Gnuplot()
+PGplot.xlabel("rho (g/cm^3)")
+PGplot.ylabel("eps (erg/g)")
+PGdata = Gnuplot.Data(PG)
+PGplot.splot(PGdata, title="Pressure (Gruneisen)")
 
 
-csTplot = Gnuplot.Gnuplot()
-csTplot.xlabel("rho/rho0")
-csTplot.ylabel("eps (J/kg)")
-csTdata = Gnuplot.Data(csT)
-csTplot.splot(csTdata, title="sound speed (Tillotson)")
+csGplot = Gnuplot.Gnuplot()
+csGplot.xlabel("rho (g/cm^3)")
+csGplot.ylabel("eps (erg/g)")
+csGdata = Gnuplot.Data(csG)
+csGplot.splot(csGdata, title="sound speed (Gruneisen)")
 
 PAplot = Gnuplot.Gnuplot()
-PAplot.xlabel("rho/rho0")
-PAplot.ylabel("eps (J/kg)")
+PAplot.xlabel("rho (g/cm^3)")
+PAplot.ylabel("eps (erg/g)")
 PAdata = Gnuplot.Data(PA)
 PAplot.splot(PAdata, title="Pressure (ANEOS)")
 
 
 csAplot = Gnuplot.Gnuplot()
-csAplot.xlabel("rho/rho0")
-csAplot.ylabel("eps (J/kg)")
+csAplot.xlabel("rho (g/cm^3)")
+csAplot.ylabel("eps (erg/g)")
 csAdata = Gnuplot.Data(csA)
 csAplot.splot(csAdata, title="sound speed (ANEOS)")
