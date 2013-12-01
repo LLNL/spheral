cafee44b711ab0648ed77fb4867726ec34791656
Naor <nmovshov@gmail.com>
2013-12-01 15:57:20 -0800

    Adiabatic derivative of trillotson pressure implemented. Effect on most of my runs seems neglible, but that's not suprising. The Tillotson unit test fails of course, but other tests are unaffected.

--- src/SolidMaterial/TillotsonEquationOfState.cc
+++ src/SolidMaterial/TillotsonEquationOfState.cc
@@ -325,48 +325,60 @@ computeDPDrho(const Scalar massDensity,
                rho = rho0*eta,
                eps = std::max(0.0, specificThermalEnergy);
 
-  double dPdrho;
+  double dPdrho_ad, dPdrho_eps, dPdeps_rho;
   const double phi = mb/(1.0 + eps/(meps0*eta*eta));
   const double chi = 1./eta - 1.0;
   const double dphidrho = (1./rho0)*((2.0*mb*meps0*eps*eta)/
                                      ((eps+meps0*eta*eta)*(eps+meps0*eta*eta)));
+  const double dphideps = -mb*meps0*eta*eta/((eps+meps0*eta*eta)*(eps+meps0*eta*eta));
 
   if (mu >= 0.0) {
 
     // Regime 1: compression, solid.
-    dPdrho = eps*(ma + phi + rho*dphidrho) + (1./rho0)*(mA + 2.0*mB*mu);
+    dPdrho_eps = eps*(ma + phi + rho*dphidrho) + (1./rho0)*(mA + 2.0*mB*mu);
+    dPdeps_rho = rho*(ma + phi + eps*dphideps);
 
   } else if (eps <= mepsLiquid) {
 
     // Regime 2: expansion, solid : same as 1, but only if rho>cutoff density
-    dPdrho = (eta > mEtaMinSolid) ? 
+    dPdrho_eps = (eta > mEtaMinSolid) ? 
               eps*(ma + phi + rho*dphidrho) + (1./rho0)*(mA + 2.0*mB*mu) : 0.0;
+    dPdeps_rho = (eta > mEtaMinSolid) ?
+              rho*(ma + phi + eps*dphideps) : 0.0;
 
   } else if (eps >= mepsVapor) {
      
     // Regime 4: expansion, gaseous.
-    dPdrho = ma*eps + 
+    dPdrho_eps = ma*eps + 
              eps*exp(-malpha*chi*chi)*(phi + rho*dphidrho + 2.0*malpha*chi*phi*rho0/rho) + 
              mA*exp(-(malpha*chi*chi+mbeta*chi))*(1./rho0 + rho0*mu*mbeta/(rho*rho) + 
                                                         2.0*rho0*mu*malpha*chi/(rho*rho));
+    dPdeps_rho = rho*(ma + exp(-malpha*chi*chi)*(phi + eps*dphideps));
 
   } else {
 
     // Regime 3: expansion, liquid.
     // Treated here as a linear combination of the solid and gaseous phases.
-    double dP2drho, dP4drho;
+    double dP2drho_eps, dP4drho_eps, dP2deps_rho, dP4deps_rho;
 
-    dP2drho = (eta > mEtaMinSolid) ? 
+    dP2drho_eps = (eta > mEtaMinSolid) ? 
                eps*(ma + phi + rho*dphidrho) + (1./rho0)*(mA + 2.0*mB*mu) : 0.0;
-    dP4drho = ma*eps + 
+    dP4drho_eps = ma*eps + 
               eps*exp(-malpha*chi*chi)*(phi + rho*dphidrho + 2.0*malpha*chi*phi*rho0/rho) + 
               mA*exp(-(malpha*chi*chi+mbeta*chi))*(1./rho0 + rho0*mu*mbeta/(rho*rho) + 
                                                          2.0*rho0*mu*malpha*chi/(rho*rho));
-    dPdrho = dP2drho + (dP4drho - dP2drho)*(eps - mepsLiquid)/(mepsVapor - mepsLiquid);
+    dP2deps_rho = (eta > mEtaMinSolid) ?
+               rho*(ma + phi + eps*dphideps) : 0.0;
+    dP4deps_rho = rho*(ma + exp(-malpha*chi*chi)*(phi + eps*dphideps));
+
+    dPdrho_eps = dP2drho_eps + (dP4drho_eps - dP2drho_eps)*(eps - mepsLiquid)/(mepsVapor - mepsLiquid);
+    dPdeps_rho = dP2deps_rho + (dP4deps_rho - dP2deps_rho)*(eps - mepsLiquid)/(mepsVapor - mepsLiquid);
+
   }
 
   // That's it.
-  return std::abs(dPdrho);
+  dPdrho_ad = dPdrho_eps + pressure(rho, eps)/(rho*rho);
+  return std::abs(dPdrho_ad);
 }
 
 }
