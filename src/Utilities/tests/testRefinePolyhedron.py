5a4d9b000c9e18756f733a8f4f5db1b75ba954de
Mike Owen <mikeowen@llnl.gov>
2014-03-21 15:40:27 -0700

    Adding general utilties for reading/writing Polyhedron objects in OBJ and OFF file formats.

--- src/Utilities/tests/testRefinePolyhedron.py
+++ src/Utilities/tests/testRefinePolyhedron.py
@@ -1,20 +1,6 @@
 import time
 from Spheral3d import *
-
-def PLC_from_Polyhedron(poly):
-    coords = vector_of_double()
-    for v in poly.vertices():
-        coords.append(v.x)
-        coords.append(v.y)
-        coords.append(v.z)
-    plc = polytope.PLC3d()
-    for f in poly.facets():
-        indices = f.ipoints
-        plc.facets.push_back(vector_of_int())
-        for i in indices:
-            plc.facets[-1].append(i)
-    assert len(plc.facets) == len(poly.facets())
-    return coords, plc
+from PolyhedronFileUtilities import *
 
 # Create a simple unit box.
 verts0 = vector_of_Vector()
@@ -30,10 +16,7 @@ poly0 = Polyhedron(verts0)
 
 # Now refine it.
 t0 = time.clock()
-poly1 = refinePolyhedron(poly0, 5)
+poly1 = refinePolyhedron(poly0, 3)
 t1 = time.clock()
 print "Requred %g seconds to refine polyhedron." % (t1 - t0)
-coords1, plc1 = PLC_from_Polyhedron(poly1)
-polytope.writePLCtoOFF(plc1, coords1, "refine_cube.off")
-
-
+writePolyhedronOFF(poly1, "refine_cube.off")
