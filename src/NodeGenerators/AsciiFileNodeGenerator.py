b205ba304d41b0bf7903c9b7e9c8b95540a8a6db
Cody Raskin <raskin1@llnl.gov>
2014-08-29 13:58:24 -0700

    Ascii File Node Generator Generalized
    The generator will now grab the field names from the first non-header (not following a #) row
    and fill the arrays corresponding to those fields. Now the file need not be in any particular order
    and as long as it has x,y,z etc in there somewhere, it should just work.

--- src/NodeGenerators/AsciiFileNodeGenerator.py
+++ src/NodeGenerators/AsciiFileNodeGenerator.py
@@ -13,6 +13,8 @@ import mpi
 rank = mpi.rank
 procs = mpi.procs
 
+delimiter = ' '
+
 #-------------------------------------------------------------------------------
 # Read in a 2-D setup.
 #-------------------------------------------------------------------------------
@@ -47,49 +49,35 @@ class AsciiFileNodeGenerator2D(NodeGeneratorBase):
         if mpi.rank == 0:
             f = open(filename,'r')
             self.f = f
-        #if readFileToMemory:
-        #self.f = f.readlines()
-        #f.close()
-        # i don't want to build in this functionality right now
-        #else:
-        # self.f = f
         else:
             self.f = None
         
         # create the field arrays
         vals = []
-        
-        self.x = []
-        self.y = []
-        self.m = []
-        self.rho = []
-        self.eps = []
-        self.vx = []
-        self.vy = []
-        self.vz = []
         self.H = []
         
-        for line in f:
-            data = line.split()
-            vals.append(data)
+        fieldNames = []
+        gotFieldNames = 0
         
-        n = len(vals) - 1
-        for i in range(n):
-            j = i + 1
-            self.x.append(float(vals[j][0]))
-            self.y.append(float(vals[j][1]))
-            # (1/h) * [[1 0 ][0 1]]
-            H = (1.0/float(vals[j][2])) * SymTensor2d.one
-            self.H.append(H)
-            
-            self.m.append(float(vals[j][3]))
-            self.rho.append(float(vals[j][4]))
-            #pressure
-            self.eps.append(float(vals[j][6]))
-            self.vx.append(float(vals[j][7]))
-            self.vy.append(float(vals[j][8]))
-            #temperature
-            #abund array
+        for line in self.f:
+            data = line.split(delimiter)
+            if data[0][0] != "#" and gotFieldNames == 1:
+                vals.append(data)
+            if data[0][0] != "#" and gotFieldNames == 0:
+                fieldNames.append(data)
+                gotFieldNames = 1
+        
+        print "in " + filename + " found " + str(len(fieldNames[0])) + " fields:"
+        for i in xrange(len(fieldNames[0])):
+            print fieldNames[0][i]
+            self.__dict__[fieldNames[0][i]] = []
+        
+        
+        n = len(vals)
+        for i in xrange(n):
+            for j in xrange(len(fieldNames[0])):
+                self.__dict__[fieldNames[0][j]].append(float(vals[i][j]))
+            self.H.append((1.0/self.h[i]) * SymTensor2d.one)
         
         
         
@@ -178,53 +166,36 @@ class AsciiFileNodeGenerator3D(NodeGeneratorBase):
         if mpi.rank == 0:
             f = open(filename,'r')
             self.f = f
-            #if readFileToMemory:
-                #self.f = f.readlines()
-                #f.close()
-                # i don't want to build in this functionality right now
-                #else:
-                # self.f = f
         else:
             self.f = None
             
         # create the field arrays
         vals = []
-        
-        self.x = []
-        self.y = []
-        self.z = []
-        self.m = []
-        self.rho = []
-        self.eps = []
-        self.vx = []
-        self.vy = []
-        self.vz = []
         self.H = []
         
-        for line in f:
-            data = line.split()
-            vals.append(data)
+        fieldNames = []
+        gotFieldNames = 0
+        
+        for line in self.f:
+            data = line.split(delimiter)
+            if data[0][0] != "#" and gotFieldNames == 1:
+                vals.append(data)
+            if data[0][0] != "#" and gotFieldNames == 0:
+                fieldNames.append(data)
+                gotFieldNames = 1
             
-        n = len(vals) - 1
-        for i in range(n):
-            j = i + 1
-            self.x.append(float(vals[j][0]))
-            self.y.append(float(vals[j][1]))
-            self.z.append(float(vals[j][2]))
-            # (1/h) * [[1 0 0][0 1 0][0 0 1]]
-            H = (1.0/float(vals[j][3])) * SymTensor3d.one
-            self.H.append(H)
-            
-            self.m.append(float(vals[j][4]))
-            self.rho.append(float(vals[j][5]))
-            #pressure
-            self.eps.append(float(vals[j][7]))
-            self.vx.append(float(vals[j][8]))
-            self.vy.append(float(vals[j][9]))
-            self.vz.append(float(vals[j][10]))
-            #temperature
-            #abund array
-            
+        print "in " + filename + " found " + str(len(fieldNames[0])) + " fields:"
+        for i in xrange(len(fieldNames[0])):
+            print fieldNames[0][i]
+            self.__dict__[fieldNames[0][i]] = []
+
+
+        n = len(vals)
+        for i in xrange(n):
+            for j in xrange(len(fieldNames[0])):
+                self.__dict__[fieldNames[0][j]].append(float(vals[i][j]))
+            self.H.append((1.0/self.h[i]) * SymTensor3d.one)
+    
         # Read in extra fields
         # maybe later...
 
