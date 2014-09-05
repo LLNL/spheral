321ad9281e60f40091a7668ce16a0974a390bca2
Cody Raskin <raskin1@llnl.gov>
2014-09-05 15:37:30 -0700

    Prelim fixes for Helmholtz Nodelist confusion

--- src/Material/HelmholtzEquationOfState.cc
+++ src/Material/HelmholtzEquationOfState.cc
@@ -49,256 +49,265 @@ extern "C" {
 namespace Spheral {
 namespace Material {
     
-using FieldSpace::Field;
-using NodeSpace::NodeList;
+    using FieldSpace::Field;
+    using NodeSpace::NodeList;
 
 
-template<typename Dimension>
-HelmholtzEquationOfState<Dimension>::
-HelmholtzEquationOfState(const NodeList<Dimension>& myNodeList,
-                         const PhysicalConstants& constants,
-                         const double minimumPressure,
-                         const double maximumPressure,
-                         const double minimumTemperature,
-                         const double maximumTemperature,
-                         const MaterialPressureMinType minPressureType,
-                         const Scalar abar0,
-                         const Scalar zbar0):
-EquationOfState<Dimension>(constants, minimumPressure, maximumPressure, minPressureType),
-mabar0(abar0),
-mzbar0(zbar0),
-mPmin(minimumPressure),
-mPmax(maximumPressure),
-mTmin(minimumTemperature),
-mTmax(maximumTemperature),
-myAbar("helmAbar",myNodeList,abar0),
-myZbar("helmZbar",myNodeList,zbar0),
-mySpecificThermalEnergy("helmSpecificThermalEnergy",myNodeList),
-myMassDensity("helmMassDensity",myNodeList),
-myTemperature("helmTemperature",myNodeList),
-myPressure("helmPressure",myNodeList),
-mySoundSpeed("helmSoundSpeed",myNodeList),
-myGamma("helmGamma",myNodeList),
-mConstants(constants)
-{
-    needUpdate = 1; // flip this on and off later
-    Fortran2(init_helm_table);
-}
-
-//------------------------------------------------------------------------------
-// Destructor.
-//------------------------------------------------------------------------------
-template<typename Dimension>
-HelmholtzEquationOfState<Dimension>::
-~HelmholtzEquationOfState() {
-}
-
-
-/* check mzbar != nullptr?? */
-
-
-//------------------------------------------------------------------------------
-// Set the pressure.
-//------------------------------------------------------------------------------
-template<typename Dimension>
-void
-HelmholtzEquationOfState<Dimension>::
-setPressure(Field<Dimension, Scalar>& Pressure,
-            const Field<Dimension, Scalar>& massDensity,
-            const Field<Dimension, Scalar>& specificThermalEnergy) const {
-    CHECK(valid());
-    int npart = massDensity.numElements();
-
-    /*
-    Fortran2(azbar)(abund, aarray, zarray, &nion, molarabund, &abar, &zbar );
-    */
-    myMassDensity = massDensity;
-    mySpecificThermalEnergy = specificThermalEnergy;
-    
-    if(needUpdate){
-        Fortran2(wrapper_invert_helm_ed)(&npart, &myMassDensity[0], &mySpecificThermalEnergy[0],
-                                         &myAbar[0], &myZbar[0], &myTemperature[0],
-                                         &myPressure[0], &mTmin, &mySoundSpeed[0]);
-    }
-    
-    for (size_t i = 0; i != npart; ++i) {
-        Pressure(i) = myPressure(i);
-    }
-}
-
-//------------------------------------------------------------------------------
-// Set the temperature.
-//------------------------------------------------------------------------------
-template<typename Dimension>
-void
-HelmholtzEquationOfState<Dimension>::
-setTemperature(Field<Dimension, Scalar>& temperature,
-               const Field<Dimension, Scalar>& massDensity,
-               const Field<Dimension, Scalar>& specificThermalEnergy) const {
-    CHECK(valid());
-    int npart = massDensity.numElements();
-    
-    /*
-     Fortran2(azbar)(abund, aarray, zarray, &nion, molarabund, &abar, &zbar );
-     */
-    
-    myMassDensity = massDensity;
-    mySpecificThermalEnergy = specificThermalEnergy;
-    
-    if(needUpdate){
-        Fortran2(wrapper_invert_helm_ed)(&npart, &myMassDensity[0], &mySpecificThermalEnergy[0],
-                                         &myAbar[0], &myZbar[0], &myTemperature[0],
-                                         &myPressure[0], &mTmin, &mySoundSpeed[0]);
+    template<typename Dimension>
+    HelmholtzEquationOfState<Dimension>::
+    HelmholtzEquationOfState(const NodeList<Dimension>& myNodeList,
+                             const PhysicalConstants& constants,
+                             const double minimumPressure,
+                             const double maximumPressure,
+                             const double minimumTemperature,
+                             const double maximumTemperature,
+                             const MaterialPressureMinType minPressureType,
+                             const Scalar abar0,
+                             const Scalar zbar0):
+    EquationOfState<Dimension>(constants, minimumPressure, maximumPressure, minPressureType),
+    mabar0(abar0),
+    mzbar0(zbar0),
+    mPmin(minimumPressure),
+    mPmax(maximumPressure),
+    mTmin(minimumTemperature),
+    mTmax(maximumTemperature),
+    mConstants(constants)
+    {
+        needUpdate = 1; // flip this on and off later
+        Fortran2(init_helm_table);
     }
 
-    for (size_t i = 0; i != massDensity.numElements(); ++i) {
-        temperature(i) = myTemperature(i);
+    //------------------------------------------------------------------------------
+    // Destructor.
+    //------------------------------------------------------------------------------
+    template<typename Dimension>
+    HelmholtzEquationOfState<Dimension>::
+    ~HelmholtzEquationOfState() {
     }
-}
 
-//------------------------------------------------------------------------------
-// Set the specific thermal energy.
-//------------------------------------------------------------------------------
-template<typename Dimension>
-void
-HelmholtzEquationOfState<Dimension>::
-setSpecificThermalEnergy(Field<Dimension, Scalar>& specificThermalEnergy,
-                         const Field<Dimension, Scalar>& massDensity,
-                         const Field<Dimension, Scalar>& temperature) const {
-    CHECK(valid());
-    int npart = massDensity.numElements();
-    
-    /*
-     Fortran2(azbar)(abund, aarray, zarray, &nion, molarabund, &abar, &zbar );
-     */
-    
-    myMassDensity = massDensity;
-    myTemperature = temperature;
-    
-    if(needUpdate){
-        Fortran2(wrapper_helmeos)(&npart, &myMassDensity[0], &mySpecificThermalEnergy[0],
-                                         &myAbar[0], &myZbar[0], &myTemperature[0],
-                                         &myPressure[0]);
-    }
-    
-    for (size_t i = 0; i != npart; ++i) {
-        specificThermalEnergy(i) = mySpecificThermalEnergy(i);
-    }
-}
 
-//------------------------------------------------------------------------------
-// Set the specific heat.
-//------------------------------------------------------------------------------
-template<typename Dimension>
-void
-HelmholtzEquationOfState<Dimension>::
-setSpecificHeat(Field<Dimension, Scalar>& specificHeat,
+    /* check mzbar != nullptr?? */
+
+
+    //------------------------------------------------------------------------------
+    // Set the pressure.
+    //------------------------------------------------------------------------------
+    template<typename Dimension>
+    void
+    HelmholtzEquationOfState<Dimension>::
+    setPressure(Field<Dimension, Scalar>& Pressure,
                 const Field<Dimension, Scalar>& massDensity,
-                const Field<Dimension, Scalar>& temperature) const {
-    CHECK(valid());
-    const double kB = mConstants.kB();
-    const double mp = mConstants.protonMass();
-    int npart = myGamma.numElements();
-    double Cv;
-    
-    for (size_t i = 0; i != npart; ++i)
-        Cv += kB/(myGamma(i)*myAbar(i)*mp);
-    specificHeat = Cv/npart;
-}
+                const Field<Dimension, Scalar>& specificThermalEnergy) const {
+        CHECK(valid());
+        
+        if(!fieldsStored) storeFields(Pressure);
+        
+        int npart = massDensity.numElements();
+        myMassDensity = massDensity;
+        mySpecificThermalEnergy = specificThermalEnergy;
+        
+        if(needUpdate){
+            Fortran2(wrapper_invert_helm_ed)(&npart, &myMassDensity[0], &mySpecificThermalEnergy[0],
+                                             &myAbar[0], &myZbar[0], &myTemperature[0],
+                                             &myPressure[0], &mTmin, &mySoundSpeed[0]);
+        }
+        
+        for (size_t i = 0; i != npart; ++i) {
+            Pressure(i) = myPressure(i);
+        }
+    }
 
-//------------------------------------------------------------------------------
-// Set the sound speed.
-//------------------------------------------------------------------------------
-template<typename Dimension>
-void
-HelmholtzEquationOfState<Dimension>::
-setSoundSpeed(Field<Dimension, Scalar>& soundSpeed,
-              const Field<Dimension, Scalar>& massDensity,
-              const Field<Dimension, Scalar>& specificThermalEnergy) const {
-    CHECK(valid());
-    int npart = massDensity.numElements();
-    
-    /*
-     Fortran2(azbar)(abund, aarray, zarray, &nion, molarabund, &abar, &zbar );
-     */
-    
-    myMassDensity = massDensity;
-    mySpecificThermalEnergy = specificThermalEnergy;
-    
-    if(needUpdate){
-        Fortran2(wrapper_invert_helm_ed)(&npart, &myMassDensity[0], &mySpecificThermalEnergy[0],
-                                         &myAbar[0], &myZbar[0], &myTemperature[0],
-                                         &myPressure[0], &mTmin, &mySoundSpeed[0]);
+    //------------------------------------------------------------------------------
+    // Set the temperature.
+    //------------------------------------------------------------------------------
+    template<typename Dimension>
+    void
+    HelmholtzEquationOfState<Dimension>::
+    setTemperature(Field<Dimension, Scalar>& temperature,
+                   const Field<Dimension, Scalar>& massDensity,
+                   const Field<Dimension, Scalar>& specificThermalEnergy) const {
+        CHECK(valid());
+
+        if(!fieldsStored) storeFields(temperature);
+        
+        int npart = massDensity.numElements();
+        myMassDensity = massDensity;
+        mySpecificThermalEnergy = specificThermalEnergy;
+        
+        if(needUpdate){
+            Fortran2(wrapper_invert_helm_ed)(&npart, &myMassDensity[0], &mySpecificThermalEnergy[0],
+                                             &myAbar[0], &myZbar[0], &myTemperature[0],
+                                             &myPressure[0], &mTmin, &mySoundSpeed[0]);
+        }
+
+        for (size_t i = 0; i != massDensity.numElements(); ++i) {
+            temperature(i) = myTemperature(i);
+        }
+    }
+
+    //------------------------------------------------------------------------------
+    // Set the specific thermal energy.
+    //------------------------------------------------------------------------------
+    template<typename Dimension>
+    void
+    HelmholtzEquationOfState<Dimension>::
+    setSpecificThermalEnergy(Field<Dimension, Scalar>& specificThermalEnergy,
+                             const Field<Dimension, Scalar>& massDensity,
+                             const Field<Dimension, Scalar>& temperature) const {
+        CHECK(valid());
+        
+        if(!fieldsStored) storeFields(specificThermalEnergy);
+        
+        int npart = massDensity.numElements();
+        myMassDensity = massDensity;
+        myTemperature = temperature;
+        
+        if(needUpdate){
+            Fortran2(wrapper_helmeos)(&npart, &myMassDensity[0], &mySpecificThermalEnergy[0],
+                                             &myAbar[0], &myZbar[0], &myTemperature[0],
+                                             &myPressure[0]);
+        }
+        
+        for (size_t i = 0; i != npart; ++i) {
+            specificThermalEnergy(i) = mySpecificThermalEnergy(i);
+        }
+    }
+
+    //------------------------------------------------------------------------------
+    // Set the specific heat.
+    //------------------------------------------------------------------------------
+    template<typename Dimension>
+    void
+    HelmholtzEquationOfState<Dimension>::
+    setSpecificHeat(Field<Dimension, Scalar>& specificHeat,
+                    const Field<Dimension, Scalar>& massDensity,
+                    const Field<Dimension, Scalar>& temperature) const {
+        CHECK(valid());
+        
+        if(!fieldsStored) storeFields(specificHeat);
+        
+        const double kB = mConstants.kB();
+        const double mp = mConstants.protonMass();
+        int npart = myGamma.numElements();
+        double Cv;
+        
+        for (size_t i = 0; i != npart; ++i)
+            Cv += kB/(myGamma(i)*myAbar(i)*mp);
+        specificHeat = Cv/npart;
+    }
+
+    //------------------------------------------------------------------------------
+    // Set the sound speed.
+    //------------------------------------------------------------------------------
+    template<typename Dimension>
+    void
+    HelmholtzEquationOfState<Dimension>::
+    setSoundSpeed(Field<Dimension, Scalar>& soundSpeed,
+                  const Field<Dimension, Scalar>& massDensity,
+                  const Field<Dimension, Scalar>& specificThermalEnergy) const {
+        CHECK(valid());
+
+        if(!fieldsStored) storeFields(soundSpeed);
+        
+        int npart = massDensity.numElements();
+        myMassDensity = massDensity;
+        mySpecificThermalEnergy = specificThermalEnergy;
+        
+        if(needUpdate){
+            Fortran2(wrapper_invert_helm_ed)(&npart, &myMassDensity[0], &mySpecificThermalEnergy[0],
+                                             &myAbar[0], &myZbar[0], &myTemperature[0],
+                                             &myPressure[0], &mTmin, &mySoundSpeed[0]);
+        }
+        
+        for (size_t i = 0; i != npart; ++i) {
+            soundSpeed(i) = mySoundSpeed(i);
+            myGamma(i) = soundSpeed(i) * soundSpeed(i) * massDensity(i) / myPressure(i);
+        }
+    }
+
+    //------------------------------------------------------------------------------
+    // Set the bulk modulus (rho DP/Drho).  This is just the pressure for a
+    // Helmholtz gas.
+    //------------------------------------------------------------------------------
+    template<typename Dimension>
+    void
+    HelmholtzEquationOfState<Dimension>::
+    setBulkModulus(Field<Dimension, Scalar>& bulkModulus,
+                   const Field<Dimension, Scalar>& massDensity,
+                   const Field<Dimension, Scalar>& specificThermalEnergy) const {
+        CHECK(valid());
+        
+        if(!fieldsStored) storeFields(bulkModulus);
+        
+        setPressure(bulkModulus, massDensity, specificThermalEnergy);
+    }
+
+    /* ACCESSORS */
+    template<typename Dimension>
+    const bool
+    HelmholtzEquationOfState<Dimension>::
+    getUpdateStatus() const {
+        return needUpdate;
+    }
+
+    template<typename Dimension>
+    void
+    HelmholtzEquationOfState<Dimension>::
+    setUpdateStatus(bool bSet){
+        needUpdate = bSet;
+    }
+
+
+    //------------------------------------------------------------------------------
+    // Access abar
+    //------------------------------------------------------------------------------
+    template<typename Dimension>
+    const FieldSpace::Field<Dimension, typename Dimension::Scalar>&
+    HelmholtzEquationOfState<Dimension>::
+    abar() const {
+        //return mabar;
+        return myAbar;
+    }
+
+    //------------------------------------------------------------------------------
+    // Access zbar
+    //------------------------------------------------------------------------------
+    template<typename Dimension>
+    const FieldSpace::Field<Dimension, typename Dimension::Scalar>&
+    HelmholtzEquationOfState<Dimension>::
+    zbar() const {
+        //return mzbar;
+        return myZbar;
+    }
+        
+    //------------------------------------------------------------------------------
+    // Determine if the EOS is in a valid state.
+    //------------------------------------------------------------------------------
+    template<typename Dimension>
+    bool
+    HelmholtzEquationOfState<Dimension>::valid() const {
+        return (1.0);
     }
     
-    for (size_t i = 0; i != npart; ++i) {
-        soundSpeed(i) = mySoundSpeed(i);
-        myGamma(i) = soundSpeed(i) * soundSpeed(i) * massDensity(i) / myPressure(i);
+    //------------------------------------------------------------------------------
+    // Store Fields to local memory
+    //------------------------------------------------------------------------------
+    template<typename Dimension>
+    void
+    HelmholtzEquationOfState::storeFields(Field<Dimension, Scalar>& thisField);
+    {
+        NodeList myNodeList     = thisField.nodeList();
+        myMassDensity           = new Field<Dimension, Scalar>("helmMassDensity",myNodeList);
+        mySpecificThermalEnergy = new Field<Dimension, Scalar>("helmSpecificThermalEnergy",myNodeList);
+        myTemperature           = new Field<Dimension, Scalar>("helmTemperature",myNodeList);
+        myPressure              = new Field<Dimension, Scalar>("helmPressure",myNodeList);
+        mySoundSpeed            = new Field<Dimension, Scalar>("helmSoundSpeed",myNodeList);
+        myGamma                 = new Field<Dimension, Scalar>("helmGamma",myNodeList);
+        myAbar                  = new Field<Dimension, Scalar>("helmAbar",myNodeList,mabar0);
+        myZbar                  = new Field<Dimension, Scalar>("helmZbar",myNodeList,mzbar0);
+        
+        fieldsStored = 1;
     }
-}
-
-//------------------------------------------------------------------------------
-// Set the bulk modulus (rho DP/Drho).  This is just the pressure for a
-// Helmholtz gas.
-//------------------------------------------------------------------------------
-template<typename Dimension>
-void
-HelmholtzEquationOfState<Dimension>::
-setBulkModulus(Field<Dimension, Scalar>& bulkModulus,
-               const Field<Dimension, Scalar>& massDensity,
-               const Field<Dimension, Scalar>& specificThermalEnergy) const {
-    CHECK(valid());
-    setPressure(bulkModulus, massDensity, specificThermalEnergy);
-    //bulkModulus += mExternalPressure;
-}
-
-/* ACCESSORS */
-template<typename Dimension>
-const bool
-HelmholtzEquationOfState<Dimension>::
-getUpdateStatus() const {
-    //return mabar;
-    return needUpdate;
-}
-
-template<typename Dimension>
-void
-HelmholtzEquationOfState<Dimension>::
-setUpdateStatus(bool bSet){
-    needUpdate = bSet;
-}
-
-
-//------------------------------------------------------------------------------
-// Access abar
-//------------------------------------------------------------------------------
-template<typename Dimension>
-const FieldSpace::Field<Dimension, typename Dimension::Scalar>&
-HelmholtzEquationOfState<Dimension>::
-abar() const {
-    //return mabar;
-    return myAbar;
-}
-
-//------------------------------------------------------------------------------
-// Access zbar
-//------------------------------------------------------------------------------
-template<typename Dimension>
-const FieldSpace::Field<Dimension, typename Dimension::Scalar>&
-HelmholtzEquationOfState<Dimension>::
-zbar() const {
-    //return mzbar;
-    return myZbar;
-}
-    
-//------------------------------------------------------------------------------
-// Determine if the EOS is in a valid state.
-//------------------------------------------------------------------------------
-template<typename Dimension>
-bool
-HelmholtzEquationOfState<Dimension>::valid() const {
-    return (1.0);
-}
 
 }
 }
