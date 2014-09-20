9d15bec8acdde281ca0a102457bc217bf5178150
villumsen <jensvvillumsen@gmail.com>
2014-09-20 06:25:16 -0700

    Less memory waste in MPI package

--- src/Fractal2.0/dens_to_slices.cc
+++ src/Fractal2.0/dens_to_slices.cc
@@ -18,64 +18,70 @@ namespace FractalSpace
 	length_1++;
 	length_S*=2;
       }
-    vector <int> counts_out(FractalNodes);
-    counts_out.assign(FractalNodes,0);
-    vector <vector <int> > dataI_out(FractalNodes);
-    vector <vector <double> > dataR_out(FractalNodes);
+
     vector <int> pos_point(3);
-    for(vector <Point*>::const_iterator point_itr=group.list_points.begin();point_itr != group.list_points.end();++point_itr)
-      {
-	Point* p_point=*point_itr;
-	bool pass=p_point->get_passive_point();
-	p_point->get_pos_point(pos_point);
-	int nx=(pos_point[0]/zoom+length_1) % length_1;
-	int ny=(pos_point[1]/zoom+length_1) % length_1;
-	int nz=(pos_point[2]/zoom+length_1) % length_1;
-	int S=mem.p_mess->WhichSlice[nx];
-	int slice_point=frac.where(nx,ny,nz,mem.p_mess->BoxS[S],mem.p_mess->BoxSL[S]);
-	if(pass)
-	  slice_point=-slice_point-1;
-	double density=p_point->get_density_point();
-	dataI_out[S].push_back(slice_point);
-	dataR_out[S].push_back(density);
-	counts_out[S]++;
-      }
-    vector <int> counts_in(FractalNodes);
+    vector <int>counts_in;
+    vector <int>counts_out;
+    vector <vector <int> > dataI_out;
+    vector <vector <double> > dataR_out;
     vector <int> dataI_in;
     vector <double> dataR_in;
-    int how_manyI=-1;
-    int how_manyR=-1;
-    int integers=1;
-    int doubles=1;
-    mem.p_file->note(true," dens to slices a ");
-    mem.p_mess->Send_Data_Some_How(0,counts_out,counts_in,integers,doubles,
-				   dataI_out,dataI_in,how_manyI,
-				   dataR_out,dataR_in,how_manyR);
-    mem.p_file->note(true," dens to slices c ");
-    dataI_out.clear();
-    dataR_out.clear();      
-    int Slice=FractalRank;
-    vector <int> BoxS;
-    BoxS=mem.p_mess->BoxS[Slice];
-    vector <int> BoxSL;
-    BoxSL=mem.p_mess->BoxSL[Slice];
-    int counterIR=0;
-    /*
-    mem.p_mess->return_point.resize(how_manyR);
-    mem.p_mess->return_node.resize(how_manyR);
-    mem.p_mess->what_Slice_point.resize(how_manyR);
-    */
-    for(int FR=0;FR<FractalNodes;FR++)
+    int LOOPS=(FractalNodes-1)/256+1;
+    for(int loop=0;loop<LOOPS;loop++)
       {
-	for(int c=0;c<counts_in[FR];c++)
+	dataI_out.clear();
+	dataR_out.clear();
+	dataI_in.clear();
+	dataR_in.clear();
+	dataI_out.resize(FractalNodes);
+	dataR_out.resize(FractalNodes);
+	counts_out.assign(FractalNodes,0);
+	counts_in.assign(FractalNodes,0);
+	int loopcount=0;
+	for(vector <Point*>::const_iterator point_itr=group.list_points.begin();point_itr != group.list_points.end();++point_itr)
 	  {
-	    int n=dataI_in[counterIR];
-	    bool pass=n < 0;
-	    if(pass)
-	      n=-n-1;
-	    if(!pass)
-	      mem.p_mess->potR[n]=dataR_in[counterIR];
-	    counterIR++;
+	    if(loopcount % LOOPS == loop)
+	      {
+		Point* p_point=*point_itr;
+		bool pass=p_point->get_passive_point();
+		p_point->get_pos_point(pos_point);
+		int nx=(pos_point[0]/zoom+length_1) % length_1;
+		int ny=(pos_point[1]/zoom+length_1) % length_1;
+		int nz=(pos_point[2]/zoom+length_1) % length_1;
+		int S=mem.p_mess->WhichSlice[nx];
+		int slice_point=frac.where(nx,ny,nz,mem.p_mess->BoxS[S],mem.p_mess->BoxSL[S]);
+		if(pass)
+		  slice_point=-slice_point-1;
+		double density=p_point->get_density_point();
+		dataI_out[S].push_back(slice_point);
+		dataR_out[S].push_back(density);
+		counts_out[S]++;
+	      }
+	  }
+	int how_manyI=-1;
+	int how_manyR=-1;
+	int integers=1;
+	int doubles=1;
+	mem.p_file->note(true," dens to slices a ");
+	mem.p_mess->Send_Data_Some_How(0,counts_out,counts_in,integers,doubles,
+				       dataI_out,dataI_in,how_manyI,
+				       dataR_out,dataR_in,how_manyR);
+	mem.p_file->note(true," dens to slices c ");
+	dataI_out.clear();
+	dataR_out.clear();      
+	int counterIR=0;
+	for(int FR=0;FR<FractalNodes;FR++)
+	  {
+	    for(int c=0;c<counts_in[FR];c++)
+	      {
+		int n=dataI_in[counterIR];
+		bool pass=n < 0;
+		if(pass)
+		  n=-n-1;
+		if(!pass)
+		  mem.p_mess->potR[n]=dataR_in[counterIR];
+		counterIR++;
+	      }
 	  }
       }
   }
