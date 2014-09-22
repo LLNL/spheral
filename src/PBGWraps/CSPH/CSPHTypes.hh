715030c2bb6e95f6a4ee075410cb5aed1534b16c
Mike Owen <mikeowen@llnl.gov>
2014-09-22 15:47:06 -0700

    Adding some missing functions.

--- src/PBGWraps/CSPH/CSPHTypes.hh
+++ src/PBGWraps/CSPH/CSPHTypes.hh
@@ -87,10 +87,11 @@ computeCSPHSumMassDensity(const NeighborSpace::ConnectivityMap<Dimension>& conne
                           const KernelSpace::TableKernel<Dimension>& W,
                           const FieldSpace::FieldList<Dimension, typename Dimension::Vector>& position,
                           const FieldSpace::FieldList<Dimension, typename Dimension::Scalar>& mass,
+                          const FieldSpace::FieldList<Dimension, typename Dimension::Scalar>& volume,
                           const FieldSpace::FieldList<Dimension, typename Dimension::SymTensor>& H,
                           const std::vector<BoundarySpace::Boundary<Dimension>*>& boundaries,
                           FieldSpace::FieldList<Dimension, typename Dimension::Scalar>& massDensity) {
-  computeCSPHSumMassDensity(connectivityMap, W, position, mass, H, 
+  computeCSPHSumMassDensity(connectivityMap, W, position, mass, volume, H, 
                             boundaries.begin(), boundaries.end(),
                             massDensity);
 }
