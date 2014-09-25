eb98f011a1501bdee57a05e7845ffe28924d16e2
Cody Raskin <raskin1@llnl.gov>
2014-09-25 10:18:48 -0700

    CSPH Switch for calculating gradP

--- src/Physics/ArtificialConduction.cc
+++ src/Physics/ArtificialConduction.cc
@@ -20,8 +20,7 @@ using FieldSpace::Field;
 //------------------------------------------------------------------------------
 template<typename Dimension>
 ArtificialConduction<Dimension>::
-ArtificialConduction(const bool gradPMode,
-                     const Scalar alphaArCond):
+ArtificialConduction(const Scalar alphaArCond):
     Physics<Dimension>(),
     mDepsDt(FieldSpace::Copy),
     mGradPMode(gradPMode)
@@ -40,16 +39,6 @@ ArtificialConduction<Dimension>::~ArtificialConduction() {
 // Accessor Fns
 //------------------------------------------------------------------------------
 template<typename Dimension>
-void
-ArtificialConduction<Dimension>::
-gradPMode(bool val) { mGradPMode = val;}
-
-template<typename Dimension>
-bool
-ArtificialConduction<Dimension>::
-gradPMode() const { return mGradPMode;}
-
-template<typename Dimension>
 const FieldList<Dimension, typename Dimension::Scalar>&
 ArtificialConduction<Dimension>::
 vsig() const { return mVsig;}
@@ -66,48 +55,85 @@ template<typename Dimension>
 void
 ArtificialConduction<Dimension>::
 initializeProblemStartup(DataBase<Dimension>& dataBase) {
-    mVsig = dataBase.newFluidFieldList(0.0, FieldList<Dimension, Field<Dimension, Scalar> >::prefix() + HydroFieldNames::artiCondSignalVelocity);
-    
-    // I don't want an incrementBoundedFieldList here. just a fieldlist
+    mVsig = dataBase.newFluidFieldList(0.0, "Artificial Conduction vsig");
+    mGradP = dataBase.newFluidFieldList(0.0, "Pressure Gradient");
 }
+    //register derivs for gradP
+//------------------------------------------------------------------------------
+// Register gradP
+//------------------------------------------------------------------------------
+template<typename Dimension>
+void
+SPHHydroBase<Dimension>::
+registerDerivatives(DataBase<Dimension>& dataBase,
+                    StateDerivatives<Dimension>& derivs) {
+    derivs.enroll(mGradP);
+    // do i want to do this?? is it sufficient merely to calculate it below in eval?
+}
+
     
 //------------------------------------------------------------------------------
 // Meat and potatoes
 //------------------------------------------------------------------------------
 template<typename Dimension>
 void
-ArtificialConduction<Dimension>::
-computeConduction(const DataBase<Dimension>& dataBase,
-                  const State<Dimension>& state) {
-    /* PSEUDO CODE ---------
-     
-    grab eps
-    grab P
-    grab rho
-    grab m
+SPHHydroBase<Dimension>::
+evaluateDerivatives(const typename Dimension::Scalar time,
+                    const typename Dimension::Scalar dt,
+                    const DataBase<Dimension>& dataBase,
+                    const State<Dimension>& state,
+                    StateDerivatives<Dimension>& derivatives) const {
     
-    if !gradPMode
-        vsig = sqrt(abs(Pi-Pj)/(0.5*(rhoi+rhoj)))
-    else
-        vsig = sqrt(abs(gradP*dr)/(0.5*(rhoi+rhoj)))
-     
-     DepsDti += mj/rhoj * (0.5 * alphaArCond * vsig * (uj-ui) * dotp(rij,gradWij)
+    const TableKernel<Dimension>& W = this->kernel();
     
-    */
+    bool CSPHisOn = 0;
     
-    const FieldList<Dimension, Scalar> pressure = state.fields(HydroFieldNames::pressure, 0.0);
-    const FieldList<Dimension, SymTensor> Hsmooth = state.fields(HydroFieldNames::H, SymTensor::zero);
-    const FieldList<Dimension, Scalar> massDensity = state.fields(HydroFieldNames::massDensity, 0.0);
+    // The connectivity map
+    const ConnectivityMap<Dimension>& connectivityMap = dataBase.connectivityMap();
+    const vector<const NodeList<Dimension>*>& nodeLists = connectivityMap.nodeLists();
+    const size_t numNodeLists = nodeLists.size();
+    
+    // The relevant fields
     const FieldList<Dimension, Scalar> mass = state.fields(HydroFieldNames::mass, 0.0);
+    const FieldList<Dimension, Vector> position = state.fields(HydroFieldNames::position, Vector::zero);
+    const FieldList<Dimension, Scalar> massDensity = state.fields(HydroFieldNames::massDensity, 0.0);
+    const FieldList<Dimension, Scalar> specificThermalEnergy = state.fields(HydroFieldNames::specificThermalEnergy, 0.0);
+    const FieldList<Dimension, SymTensor> H = state.fields(HydroFieldNames::H, SymTensor::zero);
+    const FieldList<Dimension, Scalar> pressure = state.fields(HydroFieldNames::pressure, 0.0);
+    CHECK(mass.size() == numNodeLists);
+    CHECK(position.size() == numNodeLists);
+    CHECK(massDensity.size() == numNodeLists);
+    CHECK(specificThermalEnergy.size() == numNodeLists);
+    CHECK(H.size() == numNodeLists);
+    CHECK(pressure.size() == numNodeLists);
     
-    if (!mGradPMode)
+    // The relevant derivatives
+    FieldList<Dimension, Scalar> DepsDt = derivatives.fields(IncrementFieldList<Dimension, Scalar>::prefix() + HydroFieldNames::specificThermalEnergy, 0.0);
+    FieldList<Dimension, Scalar> gradP = derivatives.fields("Pressure Gradient", 0.0);
+    CHECK(DepsDt.size() == numNodeLists);
+    CHECK(gradP.size() == numNodeLists);
+    
+    // Now check if CSPH is active
+    if (state.registered(A_CSPH))
     {
-        
+        CSPHisOn = 1;
+        const FieldList<Dimension, Scalar> A = state.fields(HydroFieldNames::A_CSPH, 0.0);
+        const FieldList<Dimension, Vector> B = state.fields(HydroFieldNames::B_CSPH, Vector::zero);
+        const FieldList<Dimension, Vector> C = state.fields(HydroFieldNames::C_CSPH, Vector::zero);
+        const FieldList<Dimension, Tensor> D = state.fields(HydroFieldNames::D_CSPH, Tensor::zero);
+        const FieldList<Dimension, Vector> gradA0 = state.fields(HydroFieldNames::gradA0_CSPH, Vector::zero);
+        const FieldList<Dimension, Vector> gradA = state.fields(HydroFieldNames::gradA_CSPH, Vector::zero);
+        const FieldList<Dimension, Tensor> gradB = state.fields(HydroFieldNames::gradB_CSPH, Tensor::zero);
+        CHECK(A.size() == numNodeLists);
+        CHECK(B.size() == numNodeLists);
+        CHECK(C.size() == numNodeLists);
+        CHECK(D.size() == numNodeLists);
+        CHECK(gradA0.size() == numNodeLists);
+        CHECK(gradA.size() == numNodeLists);
+        CHECK(gradB.size() == numNodeLists);
+        //gradP = gradientCSPH(pressure, position, mass, H, A, B, C, D, gradA, gradB, connectivityMap, W);
     }
-    else
-    {
-        
-    }
+
 }
 
 
