4a053161f9a9a3df93745a65c7d2e44bf87b5cfd
Cody Raskin <raskin1@llnl.gov>
2014-09-25 11:06:20 -0700

    Adding vsig to the state

--- src/Physics/ArtificialConduction.cc
+++ src/Physics/ArtificialConduction.cc
@@ -58,26 +58,39 @@ initializeProblemStartup(DataBase<Dimension>& dataBase) {
     mVsig = dataBase.newFluidFieldList(0.0, "Artificial Conduction vsig");
     mGradP = dataBase.newFluidFieldList(0.0, "Pressure Gradient");
 }
-    //register derivs for gradP
+
 //------------------------------------------------------------------------------
 // Register gradP
 //------------------------------------------------------------------------------
 template<typename Dimension>
 void
-SPHHydroBase<Dimension>::
+ArtificialConduction<Dimension>::
 registerDerivatives(DataBase<Dimension>& dataBase,
                     StateDerivatives<Dimension>& derivs) {
     derivs.enroll(mGradP);
     // do i want to do this?? is it sufficient merely to calculate it below in eval?
 }
 
+//------------------------------------------------------------------------------
+// Register vsig
+//------------------------------------------------------------------------------
+template<typename Dimension>
+void
+ArtificialConduction<Dimension>::
+registerState(DataBase<Dimension>& dataBase,
+              State<Dimension>& state) {
+    typedef typename State<Dimension>::PolicyPointer PolicyPointer;
+    
+    dataBase.resizeFluidFieldList(mVsig, 0.0,"Artificial Conduction vsig", false);
+    state.enroll(mVsig);
+}
     
 //------------------------------------------------------------------------------
 // Meat and potatoes
 //------------------------------------------------------------------------------
 template<typename Dimension>
 void
-SPHHydroBase<Dimension>::
+ArtificialConduction<Dimension>::
 evaluateDerivatives(const typename Dimension::Scalar time,
                     const typename Dimension::Scalar dt,
                     const DataBase<Dimension>& dataBase,
@@ -100,6 +113,7 @@ evaluateDerivatives(const typename Dimension::Scalar time,
     const FieldList<Dimension, Scalar> specificThermalEnergy = state.fields(HydroFieldNames::specificThermalEnergy, 0.0);
     const FieldList<Dimension, SymTensor> H = state.fields(HydroFieldNames::H, SymTensor::zero);
     const FieldList<Dimension, Scalar> pressure = state.fields(HydroFieldNames::pressure, 0.0);
+    const FieldList<Dimension, Scalar> vsig = state.fields("Artificial Conduction vsig", 0.0);
     CHECK(mass.size() == numNodeLists);
     CHECK(position.size() == numNodeLists);
     CHECK(massDensity.size() == numNodeLists);
@@ -133,7 +147,98 @@ evaluateDerivatives(const typename Dimension::Scalar time,
         CHECK(gradB.size() == numNodeLists);
         //gradP = gradientCSPH(pressure, position, mass, H, A, B, C, D, gradA, gradB, connectivityMap, W);
     }
-
+    
+    // Start our big loop over all FluidNodeLists.
+    size_t nodeListi = 0;
+    for (typename DataBase<Dimension>::ConstFluidNodeListIterator itr = dataBase.fluidNodeListBegin();
+         itr != dataBase.fluidNodeListEnd();
+         ++itr, ++nodeListi) {
+        const NodeList<Dimension>& nodeList = **itr;
+        
+        // Iterate over the internal nodes in this NodeList.
+        for (typename ConnectivityMap<Dimension>::const_iterator iItr = connectivityMap.begin(nodeListi);
+             iItr != connectivityMap.end(nodeListi);
+             ++iItr) {
+            const int i = *iItr;
+            
+            // Get the state for node i.
+            const Vector& ri = position(nodeListi, i);
+            const Scalar& mi = mass(nodeListi, i);
+            const Scalar& rhoi = massDensity(nodeListi, i);
+            const Scalar& epsi = specificThermalEnergy(nodeListi, i);
+            const Scalar& Pi = pressure(nodeListi, i);
+            const Scalar& vsigi = pressure(nodeListi, i);
+            const SymTensor& Hi = H(nodeListi, i);
+            if (CSPHisOn)
+            {
+            const Scalar& Ai = A(nodeListi, i);
+            const Vector& Bi = B(nodeListi, i);
+            const Vector& gradA0i = gradA0(nodeListi, i);
+            const Vector& gradAi = gradA(nodeListi, i);
+            const Tensor& gradBi = gradB(nodeListi, i);
+            CHECK(Ai > 0.0);
+            }
+            CHECK(mi > 0.0);
+            CHECK(rhoi > 0.0);
+            
+            Scalar& DepsDti = DepsDt(nodeListi, i);
+            Scalar& gradPi = gradP(nodeListi, i);
+            
+            // Get the connectivity info for this node.
+            const vector< vector<int> >& fullConnectivity = connectivityMap.connectivityForNode(&nodeList, i);
+            
+            // Iterate over the NodeLists.
+            for (size_t nodeListj = 0; nodeListj != numNodeLists; ++nodeListj) {
+                
+                // Connectivity of this node with this NodeList.  We only need to proceed if
+                // there are some nodes in this list.
+                const vector<int>& connectivity = fullConnectivity[nodeListj];
+                if (connectivity.size() > 0) {
+                    
+                    // Loop over the neighbors.
+#pragma vector always
+                    for (vector<int>::const_iterator jItr = connectivity.begin();
+                         jItr != connectivity.end();
+                         ++jItr) {
+                        const int j = *jItr;
+                        
+                        // Only proceed if this node pair has not been calculated yet.
+                        // not sure about this..... <--------------------------------
+                        if (connectivityMap.calculatePairInteraction(nodeListi, i,
+                                                                     nodeListj, j,
+                                                                     firstGhostNodej)) {
+                            // Get the state for node j
+                            const Vector& rj = position(nodeListj, j);
+                            const Scalar& mj = mass(nodeListj, j);
+                            const Scalar& rhoj = massDensity(nodeListj, j);
+                            const Scalar& epsj = specificThermalEnergy(nodeListj, j);
+                            const Scalar& Pj = pressure(nodeListj, j);
+                            const Scalar& vsigj = pressure(nodeListj, j);
+                            const SymTensor& Hj = H(nodeListj, j);
+                            if (CSPHisOn)
+                            {
+                            const Scalar& Aj = A(nodeListj, j);
+                            const Vector& Bj = B(nodeListj, j);
+                            const Vector& gradA0j = gradA0(nodeListj, j);
+                            const Vector& gradAj = gradA(nodeListj, j);
+                            const Tensor& gradBj = gradB(nodeListj, j);
+                            CHECK(Aj > 0.0 or j >= firstGhostNodej);
+                            }
+                            CHECK(mj > 0.0);
+                            CHECK(rhoj > 0.0);
+                            
+                            Scalar& DepsDtj = DepsDt(nodeListj, j);
+                            Scalar& gradPj = gradP(nodeListj, j);
+                            
+                            // Node displacement.
+                            const Vector rij = ri - rj;
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 
--- src/Physics/ArtificialConduction.hh
+++ src/Physics/ArtificialConduction.hh
@@ -30,6 +30,10 @@ namespace Spheral {
             // Destructor
             virtual ~ArtificialConduction();
             
+            // Register state vars (vsig in this case)
+            virtual void registerState(DataBaseSpace::DataBase<Dimension>& dataBase,
+                                       State<Dimension>& state);
+            
             // Provide default methods for registering and iterating derivatives.
             virtual void registerDerivatives(DataBaseSpace::DataBase<Dimension>& dataBase,
                                              StateDerivatives<Dimension>& derivs);
