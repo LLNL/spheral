f995aea1b059e15e4b45d84e88d53d535293b246
Cody Raskin <raskin1@llnl.gov>
2014-11-25 13:38:58 -0800

    Changing wendland c6 to 1h extent

--- src/Kernel/WendlandC6KernelInline.hh
+++ src/Kernel/WendlandC6KernelInline.hh
@@ -10,8 +10,8 @@ template<>
 inline
 WendlandC6Kernel< Dim<1> >::WendlandC6Kernel():
   Kernel<Dim<1>, WendlandC6Kernel< Dim<1> > >() {
-  setVolumeNormalization(99.0/74.0);
-  setKernelExtent(2.0);
+  setVolumeNormalization(15.0/4.0);
+  setKernelExtent(1.0);
   setInflectionPoint(1.0/3.0);
 }
 
@@ -19,8 +19,8 @@ template<>
 inline
 WendlandC6Kernel< Dim<2> >::WendlandC6Kernel():
   Kernel<Dim<2>, WendlandC6Kernel< Dim<2> > >() {
-  setVolumeNormalization(6435.0/(3856.0*M_PI));
-  setKernelExtent(2.0);
+  setVolumeNormalization(78.0/(7.0*M_PI));
+  setKernelExtent(1.0);
   setInflectionPoint(1.0/3.0);
 }
 
@@ -28,8 +28,8 @@ template<>
 inline
 WendlandC6Kernel< Dim<3> >::WendlandC6Kernel():
   Kernel<Dim<3>, WendlandC6Kernel< Dim<3> > >() {
-  setVolumeNormalization(45045.0/(31456.0*M_PI));
-  setKernelExtent(2.0);
+  setVolumeNormalization(1365.0/(64.0*M_PI));
+  setKernelExtent(1.0);
   setInflectionPoint(1.0/3.0);
 }
 
@@ -53,8 +53,8 @@ WendlandC6Kernel<Dimension>::kernelValue(double etaMagnitude, double Hdet) const
 
   if (etaMagnitude < 2.0) {
     double eta2 = etaMagnitude*etaMagnitude;
-    return this->volumeNormalization()*Hdet*(pow(1.0-0.5*etaMagnitude,8)*
-                                             (1.0+4.0*etaMagnitude+(25.0/2.0)*eta2+18.0*etaMagnitude*eta2));
+    return this->volumeNormalization()*Hdet*(pow(1.0-etaMagnitude,8)*
+                                             (1.0+8.0*etaMagnitude+25.0*eta2+32.0*etaMagnitude*eta2));
   } else {
     return 0.0;
   }
@@ -72,8 +72,8 @@ WendlandC6Kernel<Dimension>::gradValue(double etaMagnitude, double Hdet) const {
 
   if (etaMagnitude < 2.0) {
     double eta2 = etaMagnitude*etaMagnitude;
-    return  this->volumeNormalization()*Hdet*((1.0/256.0)*pow(etaMagnitude-2.0,7)*
-                                              etaMagnitude*(198.0*eta2+17*etaMagnitude-14.0));
+    return  this->volumeNormalization()*Hdet*(22.0*pow(etaMagnitude-1.0,7)*
+                                              etaMagnitude*(16.0*eta2+7.0*etaMagnitude+1.0));
   } else {
     return 0.0;
   }
@@ -91,8 +91,8 @@ WendlandC6Kernel<Dimension>::grad2Value(double etaMagnitude, double Hdet) const 
 
   if (etaMagnitude < 1.0) {
     const double eta2 = etaMagnitude*etaMagnitude;
-    return this->volumeNormalization()*Hdet*((1.0/256.0)*pow(etaMagnitude-2.0,6)*
-                                               (1980.0*eta2*etaMagnitude-1035.0*eta2-180.0*etaMagnitude+28.0));
+    return this->volumeNormalization()*Hdet*(22.0*pow(etaMagnitude-1.0,6)*
+                                               (160.0*eta2*etaMagnitude+15.0*eta2-6.0*etaMagnitude-1.0));
   } else {
     return 0.0;
   }
