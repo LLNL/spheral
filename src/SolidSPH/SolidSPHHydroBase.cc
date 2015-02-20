3049d372444bdf2689a91a82c471fc77c8db67af
Mike Owen <mikeowen@llnl.gov>
2015-02-20 09:14:31 -0800

    Removing a few superfluous contracts.

--- src/SolidSPH/SolidSPHHydroBase.cc
+++ src/SolidSPH/SolidSPHHydroBase.cc
@@ -467,7 +467,6 @@ evaluateDerivatives(const typename Dimension::Scalar time,
       const int fragIDi = fragIDs(nodeListi, i);
       CHECK(mi > 0.0);
       CHECK(rhoi > 0.0);
-      CHECK(omegai > 0.0);
       CHECK(Hdeti > 0.0);
 
       Scalar& rhoSumi = rhoSum(nodeListi, i);
