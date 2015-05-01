0fce79a22c31c80b1d39a67e7abc0dd278af76ed
Mike Owen <mikeowen@llnl.gov>
2015-04-30 21:34:52 -0700

    Converting the TableKernel to use parabolic interpolation rather than linear.  This seems to have a significant effect on the accuracy of the kernel estimation.

--- src/Kernel/TableKernel.cc
+++ src/Kernel/TableKernel.cc
@@ -20,6 +20,8 @@
 #include "HatKernel.hh"
 #include "QuarticSplineKernel.hh"
 #include "QuinticSplineKernel.hh"
+#include "WendlandC4Kernel.hh"
+#include "WendlandC6Kernel.hh"
 
 namespace Spheral {
 namespace KernelSpace {
@@ -153,6 +155,89 @@ sumKernelValuesAs1D(const KernelType& W,
 }
 
 //------------------------------------------------------------------------------
+// Construct from a kernel.
+//------------------------------------------------------------------------------
+template<typename Dimension>
+template<typename KernelType>
+TableKernel<Dimension>::TableKernel(const KernelType& kernel,
+                                    const int numPoints,
+                                    const double hmult):
+  Kernel<Dimension, TableKernel<Dimension> >(),
+  mKernelValues(0),
+  mGradValues(0),
+  mGrad2Values(0),
+  mAkernel(0),
+  mBkernel(0),
+  mAgrad(0),
+  mBgrad(0),
+  mAgrad2(0),
+  mBgrad2(0),
+  mNumPoints(0),
+  mStepSize(0.0),
+  mNperhValues(0),
+  mWsumValues(0),
+  mMinNperh(0.5),
+  mMaxNperh(10.0) {
+
+  // Pre-conditions.
+  VERIFY(numPoints > 0);
+  VERIFY(hmult > 0.0);
+
+  // Set the volume normalization and kernel extent.
+  this->setVolumeNormalization(kernel.volumeNormalization() / Dimension::pownu(hmult));
+  this->setKernelExtent(hmult * kernel.kernelExtent());
+  this->setInflectionPoint(hmult * kernel.inflectionPoint());
+
+  // Set the number of points and table step size.
+  CHECK(numPoints > 1);
+  mNumPoints = numPoints;
+  mStepSize = this->kernelExtent()/(numPoints - 1);
+  CHECK(stepSize() > 0.0);
+
+  // Resize the tables for our data.
+  mKernelValues = std::vector<double>(numPoints);
+  mGradValues = std::vector<double>(numPoints);
+  mGrad2Values = std::vector<double>(numPoints);
+  mAkernel = std::vector<double>(numPoints);
+  mBkernel = std::vector<double>(numPoints);
+  mAgrad = std::vector<double>(numPoints);
+  mBgrad = std::vector<double>(numPoints);
+  mAgrad2 = std::vector<double>(numPoints);
+  mBgrad2 = std::vector<double>(numPoints);
+
+  // Fill in the kernel and gradient values.  Note that we will go ahead and fold
+  // the normalization constants in here, so we don't have to multiply by them
+  // in the value lookups.
+  const double deta = mStepSize/hmult;
+  for (int i = 0; i < numPoints; ++i) {
+    CHECK(i*mStepSize >= 0.0);
+    mKernelValues[i] = kernel(i*deta, 1.0);
+    mGradValues[i] = kernel.grad(i*deta, 1.0);
+    mGrad2Values[i] = kernel.grad2(i*deta, 1.0);
+  }
+
+  // Set the delta kernel values for internal use.
+  this->setParabolicCoeffs();
+
+  // // Adjust the table kernel values to reproduce the volume integral as closely as possible.
+  // const double vol1 = simpsonsVolumeIntegral<Dimension, TableKernel<Dimension> >(*this, 0.0, kernel.kernelExtent(), 10*numPoints);
+  // const double f = 1.0/vol1; // /(this->volumeNormalization());
+  // std::cerr << "Scaling: " << f << " " << vol1 << " " << this->volumeNormalization() << std::endl;
+  // for (int i = 0; i < numPoints; ++i) {
+  //   mKernelValues[i] *= f;
+  // }
+
+  // // Reset the delta kernel values for internal use.
+  // this->setDeltaKernelValues();
+
+  // Set the table of n per h values.
+  this->setNperhValues();
+
+  // That should be it, so we should have left the kernel in a valid state.
+  ENSURE(valid());
+}
+
+//------------------------------------------------------------------------------
 // Destructor
 //------------------------------------------------------------------------------
 template<typename Dimension>
@@ -172,12 +257,14 @@ operator=(const TableKernel<Dimension>& rhs) {
     mKernelValues = rhs.mKernelValues;
     mGradValues = rhs.mGradValues;
     mGrad2Values = rhs.mGrad2Values;
-    mDeltaKernelValues = rhs.mDeltaKernelValues;
-    mDeltaGradValues = rhs.mDeltaGradValues;
-    mDeltaGrad2Values = rhs.mDeltaGrad2Values;
+    mAkernel = rhs.mAkernel;
+    mBkernel = rhs.mBkernel;
+    mAgrad = rhs.mAgrad;
+    mBgrad = rhs.mBgrad;
+    mAgrad2 = rhs.mAgrad2;
+    mBgrad2 = rhs.mBgrad2;
     mNumPoints = rhs.mNumPoints;
-    mNumPoints1 = rhs.mNumPoints1;
-    mStepSizeInv = rhs.mStepSizeInv;
+    mStepSize = rhs.mStepSize;
     mNperhValues = rhs.mNperhValues;
     mWsumValues =  rhs.mWsumValues;
     mMinNperh = rhs.mMinNperh;
@@ -201,7 +288,7 @@ equivalentNodesPerSmoothingScale(const double Wsum) const {
   CHECK((lb >= -1) and (lb <= int(mWsumValues.size()) - 1));
   const int ub = lb + 1;
   CHECK((lb == -1 and Wsum <= mWsumValues[0]) ||
-        (ub == mNumPoints and Wsum >= mWsumValues[mNumPoints1]) ||
+        (ub == mNumPoints and Wsum >= mWsumValues[mNumPoints - 1]) ||
         (Wsum >= mWsumValues[lb] and Wsum <= mWsumValues[ub]));
 
   // Now interpolate for the corresponding nodes per h (within bounds);
@@ -209,7 +296,7 @@ equivalentNodesPerSmoothingScale(const double Wsum) const {
   if (lb == -1) {
     result = mNperhValues[0];
   } else if (ub == mNumPoints) {
-    result = mNperhValues[mNumPoints1];
+    result = mNperhValues[mNumPoints - 1];
   } else {
     result = std::min(mNperhValues[ub],
                       std::max(mNperhValues[lb],
@@ -236,7 +323,7 @@ equivalentWsum(const double nPerh) const {
   CHECK((lb >= -1) and (lb <= int(mNperhValues.size()) - 1));
   const int ub = lb + 1;
   CHECK((lb == -1 and nPerh <= mNperhValues[0]) ||
-        (ub == mNumPoints and nPerh >= mNperhValues[mNumPoints1]) ||
+        (ub == mNumPoints and nPerh >= mNperhValues[mNumPoints - 1]) ||
         (nPerh >= mNperhValues[lb] and nPerh <= mNperhValues[ub]));
 
   // Now interpolate for the corresponding Wsum.
@@ -244,7 +331,7 @@ equivalentWsum(const double nPerh) const {
   if (lb == -1) {
     result = mWsumValues[0];
   } else if (ub == mNumPoints) {
-    result = mWsumValues[mNumPoints1];
+    result = mWsumValues[mNumPoints - 1];
   } else {
     result = std::min(mWsumValues[ub], 
                       std::max(mWsumValues[lb],
@@ -258,29 +345,36 @@ equivalentWsum(const double nPerh) const {
 }
 
 //------------------------------------------------------------------------------
-// Initialize the delta kernel values.
+// Initialize the parabolic fit coefficients.
 //------------------------------------------------------------------------------
 template<typename Dimension>
 void
 TableKernel<Dimension>::
-setDeltaKernelValues() {
+setParabolicCoeffs() {
+  
+  // Size stuff up.
+  REQUIRE(mNumPoints > 0);
+  mAkernel = std::vector<double>(mNumPoints);
+  mBkernel = std::vector<double>(mNumPoints);
+  mAgrad = std::vector<double>(mNumPoints);
+  mBgrad = std::vector<double>(mNumPoints);
+  mAgrad2 = std::vector<double>(mNumPoints);
+  mBgrad2 = std::vector<double>(mNumPoints);
 
-  // Size the delta fields.
-  REQUIRE(mNumPoints1 >= 0 and
-          mNumPoints1 == mNumPoints - 1);
-  mDeltaKernelValues.resize(mNumPoints1);
-  mDeltaGradValues.resize(mNumPoints1);
-  mDeltaGrad2Values.resize(mNumPoints1);
+  // Find the coefficient fits.
+  for (int i0 = 0; i0 < mNumPoints - 2; ++i0) {
+    const int i1 = i0 + 1;
+    const int i2 = i0 + 2;
+    CHECK(i2 < mNumPoints);
 
-  // Now fill in the deltas.
-  CHECK(mKernelValues.size() == mNumPoints);
-  CHECK(mGradValues.size() == mNumPoints);
-  CHECK(mGrad2Values.size() == mNumPoints);
-  for (int i = 0; i < mNumPoints1; ++i) {
-    int j = i + 1;
-    mDeltaKernelValues[i] = mKernelValues[j] - mKernelValues[i];
-    mDeltaGradValues[i] = mGradValues[j] - mGradValues[i];
-    mDeltaGrad2Values[i] = mGrad2Values[j] - mGrad2Values[i];
+    mAkernel[i1] = ((mKernelValues[i2] - mKernelValues[i1])/mStepSize - (mKernelValues[i1] - mKernelValues[i0])/mStepSize)/(2.0*mStepSize);
+    mBkernel[i1] = ((mKernelValues[i2] - mKernelValues[i1]) + (mKernelValues[i1] - mKernelValues[i0]))/(2.0*mStepSize);
+
+    mAgrad[i1] = ((mGradValues[i2] - mGradValues[i1])/mStepSize - (mGradValues[i1] - mGradValues[i0])/mStepSize)/(2.0*mStepSize);
+    mBgrad[i1] = ((mGradValues[i2] - mGradValues[i1]) + (mGradValues[i1] - mGradValues[i0]))/(2.0*mStepSize);
+
+    mAgrad2[i1] = ((mGrad2Values[i2] - mGrad2Values[i1])/mStepSize - (mGrad2Values[i1] - mGrad2Values[i0])/mStepSize)/(2.0*mStepSize);
+    mBgrad2[i1] = ((mGrad2Values[i2] - mGrad2Values[i1]) + (mGrad2Values[i1] - mGrad2Values[i0]))/(2.0*mStepSize);
   }
 }
 
@@ -293,7 +387,7 @@ TableKernel<Dimension>::
 setNperhValues(const bool scaleTo1D) {
   REQUIRE(mMinNperh > 0.0);
   REQUIRE(mMaxNperh > mMinNperh);
-  REQUIRE(mNumPoints1 > 0);
+  REQUIRE(mNumPoints > 1);
   REQUIRE(this->kernelExtent() > 0.0);
 
   // Size the Nperh array.
@@ -301,7 +395,7 @@ setNperhValues(const bool scaleTo1D) {
   mNperhValues.resize(mNumPoints);
 
   // For the allowed range of n per h, sum up the kernel values.
-  const double dnperh = (mMaxNperh - mMinNperh)/mNumPoints1;
+  const double dnperh = (mMaxNperh - mMinNperh)/(mNumPoints - 1);
   for (int i = 0; i != mNumPoints; ++i) {
     const double nperh = mMinNperh + i*dnperh;
     CHECK(nperh >= mMinNperh and nperh <= mMaxNperh);
@@ -318,7 +412,7 @@ setNperhValues(const bool scaleTo1D) {
   BEGIN_CONTRACT_SCOPE;
   ENSURE(mWsumValues.size() == mNumPoints);
   ENSURE(mNperhValues.size() == mNumPoints);
-  for (int i = 0; i != mNumPoints1; ++i) {
+  for (int i = 0; i != mNumPoints - 1; ++i) {
     ENSURE(mWsumValues[i] <= mWsumValues[i + 1]);
     ENSURE(mNperhValues[i] <= mNperhValues[i + 1]);
   }
@@ -335,14 +429,16 @@ TableKernel<Dimension>::
 valid() const {
   return (Kernel<Dimension, TableKernel<Dimension> >::valid() and
           mNumPoints > 0 and
-          mNumPoints1 == mNumPoints - 1 and
           mKernelValues.size() == mNumPoints and
           mGradValues.size() == mNumPoints and
           mGrad2Values.size() == mNumPoints and
-          mDeltaKernelValues.size() == mNumPoints - 1 and
-          mDeltaGradValues.size() == mNumPoints - 1 and
-          mDeltaGrad2Values.size() == mNumPoints - 1 and
-          mStepSizeInv > 0.0);
+          mAkernel.size() == mNumPoints and
+          mBkernel.size() == mNumPoints and
+          mAgrad.size() == mNumPoints and
+          mBgrad.size() == mNumPoints and
+          mAgrad2.size() == mNumPoints and
+          mBgrad2.size() == mNumPoints and
+          mStepSize > 0.0);
 }
 
 }
