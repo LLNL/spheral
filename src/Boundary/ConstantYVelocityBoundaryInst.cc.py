3e7d79c1d749e867e6e402ff32d2071b4c44c55e
Mike Owen <mikeowen@llnl.gov>
2015-08-14 22:05:19 -0700

    Converted more packages to the new instantiation model.

---
+++ src/Boundary/ConstantYVelocityBoundaryInst.cc.py
@@ -0,0 +1,12 @@
+text = """
+//------------------------------------------------------------------------------
+// Explicit instantiation.
+//------------------------------------------------------------------------------
+#include "Geometry/Dimension.hh"
+#include "ConstantYVelocityBoundary.cc"
+
+namespace Spheral {
+  namespace BoundarySpace {
+    template class ConstantYVelocityBoundary< Dim< %(ndim)s > >;
+}
+"""
