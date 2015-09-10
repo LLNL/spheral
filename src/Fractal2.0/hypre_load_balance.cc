ddbe8ca072a89b4f0a0872b8980936174d5c3cd5
villumsen <villumsen@localhost>
2015-09-09 17:54:37 -0700

    better Hypre load balance

--- src/Fractal2.0/hypre_load_balance.cc
+++ src/Fractal2.0/hypre_load_balance.cc
@@ -26,29 +26,48 @@ namespace FractalSpace
     int average=count_sum1/count_sum0;
     int nodes_eff=count_sum1*count_sum1/count_sum2;
 
-    int max_on_node=max((average*3)/2,mem.hypre_max_node_load);
-    bool spread_even = average >= mem.hypre_max_average_load;
+    int max_on_node=max((average*60)/20,mem.hypre_max_node_load);
+    bool spread_even = average >= max_on_node;
     bool OOM = count_max > max_on_node;
 
     HYP.ij_countsB=HYP.ij_counts;
     fprintf(PFH," Hypre on Nodes %d %d %d %d %d\n",average,count_max,nodes_eff,mem.hypre_max_node_load,max_on_node);
     fprintf(PFH," Hypre Load Balance %d %d \n",spread_even,OOM);
+    load_balance=false;
+    if(HypreNodes == 1)
+      return 0;
     if(!mem.hypre_load_balance)
       return 0;
-    if(!OOM)
-      return 0;
-    load_balance=true;
-    for(int HR=1;HR<HypreNodes;HR++)
-      HYP.ij_offsetsB[HR]=min(HYP.ij_offsetsB[HR],HYP.ij_offsetsB[HR-1]+max_on_node);
-    if(HYP.ij_offsetsB[HypreNodes]-HYP.ij_offsetsB[HypreNodes-1] > max_on_node)
-      for(int HR=HypreNodes;HR>1;HR--)
-	HYP.ij_offsetsB[HR-1]=max(HYP.ij_offsetsB[HR-1],HYP.ij_offsetsB[HR]-max_on_node);
+//     if(!OOM)
+//       return 0;
+    int loops=0;
+    while(loops < 5)
+      {
+	for(int HR=1;HR<HypreNodes;HR++)
+	  HYP.ij_offsetsB[HR]=min(HYP.ij_offsetsB[HR],HYP.ij_offsetsB[HR-1]+max_on_node);
+	if(HYP.ij_offsetsB[HypreNodes]-HYP.ij_offsetsB[HypreNodes-1] <= max_on_node)
+	  break;
+	for(int HR=HypreNodes;HR>1;HR--)
+	  HYP.ij_offsetsB[HR-1]=max(HYP.ij_offsetsB[HR-1],HYP.ij_offsetsB[HR]-max_on_node);
+	if(HYP.ij_offsetsB[1] <= max_on_node)
+	  break;
+	loops++;
+      }
     int offes=0;
+    int max_off=0;
+    int maxC=0;
+    int maxCB=0;
     for(int HR=0;HR<HypreNodes;HR++)
       {
-	int offe=max(HYP.ij_offsetsB[HR]-HYP.ij_offsets[HR],0)+max(HYP.ij_offsets[HR+1]-HYP.ij_offsetsB[HR+1],0);
+	load_balance=load_balance || (HYP.ij_offsets[HR] != HYP.ij_offsetsB[HR]);
+	int minb=min(max(HYP.ij_offsets[HR],HYP.ij_offsetsB[HR]),HYP.ij_offsets[HR+1]);
+	int maxb=max(min(HYP.ij_offsets[HR+1],HYP.ij_offsetsB[HR+1]),HYP.ij_offsets[HR]);
+	int offe=(minb-HYP.ij_offsets[HR])+(HYP.ij_offsets[HR+1]-maxb);
 	offes+=offe;
+	max_off=max(offe,max_off);
 	HYP.ij_countsB[HR]=HYP.ij_offsetsB[HR+1]-HYP.ij_offsetsB[HR];
+	maxC=max(maxC,HYP.ij_counts[HR]);
+	maxCB=max(maxCB,HYP.ij_countsB[HR]);
 	fprintf(PFH," offsets balance %7d %10d %7d %10d %7d %7d %7d \n",HR,HYP.ij_offsets[HR],HYP.ij_counts[HR],HYP.ij_offsetsB[HR],HYP.ij_countsB[HR],offe,offes);
       }
     int first_on_new_node=HYP.ij_offsetsB[HypreRank];
@@ -61,7 +80,7 @@ namespace FractalSpace
 	if(label < first_on_new_node || label > last_on_new_node)
 	  off_elements++;
       }
-    fprintf(PFH," off_elements %d \n",off_elements);
+    fprintf(PFH," off_elements %d %d %d %d %d %d %d \n",off_elements,max_off,offes,max_on_node,maxC,maxCB,HYP.ij_offsets[HypreNodes]);
     return off_elements;
   }
   template <class T> bool overlap_interval(T Imin,T Imax,T Jmin,T Jmax,T& LOW,T& HIGH)
