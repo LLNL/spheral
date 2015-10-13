c6a1015051fdb4146d6f5c4e42d60bd8a24421b1
Mike Owen <mikeowen@llnl.gov>
2015-10-13 15:18:31 -0700

    Merging default->CSPH_Summer.

---
+++ src/Kernel/VolumeIntegrationFunctions.cc
@@ -0,0 +1,72 @@
+//---------------------------------Spheral++----------------------------------//
+// VolumeIntegrationFunctions.
+// A set of helper methods to numerically evaluate the appropriate kernel
+// normalization.
+//
+// Created by JMO, Mon Jan  6 16:24:19 PST 2003
+//----------------------------------------------------------------------------//
+#include <math.h>
+#include <functional>
+
+#include "VolumeIntegrationFunctions.hh"
+#include "Utilities/simpsonsIntegration.hh"
+#include "Geometry/Dimension.hh"
+#include "Utilities/DBC.hh"
+
+namespace Spheral {
+namespace KernelSpace {
+
+using namespace std;
+//------------------------------------------------------------------------------
+// Functor to provide the apppriate integrand.
+//------------------------------------------------------------------------------
+template<typename Dimension>
+double
+volumeElement(const double r);
+
+template<>
+inline
+double
+volumeElement< Dim<1> >(const double r) {
+  return 2.0;
+}
+
+template<>
+inline
+double
+volumeElement< Dim<2> >(const double r) {
+  return 2.0*M_PI*r;
+}
+
+template<>
+inline
+double
+volumeElement< Dim<3> >(const double r) {
+  return 4.0*M_PI*r*r;
+}
+
+template<typename Dimension, typename KernelType>
+struct KernelVolumeElement {
+  KernelType mW;
+  KernelVolumeElement(const KernelType& W): mW(W) {}
+  double operator()(const double r) const {
+    return volumeElement<Dimension>(r)*mW(r, 1.0);
+  }
+};    
+
+//------------------------------------------------------------------------------
+// Use the trapezoidal rule to evaluate the volume integral of the given kernel.
+//------------------------------------------------------------------------------
+template<typename Dimension, typename KernelType>
+inline
+double simpsonsVolumeIntegral(const KernelType& W,
+                              const double rMin,
+                              const double rMax,
+                              const int numBins) {
+  return simpsonsIntegration<KernelVolumeElement<Dimension, KernelType>, double, double>
+    (KernelVolumeElement<Dimension, KernelType>(W), rMin, rMax, numBins);
+}
+  
+}
+}
+
---
+++ src/PBGWraps/Geometry/GeometryTypes.hh
@@ -0,0 +1,288 @@
+#ifndef __PBGWRAPS_GEOMETRYTYPES__
+#define __PBGWRAPS_GEOMETRYTYPES__
+
+#include "Python.h"
+#include <vector>
+#include <string>
+#include <sstream>
+#include "Geometry/Dimension.hh"
+#include "Geometry/GeomVector.hh"
+#include "Geometry/Geom3Vector.hh"
+#include "Geometry/GeomTensor.hh"
+#include "Geometry/GeomSymmetricTensor.hh"
+#include "Geometry/GeomThirdRankTensor.hh"
+#include "Geometry/GeomFourthRankTensor.hh"
+#include "Geometry/EigenStruct.hh"
+#include "Geometry/computeEigenValues.hh"
+#include "Geometry/GeomPlane.hh"
+#include "Geometry/GeomPolygon.hh"
+#include "Geometry/GeomPolyhedron.hh"
+#include "Geometry/invertRankNTensor.hh"
+#include "Utilities/DataTypeTraits.hh"
+
+using namespace Spheral;
+
+namespace Spheral {
+
+//------------------------------------------------------------------------------
+// Names!
+//------------------------------------------------------------------------------
+typedef GeomVector<1> Vector1d;
+typedef GeomVector<2> Vector2d;
+typedef GeomVector<3> Vector3d;
+
+typedef GeomTensor<1> Tensor1d;
+typedef GeomTensor<2> Tensor2d;
+typedef GeomTensor<3> Tensor3d;
+
+typedef GeomSymmetricTensor<1> SymTensor1d;
+typedef GeomSymmetricTensor<2> SymTensor2d;
+typedef GeomSymmetricTensor<3> SymTensor3d;
+
+typedef GeomThirdRankTensor<1> ThirdRankTensor1d;
+typedef GeomThirdRankTensor<2> ThirdRankTensor2d;
+typedef GeomThirdRankTensor<3> ThirdRankTensor3d;
+
+typedef GeomFourthRankTensor<1> FourthRankTensor1d;
+typedef GeomFourthRankTensor<2> FourthRankTensor2d;
+typedef GeomFourthRankTensor<3> FourthRankTensor3d;
+
+typedef EigenStruct<1> EigenStruct1d;
+typedef EigenStruct<2> EigenStruct2d;
+typedef EigenStruct<3> EigenStruct3d;
+
+typedef GeomPlane<Dim<1> > Plane1d;
+typedef GeomPlane<Dim<2> > Plane2d;
+typedef GeomPlane<Dim<3> > Plane3d;
+
+typedef GeomFacet2d Facet2d;
+typedef GeomPolygon Polygon;
+
+typedef GeomFacet3d Facet3d;
+typedef GeomPolyhedron Polyhedron;
+
+}
+
+typedef std::vector<Spheral::Facet2d> vector_of_Facet2d;
+typedef std::vector<Spheral::Facet3d> vector_of_Facet3d;
+
+//------------------------------------------------------------------------------
+// Sequence methods for geometric types.
+//------------------------------------------------------------------------------
+namespace Spheral {
+
+template<typename Type>
+inline
+unsigned
+sizeGeomType(Type& self) {
+  return DataTypeTraits<Type>::numElements(self);
+}
+
+template<typename Type>
+inline
+double
+indexGeomType(Type& self,
+              int index) {
+  const int n = DataTypeTraits<Type>::numElements(self);
+  if (index < 0) index += n;
+  if (index < n) {
+    return *(self.begin() + index);
+  } else {
+    PyErr_SetString(PyExc_IndexError, "Index out of range");
+    return 0.0;
+  }
+}
+
+template<typename Type>
+inline
+std::vector<double>
+sliceGeomType(Type& self,
+              int i1,
+              int i2) {
+  std::vector<double> result;
+  const int n = DataTypeTraits<Type>::numElements(self);
+  if (i1 < 0) i1 += n;
+  if (i2 < 0) i2 += n;
+  for (int i = i1; i < i2; ++i) result.push_back(indexGeomType(self, i));
+  return result;
+}
+
+template<typename Type>
+inline
+int
+assignToGeomType(Type& self, 
+                 int index,
+                 const double value) {
+  const int n = DataTypeTraits<Type>::numElements(self);
+  if (index < 0) index += n;
+  if (index >= n) {
+    PyErr_SetString(PyExc_IndexError, "Container index out of range");
+    return -1;
+  } else {
+    *(self.begin() + index) = value;
+    return 0;
+  }
+}
+
+template<typename Type>
+inline
+int
+containsGeomType(Type& self,
+                 const double value) {
+  int result = 0;
+  const int n = DataTypeTraits<Type>::numElements(self);
+  int i = 0;
+  while (result == 0 and i++ < n) {
+    result = ((*(self.begin() + i) == value) ? 1 : 0);
+  }
+  return result;
+}
+
+//------------------------------------------------------------------------------
+// Set the given element of a third rank tensor.
+//------------------------------------------------------------------------------
+template<typename TRT>
+inline
+void
+assignThirdRankTensorElement(TRT& self,
+                             const size_t i,
+                             const size_t j,
+                             const size_t k,
+                             const double val) {
+  self(i,j,k) = val;
+}
+
+//------------------------------------------------------------------------------
+// Set the given element of a third rank tensor.
+//------------------------------------------------------------------------------
+template<typename TRT>
+inline
+void
+assignFourthRankTensorElement(TRT& self,
+                              const size_t i,
+                              const size_t j,
+                              const size_t k,
+                              const size_t m,
+                              const double val) {
+  self(i,j,k,m) = val;
+}
+
+//------------------------------------------------------------------------------
+// Nice string representations (Vector)
+//------------------------------------------------------------------------------
+template<typename Vector>
+inline
+std::string
+printReprVector(const Vector& val) {
+  std::stringstream s;
+  s << "Vector" << Vector::nDimensions << "d( ";
+  for (size_t i = 0; i != Vector::nDimensions; ++i) {
+    s << val(i) << " ";
+  }
+  s << ")";
+  return s.str();
+}
+
+//------------------------------------------------------------------------------
+// Nice string representations (Tensor)
+//------------------------------------------------------------------------------
+template<typename Tensor>
+inline
+std::string
+printReprTensor(const Tensor& val) {
+  std::stringstream s;
+  s << "Tensor" << Tensor::nDimensions << "d(";
+  for (size_t row = 0; row != Tensor::nDimensions; ++row) {
+    s << "( ";
+    for (size_t col = 0; col != Tensor::nDimensions; ++col) {
+      s << val(row, col) << " ";
+    }
+    s << ")";
+  }
+  s << ")";
+  return s.str();
+}
+
+//------------------------------------------------------------------------------
+// Nice string representations (ThirdRankTensor)
+//------------------------------------------------------------------------------
+template<typename TRTensor>
+inline
+std::string
+printReprThirdRankTensor(const TRTensor& val) {
+  std::stringstream s;
+  s << "Tensor" << TRTensor::nDimensions << "d(";
+  for (size_t i = 0; i != TRTensor::nDimensions; ++i) {
+    s << "( ";
+    for (size_t j = 0; j != TRTensor::nDimensions; ++j) {
+      s << "( ";
+      for (size_t k = 0; k != TRTensor::nDimensions; ++k) {
+        s << val(i, j, k) << " ";
+      }
+      s << ")";
+    }
+    s << ")";
+  }
+  s << ")";
+  return s.str();
+}
+
+//------------------------------------------------------------------------------
+// Nice string representations (FourthRankTensor)
+//------------------------------------------------------------------------------
+template<typename FRTensor>
+inline
+std::string
+printReprFourthRankTensor(const FRTensor& val) {
+  std::stringstream s;
+  s << "Tensor" << FRTensor::nDimensions << "d(";
+  for (size_t i = 0; i != FRTensor::nDimensions; ++i) {
+    s << "( ";
+    for (size_t j = 0; j != FRTensor::nDimensions; ++j) {
+      s << "( ";
+      for (size_t k = 0; k != FRTensor::nDimensions; ++k) {
+        s << "( ";
+        for (size_t m = 0; m != FRTensor::nDimensions; ++m) {
+          s << val(i, j, k, m) << " ";
+        }
+        s << ")";
+      }
+      s << ")";
+    }
+    s << ")";
+  }
+  s << ")";
+  return s.str();
+}
+
+//------------------------------------------------------------------------------
+// Provide a function to construct a Geometry type (Vector, Tensor, etc.) from
+// a sequence.
+//------------------------------------------------------------------------------
+template<typename GeomType>
+GeomType*
+constructGeomTypeFromSequence(PyObject* obj) {
+  GeomType* result = new GeomType;
+  if (PySequence_Check(obj) != 1) {
+    PyErr_SetString(PyExc_ValueError, "Error: attempt to construct a GeomType from a non-sequence");
+    return result;
+  }
+  const Py_ssize_t n = PySequence_Size(obj);
+  if (n != DataTypeTraits<GeomType>::numElements(*result)) {
+    PyErr_SetString(PyExc_ValueError, "Error: attempt to construct a GeomType from a sequence of incorrect size.");
+    return result;
+  }
+  for (Py_ssize_t i = 0; i != n; ++i) {
+    PyObject* numobj = PySequence_GetItem(obj, i);
+    if (PyNumber_Check(numobj) != 1) {
+      PyErr_SetString(PyExc_ValueError, "Error: attempt to construct a GeomType from a sequence containing non-numbers.");
+      return result;
+    }
+    *(result->begin() + i) = PyFloat_AsDouble(numobj);
+  }
+  return result;
+}
+
+}
+
+#endif
