7a964da7f91d552f0fc7e231aad51b0e65661794
Mike Owen <mikeowen@llnl.gov>
2015-11-23 22:44:57 -0800

    Adding an option to correct the initial density & pressure with the sum defintion in the triple point.

--- tests/Hydro/TriplePoint/TriplePoint-xy.py
+++ tests/Hydro/TriplePoint/TriplePoint-xy.py
@@ -53,6 +53,10 @@ commandLine(
     nx3 = 120,
     ny3 = 30,
 
+    # Optionally set the initial density with the sum definition, and recompute
+    # the energy to re-establish the the initial pressures.
+    relaxInitialDensity = False,
+
     nPerh = 1.51,
     KernelConstructor = BSplineKernel,
     order = 5,
@@ -61,7 +65,7 @@ commandLine(
     CRKSPH = False,
     ASPH = False,
     SPH = True,   # This just chooses the H algorithm -- you can use this with CRKSPH for instance.
-    filter = 0.2,  # For CRKSPH
+    filter = 0.0,  # For CRKSPH
     Qconstructor = MonaghanGingoldViscosity,
     #Qconstructor = TensorMonaghanGingoldViscosity,
     boolReduceViscosity = False,
@@ -126,7 +130,7 @@ elif CRKSPH:
         HydroConstructor = ACRKSPHHydro
     else:
         HydroConstructor = CRKSPHHydro
-    #Qconstructor = CRKSPHMonaghanGingoldViscosity
+    Qconstructor = CRKSPHMonaghanGingoldViscosity
 else:
     if ASPH:
         HydroConstructor = ASPHHydro
@@ -357,7 +361,6 @@ packages = [hydro]
 #-------------------------------------------------------------------------------
 # Construct the MMRV physics object.
 #-------------------------------------------------------------------------------
-
 if boolReduceViscosity:
     #q.reducingViscosityCorrection = True
     evolveReducingViscosityMultiplier = MorrisMonaghanReducingViscosity(q,nh,aMin,aMax)
@@ -426,6 +429,38 @@ control = SpheralController(integrator, WT,
                             skipInitialPeriodicWork = (HydroConstructor in (SVPHFacetedHydro, ASVPHFacetedHydro)),
                             SPH = SPH)
 output("control")
+
+#-------------------------------------------------------------------------------
+# Optionally compute the sum density and recompute the initial conditions.
+#-------------------------------------------------------------------------------
+if relaxInitialDensity and control.totalSteps == 0:
+    state = State(db, integrator.physicsPackages())
+    derivs = StateDerivatives(db, integrator.physicsPackages())
+    integrator.preStepInitialize(state, derivs)
+    integrator.initializeDerivatives(0.0, 0.0, state, derivs)
+    cm = db.connectivityMap()
+    mass = state.scalarFields(HydroFieldNames.mass)
+    position = state.vectorFields(HydroFieldNames.position)
+    H = state.symTensorFields(HydroFieldNames.H)
+    rho = state.scalarFields(HydroFieldNames.massDensity)
+    if CRKSPH:
+        vol = state.scalarFields(HydroFieldNames.volume)
+        A = state.scalarFields(HydroFieldNames.A_CRKSPH)
+        B = state.vectorFields(HydroFieldNames.B_CRKSPH)
+        C = state.tensorFields(HydroFieldNames.C_CRKSPH)
+        computeCRKSPHSumMassDensity(cm, WT, position, mass, vol, H, A, B, C, hydro.correctionOrder, rho)
+    else:
+        computeSPHSumMassDensity(cm, WT, True, position, mass, H, rho)
+    for (nodes, P0) in ((leftNodes, P1),
+                        (topNodes, P2),
+                        (bottomNodes, P3)):
+        rhof = nodes.massDensity()
+        epsf = nodes.specificThermalEnergy()
+        for i in xrange(nodes.numInternalNodes):
+            epsf[i] = P0/((gamma1 - 1.0)*rhof[i])
+
+    # Force another visit dumps so we can see what changed.
+    control.dropViz(0, 0.0, 0.0)
 
 #-------------------------------------------------------------------------------
 # Advance to the end time.
