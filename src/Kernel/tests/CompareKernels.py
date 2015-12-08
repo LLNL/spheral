7c6517466e20da3c5184d58c39ebe57d2ea5a42e
Cody Raskin <raskin1@llnl.gov>
2015-12-08 09:56:22 -0800

    A kernel comparison script (currently 2d kernels)

--- src/Kernel/tests/CompareKernels.py
+++ src/Kernel/tests/CompareKernels.py
@@ -20,13 +20,15 @@ def plotW(plot, W, xmin=0.0, xmax=2.0, numPnts=200, Hdet=1.0, title='',
             i3 += abs(y[i]+y[i-1])/2.0 * dx * x[i]*x[i]
     Pi = 3.14159
     print "{0:3.3f} {1:3.3f} {2:3.3f}".format(i1*2.0,i2*2.0,i3)
-    plot('set xrange [%f:%f]' % (xmin, xmax))
-    plot.xlabel('r')
-    plot.ylabel('W(r)')
+    plot('set xrange [0:1]')
+    plot.xlabel('eta')
+    plot.ylabel('W')
     if title:
         plot.title(title)
-    data = Gnuplot.Data(x, y, with_='lines', title=lineTitle)
+    data = Gnuplot.Data(x/xmax, y/y[0], with_='lines', title=lineTitle)
+
     plot.replot(data)
+    plot('set xrange[0:1]')
     return
 
 import sys, string
@@ -39,24 +41,14 @@ dx = 1.0/(numPts - 1)
 ################################################################################
 numPoints = 100
 
-kernelDict = {'spline': [BSplineKernel1d(),
-                         BSplineKernel2d(),
-                         BSplineKernel3d()],
-              'w4spline': [W4SplineKernel1d(),
-                           W4SplineKernel2d(),
-                           W4SplineKernel3d()],
-              'wendlandc4': [WendlandC4Kernel1d(),
-                             WendlandC4Kernel2d(),
-                             WendlandC4Kernel3d()],
-              'wendlandc6': [WendlandC6Kernel1d(),
-                             WendlandC6Kernel2d(),
-                             WendlandC6Kernel3d()],
+kernelDict = {'spline': [BSplineKernel2d()],
+              'w4spline': [W4SplineKernel2d()],
+              'wendlandc4': [WendlandC4Kernel2d()],
+              'wendlandc6': [WendlandC6Kernel2d()],
               'expinv': [ExpInvKernel1d(),
                          ExpInvKernel2d(),
                          ExpInvKernel3d()],
-              'quartic': [QuarticSplineKernel1d(),
-                          QuarticSplineKernel2d(),
-                          QuarticSplineKernel3d()],
+              'quartic': [QuarticSplineKernel3d()],
 ##              'gauss': [GaussianKernel1d(3),
 ##                        GaussianKernel2d(3),
 ##                        GaussianKernel3d(3)],
@@ -78,18 +70,10 @@ kernelDict = {'spline': [BSplineKernel1d(),
 ##                        NSincPolynomialKernel2d(5)],
 ##              'poly7': [NSincPolynomialKernel1d(7),
 ##                        NSincPolynomialKernel2d(7)],
-              'spline3': [NBSplineKernel1d(3),
-                          NBSplineKernel2d(3),
-                          NBSplineKernel3d(3)],
-              'spline5': [NBSplineKernel1d(5),
-                          NBSplineKernel2d(5),
-                          NBSplineKernel3d(5)],
-              'spline7': [NBSplineKernel1d(7),
-                          NBSplineKernel2d(7),
-                          NBSplineKernel3d(7)],
-              'spline9': [NBSplineKernel1d(9),
-                          NBSplineKernel2d(9),
-                          NBSplineKernel3d(9)],
+              'spline3': [NBSplineKernel2d(3)],
+              'spline5': [NBSplineKernel2d(5)],
+              'spline7': [NBSplineKernel2d(7)],
+              'spline9': [NBSplineKernel2d(9)],
 ##              'spline11': [NBSplineKernel1d(11),
 ##                           NBSplineKernel2d(11),
 ##                           NBSplineKernel3d(11)],
@@ -119,36 +103,18 @@ titleDict = {'spline':  'B Spline Kernel',
              }
 
 plots = []
+for i in xrange(1):
+    plots.append(generateNewGnuPlot())
 for kernel in kernels:
     title(titleDict[kernel])
-    for i in xrange(6):
-        plots.append(generateNewGnuPlot())
-    for W in kernelDict[kernel]:
-        output("W")
-        output("W.volumeNormalization")
-        output("W.kernelExtent")
-        plotW(plots[-6], W.kernelValue, 0.0, W.kernelExtent,
-              title = titleDict[kernel],
-              lineTitle = str(W))
-        plotW(plots[-5], W.gradValue, 0.0, W.kernelExtent,
-              title = titleDict[kernel] + ' Gradient',
-              lineTitle = str(W))
-        plotW(plots[-4], W.grad2Value, 0.0, W.kernelExtent,
-              title = titleDict[kernel] + ' second derivative',
-              lineTitle = str(W))
 
+    for W in kernelDict[kernel]:
         # Build a tabular version of the kernel
         WT = eval('TableKernel' + str(W).split()[0][-2:] + '(W, numPoints)')
         output("WT")
         output("WT.volumeNormalization")
         output("WT.kernelExtent")
-        plotW(plots[-3], WT.kernelValue, 0.0, W.kernelExtent,
-              title='Table ' + titleDict[kernel],
-              lineTitle = str(W))
-        plotW(plots[-2], WT.gradValue, 0.0, W.kernelExtent,
-              title = 'Table ' + titleDict[kernel] + ' Gradient',
-              lineTitle = str(W))
-        plotW(plots[-1], WT.grad2Value, 0.0, W.kernelExtent,
-              title = 'Table ' + titleDict[kernel] + ' second derivative',
-              lineTitle = str(W))
+        plotW(plots[-1], WT.kernelValue, 0.0, W.kernelExtent,
+              title='Kernels',
+              lineTitle = titleDict[kernel])
 
