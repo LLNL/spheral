3a5e673abe0c0384131990182099e80b508e5a8b
Mike Owen <mikeowen@llnl.gov>
2015-12-23 15:26:59 -0800

    Turning off the tensile correction artificial pressure by default.

--- src/SPH/PSPHHydros.py
+++ src/SPH/PSPHHydros.py
@@ -26,7 +26,7 @@ class %(classname)s%(dim)s(PSPHHydroBase%(dim)s):
                  sumMassDensityOverAllNodeLists = True,
                  densityUpdate = RigorousSumDensity,
                  HUpdate = IdealH,
-                 epsTensile = 0.3,
+                 epsTensile = 0.0,
                  nTensile = 4.0,
                  xmin = Vector%(dim)s(-1e100, -1e100, -1e100),
                  xmax = Vector%(dim)s( 1e100,  1e100,  1e100)):
