6be71dbd227a764712c2a377cff37fe9aff3c5e3
Mike Owen <mikeowen@llnl.gov>
2016-01-10 15:59:08 -0800

    Fixing the Bonet & Kulasegaraum quintic kernel for 1D.

--- src/Kernel/QuinticSplineKernel.cc
+++ src/Kernel/QuinticSplineKernel.cc
@@ -1,44 +1,13 @@
-#include "Geometry/Dimension.hh"
+#include "QuinticSplineKernel.hh"
 #include "Utilities/FastMath.hh"
 
 namespace Spheral {
 namespace KernelSpace {
 
 //------------------------------------------------------------------------------
-// Empty constructor
-//------------------------------------------------------------------------------
-template<>
-inline
-QuinticSplineKernel< Dim<1> >::QuinticSplineKernel():
-  Kernel<Dim<1>, QuinticSplineKernel< Dim<1> > >() {
-  setVolumeNormalization(3.0/48.0);
-  setKernelExtent(2.0);
-  setInflectionPoint(0.342037); // (2.0/15.0*(7.0 - pow(2.0, 1.0/3.0) - pow(22.0, 2.0/3.0)));
-}
-
-template<>
-inline
-QuinticSplineKernel< Dim<2> >::QuinticSplineKernel():
-  Kernel<Dim<2>, QuinticSplineKernel< Dim<2> > >() {
-  setVolumeNormalization(3.0/(16.0*M_PI));
-  setKernelExtent(2.0);
-  setInflectionPoint(0.342037); // (2.0/15.0*(7.0 - pow(2.0, 1.0/3.0) - pow(22.0, 2.0/3.0)));
-}
-
-template<>
-inline
-QuinticSplineKernel< Dim<3> >::QuinticSplineKernel():
-  Kernel<Dim<3>, QuinticSplineKernel< Dim<3> > >() {
-  setVolumeNormalization(7.0/(40.0*M_PI));
-  setKernelExtent(2.0);
-  setInflectionPoint(0.342037); // (2.0/15.0*(7.0 - pow(2.0, 1.0/3.0) - pow(22.0, 2.0/3.0)));
-}
-
-//------------------------------------------------------------------------------
 // Destructor
 //------------------------------------------------------------------------------
 template<typename Dimension>
-inline
 QuinticSplineKernel<Dimension>::~QuinticSplineKernel() {
 }
 
@@ -46,7 +15,6 @@ QuinticSplineKernel<Dimension>::~QuinticSplineKernel() {
 // Return the kernel weight for a given normalized distance.
 //------------------------------------------------------------------------------
 template<typename Dimension>
-inline
 double
 QuinticSplineKernel<Dimension>::kernelValue(double etaMagnitude, double Hdet) const {
   REQUIRE(etaMagnitude >= 0.0);
@@ -65,7 +33,6 @@ QuinticSplineKernel<Dimension>::kernelValue(double etaMagnitude, double Hdet) co
 // Return the gradient value for a given normalized distance.
 //------------------------------------------------------------------------------
 template<typename Dimension>
-inline
 double
 QuinticSplineKernel<Dimension>::gradValue(double etaMagnitude, double Hdet) const {
   REQUIRE(etaMagnitude >= 0.0);
@@ -84,7 +51,6 @@ QuinticSplineKernel<Dimension>::gradValue(double etaMagnitude, double Hdet) cons
 // Return the second derivative value for a given normalized distance.
 //------------------------------------------------------------------------------
 template<typename Dimension>
-inline
 double
 QuinticSplineKernel<Dimension>::grad2Value(double etaMagnitude, double Hdet) const {
   REQUIRE(etaMagnitude >= 0.0);
