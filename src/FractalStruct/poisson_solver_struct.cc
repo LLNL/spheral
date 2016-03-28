321808eb0aa871b598a4b30662e2226647e196c3
villumsen <villumsen@localhost>
2016-03-28 14:56:20 -0700

    updated struct files

--- src/FractalStruct/poisson_solver_struct.cc
+++ src/FractalStruct/poisson_solver_struct.cc
@@ -5,45 +5,39 @@ namespace FractalSpace
 {
   void poisson_solver_struct(Fractal& fractal,Fractal_Memory& mem,const int& level)
   {
+    static int _COUNTER=0;
     int RANK=-1;
     MPI_Comm_rank(MPI_COMM_WORLD,&RANK);
-//     bool Ranky = RANK == 21;
-//     Ranky=false;
     int spacing=Misc::pow(2,fractal.get_level_max()-level);
-//     for(int ni=0;ni<2;ni++)
+    cerr << "POISSON A " << RANK << " " << level << " " << " " << _COUNTER << endl;
     for(int ni=0;ni<2;ni++)
       {
+	cerr << "POISSON B " << RANK << " " << level << " " << ni << " " << _COUNTER << endl;
 	vector <vector <Point*> >hypre_points;
 	vector <vector <Point*> >SPoints;
 	vector <vector <int> >SBoxes;
-	mem.p_mess->Full_Stop_Do_Not_Argue();
-	cerr << " structAA " << RANK << " " << ni << " " << level  << " " << ni << " " << spacing << endl;
+	cerr << " HYPRE RES B " <<  RANK << " " << level << " " << ni << " " << _COUNTER << endl;
 	hypre_points_struct(mem,mem.all_groups[level],hypre_points,ni == 1,level);
-	mem.p_mess->Full_Stop_Do_Not_Argue();
-	cerr << " structBA " << RANK << " " << ni << " " << level  << " " << ni << " " << spacing << endl;
+	cerr << " HYPRE RES C " <<  RANK << " " << level << " " << ni << " " << _COUNTER << endl;
 	hypre_points_boxes(hypre_points,spacing,SBoxes,SPoints);
-	mem.p_mess->Full_Stop_Do_Not_Argue();
-	cerr << " boxA " << RANK << " " << ni << " " << level << " " << SBoxes.size() << " " << ni << " " << spacing << endl;
+	cerr << " HYPRE RES D " <<  RANK << " " << level << " " << ni << " " << _COUNTER << endl;
 	hypre_points.clear();
-	cerr << " createAA " << RANK << " " << ni << " " << level  << " " << ni << " " << spacing << endl;
 	hypre_world_create(mem,level,SBoxes,ni == 1);
-	mem.p_mess->Full_Stop_Do_Not_Argue();
-	cerr << " createBA " << RANK << " " << ni << " " << level  << " " << ni << " " << spacing << endl;
+	cerr << " HYPRE RES E " <<  RANK << " " << level << " " << ni << " " << _COUNTER << endl;
 	if(mem.p_mess->IAmAHypreNode)
 	  {
 	    hypre_solve_struct(mem,level,SBoxes,SPoints);
-	    cerr << " runA " << RANK << " " << ni << " " << level << " " << ni << " " << spacing << endl;
-	    if(ni == 1)
+	    if(ni == 100)
 	      add_buffer_values(mem,level,SBoxes,SPoints);
-	    cerr << " runB " << RANK << " " << ni << " " << level << " " << ni << " " << spacing << endl;
+	    cerr << " HYPRE RES F " <<  RANK << " " << level << " " << ni << " " << _COUNTER << endl;
 	  }
-	mem.p_mess->Full_Stop_Do_Not_Argue();
-	cerr << " runC " << RANK << " " << ni << " " << level << " " << ni << " " << spacing << endl;
+	cerr << " HYPRE RES G " <<  RANK << " " << level << " " << ni << " " << _COUNTER << endl;
 	mem.p_mess->HypreGroupFree();
-	cerr << " freeA " << RANK << " " << ni << " " << level  << " " << ni << " " << spacing << endl;
-	mem.p_mess->Full_Stop_Do_Not_Argue();
+	cerr << " HYPRE RES H " <<  RANK << " " << level << " " << ni << " " << _COUNTER << endl;
 	SBoxes.clear();
 	SPoints.clear();
+	_COUNTER++;
       }
+    cerr << " HYPRE RES I " <<  RANK << " " << level << " " << " " << _COUNTER << endl;
   }
 }
