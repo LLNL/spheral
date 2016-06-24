160fc12e07471481b13c35c5c7e78698899ebd0f
Mike Owen <mikeowen@llnl.gov>
2016-06-24 16:57:52 -0600

    Some more modification of how we apply damage in solid SPH.  This reduces the unphysical growth of the damaged area, mostly by damaging the contribution to the velocity gradient.

--- tests/Damage/TensileRod/TensileRod-GradyKipp-1d.py
+++ tests/Damage/TensileRod/TensileRod-GradyKipp-1d.py
@@ -153,7 +153,7 @@ commandLine(length = 3.0,
 
             testtol = 1.0e-4,
             clearDirectories = False,
-            referenceFile = "Reference/TensileRod-GradyKipp-1d-1proc-reproducing-20160308.txt",
+            referenceFile = "Reference/TensileRod-GradyKipp-1d-1proc-reproducing-20160624.txt",
             dataDirBase = "dumps-TensileRod-1d",
             outputFile = "None",
             comparisonFile = "None",
