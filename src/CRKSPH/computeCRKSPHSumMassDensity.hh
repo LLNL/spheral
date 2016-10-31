a659a0dc1e862724666b095a1e833a407db07340
Mike Owen <mikeowen@llnl.gov>
2016-10-30 21:39:38 -0700

    Simplifying how we apply the CRK sum mass density, and fiddling with the filter application.  Filtering is not good yet, but interestingly VoronoiCellDensity appears to be viable density update method now.

--- src/CRKSPH/computeCRKSPHSumMassDensity.hh
+++ src/CRKSPH/computeCRKSPHSumMassDensity.hh
@@ -9,8 +9,6 @@
 #include "Field/FieldList.hh"
 #include "Neighbor/ConnectivityMap.hh"
 #include "Kernel/TableKernel.hh"
-#include "Boundary/Boundary.hh"
-#include "CRKSPHCorrectionParams.hh"
 
 namespace Spheral {
   namespace CRKSPHSpace {
@@ -23,10 +21,6 @@ namespace Spheral {
                                 const FieldSpace::FieldList<Dimension, typename Dimension::Scalar>& mass,
                                 const FieldSpace::FieldList<Dimension, typename Dimension::Scalar>& vol,
                                 const FieldSpace::FieldList<Dimension, typename Dimension::SymTensor>& H,
-                                const FieldSpace::FieldList<Dimension, typename Dimension::Scalar>& A,
-                                const FieldSpace::FieldList<Dimension, typename Dimension::Vector>& B,
-                                const FieldSpace::FieldList<Dimension, typename Dimension::Tensor>& C,
-                                const CRKOrder correctionOrder,
                                 FieldSpace::FieldList<Dimension, typename Dimension::Scalar>& massDensity);
   }
 }
--- src/CRKSPH/computeCRKSPHSumMassDensityInst.cc.py
+++ src/CRKSPH/computeCRKSPHSumMassDensityInst.cc.py
@@ -10,15 +10,11 @@ namespace Spheral {
   namespace CRKSPHSpace {
     template void computeCRKSPHSumMassDensity(const ConnectivityMap<Dim< %(ndim)s > >&, 
                                               const TableKernel<Dim< %(ndim)s > >&, 
-                                              const FieldList<Dim< %(ndim)s >, Dim< %(ndim)s >::Vector>&,
-                                              const FieldList<Dim< %(ndim)s >, Dim< %(ndim)s >::Scalar>&,
-                                              const FieldList<Dim< %(ndim)s >, Dim< %(ndim)s >::Scalar>&,
-                                              const FieldList<Dim< %(ndim)s >, Dim< %(ndim)s >::SymTensor>&,
-                                              const FieldList<Dim< %(ndim)s >, Dim< %(ndim)s >::Scalar>& A,
-                                              const FieldList<Dim< %(ndim)s >, Dim< %(ndim)s >::Vector>& B,
-                                              const FieldList<Dim< %(ndim)s >, Dim< %(ndim)s >::Tensor>& C,
-                                              const CRKOrder correctionOrder,
-                                              FieldList<Dim< %(ndim)s >, Dim< %(ndim)s >::Scalar>&);
+                                              const FieldList<Dim< %(ndim)s >, Dim< %(ndim)s >::Vector>& position,
+                                              const FieldList<Dim< %(ndim)s >, Dim< %(ndim)s >::Scalar>& mass,
+                                              const FieldList<Dim< %(ndim)s >, Dim< %(ndim)s >::Scalar>& vol,
+                                              const FieldList<Dim< %(ndim)s >, Dim< %(ndim)s >::SymTensor>& H,
+                                              FieldList<Dim< %(ndim)s >, Dim< %(ndim)s >::Scalar>& massDensity);
   }
 }
 """
