68572067d6b5258a221978a103d4192a710535e4
Mike Owen <mikeowen@llnl.gov>
2016-11-11 16:39:06 -0800

    Got the full 2D medial generator test going.  Still no holes for now.

--- tests/Generators/testMedialGenerator2d.py
+++ tests/Generators/testMedialGenerator2d.py
@@ -1,5 +1,6 @@
 from Spheral2d import *
 from MedialGenerator import *
+from CompositeNodeDistribution import *
 from SpheralTestUtilities import *
 from VoronoiDistributeNodes import distributeNodes2d as distributeNodes
 from siloPointmeshDump import *
@@ -13,7 +14,8 @@ commandLine(hmin     = 1e-5,
             n4       = 400,
             nPerh   = 2.01,
             maxIterations = 200,
-            fracTol = 1e-3)
+            fracTol = 1e-3,
+            noholes = True)     # Optionally avoid using holes in the boundaries.
 
 #-------------------------------------------------------------------------------
 # Material properties.
@@ -82,12 +84,11 @@ for i in xrange(len(bcpoints)):
 Hboundary = Polygon(bcpoints, bcfacets)
 
 # A couple of concentric circles.
-n = 120
+n = 40
 bcpoints1 = vector_of_Vector(n)
 bcpoints2 = vector_of_Vector(n)
 bcfacets = vector_of_vector_of_unsigned(n, vector_of_unsigned(2))
 for i in xrange(n):
-    j = n - i - 1
     bcpoints1[i] = Vector(5.0 + 1.5*cos(i*2.0*pi/n), 1.5 + 1.5*sin(i*2.0*pi/n))
     bcpoints2[i] = Vector(5.0 + 0.5*cos(i*2.0*pi/n), 1.5 + 0.5*sin(i*2.0*pi/n))
     bcfacets[i][0] = i
@@ -95,6 +96,21 @@ for i in xrange(n):
 outerCircle = Polygon(bcpoints1, bcfacets)
 innerCircle = Polygon(bcpoints2, bcfacets)
 
+# In the no holes case we need to generate a split version of the outer circle.
+nhalf = n/2
+bcpoints1_left = vector_of_Vector(n)
+bcpoints1_right = vector_of_Vector(n)
+bcpoints2_left = vector_of_Vector(n)
+bcpoints2_right = vector_of_Vector(n)
+for i in xrange(nhalf):
+    theta = i*pi/(nhalf - 1) - pi/2.0
+    bcpoints1_right[i]       = Vector(5.0 + 1.5*cos(theta),      1.5 + 1.5*sin(theta))
+    bcpoints1_right[i+nhalf] = Vector(5.0 + 0.5*cos(-theta),     1.5 + 0.5*sin(-theta))
+    bcpoints1_left[i]        = Vector(5.0 + 1.5*cos(pi + theta), 1.5 + 1.5*sin(pi + theta))
+    bcpoints1_left[i+nhalf]  = Vector(5.0 + 0.5*cos(pi - theta), 1.5 + 0.5*sin(pi - theta))
+outerCircle_left = Polygon(bcpoints1_left, bcfacets)
+outerCircle_right = Polygon(bcpoints1_right, bcfacets)
+    
 # A box surrounding the whole thing.
 bcpoints = vector_of_Vector()
 bcfacets = vector_of_vector_of_unsigned()
@@ -105,6 +121,48 @@ for i in xrange(len(bcpoints)):
     bcfacets[-1][0] = i
     bcfacets[-1][1] = (i + 1) % len(bcpoints)
 outerBox = Polygon(bcpoints, bcfacets)
+
+# And the no hole version of the surrounding box, broken into three pieces.
+bcpoints = vector_of_Vector()
+bcfacets = vector_of_vector_of_unsigned()
+for p in [(-1,-1), (3,-1),
+          (3,0), (2,0), (2,1), (1,1), (1,0), (0,0),
+          (0,3), (1,3), (1,2), (2,2), (2,3), (3,3),
+          (3,4), (-1,4)]:
+    bcpoints.append(Vector(*p))
+for i in xrange(len(bcpoints)):
+    bcfacets.append(vector_of_unsigned(2))
+    bcfacets[-1][0] = i
+    bcfacets[-1][1] = (i + 1) % len(bcpoints)
+outerBox_left = Polygon(bcpoints, bcfacets)
+
+bcpoints = vector_of_Vector()
+bcfacets = vector_of_vector_of_unsigned()
+for p in [(3,-1), (5, -1)]:
+    bcpoints.append(Vector(*p))
+for i in xrange(nhalf):
+    theta = 1.5*pi - i*pi/(nhalf - 1)
+    bcpoints.append(Vector(5.0 + 1.5*cos(theta),      1.5 + 1.5*sin(theta)))
+for p in [(5, 4), (3,4)]:
+    bcpoints.append(Vector(*p))
+for i in xrange(len(bcpoints)):
+    bcfacets.append(vector_of_unsigned(2))
+    bcfacets[-1][0] = i
+    bcfacets[-1][1] = (i + 1) % len(bcpoints)
+outerBox_mid = Polygon(bcpoints, bcfacets)
+
+bcpoints = vector_of_Vector()
+bcfacets = vector_of_vector_of_unsigned()
+for p in [(5, -1), (8,-1), (8,4), (5,4)]:
+    bcpoints.append(Vector(*p))
+for i in xrange(nhalf):
+    theta = 0.5*pi - i*pi/(nhalf - 1)
+    bcpoints.append(Vector(5.0 + 1.5*cos(theta),      1.5 + 1.5*sin(theta)))
+for i in xrange(len(bcpoints)):
+    bcfacets.append(vector_of_unsigned(2))
+    bcfacets[-1][0] = i
+    bcfacets[-1][1] = (i + 1) % len(bcpoints)
+outerBox_right = Polygon(bcpoints, bcfacets)
 
 #-------------------------------------------------------------------------------
 # Generate them nodes.
@@ -117,20 +175,37 @@ print "Generator 1"
 generator1 = MedialGenerator2d(n = n1,
                                rho = 1.0, # rhoprofile1,
                                boundary = Hboundary,
-                               maxIterations = 1, # maxIterations,
+                               maxIterations = maxIterations,
                                fracTol = fracTol,
                                tessellationFileName = "test_medial_nodes1_maxiter=%i_tol=%g" % (maxIterations, fracTol),
                                nNodePerh = nPerh)
 
 print "Generator 2"
-generator2 = MedialGenerator2d(n = n2,
-                               rho = 1.0,
-                               boundary = outerCircle,
-                               #holes = [innerCircle],
-                               maxIterations = maxIterations,
-                               fracTol = fracTol,
-                               tessellationFileName = "test_medial_nodes2_maxiter=%i_tol=%g" % (maxIterations, fracTol),
-                               nNodePerh = nPerh)
+if noholes:
+    generator2_left = MedialGenerator2d(n = n2/2,
+                                        rho = 1.0,
+                                        boundary = outerCircle_left,
+                                        maxIterations = maxIterations,
+                                        fracTol = fracTol,
+                                        tessellationFileName = "test_medial_nodes2_left_maxiter=%i_tol=%g" % (maxIterations, fracTol),
+                                        nNodePerh = nPerh)
+    generator2_right = MedialGenerator2d(n = n2/2,
+                                         rho = 1.0,
+                                         boundary = outerCircle_right,
+                                         maxIterations = maxIterations,
+                                         fracTol = fracTol,
+                                         tessellationFileName = "test_medial_nodes2_right_maxiter=%i_tol=%g" % (maxIterations, fracTol),
+                                         nNodePerh = nPerh)
+    generator2 = CompositeNodeDistribution(generator2_left, generator2_right)
+else:
+    generator2 = MedialGenerator2d(n = n2,
+                                   rho = 1.0,
+                                   boundary = outerCircle,
+                                   holes = [innerCircle],
+                                   maxIterations = maxIterations,
+                                   fracTol = fracTol,
+                                   tessellationFileName = "test_medial_nodes2_maxiter=%i_tol=%g" % (maxIterations, fracTol),
+                                   nNodePerh = nPerh)
 
 print "Generator 3"
 generator3 = MedialGenerator2d(n = n3,
@@ -142,14 +217,38 @@ generator3 = MedialGenerator2d(n = n3,
                                nNodePerh = nPerh)
 
 print "Generator 4"
-generator4 = MedialGenerator2d(n = n4,
-                               rho = 0.1,
-                               boundary = outerBox,
-                               holes = [Hboundary, outerCircle],
-                               maxIterations = maxIterations,
-                               fracTol = fracTol,
-                               tessellationFileName = "test_medial_nodes4_maxiter=%i_tol=%g" % (maxIterations, fracTol),
-                               nNodePerh = nPerh)
+if noholes:
+    generator4_left = MedialGenerator2d(n = int(n4 * 4.0/9.0),
+                                        rho = 0.1,
+                                        boundary = outerBox_left,
+                                        maxIterations = maxIterations,
+                                        fracTol = fracTol,
+                                        tessellationFileName = "test_medial_nodes4_left_maxiter=%i_tol=%g" % (maxIterations, fracTol),
+                                        nNodePerh = nPerh)
+    generator4_mid = MedialGenerator2d(n = int(n4 * 2.0/9.0),
+                                       rho = 0.1,
+                                       boundary = outerBox_mid,
+                                       maxIterations = maxIterations,
+                                       fracTol = fracTol,
+                                       tessellationFileName = "test_medial_nodes4_mid_maxiter=%i_tol=%g" % (maxIterations, fracTol),
+                                       nNodePerh = nPerh)
+    generator4_right = MedialGenerator2d(n = int(n4 * 3.0/9.0),
+                                         rho = 0.1,
+                                         boundary = outerBox_right,
+                                         maxIterations = maxIterations,
+                                         fracTol = fracTol,
+                                         tessellationFileName = "test_medial_nodes4_right_maxiter=%i_tol=%g" % (maxIterations, fracTol),
+                                         nNodePerh = nPerh)
+    generator4 = CompositeNodeDistribution(generator4_left, generator4_mid, generator4_right)
+else:
+    generator4 = MedialGenerator2d(n = n4,
+                                   rho = 0.1,
+                                   boundary = outerBox,
+                                   holes = [Hboundary, outerCircle],
+                                   maxIterations = maxIterations,
+                                   fracTol = fracTol,
+                                   tessellationFileName = "test_medial_nodes4_maxiter=%i_tol=%g" % (maxIterations, fracTol),
+                                   nNodePerh = nPerh)
 
 distributeNodes((nodes1, generator1),
                 (nodes2, generator2),
