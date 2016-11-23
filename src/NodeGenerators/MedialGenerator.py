f9b74017a25e53d3d6ed5972796eb20507972fdd
Mike Owen <mikeowen@llnl.gov>
2016-11-23 15:05:29 -0800

    3D MedialGenerator tests passing in parallel now too.

--- src/NodeGenerators/MedialGenerator.py
+++ src/NodeGenerators/MedialGenerator.py
@@ -302,8 +302,10 @@ class MedialGenerator2d(MedialGeneratorBase):
 #-------------------------------------------------------------------------------
 # 3D Generator.  Seeds positions within the given boundary using the Sobol 
 # sequence to create the seeds.
+# Note as currently implemented this code does not necessarily generate 
+# identical output for different numbers of processors!
 #-------------------------------------------------------------------------------
-class MedialGenerator3d(NodeGeneratorBase):
+class MedialGenerator3d(MedialGeneratorBase):
 
     #---------------------------------------------------------------------------
     # Constructor.
@@ -318,126 +320,48 @@ class MedialGenerator3d(NodeGeneratorBase):
                  fracTol = 1.0e-3,
                  tessellationFileName = None,
                  nNodePerh = 2.01,
-                 offset = (0.0, 0.0),
+                 offset = (0.0, 0.0, 0.0),
                  rejecter = None,
-                 randomseed = 492739149274):
+                 randomseed = 492739149274,
+                 maxNodesPerDomain = 1000):
 
-        assert n > 0
+        # The base generator does most of the work.
+        MedialGeneratorBase.__init__(self,
+                                     ndim = 3,
+                                     n = n,
+                                     rho = rho,
+                                     boundary = boundary,
+                                     gradrho = gradrho,
+                                     holes = holes,
+                                     maxIterations = maxIterations,
+                                     fracTol = fracTol,
+                                     tessellationFileName = tessellationFileName,
+                                     nNodePerh = nNodePerh,
+                                     randomseed = randomseed,
+                                     maxNodesPerDomain = maxNodesPerDomain)
 
-        # Load our handy 3D aliases.
-        import Spheral3d as sph
 
-        # Did we get passed a function or a constant for the density?
-        if type(rho) == type(1.0):
-            def rhofunc(posi):
-                return rho
-        else:
-            rhofunc = rho
-        self.rhofunc = rhofunc
-
-        # Create a temporary NodeList we'll use store and update positions.
-        eos = sph.GammaLawGasMKS(2.0, 2.0)
-        WT = sph.TableKernel(sph.NBSplineKernel(7), 1000)
-        nodes = sph.makeFluidNodeList("tmp generator nodes", 
-                                      eos,
-                                      hmin = 1e-10,
-                                      hmax = 1e10,
-                                      kernelExtent = WT.kernelExtent,
-                                      hminratio = 1.0,
-                                      nPerh = nNodePerh)
-
-        # Make a first pass looking for the maximum density (roughly).
-        pos = nodes.positions()
-        mass = nodes.mass()
-        rhof = nodes.massDensity()
-        H = nodes.Hfield()
-        nodes.numInternalNodes = n
-        length = max(boundary.xmax.x - boundary.xmin.x,
-                     boundary.xmax.y - boundary.xmin.y)
-        rhomax = 0.0
-        seed = 0
-        i = 0
-        while i < n:
-            [coords, seed] = i4_sobol(3, seed)
-            p = boundary.xmin + length*Vector3d(coords[0], coords[1], coords[2])
-            ihole = 0
-            use = boundary.contains(p, False)
-            if use:
-                while use and ihole < len(holes):
-                    use = not holes[ihole].contains(p, True)
-                    ihole += 1
-            if use:
-                rhomax = max(rhomax, rhofunc(p))
-                i += 1
-        rhomax = mpi.allreduce(rhomax, mpi.MAX)
-        print "MedialGenerator: selected a maximum density of ", rhomax
-
-        # Initialize the desired number of generators in the boundary using the Sobol sequence.
-        rangen = random.Random(randomseed)
-        volbound = boundary.volume
-        seed = 0
-        i = 0
-        while i < n:
-            [coords, seed] = i4_sobol(3, seed)
-            p = boundary.xmin + length*Vector3d(coords[0], coords[1], coords[2])
-            use = boundary.contains(p, False)
-            if use:
-                ihole = 0
-                while use and ihole < len(holes):
-                    use = not holes[ihole].contains(p, True)
-                    ihole += 1
-            if use:
-                rhoi = rhofunc(p)
-                if rangen.uniform(0.0, 1.0) < rhoi/rhomax:
-                    pos[i] = p
-                    rhof[i] = rhoi
-                    mass[i] = rhoi * volbound/n  # Not actually correct, but mass will be updated in centroidalRelaxNodes
-                    hi = 2.0 * nNodePerh * (volbound/(n*4.0/3.0*pi))**(1.0/3.0)
-                    assert hi > 0.0
-                    H[i] = SymTensor3d(1.0/hi, 0.0, 0.0,
-                                       0.0, 1.0/hi, 0.0,
-                                       0.0, 0.0, 1.0/hi)
-                    i += 1
-
-        # Iterate the points toward centroidal relaxation.
-        vol, surfacePoint = centroidalRelaxNodes([(nodes, boundary, holes)],
-                                                 W = WT,
-                                                 rho = rhofunc,
-                                                 gradrho = gradrho,
-                                                 maxIterations = maxIterations,
-                                                 tessellationFileName = tessellationFileName)
-
-        # Now we can fill out the usual Spheral generator info.
-        self.x, self.y, self.z, self.m, self.H = [], [], [], [], []
-        for i in xrange(n):
-            self.x.append(pos[i].x)
-            self.y.append(pos[i].y)
-            self.z.append(pos[i].z)
-            self.m.append(vol(0,i) * rhofunc(pos[i]))
-            hi = nNodePerh * (vol(0,i)/(4.0/3.0*pi))**(1.0/3.0)
-            assert hi > 0.0
-            self.H.append(SymTensor3d(1.0/hi, 0.0, 0.0,
-                                      0.0, 1.0/hi, 0.0,
-                                      0.0, 0.0, 1.0/hi))
-        assert len(self.x) == n
-        assert len(self.y) == n
-        assert len(self.z) == n
-        assert len(self.m) == n
-        assert len(self.H) == n
+        # Convert to our now regrettable standard coordinate storage for generators.
+        self.x = [x.x + offset[0] for x in self.pos]
+        self.y = [x.y + offset[1] for x in self.pos]
+        self.z = [x.z + offset[2] for x in self.pos]
+        del self.pos
 
         # If the user provided a "rejecter", give it a pass
         # at the nodes.
         if rejecter:
-            self.x, self.y, self.z, self.m, self.H = rejecter(self.x,
-                                                              self.y,
-                                                              self.z,
-                                                              self.m,
-                                                              self.H)
+            self.x, self.y, self.z, self.m, self.H, self.vol, self.surface = rejecter(self.x,
+                                                                                      self.y,
+                                                                                      self.z,
+                                                                                      self.m,
+                                                                                      self.H,
+                                                                                      self.vol,
+                                                                                      self.surface)
 
-        # Have the base class break up the serial node distribution
-        # for parallel cases.
-        NodeGeneratorBase.__init__(self, True,
-                                   self.x, self.y, self.z, self.m, self.H)
+        # Initialize the base class, taking into account the fact we've already broken
+        # up the nodes between domains.
+        NodeGeneratorBase.__init__(self, False,
+                                   self.x, self.y, self.z, self.m, self.H, self.vol, self.surface)
         return
 
     #---------------------------------------------------------------------------
@@ -446,7 +370,6 @@ class MedialGenerator3d(NodeGeneratorBase):
     def localPosition(self, i):
         assert i >= 0 and i < len(self.x)
         assert len(self.x) == len(self.y)
-        assert len(self.x) == len(self.z)
         return Vector3d(self.x[i], self.y[i], self.z[i])
     
     #---------------------------------------------------------------------------
@@ -461,7 +384,7 @@ class MedialGenerator3d(NodeGeneratorBase):
     #---------------------------------------------------------------------------
     def localMassDensity(self, i):
         assert i >= 0 and i < len(self.H)
-        return self.rhofunc(Vector3d(self.x[i], self.y[i], self.z[i]))
+        return self.rhofunc(Vector2d(self.x[i], self.y[i], self.z[i]))
     
     #---------------------------------------------------------------------------
     # Get the H tensor for the given node index.
