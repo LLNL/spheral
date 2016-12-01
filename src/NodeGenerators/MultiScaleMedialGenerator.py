74a5d793f9b96759aa17c6772c0778d553badde6
Mike Owen <mikeowen@llnl.gov>
2016-12-01 11:40:55 -0800

    Generalized MultiScaleMedialGenerator for 2D and 3D.

--- src/NodeGenerators/MultiScaleMedialGenerator.py
+++ src/NodeGenerators/MultiScaleMedialGenerator.py
@@ -5,9 +5,176 @@ from NodeGeneratorBase import *
 from MedialGenerator import *
 
 #-------------------------------------------------------------------------------
-# 
+# Dimension agnostic base version.
 #-------------------------------------------------------------------------------
-class MultiScaleMedialGenerator2d(NodeGeneratorBase):
+class MultiScaleMedialGeneratorBase(NodeGeneratorBase):
+
+    #---------------------------------------------------------------------------
+    # Constructor.
+    #---------------------------------------------------------------------------
+    def __init__(self,
+                 ndim,
+                 n,
+                 rho,
+                 boundary,
+                 nstart,
+                 gradrho,
+                 holes,
+                 centroidFrac,
+                 maxIterationsPerStage,
+                 fracTol,
+                 tessellationFileName,
+                 nNodePerh,
+                 offset,
+                 rejecter,
+                 randomseed,
+                 maxNodesPerDomain):
+
+        # Load our handy aliases.
+        if ndim == 2:
+            import Spheral2d as sph
+        else:
+            import Spheral3d as sph
+
+        # Kick things off by generating the coarsest level.
+        ntarget = min(n, nstart)
+        gen = MedialGeneratorBase(ndim = ndim,
+                                  n = ntarget,
+                                  rho = rho,
+                                  boundary = boundary,
+                                  gradrho = gradrho,
+                                  holes = holes,
+                                  centroidFrac = centroidFrac,
+                                  maxIterations = maxIterationsPerStage,
+                                  fracTol = fracTol,
+                                  tessellationFileName = tessellationFileName + "_gen000",
+                                  nNodePerh = nNodePerh,
+                                  randomseed = randomseed,
+                                  maxNodesPerDomain = maxNodesPerDomain,
+                                  seedPositions = None)
+
+
+        # Iterate from coarse generators to fine until we hit the target number of
+        # points.
+        rangen = random.Random(randomseed)
+        igeneration = 0
+        while gen.globalNumNodes() != n:
+            igeneration += 1
+
+            # Split the generators we've created from the last stage.
+            ntarget = min(n, ntarget * 2**ndim)
+            seedPositions = []
+            while mpi.allreduce(len(seedPositions), mpi.SUM) < ntarget:
+                zeta = sph.Vector(rangen.uniform(-0.5, 0.5), rangen.uniform(-0.5, 0.5), rangen.uniform(-0.5, 0.5))
+                for i in xrange(gen.localNumNodes()):
+                    hscale = (gen.vol[i]/pi)**(1.0/ndim)
+                    xi = gen.pos[i] + hscale*zeta
+                    if boundary.contains(xi, False):
+                        use = True
+                        ihole = 0
+                        while use and ihole < len(holes):
+                            use = not holes[ihole].contains(xi, False)
+                            ihole += 1
+                        if use:
+                            seedPositions.append(xi)
+
+            # Remove any excess points.
+            nglobal = mpi.allreduce(len(seedPositions), mpi.SUM)
+            while nglobal > ntarget:
+                if mpi.rank < nglobal - ntarget and len(seedPositions) > 0:
+                    seedPositions = seedPositions[:-1]
+                nglobal = mpi.allreduce(len(seedPositions), mpi.SUM)
+            assert nglobal == ntarget
+
+            # Now let the MedialGenerator do its thing.
+            gen = MedialGeneratorBase(ndim = ndim,
+                                      n = ntarget,
+                                      seedPositions = seedPositions,
+                                      rho = rho,
+                                      boundary = boundary,
+                                      gradrho = gradrho,
+                                      holes = holes,
+                                      centroidFrac = centroidFrac,
+                                      maxIterations = maxIterationsPerStage,
+                                      fracTol = fracTol,
+                                      tessellationFileName = tessellationFileName + "_gen%03i" % igeneration,
+                                      nNodePerh = nNodePerh,
+                                      randomseed = randomseed,
+                                      maxNodesPerDomain = maxNodesPerDomain)
+
+        # Convert to our now regrettable standard coordinate storage for generators.
+        self.x = [x.x + offset[0] for x in gen.pos]
+        self.y = [x.y + offset[1] for x in gen.pos]
+        if ndim == 3:
+            self.z = [x.z + offset[2] for x in gen.pos]
+
+        # Copy the rest of the state.
+        self.ndim = ndim
+        self.rhofunc = gen.rhofunc
+        self.m = list(gen.m)
+        self.H = list(gen.H)
+        self.vol = list(gen.vol)
+        self.surface = list(gen.surface)
+
+        # Some dimension trickery.
+        if ndim == 2:
+            args = (self.x, self.y, self.m, self.H, self.vol, self.surface)
+        else:
+            args = (self.x, self.y, self.z, self.m, self.H, self.vol, self.surface)
+
+        # If the user provided a "rejecter", give it a pass
+        # at the nodes.
+        if rejecter:
+            newargs = rejecter(*args)
+            if ndim == 2:
+                self.x, self.y, self.m, self.H, self.vol, self.surface = newargs
+                args = (self.x, self.y, self.m, self.H, self.vol, self.surface)
+            else:
+                self.x, self.y, self.z, self.m, self.H, self.vol, self.surface = newargs
+                args = (self.x, self.y, self.z, self.m, self.H, self.vol, self.surface)
+
+        # Initialize the base class, taking into account the fact we've already broken
+        # up the nodes between domains.
+        NodeGeneratorBase.__init__(self, False, *args)
+        return
+
+    #---------------------------------------------------------------------------
+    # Get the position for the given node index.
+    #---------------------------------------------------------------------------
+    def localPosition(self, i):
+        assert i >= 0 and i < len(self.x)
+        assert len(self.x) == len(self.y)
+        if self.ndim == 2:
+            return Vector2d(self.x[i], self.y[i])
+        else:
+            assert len(self.x) == len(self.z)
+            return Vector2d(self.x[i], self.y[i], self.z[i])
+
+    #---------------------------------------------------------------------------
+    # Get the mass for the given node index.
+    #---------------------------------------------------------------------------
+    def localMass(self, i):
+        assert i >= 0 and i < len(self.m)
+        return self.m[i]
+    
+    #---------------------------------------------------------------------------
+    # Get the mass density for the given node index.
+    #---------------------------------------------------------------------------
+    def localMassDensity(self, i):
+        assert i >= 0 and i < len(self.H)
+        return self.rhofunc(self.localPosition(i))
+    
+    #---------------------------------------------------------------------------
+    # Get the H tensor for the given node index.
+    #---------------------------------------------------------------------------
+    def localHtensor(self, i):
+        assert i >= 0 and i < len(self.H)
+        return self.H[i]
+        
+#-------------------------------------------------------------------------------
+# 2D
+#-------------------------------------------------------------------------------
+class MultiScaleMedialGenerator2d(MultiScaleMedialGeneratorBase):
 
     #---------------------------------------------------------------------------
     # Constructor.
@@ -29,125 +196,66 @@ class MultiScaleMedialGenerator2d(NodeGeneratorBase):
                  randomseed = 492739149274,
                  maxNodesPerDomain = 1000):
 
-        # Kick things off by generating the coarsest level.
-        ntarget = min(n, nstart)
-        gen = MedialGeneratorBase(ndim = 2,
-                                  n = ntarget,
-                                  rho = rho,
-                                  boundary = boundary,
-                                  gradrho = gradrho,
-                                  holes = holes,
-                                  centroidFrac = centroidFrac,
-                                  maxIterations = maxIterationsPerStage,
-                                  fracTol = fracTol,
-                                  tessellationFileName = tessellationFileName + "_gen000",
-                                  nNodePerh = nNodePerh,
-                                  randomseed = randomseed,
-                                  maxNodesPerDomain = maxNodesPerDomain,
-                                  seedPositions = None)
-
-        # Iterate from coarse generators to fine until we hit the target number of
-        # points.
-        rangen = random.Random(randomseed)
-        igeneration = 0
-        while gen.globalNumNodes() != n:
-            igeneration += 1
-
-            # Split the generators we've created from the last stage.
-            ntarget = min(n, 4*ntarget)
-            seedPositions = []
-            while mpi.allreduce(len(seedPositions), mpi.SUM) < ntarget:
-                zeta = Vector2d(rangen.uniform(-0.5, 0.5), rangen.uniform(-0.5, 0.5))
-                for i in xrange(gen.localNumNodes()):
-                    hscale = sqrt(gen.vol[i]/pi)
-                    xi = gen.pos[i] + hscale*zeta
-                    if boundary.contains(xi, False):
-                        use = True
-                        ihole = 0
-                        while use and ihole < len(holes):
-                            use = not holes[ihole].contains(xi, False)
-                            ihole += 1
-                        if use:
-                            seedPositions.append(xi)
-
-            # Remove any excess points.
-            nglobal = mpi.allreduce(len(seedPositions), mpi.SUM)
-            while nglobal > ntarget:
-                if mpi.rank < nglobal - ntarget and len(seedPositions) > 0:
-                    seedPositions = seedPositions[:-1]
-                nglobal = mpi.allreduce(len(seedPositions), mpi.SUM)
-            assert nglobal == ntarget
-
-            # Now let the MedialGenerator do its thing.
-            gen = MedialGeneratorBase(ndim = 2,
-                                      n = ntarget,
-                                      seedPositions = seedPositions,
-                                      rho = rho,
-                                      boundary = boundary,
-                                      gradrho = gradrho,
-                                      holes = holes,
-                                      centroidFrac = centroidFrac,
-                                      maxIterations = maxIterationsPerStage,
-                                      fracTol = fracTol,
-                                      tessellationFileName = tessellationFileName + "_gen%03i" % igeneration,
-                                      nNodePerh = nNodePerh,
-                                      randomseed = randomseed,
-                                      maxNodesPerDomain = maxNodesPerDomain)
-
-        # Convert to our now regrettable standard coordinate storage for generators.
-        self.x = [x.x + offset[0] for x in gen.pos]
-        self.y = [x.y + offset[1] for x in gen.pos]
-
-        # Copy the rest of the state.
-        self.rhofunc = gen.rhofunc
-        self.m = list(gen.m)
-        self.H = list(gen.H)
-        self.vol = list(gen.vol)
-        self.surface = list(gen.surface)
-
-        # If the user provided a "rejecter", give it a pass
-        # at the nodes.
-        if rejecter:
-            self.x, self.y, self.m, self.H, self.vol, self.surface = rejecter(self.x,
-                                                                              self.y,
-                                                                              self.m,
-                                                                              self.H,
-                                                                              self.vol,
-                                                                              self.surface)
-
-        # Initialize the base class, taking into account the fact we've already broken
-        # up the nodes between domains.
-        NodeGeneratorBase.__init__(self, False,
-                                   self.x, self.y, self.m, self.H, self.vol, self.surface)
+        MultiScaleMedialGeneratorBase.__init__(self,
+                                               ndim = 2,
+                                               n = n,
+                                               rho = rho,
+                                               boundary = boundary,
+                                               nstart = nstart,
+                                               gradrho = gradrho,
+                                               holes = holes,
+                                               centroidFrac = centroidFrac,
+                                               maxIterationsPerStage = maxIterationsPerStage,
+                                               fracTol = fracTol,
+                                               tessellationFileName = tessellationFileName,
+                                               nNodePerh = nNodePerh,
+                                               offset = offset,
+                                               rejecter = rejecter,
+                                               randomseed = randomseed,
+                                               maxNodesPerDomain = maxNodesPerDomain)
         return
 
-    #---------------------------------------------------------------------------
-    # Get the position for the given node index.
-    #---------------------------------------------------------------------------
-    def localPosition(self, i):
-        assert i >= 0 and i < len(self.x)
-        assert len(self.x) == len(self.y)
-        return Vector2d(self.x[i], self.y[i])
-    
-    #---------------------------------------------------------------------------
-    # Get the mass for the given node index.
-    #---------------------------------------------------------------------------
-    def localMass(self, i):
-        assert i >= 0 and i < len(self.m)
-        return self.m[i]
-    
-    #---------------------------------------------------------------------------
-    # Get the mass density for the given node index.
-    #---------------------------------------------------------------------------
-    def localMassDensity(self, i):
-        assert i >= 0 and i < len(self.H)
-        return self.rhofunc(Vector2d(self.x[i], self.y[i]))
-    
-    #---------------------------------------------------------------------------
-    # Get the H tensor for the given node index.
-    #---------------------------------------------------------------------------
-    def localHtensor(self, i):
-        assert i >= 0 and i < len(self.H)
-        return self.H[i]
+#-------------------------------------------------------------------------------
+# 2D
+#-------------------------------------------------------------------------------
+class MultiScaleMedialGenerator3d(MultiScaleMedialGeneratorBase):
 
-        
+    #---------------------------------------------------------------------------
+    # Constructor.
+    #---------------------------------------------------------------------------
+    def __init__(self,
+                 n,
+                 rho,
+                 boundary,
+                 nstart = 100,
+                 gradrho = None,
+                 holes = [],
+                 centroidFrac = 1.0,
+                 maxIterationsPerStage = 100,
+                 fracTol = 1.0e-3,
+                 tessellationFileName = None,
+                 nNodePerh = 2.01,
+                 offset = (0.0, 0.0),
+                 rejecter = None,
+                 randomseed = 492739149274,
+                 maxNodesPerDomain = 1000):
+
+        MultiScaleMedialGeneratorBase.__init__(self,
+                                               ndim = 3,
+                                               n = n,
+                                               rho = rho,
+                                               boundary = boundary,
+                                               nstart = nstart,
+                                               gradrho = gradrho,
+                                               holes = holes,
+                                               centroidFrac = centroidFrac,
+                                               maxIterationsPerStage = maxIterationsPerStage,
+                                               fracTol = fracTol,
+                                               tessellationFileName = tessellationFileName,
+                                               nNodePerh = nNodePerh,
+                                               offset = offset,
+                                               rejecter = rejecter,
+                                               randomseed = randomseed,
+                                               maxNodesPerDomain = maxNodesPerDomain)
+        return
+
