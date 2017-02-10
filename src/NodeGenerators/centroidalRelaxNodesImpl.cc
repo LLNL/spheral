38b4aa23a7aa647937227c618cfa16640eb2b37f
Mike Owen <mikeowen@llnl.gov>
2017-02-10 15:36:54 -0800

    Some minor optimizations, but not enough yet!

--- src/NodeGenerators/centroidalRelaxNodesImpl.cc
+++ src/NodeGenerators/centroidalRelaxNodesImpl.cc
@@ -28,6 +28,7 @@ centroidalRelaxNodesImpl(DataBaseSpace::DataBase<Dimension>& db,
                          const KernelSpace::TableKernel<Dimension>& W,
                          const PythonBoundFunctors::SpheralFunctor<typename Dimension::Vector, double>& rhofunc,
                          const PythonBoundFunctors::SpheralFunctor<typename Dimension::Vector, typename Dimension::Vector>& gradrhofunc,
+                         const bool rhoConst,
                          const bool useGradRhoFunc,
                          std::vector<BoundarySpace::Boundary<Dimension>*>& boundaries,
                          const unsigned maxIterations,
@@ -75,6 +76,9 @@ centroidalRelaxNodesImpl(DataBaseSpace::DataBase<Dimension>& db,
   auto gradm3 = db.newFluidFieldList(FourthRankTensor::zero, "gradm3");
   auto gradm4 = db.newFluidFieldList(FifthRankTensor::zero, "gradm4");
 
+  // Make a dummy set of cells so we don't ask computeVoronoiVolume to compute the return FacetedVolumes every step.
+  FieldList<Dimension, FacetedVolume> dummyCells;
+
   // Make sure the density starts out consistently, and kick-start the volume using m/rho.
   for (auto nodeListi = 0U; nodeListi != numNodeLists; ++nodeListi) {
     const auto n = rhof[nodeListi]->numInternalElements();
@@ -97,7 +101,7 @@ centroidalRelaxNodesImpl(DataBaseSpace::DataBase<Dimension>& db,
       for (auto& nodes: db.fluidNodeListPtrs()) {
         nodes->numGhostNodes(0);
         nodes->neighbor().updateNodes();
-        for (unsigned i = 0; i != nodes->numInternalNodes(); ++i) rhof(nodeListi, i) = rhofunc(pos(nodeListi, i));
+        if (not rhoConst) for (unsigned i = 0; i != nodes->numInternalNodes(); ++i) rhof(nodeListi, i) = rhofunc(pos(nodeListi, i));
       }
       ++nodeListi;
     }
@@ -114,7 +118,7 @@ centroidalRelaxNodesImpl(DataBaseSpace::DataBase<Dimension>& db,
     // Compute the new volumes and centroids (note this uses the old rho gradient, not quite right,
     // but expedient/efficient).
     CRKSPHSpace::computeVoronoiVolume(pos, H, rhof, gradRhof, cm, W.kernelExtent(), volumeBoundaries, holes, 
-                                      surfacePoint, vol, deltaCentroid, cells);
+                                      surfacePoint, vol, deltaCentroid, dummyCells);
      
     // Apply boundary conditions.
     for (auto& bc: boundaries) {
@@ -123,23 +127,27 @@ centroidalRelaxNodesImpl(DataBaseSpace::DataBase<Dimension>& db,
     }
     for (auto& bc: boundaries) bc->finalizeGhostBoundary();
 
-    // If the user provided a gradrho method, we can use it.  Otherwise we need to numerically evaluate
-    // the density gradient.
-    if (useGradRhoFunc) {
-      for (unsigned nodeListi = 0U; nodeListi != numNodeLists; ++nodeListi) {
-        const auto n = rhof[nodeListi]->numInternalElements();
-        for (auto i = 0U; i != n; ++i) {
-          gradRhof(nodeListi, i) = gradrhofunc(pos(nodeListi, i));
-        }
-      }
+    // If the density is constant we can entirely skip finding the gradient.
+    if (not rhoConst) {
 
-    } else {
-      // Use RK to numerically compute the new mass density gradient.
-      CRKSPHSpace::computeCRKSPHMoments(cm, W, vol, pos, H, correctionOrder, NodeCoupling(),
-                                        m0, m1, m2, m3, m4, gradm0, gradm1, gradm2, gradm3, gradm4);
-      CRKSPHSpace::computeCRKSPHCorrections(m0, m1, m2, m3, m4, gradm0, gradm1, gradm2, gradm3, gradm4, H, correctionOrder,
-                                            A, B, C, gradA, gradB, gradC);
-      gradRhof.assignFields(CRKSPHSpace::gradientCRKSPH(rhof, pos, vol, H, A, B, C, gradA, gradB, gradC, cm, correctionOrder, W));
+      // If the user provided a gradrho method, we can use it.  Otherwise we need to numerically evaluate
+      // the density gradient.
+      if (useGradRhoFunc) {
+        for (unsigned nodeListi = 0U; nodeListi != numNodeLists; ++nodeListi) {
+          const auto n = rhof[nodeListi]->numInternalElements();
+          for (auto i = 0U; i != n; ++i) {
+            gradRhof(nodeListi, i) = gradrhofunc(pos(nodeListi, i));
+          }
+        }
+
+      } else {
+        // Use RK to numerically compute the new mass density gradient.
+        CRKSPHSpace::computeCRKSPHMoments(cm, W, vol, pos, H, correctionOrder, NodeCoupling(),
+                                          m0, m1, m2, m3, m4, gradm0, gradm1, gradm2, gradm3, gradm4);
+        CRKSPHSpace::computeCRKSPHCorrections(m0, m1, m2, m3, m4, gradm0, gradm1, gradm2, gradm3, gradm4, H, correctionOrder,
+                                              A, B, C, gradA, gradB, gradC);
+        gradRhof.assignFields(CRKSPHSpace::gradientCRKSPH(rhof, pos, vol, H, A, B, C, gradA, gradB, gradC, cm, correctionOrder, W));
+      }
     }
      
     // Displace the points and update point masses.
@@ -156,7 +164,10 @@ centroidalRelaxNodesImpl(DataBaseSpace::DataBase<Dimension>& db,
             }
           }
         }
-        if (vol(nodeListi, i) > 0.0) avgdelta += delta.magnitude()/Dimension::rootnu(vol(nodeListi, i));
+        if (vol(nodeListi, i) > 0.0) {
+          avgdelta += delta.magnitude()/Dimension::rootnu(vol(nodeListi, i));
+          H(nodeListi, i) = SymTensor::one / (2.0*Dimension::rootnu(vol(nodeListi, i)));  // Not correct, but hopefully good enough for our iterative Voronoi purposes.
+        }
         pos(nodeListi, i) += delta;
         rhof(nodeListi, i) = rhofunc(pos(nodeListi, i));
         mass(nodeListi, i) = rhof(nodeListi,i)*vol(nodeListi,i);
@@ -166,8 +177,15 @@ centroidalRelaxNodesImpl(DataBaseSpace::DataBase<Dimension>& db,
                 allReduce(db.numInternalNodes(), MPI_SUM, Communicator::communicator()));
     if (Process::getRank() == 0) cout << "centroidalRelaxNodes iteration " << iter << " avg delta frac " << avgdelta << endl;
         
-    // Update the H tensors a bit.
-    iterateIdealH(db, boundaries, W, NodeSpace::ASPHSmoothingScale<Dimension>(), 2);
+    // // Update the H tensors a bit.
+    // iterateIdealH(db, boundaries, W, NodeSpace::ASPHSmoothingScale<Dimension>(), 2);
+  }
+
+  // If requested to return the FacetedVolumes, make one last call to fill 'em in.
+  if (cells.size() > 0) {
+    const auto& cm = db.connectivityMap();
+    CRKSPHSpace::computeVoronoiVolume(pos, H, rhof, gradRhof, cm, W.kernelExtent(), volumeBoundaries, holes, 
+                                      surfacePoint, vol, deltaCentroid, cells);
   }
 
   // Return how many iterations we actually took.
