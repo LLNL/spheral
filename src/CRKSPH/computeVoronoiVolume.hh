33a1fd6899bbbc64db456bc4439ad72e97bcbf44
Cody Raskin <raskin1@llnl.gov>
2017-02-16 14:47:59 -0800

    Adding weighted voronoi volume calculations for 1-3d

--- src/CRKSPH/computeVoronoiVolume.hh
+++ src/CRKSPH/computeVoronoiVolume.hh
@@ -30,6 +30,20 @@ namespace Spheral {
                          FieldSpace::FieldList<Dim<1>, Dim<1>::Scalar>& vol,
                          FieldSpace::FieldList<Dim<1>, Dim<1>::Vector>& deltaMedian,
                          FieldSpace::FieldList<Dim<1>, Dim<1>::FacetedVolume>& cells);
+    void
+    computeWeightedVoronoiVolume(const FieldSpace::FieldList<Dim<1>, Dim<1>::Vector>& position,
+                                 const FieldSpace::FieldList<Dim<1>, Dim<1>::SymTensor>& H,
+                                 const FieldSpace::FieldList<Dim<1>, Dim<1>::Scalar>& rho,
+                                 const FieldSpace::FieldList<Dim<1>, Dim<1>::Vector>& gradRho,
+                                 const NeighborSpace::ConnectivityMap<Dim<1> >& connectivityMap,
+                                 const Dim<1>::Scalar kernelExtent,
+                                 const std::vector<Dim<1>::FacetedVolume>& boundaries,
+                                 const std::vector<std::vector<Dim<1>::FacetedVolume> >& holes,
+                                 const FieldSpace::FieldList<Dim<1>, Dim<1>::Scalar>& weight,
+                                 FieldSpace::FieldList<Dim<1>, int>& surfacePoint,
+                                 FieldSpace::FieldList<Dim<1>, Dim<1>::Scalar>& vol,
+                                 FieldSpace::FieldList<Dim<1>, Dim<1>::Vector>& deltaMedian,
+                                 FieldSpace::FieldList<Dim<1>, Dim<1>::FacetedVolume>& cells);
 #endif
 
 #ifdef SPHERAL2D
@@ -78,6 +92,21 @@ namespace Spheral {
                          FieldSpace::FieldList<Dim<3>, Dim<3>::Scalar>& vol,
                          FieldSpace::FieldList<Dim<3>, Dim<3>::Vector>& deltaMedian,
                          FieldSpace::FieldList<Dim<3>, Dim<3>::FacetedVolume>& cells);
+      
+    void
+    computeWeightedVoronoiVolume(const FieldSpace::FieldList<Dim<3>, Dim<3>::Vector>& position,
+                                 const FieldSpace::FieldList<Dim<3>, Dim<3>::SymTensor>& H,
+                                 const FieldSpace::FieldList<Dim<3>, Dim<3>::Scalar>& rho,
+                                 const FieldSpace::FieldList<Dim<3>, Dim<3>::Vector>& gradRho,
+                                 const NeighborSpace::ConnectivityMap<Dim<3> >& connectivityMap,
+                                 const Dim<3>::Scalar kernelExtent,
+                                 const std::vector<Dim<3>::FacetedVolume>& boundaries,
+                                 const std::vector<std::vector<Dim<3>::FacetedVolume> >& holes,
+                                 const FieldSpace::FieldList<Dim<3>, Dim<3>::Scalar>& weight,
+                                 FieldSpace::FieldList<Dim<3>, int>& surfacePoint,
+                                 FieldSpace::FieldList<Dim<3>, Dim<3>::Scalar>& vol,
+                                 FieldSpace::FieldList<Dim<3>, Dim<3>::Vector>& deltaMedian,
+                                 FieldSpace::FieldList<Dim<3>, Dim<3>::FacetedVolume>& cells);
 #endif
 
   }
