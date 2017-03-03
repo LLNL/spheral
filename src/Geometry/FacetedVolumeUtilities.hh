c2cc3bbb3c278aa565d2e9c1b0cf922e99e47b28
Mike Owen <mikeowen@llnl.gov>
2017-03-03 15:22:29 -0800

    Toying with optimizations of the polyon and polyhedron.

--- src/Geometry/FacetedVolumeUtilities.hh
+++ src/Geometry/FacetedVolumeUtilities.hh
@@ -32,34 +32,30 @@ computeAncillaryGeometry(const PolyType& poly,
 
   // Find the facets surrounding each vertex.
   vertexFacetConnectivity = vector<vector<unsigned> >(nverts);
-  {
-    vector<set<unsigned> > uniqueFacetIDs(nverts);
-    for (unsigned fi = 0; fi != nfacets; ++fi) {
-      const Facet& facet = facets[fi];
-      const vector<unsigned>& ipoints = facet.ipoints();
-      for (const unsigned vi: ipoints) {
-        uniqueFacetIDs[vi].insert(fi);
-      }
+  for (unsigned fi = 0; fi != nfacets; ++fi) {
+    const Facet& facet = facets[fi];
+    const vector<unsigned>& ipoints = facet.ipoints();
+    for (const unsigned vi: ipoints) {
+      vertexFacetConnectivity[vi].push_back(fi);
     }
-    for (unsigned i = 0; i != nverts; ++i) {
-      vertexFacetConnectivity[i].assign(uniqueFacetIDs[i].begin(), uniqueFacetIDs[i].end());
-    }
+  }
+  for (unsigned i = 0; i != nverts; ++i) {
+    std::sort(vertexFacetConnectivity[i].begin(), vertexFacetConnectivity[i].end());
+    vertexFacetConnectivity[i].erase(std::unique(vertexFacetConnectivity[i].begin(), vertexFacetConnectivity[i].end()), vertexFacetConnectivity[i].end());
   }
   CHECK(vertexFacetConnectivity.size() == nverts);
 
   // Construct the facet->facet connectivity.
   facetFacetConnectivity = vector<vector<unsigned> >(nfacets);
-  {
-    vector<set<unsigned> > uniqueFacetIDs(nfacets);
-    for (const vector<unsigned>& vertexFacets: vertexFacetConnectivity) {
-      for (const unsigned fi: vertexFacets) {
-        uniqueFacetIDs[fi].insert(vertexFacets.begin(), vertexFacets.end());
-      }
+  for (const vector<unsigned>& vertexFacets: vertexFacetConnectivity) {
+    for (const unsigned fi: vertexFacets) {
+      std::copy(vertexFacets.begin(), vertexFacets.end(), std::back_inserter(facetFacetConnectivity[fi]));
     }
-    for (unsigned i = 0; i != nfacets; ++i) {
-      facetFacetConnectivity[i].assign(uniqueFacetIDs[i].begin(), uniqueFacetIDs[i].end());
-      CHECK(find(facetFacetConnectivity[i].begin(), facetFacetConnectivity[i].end(), i) != facetFacetConnectivity[i].end());
-    }
+  }
+  for (unsigned i = 0; i != nfacets; ++i) {
+    std::sort(facetFacetConnectivity[i].begin(), facetFacetConnectivity[i].end());
+    facetFacetConnectivity[i].erase(std::unique(facetFacetConnectivity[i].begin(), facetFacetConnectivity[i].end()), facetFacetConnectivity[i].end());
+    CHECK(find(facetFacetConnectivity[i].begin(), facetFacetConnectivity[i].end(), i) != facetFacetConnectivity[i].end());
   }
   CHECK(facetFacetConnectivity.size() == nfacets);
 
