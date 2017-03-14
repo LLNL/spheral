a2decc33f13a94c3434e47ad75ec55050d5ec8b4
Mike Owen <mikeowen@llnl.gov>
2017-03-13 23:41:10 -0700

    More work on the Physics module.

--- src/Pybind11Wraps/Physics/SpheralPhysics.cc
+++ src/Pybind11Wraps/Physics/SpheralPhysics.cc
@@ -10,8 +10,12 @@
 #include "Physics/Physics.hh"
 #include "Physics/GenericHydro.hh"
 #include "Physics/GenericBodyForce.hh"
+#include "Boundary/Boundary.hh"
+#include "ArtificialViscosity/ArtificialViscosity.hh"
+#include "Kernel/TableKernel.hh"
 
 #include "PyAbstractPhysics.hh"
+#include "PyPhysics.hh"
 #include "Pybind11Wraps/DataOutput/PyRestartMethods.hh"
 
 namespace py = pybind11;
@@ -57,17 +61,18 @@ void virtualPhysicsBindings(py::module& m, PB11Obj& obj) {
     .def("registerState", &Obj::registerState, "dataBase"_a, "state"_a)
     .def("registerDerivatives", &Obj::registerDerivatives, "dataBase"_a, "state"_a)
     .def("label", &Obj::label)
-    .def("applyGhostBoundaries", &Obj::applyGhostBoundaries)
-    .def("enforceBoundaries", &Obj::enforceBoundaries)
-    .def("initialize", &Obj::initialize)
-    .def("finalize", &Obj::finalize)
-    .def("finalizeDerivatives", &Obj::finalizeDerivatives)
-    .def("postStateUpdate", &Obj::postStateUpdate)
+    .def("applyGhostBoundaries", &Obj::applyGhostBoundaries, "state"_a, "derivs"_a)
+    .def("enforceBoundaries", &Obj::enforceBoundaries, "state"_a, "derivs"_a)
+    .def("initializeProblemStartup", &Obj::initializeProblemStartup)
+    .def("preStepInitialize", &Obj::preStepInitialize, "dataBase"_a, "state"_a, "derivs"_a)
+    .def("initialize", &Obj::initialize, "time"_a, "dt"_a, "dataBase"_a, "state"_a, "derivs"_a)
+    .def("finalize", &Obj::finalize, "time"_a, "dt"_a, "dataBase"_a, "state"_a, "derivs"_a)
+    .def("finalizeDerivatives", &Obj::finalizeDerivatives, "time"_a, "dt"_a, "dataBase"_a, "state"_a, "derivs"_a)
+    .def("postStateUpdate", &Obj::postStateUpdate, "dataBase"_a, "state"_a, "derivs"_a)
     .def("requireConnectivity", &Obj::requireConnectivity)
     .def("requireGhostConnectivity", &Obj::requireGhostConnectivity)
     .def("extraEnergy", &Obj::extraEnergy)
     .def("extraMomentum", &Obj::extraMomentum)
-
     ;
 }
 
@@ -84,6 +89,8 @@ void dimensionBindings(py::module& m, const std::string suffix) {
   typedef typename Dimension::ThirdRankTensor ThirdRankTensor;
   typedef Spheral::GeomPlane<Dimension> Plane;
   using Spheral::NodeSpace::NodeList;
+  using Spheral::KernelSpace::TableKernel;
+  using Spheral::ArtificialViscositySpace::ArtificialViscosity;
 
   //............................................................................
   // Physics
@@ -95,6 +102,50 @@ void dimensionBindings(py::module& m, const std::string suffix) {
     
     // Constructors
     .def(py::init<>())
+
+    // Methods
+    .def("appendBoundary", &Phys::appendBoundary, "boundary"_a)
+    .def("prependBoundary", &Phys::prependBoundary, "boundary"_a)
+    .def("clearBoundaries", &Phys::clearBoundaries)
+    .def("haveBoundary", &Phys::haveBoundary, "boundary"_a)
+    .def("boundaryConditions", &Phys::boundaryConditions)
+    ;
+
+  //............................................................................
+  // GenericHydro
+  typedef GenericHydro<Dimension> GH;
+  py::class_<GH, Phys,
+             PyAbstractPhysics<Dimension, GH>> ghPB11(m, ("GenericHydro" + suffix).c_str());
+  //  virtualPhysicsBindings<Dimension, Phys>(m, ghPB11);
+  ghPB11
+
+    // Constructors
+    .def(py::init<const TableKernel<Dimension>&, const TableKernel<Dimension>&, ArtificialViscosity<Dimension>&, const double, const bool>(),
+         "W"_a, "WPi"_a, "Q"_a, "cfl"_a, "useVelocityMagnitudeForDt"_a)
+
+    // Methods
+    .def("artificialViscosity", &GH::artificialViscosity)
+    .def("kernel", &GH::kernel)
+    .def("PiKernel", &GH::PiKernel)
+
+    // Virtual methods
+    .def("dt", &GH::dt, "dataBase"_a, "state"_a, "derivs"_a, "currentTime"_a)
+
+    // Attributes
+    .def_property("cfl", (Scalar (GH::*)() const) &GH::cfl, (void (GH::*)(Scalar)) &GH::cfl)
+    .def_property("useVelocityMagnitudeForDt", (bool (GH::*)() const) &GH::useVelocityMagnitudeForDt, (void (GH::*)(bool)) &GH::useVelocityMagnitudeForDt)
+    .def_property_readonly("minMasterNeighbor", &GH::minMasterNeighbor)
+    .def_property_readonly("maxMasterNeighbor", &GH::maxMasterNeighbor)
+    .def_property_readonly("averageMasterNeighbor", &GH::averageMasterNeighbor)
+    .def_property_readonly("minCoarseNeighbor", &GH::minCoarseNeighbor)
+    .def_property_readonly("maxCoarseNeighbor", &GH::maxCoarseNeighbor)
+    .def_property_readonly("averageCoarseNeighbor", &GH::averageCoarseNeighbor)
+    .def_property_readonly("minRefineNeighbor", &GH::minRefineNeighbor)
+    .def_property_readonly("maxRefineNeighbor", &GH::maxRefineNeighbor)
+    .def_property_readonly("averageRefineNeighbor", &GH::averageRefineNeighbor)
+    .def_property_readonly("minActualNeighbor", &GH::minActualNeighbor)
+    .def_property_readonly("maxActualNeighbor", &GH::maxActualNeighbor)
+    .def_property_readonly("averageActualNeighbor", &GH::averageActualNeighbor)
     ;
 
   //............................................................................
