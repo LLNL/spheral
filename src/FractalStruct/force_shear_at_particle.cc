6e538c9c0a19c4b4fee66b6b60e7c8b7aca078e5
villumsen <villumsen@localhost>
2017-04-01 12:46:17 -0700

    Updating some outdated c++98 code

--- src/FractalStruct/force_shear_at_particle.cc
+++ src/FractalStruct/force_shear_at_particle.cc
@@ -5,7 +5,6 @@ namespace FractalSpace
 {
   void force_shear_at_particle(Fractal_Memory& fractal_memory,Fractal& fractal)
   { 
-    //    ofstream& FileFractal=fractal_memory.p_fractal->p_file->DUMPS;
     ofstream& FileFractal=fractal_memory.p_fractal->p_file->FileFractal;
     vector <double> dens(8);
     vector <double> weights(8);
@@ -16,108 +15,61 @@ namespace FractalSpace
     vector <double> f_yz(8);
     vector <double> f_zz(8);
     vector <double> shear(6);
-    vector <double> shear2(6);
-    shear2.assign(6,0.0);
+    vector <double> shear2(6,0.0);
     const  double scale=(double)(fractal.get_grid_length()*Misc::pow(2,fractal.get_level_max()));
     double fourpiinv=1.0/(16.0*atan(1.0));
-    //
     for(int level=0;level <= fractal.get_level_max();level++)
       {
-	for(vector <Group*>::const_iterator group_itr=fractal_memory.all_groups[level].begin();
-	    group_itr!=fractal_memory.all_groups[level].end();group_itr++)
+	for(auto &p_group : fractal_memory.all_groups[level])
 	  {
-	    Group* p_group=*group_itr;
-	    Group& group=*p_group;
-	    double d_inv=pow(2.0,group.get_level()-fractal.get_level_max());
-	    for(vector<Point*>::const_iterator point_itr=group.list_points.begin();point_itr !=group.list_points.end();++point_itr)
+	    double d_inv=pow(2.0,p_group->get_level()-fractal.get_level_max());
+	    for(auto &p_point : p_group->list_points)
 	      {
-		Point& point=**point_itr;
-		//		if(point.get_buffer_point()) continue;
+		Point& point=*p_point;
 		bool not_yet=true;
-		//
-		//	FileFractal << "doing point " << *point_itr << "\n";
-		for(vector<Particle*>::const_iterator particle_itr=point.list_particles.begin();particle_itr !=point.list_particles.end();++particle_itr)
+		for(auto &p_part : point.list_particles)
 		  {
-		    Particle& particle=**particle_itr;
+		    Particle& particle=*p_part;
 		    if(!particle.get_real_particle())
 		      continue;
-		    // FileFractal << "doing particle " << *particle_itr << "\n";
-		    // FileFractal << "p group " << p_group << " " << particle.get_p_highest_level_group() << "\n";
-		    if(p_group == particle.get_p_highest_level_group())
+		    if(p_group != particle.get_p_highest_level_group())
+		      continue;
+		    if(not_yet)
 		      {
-			if(not_yet)
-			  {
-			    // FileFractal << " not yet a " << &point << "\n";
-			    point.get_field_shear_values(f_xx,f_xy,f_xz,f_yy,f_yz,f_zz);
-			    not_yet=false;
-			    // FileFractal << " not yet b " << "\n";
-			  }
-			//
-			double d_x,d_y,d_z;
-			vector <double> pos(3);
-			// FileFractal << "pos a" << "\n";
-			particle.get_pos(pos);
-			// FileFractal << "pos b" << "\n";
-			point.get_deltas(pos,d_x,d_y,d_z,scale,d_inv);
-			// FileFractal << "delta a" << "\n";
-			Misc::set_weights(weights,d_x,d_y,d_z);
-			Misc::sum_prod<double>(0,7,1,shear,weights,f_xx,f_xy,f_xz,f_yy,f_yz,f_zz);
-			//			particle.dump(FileFractal);
-			if(fractal_memory.calc_density_particle)
-			  {
-			    // FileFractal << "shear densa " << "\n";
-			    particle.set_density(-(shear[0]+shear[3]+shear[5])*fourpiinv);
-			    // FileFractal << "shear densb " << "\n";
-			  }
-			if(fractal_memory.calc_shear)
-			  {
-			    //			    FileFractal << " SHEARPART " << p_group << " ";
-			    for(int i=0;i<6;i++)
-			      {
-				//				FileFractal << shear[i] << " ";
-				shear2[i]+=shear[i]*shear[i];
-			      }
-
-			    //			    FileFractal << "\n";			    
-			    double r=0.0;
-			    max_predict(fractal_memory,fractal,shear,r);
-			    if(fractal_memory.start_up) 
-			      {
-				// FileFractal << "sheara " << r << "\n";
-				particle.set_rad_max(r);
-				// FileFractal << "shearb " << r << "\n";
-			      }
-			  }
+			point.get_field_shear_values(f_xx,f_xy,f_xz,f_yy,f_yz,f_zz);
+			not_yet=false;
 		      }
+		    double d_x,d_y,d_z;
+		    vector <double> pos(3);
+		    particle.get_pos(pos);
+		    point.get_deltas(pos,d_x,d_y,d_z,scale,d_inv);
+		    Misc::set_weights(weights,d_x,d_y,d_z);
+		    Misc::sum_prod<double>(0,7,1,shear,weights,f_xx,f_xy,f_xz,f_yy,f_yz,f_zz);
+		    if(fractal_memory.calc_density_particle)
+		      particle.set_density(-(shear[0]+shear[3]+shear[5])*fourpiinv);
+		    if(!fractal_memory.calc_shear)
+		      continue;
+		    for(int i=0;i<6;i++)
+		      shear2[i]+=shear[i]*shear[i];
+		    double r=0.0;
+		    max_predict(fractal_memory,fractal,shear,r);
+		    if(fractal_memory.start_up) 
+		      particle.set_rad_max(r);
 		  }
 	      }
 	  }
       }
-
     for(int level=0;level <= fractal.get_level_max();level++)
-      {
-	for(vector <Group*>::const_iterator group_itr=fractal_memory.all_groups[level].begin();
-	    group_itr!=fractal_memory.all_groups[level].end();group_itr++)
-	  {
-	    Group& group=**group_itr;
-	    for(vector<Point*>::const_iterator point_itr=group.list_points.begin();point_itr !=group.list_points.end();++point_itr)
-	      (*point_itr)->force_shear_point_zero();
-	  }
-      }
-
-
+      for(auto &p_group : fractal_memory.all_groups[level])
+	for(auto &p_point : p_group->list_points)
+	  p_point->force_shear_point_zero();
     if(fractal_memory.calc_shear)
       {
 	double spam=(double)fractal.get_number_particles();
 	FileFractal << " shear2 " ;
-	FileFractal << shear2[0]/spam << " " ;
-	FileFractal << shear2[1]/spam << " " ;
-	FileFractal << shear2[2]/spam << " " ;
-	FileFractal << shear2[3]/spam << " " ;
-	FileFractal << shear2[4]/spam << " " ;
-	FileFractal << shear2[5]/spam << " " ;
+	for(int ni=0;ni<6;ni++)
+	  FileFractal << shear2[ni]/spam << " " ;
 	FileFractal << "\n";
-	//    assert(0);
       }
   }
 }
