990413922f16152a6c9e11fe2eeeb36d4931f70f
villumsen <jensvvillumsen@gmail.com>
2017-04-10 13:21:39 -0700

    Changing the minimum number of particles at a point to depend on the level. This can allow more of the volume to be solved at higher resolution

--- src/FractalStruct/check_high.cc
+++ src/FractalStruct/check_high.cc
@@ -20,4 +20,21 @@ namespace FractalSpace
       }
     return false;
   }
+  bool check_high(Point& point,Fractal_Memory& mem)
+  {
+    //--------------------------------------------------------------------------------------------------------------------------------
+    // Guess what, as Nina says.
+    //--------------------------------------------------------------------------------------------------------------------------------
+    if(point.list_particles.size() < mem.minimum_number_var[mem.level])
+      return false;
+    unsigned int np=0;
+    for(auto &part : point.list_particles)
+      {
+	if(part->get_p_highest_level_group() != 0)
+	  np++;
+	if(np >= mem.minimum_number_var[mem.level])
+	  return true;
+      }
+    return false;
+  }
 }
