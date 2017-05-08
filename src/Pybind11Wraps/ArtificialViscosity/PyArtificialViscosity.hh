55f532f11f5037580c4309650c62f3006aa16db7
Mike Owen <mikeowen@llnl.gov>
2017-05-07 23:45:49 -0700

    Working on the Q pybind11 bindings.

--- src/Pybind11Wraps/ArtificialViscosity/PyArtificialViscosity.hh
+++ src/Pybind11Wraps/ArtificialViscosity/PyArtificialViscosity.hh
@@ -1,18 +1,10 @@
 //------------------------------------------------------------------------------
-// Trampoline classes for abstract ArtificialViscosity interface.
+// Trampoline classes for virtual ArtificialViscosity interface.
 //------------------------------------------------------------------------------
-#ifndef __Spheral_PyAbstractArtificialViscosity__
-#define __Spheral_PyAbstractArtificialViscosity__
+#ifndef __Spheral_PyArtificialViscosity__
+#define __Spheral_PyArtificialViscosity__
 
-#include <utility>
-
-#include "ArtificialViscosity/ArtificialViscosity.hh"
-#include "DataBase/State.hh"
-#include "DataBase/StateDerivatives.hh"
-#include "FileIO/FileIO.hh"
-#include "DataBase/DataBase.hh"
-
-#include "Pybind11Wraps/DataOutput/PyRestartMethods.hh"
+#include "PyAbstractArtificialViscosity.hh"
 
 using Spheral::NodeSpace::NodeList;
 using Spheral::FieldSpace::Field;
@@ -23,10 +15,10 @@ namespace Spheral {
 namespace ArtificialViscositySpace {
 
 //------------------------------------------------------------------------------
-// PyAbstractArtificialViscosity
+// PyArtificialViscosity
 //------------------------------------------------------------------------------
 template<typename Dimension, class ArtificialViscosityBase>
-class PyAbstractArtificialViscosity: public ArtificialViscosityBase {
+class PyArtificialViscosity: public ArtificialViscosityBase {
 public:
   using ArtificialViscosityBase::ArtificialViscosityBase;  // inherit constructors
 
@@ -35,22 +27,6 @@ public:
   typedef typename Dimension::Tensor Tensor;
   typedef typename Dimension::SymTensor SymTensor;
   typedef typename Dimension::ThirdRankTensor ThirdRankTensor;
-  typedef typename ArtificialViscosityBase::ConstBoundaryIterator ConstBoundaryIterator;
-
-  virtual void initialize(const DataBase<Dimension>& dataBase,
-                          const State<Dimension>& state,
-                          const StateDerivatives<Dimension>& derivs,
-                          ConstBoundaryIterator boundaryBegin,
-                          ConstBoundaryIterator boundaryEnd,
-                          const Scalar time,
-                          const Scalar dt,
-                          const KernelSpace::TableKernel<Dimension>& W) override {
-    PYBIND11_OVERLOAD(void,                    // Return type
-                      ArtificialViscosityBase, // Parent class
-                      initialize,              // name of method
-                      dataBase, state, derivs, boundaryBegin, boundaryEnd, time, dt, W      // arguments
-      );
-  }
 
   virtual std::pair<Tensor, Tensor> Piij(const unsigned nodeListi, const unsigned i, 
                                          const unsigned nodeListj, const unsigned j,
@@ -67,10 +43,10 @@ public:
                                          const Scalar csj,
                                          const SymTensor& Hj) const override {
     typedef std::pair<Tensor, Tensor> ReturnType;
-    PYBIND11_OVERLOAD_PURE(ReturnType,                                        // Return type
-                           ArtificialViscosityBase,                           // Parent class
-                           Piij,                                              // name of method
-                           nodeListi, i, nodeListj, j, xi, etai, vi, rhoi, csi, Hi, xj, etaj, vj, rhoj, csj, Hj       // arguments
+    PYBIND11_OVERLOAD(ReturnType,                                        // Return type
+                      ArtificialViscosityBase,                           // Parent class
+                      Piij,                                              // name of method
+                      nodeListi, i, nodeListj, j, xi, etai, vi, rhoi, csi, Hi, xj, etaj, vj, rhoj, csj, Hj       // arguments
       );
   }
 
