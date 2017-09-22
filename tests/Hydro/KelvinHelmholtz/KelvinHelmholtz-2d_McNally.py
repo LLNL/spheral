1b0bc66c9c3f127994274e98b3b01b5d18a036de
Mike Owen <mikeowen@llnl.gov>
2017-09-22 09:34:44 -0700

    Simplyfing choice of viz output for KH script.

--- tests/Hydro/KelvinHelmholtz/KelvinHelmholtz-2d_McNally.py
+++ tests/Hydro/KelvinHelmholtz/KelvinHelmholtz-2d_McNally.py
@@ -111,7 +111,7 @@ commandLine(nx1 = 256,
             HopkinsConductivity = False,     # For PSPH
             evolveTotalEnergy = False,       # Only for SPH variants -- evolve total rather than specific energy
 
-            useVoronoiOutput = False,
+            useVoronoiOutput = True,
             clearDirectories = False,
             restoreCycle = -1,
             restartStep = 100,
@@ -161,7 +161,6 @@ dataDir = os.path.join(dataDir,
                        "nPerh=%g-Qhmult=%g" % (nPerh, Qhmult))
 restartDir = os.path.join(dataDir, "restarts")
 vizDir = os.path.join(dataDir, "visit")
-vizDirVor = os.path.join(dataDir, "visit-cells")
 restartBaseName = os.path.join(restartDir, "KelvinHelmholtz-2d_McNally")
 vizBaseName = "KelvinHelmholtz-2d_McNally"
 
@@ -176,8 +175,6 @@ if mpi.rank == 0:
         os.makedirs(restartDir)
     if not os.path.exists(vizDir):
         os.makedirs(vizDir)
-    if not os.path.exists(vizDirVor):
-        os.makedirs(vizDirVor)
 mpi.barrier()
 
 #-------------------------------------------------------------------------------
@@ -486,7 +483,7 @@ output("integrator.verbose")
 #-------------------------------------------------------------------------------
 # Make the problem controller.
 #-------------------------------------------------------------------------------
-if useVoronoiOutput:
+if useVoronoiOutput and crksph:
     import SpheralVoronoiSiloDump
     vizMethod = SpheralVoronoiSiloDump.dumpPhysicsState
 else:
@@ -506,24 +503,6 @@ control = SpheralController(integrator, WT,
                             vizTime = vizTime,
                             SPH = (not asph))
 output("control")
-
-#-------------------------------------------------------------------------------
-# Make a special viz method that uses our cell output.
-#-------------------------------------------------------------------------------
-def dropVorViz(cycle, Time, dt):
-    SpheralVoronoiSiloDump.dumpPhysicsState(integrator,
-                                            baseFileName = vizBaseName,
-                                            baseDirectory = vizDirVor,
-                                            currentTime = Time,
-                                            currentCycle = cycle,
-                                            dumpGhosts = False,
-                                            dumpDerivatives = False,
-                                            boundaries = integrator.uniqueBoundaryConditions())
-if crksph:
-    control.appendPeriodicWork(dropVorViz, vizCycle)
-    control.appendPeriodicTimeWork(dropVorViz, vizTime)
-    if control.totalSteps == 0:
-        dropVorViz(0, 0.0, 1.0)
 
 #-------------------------------------------------------------------------------
 # Add a method for measuring the mixing scale.
