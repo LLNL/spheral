d895d1d778f13b4e58cdb2b790e61f535d97bd73
Mike Owen <mikeowen@llnl.gov>
2017-12-31 15:55:47 -0800

    Fixing some comments.

--- src/Geometry/polyclipper2d.cc
+++ src/Geometry/polyclipper2d.cc
@@ -1,16 +1,15 @@
-//---------------------------------Spheral++----------------------------------//
-// Clip a faceted volume (box, polygon, or polyhedron) by a plane in place.
+//---------------------------------PolyClipper--------------------------------//
+// Clip a faceted volume (polygon or polyhedron) by a set of planes in place.
 //
 // We use the convention that any portion of the faceted volume "below" the 
 // plane is clipped, i.e., only the portion of the faceted volume "above" the 
 // plane
-//    plane.compare(point) <= 0
+//    plane.compare(point) >= 0
 // is retained.
 //
-// The algorithms herein are roughly based on the approach outlined in 
+// The algorithms herein are based on R3D as outlined in 
 // Powell, D., & Abel, T. (2015). An exact general remeshing scheme applied to 
 // physically conservative voxelization. Journal of Computational Physics, 297, 340–356.
-// though I think not exactly the same.
 //
 // Created by J. Michael Owen, Tue Nov 28 10:00:51 PST 2017
 //----------------------------------------------------------------------------//
