bffcd7b6c88c77f9e654ce34d43674dc56c2ae29
Mike Owen <mikeowen@llnl.gov>
2018-03-16 22:15:10 -0700

    Getting near full auto generation of trampoline foolishness.

--- src/Pybind11Wraps/Boundary/PyBoundary.py
+++ src/Pybind11Wraps/Boundary/PyBoundary.py
@@ -20,8 +20,136 @@ using Spheral::DataBaseSpace::DataBase;
 """
         return
 
+    def setAllGhostNodes(self,
+                         args = [("DataBase<Dimension>&", "dataBase")]):
+        return "void"
+
+    def setAllViolationNodes(self,
+                             args = [("DataBase<Dimension>&", "dataBase")]):
+        return "void"
+
+    def cullGhostNodes(self,
+                       args = [("const FieldList<Dimension, int>", "flagSet"),
+                               ("FieldList<Dimension, int>&", "old2newIndexMap"),
+                               ("std::vector<int>&", "numNodesRemoved")]):
+        return "void"
+
     def setGhostNodes(self,
-                      args = [("NodeList<Dimension>&", "nodeList")]):
+                      args = [("NodeList<Dimension>&", "nodeList")],
+                      pure = True):
+        return "void"
+
+    def updateGhostNodes(self,
+                         args = [("NodeList<Dimension>&", "nodeList")],
+                         pure = True):
+        return "void"
+
+    def applyGhostBoundary(self,
+                           args = [("Field<Dimension, int>&", "field")],
+                           const = True,
+                           pure = True):
+        return "void"
+
+    def applyGhostBoundary(self,
+                           args = [("Field<Dimension, Scalar>&", "field")],
+                           const = True,
+                           pure = True):
+        return "void"
+
+    def applyGhostBoundary(self,
+                           args = [("Field<Dimension, Vector>&", "field")],
+                           const = True,
+                           pure = True):
+        return "void"
+
+    def applyGhostBoundary(self,
+                           args = [("Field<Dimension, Tensor>&", "field")],
+                           const = True,
+                           pure = True):
+        return "void"
+
+    def applyGhostBoundary(self,
+                           args = [("Field<Dimension, SymTensor>&", "field")],
+                           const = True,
+                           pure = True):
+        return "void"
+
+    def applyGhostBoundary(self,
+                           args = [("Field<Dimension, ThirdRankTensor>&", "field")],
+                           const = True,
+                           pure = True):
+        return "void"
+
+    def applyGhostBoundary(self,
+                             args = [("Field<Dimension, std::vector<Scalar>>&", "field")],
+                             const = True,
+                             pure = True):
+        return "void"
+
+    def setViolationNodes(self,
+                          args = [("NodeList<Dimension>&", "nodeList")],
+                          pure = True):
+        return "void"
+
+    def updateViolationNodes(self,
+                             args = [("NodeList<Dimension>&", "nodeList")],
+                             pure = True):
+        return "void"
+
+    def enforceBoundary(self,
+                          args = [("Field<Dimension, int>&", "field")],
+                          const = True,
+                          pure = True):
+        return "void"
+
+    def enforceBoundary(self,
+                        args = [("Field<Dimension, Scalar>&", "field")],
+                        const = True,
+                        pure = True):
+        return "void"
+
+    def enforceBoundary(self,
+                        args = [("Field<Dimension, Vector>&", "field")],
+                        const = True,
+                        pure = True):
+        return "void"
+
+    def enforceBoundary(self,
+                        args = [("Field<Dimension, Tensor>&", "field")],
+                        const = True,
+                        pure = True):
+        return "void"
+
+    def enforceBoundary(self,
+                        args = [("Field<Dimension, SymTensor>&", "field")],
+                        const = True,
+                        pure = True):
+        return "void"
+
+    def enforceBoundary(self,
+                        args = [("Field<Dimension, ThirdRankTensor>&", "field")],
+                        const = True,
+                        pure = True):
+        return "void"
+
+    def initializeProblemStartup(self):
+        return "void"
+
+    def finalizeGhostBoundary(self):
+        return "void"
+
+    def reset(self,
+              args = [("const DataBase<Dimension>&", "dataBase")]):
+        return "void"
+
+    def numGhostNodes(self,
+                      const = True):
+        return "int"
+
+    def clip(self,
+             args = [("Vector&", "xmin"),
+                     ("Vector&", "xmax")],
+             const = True):
         return "void"
 
 if __name__ == "__main__":
