1bb8b31858337749fc41db61c274ce7a95c31cc7
Mike Owen <mikeowen@llnl.gov>
2018-04-02 21:51:42 -0700

    Experimenting with mapping returnType "None" -> "void".

---
+++ src/Pybind11Wraps/Boundary/PyPlanarBoundary.py
@@ -0,0 +1,39 @@
+from TrampolineGenerator import *
+
+class PyPlanarBoundary(TrampolineGenerator):
+
+    def __init__(self):
+        TrampolineGenerator.__init__(self)
+        self.includes = ["Geometry/GeomPlane.hh",
+                         "NodeList/NodeList.hh",
+                         "Field/Field.hh",
+                         "DataBase/DataBase.hh"]
+        self.namespaces = ["Spheral", "BoundarySpace"]
+        self.templates = ["Dimension"]
+        self.preamble = """
+using Spheral::NodeSpace::NodeList;
+using Spheral::FieldSpace::Field;
+using Spheral::FieldSpace::FieldList;
+using Spheral::DataBaseSpace::DataBase;
+"""
+        return
+
+    def setGhostNodes(self,
+                      args = [("NodeList<Dimension>&", "nodeList")]):
+        return
+
+    def updateGhostNodes(self,
+                           args = [("NodeList<Dimension>&", "nodeList")]):
+        return
+
+    def setViolationNodes(self,
+                          args = [("NodeList<Dimension>&", "nodeList")]):
+        return
+
+    def updateViolationNodes(self,
+                             args = [("NodeList<Dimension>&", "nodeList")]):
+        return
+
+    def valid(self,
+              const = True):
+        return "bool"
