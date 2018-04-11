8687acfe90ec116c1d46cde31f587983318cb1e6
Mike Owen <mikeowen@llnl.gov>
2018-04-11 16:51:20 -0700

    Organizing generation.

---
+++ src/Pybind11Wraps/PYB11ClassDecorators.py
@@ -0,0 +1,38 @@
+#-------------------------------------------------------------------------------
+# Decorators for attributes to classes.
+#-------------------------------------------------------------------------------
+from functools import wraps
+
+#-------------------------------------------------------------------------------
+# Singleton
+#-------------------------------------------------------------------------------
+class singleton:
+
+    def __init__(self, state=True):
+        self.state = state
+        return
+
+    def __call__(self, cls):
+        @wraps(cls)
+        class Wrapped:
+            cls.__pyb11_singleton = self.state
+        return Wrapped
+
+#-------------------------------------------------------------------------------
+# Virtual method
+#-------------------------------------------------------------------------------
+def virtual(f):
+    @wraps(f)
+    def wrapper(*args, **kwds):
+        f.__pyb11_virtual = True
+    return wrapper
+
+#-------------------------------------------------------------------------------
+# Pure virtual method
+#-------------------------------------------------------------------------------
+def pure_virtual(f):
+    @wraps(f)
+    def wrapper(*args, **kwds):
+        f.__pyb11_pure_virtual = True
+    return wrapper
+
