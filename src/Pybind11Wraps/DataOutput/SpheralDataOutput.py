1e9b4a36a3672746c4f2caf3d0989cef0a0e0434
Mike Owen <mikeowen@llnl.gov>
2018-04-11 21:40:24 -0700

    Checkpoint.

--- src/Pybind11Wraps/DataOutput/SpheralDataOutput.py
+++ src/Pybind11Wraps/DataOutput/SpheralDataOutput.py
@@ -4,6 +4,10 @@ Spheral DataOuput module.
 Provides the fundamental classes for Restart in Spheral.
 """
 
+import sys
+sys.path.append("..")
+from PYB11Generator import *
+
 #-------------------------------------------------------------------------------
 # Includes
 #-------------------------------------------------------------------------------
@@ -11,7 +15,7 @@ includes = ["<vector>",
             "<string>",
             '"Geometry/Dimension"',
             '"DataOutput/RestartRegistrar.hh"',
-            '"RestartableObject.hh"',
+            '"DataOutput/RestartableObject.hh"',
             '"FileIO/FileIO.hh"']
 
 #-------------------------------------------------------------------------------
@@ -22,32 +26,65 @@ namespaces = ["Spheral", "DataOutput"]
 #-------------------------------------------------------------------------------
 # RestartRegistrar
 #-------------------------------------------------------------------------------
+@singleton
 class RestartRegistrar:
-    singlegton = True
 
     def removeExpiredPointers(self):
         "Clear all expired objects from the RestartRegistrar."
         return
 
+    @const
     def uniqueLabels(self):
         "Return a std::vector of the labels for each known restart handle."
         return
 
+    @const
     def printLabels(self):
         "Print out the labels for all known restart handles."
         return
 
+    @const
     def dumpState(self):
         "Dump the state of all restartable handles"
         return
 
+    @const
     def restoreState(self):
         "Restore the state of all restartable handles"
         return
 
 #-------------------------------------------------------------------------------
-# 
+# RestartableObject
 #-------------------------------------------------------------------------------
+class RestartableObject:
+
+    def __init__(self,
+                 args = [("PyObject*", "self"),
+                         ("const unsigned", "priority")]):
+        "Construct with the given object and priority."
+        return
+
+    @virtual
+    @const
+    def label(self):
+        "Define the label for storing this object in a restart file."
+        return "std::string"
+
+    @virtual
+    @const
+    def dumpState(self,
+                  args = [("FileIOSpace::FileIO&", "file"),
+                          ("const std::string", "pathName")]):
+        "Write this objects state to the file under the given path."
+        return "void"
+
+    @virtual
+    def restoreState(self,
+                     args = [("const FileIOSpace::FileIO&", "file"),
+                             ("const std::string", "pathName")]):
+        "Read the state for this object from the given file and path."
+        return "void"
+
 #-------------------------------------------------------------------------------
 # 
 #-------------------------------------------------------------------------------
