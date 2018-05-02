8542949b8d59eaaa816b46132d51a44b515238f9
Mike Owen <mikeowen@llnl.gov>
2018-05-02 15:02:49 -0700

    Cleaning up Jens' fractal packages a bit.

--- src/FractalForce/MPI/gather_particles_empty.cc
+++
@@ -1,9 +0,0 @@
-#include "libs.hh"
-#include "classes.hh"
-#include "headers.hh"
-namespace FractalSpace
-{
-  void gather_particles(Fractal_Memory& mem,Fractal& frac)
-  {
-  }
-}
--- src/FractalForce/MPI/scatter_particles_empty.cc
+++
@@ -1,9 +0,0 @@
-#include "libs.hh"
-#include "classes.hh"
-#include "headers.hh"
-namespace FractalSpace
-{
-  void scatter_particles(Fractal_Memory& mem,Fractal& frac)
-  {
-  }
-}
