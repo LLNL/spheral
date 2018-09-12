0da4305647d67182723bbc8deb7c76ba2e4cd0c3
Mike Owen <mikeowen@llnl.gov>
2018-09-12 14:27:53 -0700

    Checkpoint.

--- src/Pybind11Wraps/PolyClipper/PolyClipper.py
+++ src/Pybind11Wraps/PolyClipper/PolyClipper.py
@@ -10,17 +10,28 @@ from PYB11Decorators import *
 includes = ['"Geometry/polyclipper.hh"']
 
 #-------------------------------------------------------------------------------
-# Plane
+# Plane2d
 #-------------------------------------------------------------------------------
 @PYB11cppname("PolyClipper::Plane2d")
 class PolyClipperPlane2d:
-    "Plane class for polyclipper in %i dimensions." % 2
+    """Plane class for polyclipper in 2 dimensions."""
+    ndim = 2    
 
     # Constructors
     def pyinit0(self):
         "Default constructor"
         return
 
-    def pyinit1(rhs = "const PolyClipper::Plane%id" % 2):
+    def pyinit1(rhs = "const PolyClipper::Plane%id" % ndim):
         "Copy constructor"
+        return
+
+    def pyinit2(dist = "double",
+                normal = "const PolyClipper::Plane%id::Vector&" % ndim):
+        "Construct with a distance and normal."
+        return
+
+    def pyinit3(point = "const PolyClipper::Plane%id::Vector&" % ndim,
+                normal = "const PolyClipper::Plane%id::Vector&" % ndim):
+        "Construct with a point and normal."
         return
