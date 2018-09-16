99383a27fd2385b9e5238b771e4c10520f930454
Mike Owen <mikeowen@llnl.gov>
2018-09-16 14:28:44 -0700

    Checkpoint working on Tensor.

--- src/Pybind11Wraps/Geometry/SpheralGeometry.py
+++ src/Pybind11Wraps/Geometry/SpheralGeometry.py
@@ -57,7 +57,7 @@ vector_of_Facet2d = PYB11_bind_vector("GeomFacet2d", opaque=True)
 vector_of_Facet3d = PYB11_bind_vector("GeomFacet3d", opaque=True)
 
 #-------------------------------------------------------------------------------
-# Worker for adding methods to Vector.
+# Vector template
 #-------------------------------------------------------------------------------
 @PYB11template("ndim")
 class Vector:
@@ -288,3 +288,195 @@ Vector3d = PYB11TemplateClass(Vector,
                               cppname = "Dim<3>::Vector",
                               pyname = "Vector3d",
                               docext = " (3D).")
+
+#-------------------------------------------------------------------------------
+# Tensor (rank 2) template
+#-------------------------------------------------------------------------------
+@PYB11template("ndim")
+class Tensor:
+    "Spheral Geometric Tensor (rank 2: %(ndim)sx%(ndim)s) class"
+
+    # Static attributes
+    @PYB11static
+    @PYB11readonly
+    def nDimensions(self):
+        "Number of dimensions"
+
+    @PYB11static
+    @PYB11readonly
+    def numElements(self):
+        "Number of elements stored in the type."
+
+    @PYB11static
+    @PYB11readonly
+    def zero(self):
+        "The zero value equivalent."
+
+    @PYB11static
+    @PYB11readonly
+    def one(self):
+        "The unit value equivalent."
+
+    # Constructors
+    def pyinit0(self):
+        "Default constructor"
+
+    def pyinit1(self,
+                rhs = "const Dim<%(ndim)s>::Tensor"):
+        "Copy constructor (tensor)"
+
+    def pyinit2(self,
+                rhs = "const Dim<%(ndim)s>::SymTensor"):
+        "Copy constructor (symmetric tensor)"
+
+    def pyinit3(self,
+                xx="double"):
+        "Construct with element values."
+
+    def pyinit3(self,
+                xx="double", xy="double",
+                yx="double", yy="double"):
+        "Construct with element values."
+
+    def pyinit4(self,
+                xx="double", xy="double", xz="double",
+                yx="double", yy="double", yz="double",
+                zx="double", zy="double", zz="double"):
+        "Construct with element values."
+
+    # Sequence methods
+    @PYB11implementation("[](const Dim<%(ndim)s>::Tensor& self) { return Dim<%(ndim)s>::Tensor::numElements; }")
+    def __len__(self):
+        "The size (number of elements) of the Tensor."
+
+    @PYB11implementation("[](const Dim<%(ndim)s>::Tensor &s, size_t i) { if (i >= Dim<%(ndim)s>::Tensor::numElements) throw py::index_error(); return s[i]; }") 
+    def __getitem__(self):
+        "Python indexing to get an element."
+
+    @PYB11implementation("[](Dim<%(ndim)s>::Tensor &s, size_t i, float v) { if (i >= Dim<%(ndim)s>::Tensor::numElements) throw py::index_error(); s[i] = v; }") 
+    def __setitem__(self):
+        "Python indexing to set an element."
+
+    @PYB11implementation("[](const Dim<%(ndim)s>::Tensor &s) { return py::make_iterator(s.begin(), s.end()); }")
+    def __iter__(self):
+        "Python iteration through a Tensor."
+
+    @PYB11const
+    def __call__(self,
+                 row="Dim<%(ndim)s>::Tensor::size_type", 
+                 col="Dim<%(ndim)s>::Tensor::size_type"):
+        "Extract the (row, column) element."
+        return "double"
+
+    # String representation
+    @PYB11implementation("""
+[](const Dim<%(ndim)s>::Tensor& self) {
+  std::string result = "Tensor" + std::to_string(%(ndim)s) + "d(";
+  for (auto val: self) result += (" " + std::to_string(val) + " ");
+  result += ")";
+  return result;
+}""")
+    def __repr__(self):
+        return
+
+    # Operators
+    def __neg__(self):
+        return
+    def __add__(self):
+        return
+    def __sub__(self):
+        return
+    def __mul__(self):
+        return
+    def __iadd__(self):
+        return
+    def __isub__(self):
+        return
+
+    def __add__(self, rhs="Dim<%(ndim)s>::SymTensor()"):
+        return
+    def __sub__(self, rhs="Dim<%(ndim)s>::SymTensor()"):
+        return
+    def __mul__(self, rhs="Dim<%(ndim)s>::SymTensor()"):
+        return
+
+    def __mul__(self, rhs="float()"):
+        return
+    def __rmul__(self, rhs="float()"):
+        return
+    def __div__(self, rhs="float()"):
+        return
+    def __imul__(self, rhs="float()"):
+        return
+    def __idiv__(self, rhs="float()"):
+        return
+
+    # Comparison
+    def __eq__(self):
+        return
+    def __ne__(self):
+        return
+    def __lt__(self):
+        return
+    def __gt__(self):
+        return
+    def __le__(self):
+        return
+    def __ge__(self):
+        return
+
+    # Methods
+    def getRow(self):
+        "Extract the indexed row as a Vector%(ndim)sd."
+    def getColumn(self):
+        "Extract the indexed column as a Vector%(ndim)sd."
+    def Zero(self):
+        "Zero out the elements."
+    def Identity(self):
+        "Set equal to the I tensor."
+    def Symmetric(self):
+        "Return a SymTensor with the symmetric portion of this tensor."
+    def SkewSymmetric(self):
+        "Return a Tensor with the skew-symmetric portion of this tensor."
+    def Transpose(self):
+        "Return the transpose of this Tensor."
+    def Inverse(self):
+        "Return the inverse of the tensor."
+    def diagonalElements(self):
+        "Return a Vector%(ndim)sd with the diagonal elements of this tensor."
+    def Trace(self):
+        "Compute the trace (sum of diagonal elements)."
+    def Determinant(self):
+        "Compute the determinant of the tensor."
+    @PYB11const
+    def dot(self, rhs="const Dim<%(ndim)s>::Vector&"):
+        "Product with a Vector%(ndim)sd."
+        return "Dim<%(ndim)s>::Vector"
+    @PYB11const
+    def doubledot(self, rhs="const Dim<%(ndim)s>::Tensor&"):
+        "Double dot contraction with another tensor (returns a scalar)."
+        return "double"
+    @PYB11const
+    def doubledot(self, rhs="const Dim<%(ndim)s>::SymTensor&"):
+        "Double dot contraction with a SymTensor (returns a scalar)."
+        return "double"
+    def selfDoubledot(self):
+        "Double dot contraction with ourself (returns a scalar)."
+    def square(self):
+        "Compute the product with ourself as a matrix product."
+    def squareElements(self):
+        "Returns the element-wise square of the tensor."
+    def eigenValues(self):
+        "Return a Vector%(ndim)sd with the eigenvalues of this tensor."
+    def rotationalTransform(self):
+        "Apply the given rotational transform to the tensor."
+    def maxAbsElement(self):
+        "Return the maximum of the absolute values of the elements."
+
+#-------------------------------------------------------------------------------
+# Tensor instantiations.
+#-------------------------------------------------------------------------------
+Tensor1d = PYB11TemplateClass(Tensor,
+                              template_parameters = ("1"),
+                              cppname = "Dim<1>::Tensor",
+                              pyname = "Tensor1d")
