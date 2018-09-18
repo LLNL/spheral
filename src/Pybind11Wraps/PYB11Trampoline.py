19ddd53ae5f2d8bb2d69d825f0d19b38a5d36c85
Mike Owen <mikeowen@llnl.gov>
2018-09-17 22:00:08 -0700

    Trampoline and virtual methods tentatively working for non-templated classes.

--- src/Pybind11Wraps/PYB11Trampoline.py
+++ src/Pybind11Wraps/PYB11Trampoline.py
@@ -3,6 +3,7 @@
 #-------------------------------------------------------------------------------
 import inspect
 import sys
+import StringIO
 
 from PYB11utils import *
 
@@ -11,164 +12,67 @@ from PYB11utils import *
 #
 # Generate the trampoline class, including pure virtual hooks.
 #-------------------------------------------------------------------------------
-def PYB11generateTrampoline(klass, klassattrs, ss):
-    klassinst = klass()
+def PYB11generateTrampoline(klass, klassattrs, ssout):
 
     # Common preliminary code
-    PYB11generateTrampolineClassStart(klass, klassattrs, ss)
+    PYB11generateTrampolineClassStart(klass, klassattrs, ssout)
 
-    # Bind the virtual methods
-    methods = [(mname, meth) for (mname, meth) in PYB11ClassMethods(klass)
-               if not PYB11attrs(meth)["ignore"] and
-               (PYB11attrs(meth)["virtual"] or PYB11attrs(meth)["pure_virtual"])]
-    for mname, meth in methods:
-        methattrs = PYB11attrs(meth)
-        methattrs["returnType"] = eval("klassinst." + mname + "()")
-        assert methattrs["returnType"]    # We require the full spec for virtual methods
-        ss("  virtual %(returnType)s %(cppname)s(" % methattrs)
+    # Bind the (unique) virtual methods for all classes up the inheritance tree.
+    boundMethods = []
+    for bklass in inspect.getmro(klass):
+        bklassinst = bklass()
+        bklassattrs = PYB11attrs(bklass)
+        methods = [(mname, meth) for (mname, meth) in PYB11ClassMethods(bklass)
+                   if not PYB11attrs(meth)["ignore"] and
+                   (PYB11attrs(meth)["virtual"] or PYB11attrs(meth)["pure_virtual"])]
+        for mname, meth in methods:
+            
+            # We build this method string up independent of the output stream
+            # until we determine if it's already been generated.
+            fs = StringIO.StringIO()
+            ss = fs.write
 
-        # Fill out the argument list for this method
-        args = PYB11parseArgs(meth)
-        for i, (argType, argName, default) in enumerate(args):
-            ss(argType)
-            if i < len(args) - 1:
-                ss(", ")
-        if methattrs["const"]:
-            ss(") const override { ")
-        else:
-            ss(") override { ")
+            methattrs = PYB11attrs(meth)
+            methattrs["returnType"] = eval("bklassinst." + mname + "()")
+            assert methattrs["returnType"]    # We require the full spec for virtual methods
+            ss("  virtual %(returnType)s %(cppname)s(" % methattrs)
 
-        if methattrs["pure_virtual"]:
-            ss("    PYBIND11_OVERLOAD_PURE(%s, " % methattrs["returnType"])
-        else:
-            ss("    PYBIND11_OVERLOAD(%s, " % methattrs["returnType"])
-        ss(" %(namespace)s%(cppname)s," % klassattrs)
-        if len(args) > 0:
-            ss(" %(cppname)s, " % methattrs)
-        else:
-            ss(" %(cppname)s);" % methattrs)
-
-        for i, (argType, argName, default) in enumerate(args):
-            if i < nargs - 1:
-                ss(argName + ", ")
+            # Fill out the argument list for this method
+            args = PYB11parseArgs(meth)
+            for i, (argType, argName, default) in enumerate(args):
+                ss(argType)
+                if i < len(args) - 1:
+                    ss(", ")
+            if methattrs["const"]:
+                ss(") const override { ")
             else:
-                ss(argName + ");")
-        ss(" }\n")
+                ss(") override { ")
+
+            if methattrs["pure_virtual"]:
+                ss("PYBIND11_OVERLOAD_PURE(%s, " % methattrs["returnType"])
+            else:
+                ss("PYBIND11_OVERLOAD(%s, " % methattrs["returnType"])
+            ss(" %(namespace)s%(cppname)s," % klassattrs)
+            if len(args) > 0:
+                ss(" %(cppname)s, " % methattrs)
+            else:
+                ss(" %(cppname)s);" % methattrs)
+
+            for i, (argType, argName, default) in enumerate(args):
+                if i < nargs - 1:
+                    ss(argName + ", ")
+                else:
+                    ss(argName + ");")
+            ss(" }\n")
+
+            # Is this a new binding?
+            if not fs.getvalue() in boundMethods:
+                ssout(fs.getvalue())
+                boundMethods.append(fs.getvalue())
+            fs.close()
 
     # Closing
-    PYB11generateTrampolineClassEnd(klass, klassattrs, ss)
-    return
-
-#-------------------------------------------------------------------------------
-# generateBindingFunction
-#
-# Generate a function that provides pybind11 bindings for the virtual methods.
-#-------------------------------------------------------------------------------
-def PYB11generateBindingFunction(obj):
-    ss = sys.stdout.write
-    name = obj.__class__.__name__
-
-    # Compiler guard.
-    ss("""//------------------------------------------------------------------------------
-// Pybind11 binding for virtual methods in %(name)s
-//------------------------------------------------------------------------------
-#ifndef __pybind11Bindings_%(name)s__
-#define __pybing11Bindings_%(name)s__
-
-""" % {"name" : name})
-
-    # Includes
-    for inc in obj.includes:
-        ss('#include "%s"\n' % inc)
-    ss("\n")
-
-    # Preamble
-    if obj.preamble:
-        ss(obj.preamble + "\n")
-
-    # Namespaces
-    for ns in obj.namespaces:
-        ss("namespace " + ns + " {\n")
-    ss("\n")
-
-    # Template parameters
-    ss("template<")
-    for tp in obj.templates:
-        ss("typename %s, " % tp)
-    ss("typename Obj, typename PB11Obj>\n")
-
-    # Function spec
-    ss("void virtual%sBindings(PB11Obj& obj) {\n\n" % name)
-
-    # typedefs
-    if "Dimension" in obj.templates:
-        ss("""
-  typedef typename Dimension::Scalar Scalar;
-  typedef typename Dimension::Vector Vector;
-  typedef typename Dimension::Tensor Tensor;
-  typedef typename Dimension::SymTensor SymTensor;
-  typedef typename Dimension::ThirdRankTensor ThirdRankTensor;
-
-""");
-
-    # Bind methods.
-    ss("  // Methods\n")
-    methods = [(name, meth) for (name, meth) in inspect.getmembers(obj, predicate=inspect.ismethod)
-               if name[:2] != "__"]
-    for name, method in methods:
-
-        # Get the return type and arguments.
-        returnType = method()
-        stuff = inspect.getargspec(method)
-        if "args" in stuff.args:
-            args = stuff.defaults[stuff.args.index("args") - 1]
-        else:
-            args = []
-        nargs = len(args)
-
-        # Is this method const?
-        if "const" in stuff.args:
-            const = stuff.defaults[stuff.args.index("const") - 1]
-        else:
-            const = False
-
-        # Is there a doc string?
-        if "doc" in stuff.args:
-            doc = stuff.defaults[stuff.args.index("doc") - 1]
-        else:
-            doc = None
-
-        # Because python does not have function overloading, we provide the ability
-        # to rename the c++ method.
-        if "name" in stuff.args:
-            name = stuff.defaults[stuff.args.index("name") - 1]
-
-        # Write the binding
-        dvals = {"name" : name, "returnType" : returnType}
-        ss('  obj.def("%(name)s", (%(returnType)s (Obj::*)(' % dvals)
-        for i, (argType, argName, default) in enumerate(__parseArgs(args)):
-            ss(argType)
-            if i < nargs - 1:
-                ss(", ")
-        if const:
-            ss(") const)")
-        else:
-            ss("))")
-        ss(" &Obj::" + name)
-        for argType, argName, default in __parseArgs(args):
-            ss(', "%s"_a' % argName)
-            if default:
-                ss("=" + default)
-        if doc:
-            ss(',\n          "%s"' % doc)
-        ss(");\n")
-
-    # Closing
-    ss("}\n\n")
-    for ns in obj.namespaces:
-        ss("}\n")
-    ss("#endif\n")
-    
+    PYB11generateTrampolineClassEnd(klass, klassattrs, ssout)
     return
 
 #-------------------------------------------------------------------------------
@@ -197,7 +101,7 @@ class PYB11Trampoline%(cppname)s: public %(cppname)s {
 public:
   using %(cppname)s::%(cppname)s;   // inherit constructors
 
- """ % klassattrs)
+""" % klassattrs)
 
     return
 
--- src/Pybind11Wraps/testPYB11/testInheritance.py
+++ src/Pybind11Wraps/testPYB11/testInheritance.py
@@ -19,6 +19,8 @@ namespace Aspace {
     A() { std::cerr << "A()" << std::endl; }
     virtual ~A() { std::cerr << "~A()" << std::endl; }
     virtual void do_something() const { std::cerr << "A::do_something" << std::endl; }
+    virtual void do_something_else() const { std::cerr << "A::do_something_else" << std::endl; }
+    virtual int yet_another_method() const { std::cerr << "A::yet_another_method" << std::endl; return 42; }
   };
 }
 
@@ -46,6 +48,18 @@ class A:
     def do_something(self):
         "A virtual do_something method."
         return "void"
+
+    @PYB11virtual
+    @PYB11const
+    def do_something_else(self):
+        "A virtual do_something_else method."
+        return "void"
+
+    @PYB11virtual
+    @PYB11const
+    def yet_another_method(self):
+        "A virtual yet_another_method."
+        return "int"
 
 #-------------------------------------------------------------------------------
 # B
