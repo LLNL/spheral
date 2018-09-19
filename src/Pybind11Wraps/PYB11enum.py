ac6d79f169291b6d9a8b938035b5af361580c49f
Mike Owen <mikeowen@llnl.gov>
2018-09-19 08:41:54 -0700

    Working on enum.

--- src/Pybind11Wraps/PYB11enum.py
+++ src/Pybind11Wraps/PYB11enum.py
@@ -11,7 +11,7 @@ def PYB11generateModuleEnums(modobj, ss):
     enums = [x for x in dir(modobj) if isinstance(eval("modobj.%s" % x), PYB11enumType)]
     for name in enums:
         inst = eval("modobj.%s" % name)
-        inst(ss)
+        inst(modobj, ss)
     return
 
 #-------------------------------------------------------------------------------
@@ -21,33 +21,50 @@ class PYB11enumType:
 
     def __init__(self,
                  values,
-                 export_values = False):
+                 name,
+                 namespace = "",
+                 cppname = None,
+                 export_values = False,
+                 doc = None):
         self.values = values
+        self.__name__ = name
+        self.namespace = namespace
+        if self.namespace and self.namespace[:-2] != "::":
+            self.namespace += "::"
+        if cppname:
+            self.cppname = cppname
+        else:
+            self.cppname = name
         self.export_values = export_values
-        print type(self).__name__
+        self.doc = doc
         return
 
     def __call__(self,
-                 ssout,
+                 modobj,
+                 ss,
                  klass = None):
-        print dir(self)
-        print dir(self.__class__)
-        print type(self).__name__
         enumattrs = PYB11attrs(self)
+        enumattrs["namespace"] = self.namespace
+        enumattrs["cppname"] = self.cppname
         if klass:
             klassattrs = PYB11attrs(klass)
 
         ss("  py::enum_<%(namespace)s%(cppname)s>(" % enumattrs)
         if klass:
-            ss('%(pyname)s, ' % klassattrs + '"%(pyname)s")\n' % enumattrs)
+            ss('%(pyname)s, ' % klassattrs + '"%(pyname)s"' % enumattrs)
         else:
-            ss('"%(pyname)s")\n' % enumattrs)
+            ss('m, "%(pyname)s"' % enumattrs)
+
+        if self.doc:
+            ss(', "%s")\n' % self.doc)
+        else:
+            ss(')\n')
 
         for value in self.values:
             ss('    .value("%s", ' % value)
             if klass:
                 ss('%(namespace)s%(cppname)s::' % klassattrs)
-            ss('%(cppname)s)\n' % enumattrs)
+            ss('%(namespace)s%(cppname)s::' % enumattrs + value + ')\n')
 
         if self.export_values:
             ss('    .export_values();\n')
---
+++ src/Pybind11Wraps/testPYB11/testEnums.py
@@ -0,0 +1,64 @@
+"""
+This is a test module for enum binding in PYB11.
+"""
+
+from PYB11Decorators import *
+from PYB11STLmethods import *
+from PYB11property import *
+from PYB11class import *
+from PYB11function import *
+from PYB11enum import *
+
+# List the files we want to include.
+includes = ['<iostream>']
+
+# We can specify arbitrary C++ to be inserted at the beginning of the file.
+preamble = """
+namespace Aspace {
+  class A {
+  public:
+    A() { std::cerr << "A()" << std::endl; }
+    virtual ~A() { std::cerr << "~A()" << std::endl; }
+    virtual void do_something() const { std::cerr << "A::do_something" << std::endl; }
+    virtual void do_something_else() const { std::cerr << "A::do_something_else" << std::endl; }
+    virtual int yet_another_method() const { std::cerr << "A::yet_another_method" << std::endl; return 42; }
+  };
+
+  enum class Color { black, white, red, blue, yellow };
+}
+
+"""
+
+#-------------------------------------------------------------------------------
+# A
+#-------------------------------------------------------------------------------
+@PYB11namespace("Aspace")
+class A:
+
+    def pyinit(self):
+        "Default constructor."
+
+    @PYB11virtual
+    @PYB11const
+    def do_something(self):
+        "A virtual do_something method."
+        return "void"
+
+    @PYB11virtual
+    @PYB11const
+    def do_something_else(self):
+        "A virtual do_something_else method."
+        return "void"
+
+    @PYB11virtual
+    @PYB11const
+    def yet_another_method(self):
+        "A virtual yet_another_method."
+        return "int"
+
+#-------------------------------------------------------------------------------
+# Color
+#-------------------------------------------------------------------------------
+Color = PYB11enumType(("black", "white", "red", "blue", "yellow"),
+                      name="Color",
+                      namespace="Aspace")
