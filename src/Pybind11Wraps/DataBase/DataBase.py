5fc3b7cd692593a4e47e1a8d72ccf913e1305206
Mike Owen <mikeowen@llnl.gov>
2018-09-26 22:20:51 -0700

    Checkpoint on DataBase.

--- src/Pybind11Wraps/DataBase/DataBase.py
+++ src/Pybind11Wraps/DataBase/DataBase.py
@@ -50,6 +50,75 @@ class DataBase:
         "Get the connectivity map as a std::shared_ptr, optionally including ghost connectivity"
         return "ConnectivityMapPtr"
 
+    def appendNodeList(self, nodeList="SolidNodeList<%(Dimension)s>&"):
+        "Add a SolidNodeList"
+        return "void"
+
+    @PYB11pycppname("appendNodeList")
+    def appendNodeList1(self, nodeList="FluidNodeList<%(Dimension)s>&"):
+        "Add a FluidNodeList"
+        return "void"
+
+    @PYB11pycppname("appendNodeList")
+    def appendNodeList2(self, nodeList="NodeList<%(Dimension)s>&"):
+        "Add a NodeList"
+        return "void"
+
+    def deleteNodeList(self, nodeList="SolidNodeList<%(Dimension)s>&"):
+        "Remove a SolidNodeList"
+        return "void"
+
+    @PYB11pycppname("deleteNodeList")
+    def deleteNodeList1(self, nodeList="FluidNodeList<%(Dimension)s>&"):
+        "Remove a FluidNodeList"
+        return "void"
+
+    @PYB11pycppname("deleteNodeList")
+    def deleteNodeList2(self, nodeList="NodeList<%(Dimension)s>&"):
+        "Remove a NodeList"
+        return "void"
+
+    @PYB11const
+    def haveNodeList(self, nodeList="NodeList<%(Dimension)s>&"):
+        "Check if a NodeList is in the DataBase"
+        return "bool"
+
+    @PYB11const
+    def setMasterNodeLists(self,
+                           position = "const Vector&",
+                           H = "const SymTensor&",
+                           masterLists = "std::vector<std::vector<int>>&",
+                           coarseNeighbors = "std::vector<std::vector<int>>&"):
+        "Set the master/coarse neighbor lists for all NodeLists"
+        return "void"
+
+    @PYB11const
+    def setMasterFluidNodeLists(self,
+                                position = "const Vector&",
+                                H = "const SymTensor&",
+                                masterLists = "std::vector<std::vector<int>>&",
+                                coarseNeighbors = "std::vector<std::vector<int>>&"):
+        "Set the master/coarse neighbor lists for all NodeLists"
+        return "void"
+
+    @PYB11const
+    def setRefineNodeLists(self,
+                           position = "const Vector&",
+                           H = "const SymTensor&",
+                           coarseNeighbors = "const std::vector<std::vector<int>>&",
+                           refineNeighbors = "std::vector<std::vector<int>>&"):
+        "Set the refine neighbor lists for all NodeLists"
+        return "void"
+
+    @PYB11const
+    def setRefineFluidNodeLists(self,
+                                position = "const Vector&",
+                                H = "const SymTensor&",
+                                coarseNeighbors = "const std::vector<std::vector<int>>&",
+                                refineNeighbors = "std::vector<std::vector<int>>&"):
+        "Set the refine neighbor lists for all FluidNodeLists"
+        return "void"
+
     #...........................................................................
     # Properties
     numNodeLists = PYB11property("int", "numNodeLists", doc="Number of NodeLists in DataBase")
@@ -63,3 +132,7 @@ class DataBase:
     globalNumNodes = PYB11property("int", "globalNumNodes", doc="Number of nodes in all NodeLists in DataBase across all processors")
     globalNumInternalNodes = PYB11property("int", "globalNumInternalNodes", doc="Number of internal nodes in all NodeLists in DataBase across all processors")
     globalNumGhostNodes = PYB11property("int", "globalNumGhostNodes", doc="Number of ghost nodes in all NodeLists in DataBase across all processors")
+
+    nodeListPtrs = PYB11property("const std::vector<NodeList<%(Dimension)s>*>&", "nodeListPtrs", doc="The set of NodeLists in the DataBase")
+    fluidNodeListPtrs = PYB11property("const std::vector<FluidNodeList<%(Dimension)s>*>&", "fluidNodeListPtrs", doc="The set of FluidNodeLists in the DataBase")
+    solidNodeListPtrs = PYB11property("const std::vector<SolidNodeList<%(Dimension)s>*>&", "solidNodeListPtrs", doc="The set of SolidNodeLists in the DataBase")
