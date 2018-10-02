abb05f2552d1fa78d06c88d5f0ff933ad948b365
Mike Owen <mikeowen@llnl.gov>
2018-10-02 16:52:50 -0700

    Checkpoint -- starting Gravity.

---
+++ src/Pybind11Wraps/Gravity/GravityMOD.py
@@ -0,0 +1,42 @@
+"""
+Spheral Gravity module.
+
+Provides the implementations of gravitational physics packages in Spheral.
+"""
+
+from PYB11Generator import *
+from spheralDimensions import *
+dims = spheralDimensions()
+
+#-------------------------------------------------------------------------------
+# Includes
+#-------------------------------------------------------------------------------
+includes = ['"Geometry/Dimension.hh"',
+            '"Physics/GenericBodyForce.hh"',
+            '"Gravity/NBodyGravity.hh"',
+            '"Gravity/TreeGravity.hh"',
+            '<vector>',
+            '<string>',
+            '<iterator>']
+
+#-------------------------------------------------------------------------------
+# Namespaces
+#-------------------------------------------------------------------------------
+namespaces = ["Spheral"]
+
+#-------------------------------------------------------------------------------
+# Enums
+#-------------------------------------------------------------------------------
+GravityTimeStepType = PYB11enum(("AccelerationRatio", "DynamicalTime"), export_values=True,
+                                doc="Algorithm choice for choosing timesteps for gravity packages")
+
+#-------------------------------------------------------------------------------
+# Instantiate our types
+#-------------------------------------------------------------------------------
+from NBodyGravity import *
+
+for ndim in dims:
+    exec('''
+NBodyGravity%(ndim)id = PYB11TemplateClass(NBodyGravity, template_parameters="%(Dimension)s")
+''' % {"ndim"      : ndim,
+       "Dimension" : "Dim<" + str(ndim) + ">"})
