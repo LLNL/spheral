b847db257cb4c9ab762000839cf241b172157335
Mike Owen <mikeowen@llnl.gov>
2018-10-02 17:29:16 -0700

    Checkpoint.

--- src/Pybind11Wraps/Gravity/GravityMOD.py
+++ src/Pybind11Wraps/Gravity/GravityMOD.py
@@ -28,15 +28,17 @@ namespaces = ["Spheral"]
 # Enums
 #-------------------------------------------------------------------------------
 GravityTimeStepType = PYB11enum(("AccelerationRatio", "DynamicalTime"), export_values=True,
-                                doc="Algorithm choice for choosing timesteps for gravity packages")
+                                doc="Algorithm choice for choosing timesteps for TreeGravity")
 
 #-------------------------------------------------------------------------------
 # Instantiate our types
 #-------------------------------------------------------------------------------
 from NBodyGravity import *
+from TreeGravity import *
 
 for ndim in dims:
     exec('''
 NBodyGravity%(ndim)id = PYB11TemplateClass(NBodyGravity, template_parameters="%(Dimension)s")
+TreeGravity%(ndim)id = PYB11TemplateClass(TreeGravity, template_parameters="%(Dimension)s")
 ''' % {"ndim"      : ndim,
        "Dimension" : "Dim<" + str(ndim) + ">"})
---
+++ src/Pybind11Wraps/Gravity/TreeGravity.py
@@ -0,0 +1,111 @@
+#-------------------------------------------------------------------------------
+# TreeGravity base class
+#-------------------------------------------------------------------------------
+from PYB11Generator import *
+from GenericBodyForce import *
+
+@PYB11template("Dimension")
+class TreeGravity(GenericBodyForce):
+
+    typedefs = """
+    typedef %(Dimension)s DIM;
+    typedef typename DIM::Scalar Scalar;
+    typedef typename DIM::Vector Vector;
+    typedef typename DIM::Tensor Tensor;
+    typedef typename DIM::SymTensor SymTensor;
+    typedef typename DIM::ThirdRankTensor ThirdRankTensor;
+    typedef typename Physics<DIM>::TimeStepType TimeStepType;
+"""
+
+    #...........................................................................
+    # Constructors
+    def pyinit(self,
+               G = "const double",
+               plummerSofteningLength = "const double",
+               opening = "const double",
+               ftimestep = "const double",
+               timeStepChoice = "GravityTimeStepType"):
+        "TreeGravity constructor"
+
+    #...........................................................................
+    # Virtual methods
+    @PYB11virtual
+    def registerState(self,
+                      dataBase = "DataBase<DIM>&",
+                      state = "State<DIM>&"):
+        "Register the state you want carried around (and potentially evolved), as well as the policies for such evolution."
+        return "void"
+
+    @PYB11virtual
+    @PYB11const
+    def evaluateDerivatives(self,
+                            time = "const Scalar",
+                            dt = "const Scalar",
+                            dataBase = "const DataBase<DIM>&",
+                            state = "const State<DIM>&",
+                            derivs = "StateDerivatives<DIM>&"):
+        "Increment the derivatives."
+        return "void"
+
+    @PYB11virtual
+    @PYB11const
+    def dt(dataBase = "const DataBase<DIM>&", 
+           state = "const State<DIM>&",
+           derivs = "const StateDerivatives<DIM>&",
+           currentTime = "const Scalar"):
+        "Vote on a time step."
+        return "TimeStepType"
+
+    @PYB11virtual
+    def initializeProblemStartup(self,
+                                 dataBase = "DataBase<DIM>&"):
+        "An optional hook to initialize once when the problem is starting up."
+        return "void"
+
+    @PYB11virtual
+    def preStepInitialize(self,
+                          dataBase = "const DataBase<DIM>&", 
+                          state = "State<DIM>&",
+                          derivs = "StateDerivatives<DIM>&"):
+        "Optional hook to be called at the beginning of a time step."
+        return "void"
+
+    @PYB11virtual
+    def finalize(self,
+                 time = "const Scalar", 
+                 dt = "const Scalar",
+                 dataBase = "DataBase<DIM>&", 
+                 state = "State<DIM>&",
+                 derivs = "StateDerivatives<DIM>&"):
+        "Similarly packages might want a hook to do some post-step finalizations.  Really we should rename this post-step finalize."
+        return "void"
+
+    @PYB11virtual
+    @PYB11const
+    def label(self):
+        "It's useful to have labels for Physics packages.  We'll require this to have the same signature as the restart label."
+        return "std::string"
+
+    @PYB11virtual
+    @PYB11const
+    def requireConnectivity(self):
+        "Some physics does not require the connectivity be constructed."
+        return "bool"
+
+    @PYB11virtual
+    @PYB11const
+    def extraEnergy(self):
+        "Many physics packages will have their own representations of energy in the system (gravitational potential energy, radiative losses, etc.)"
+        return "Scalar"
+
+    @PYB11virtual
+    @PYB11const
+    def valid(self):
+        return "bool"
+
+    #...........................................................................
+    # Properties
+    potential = PYB11property("const FieldList<DIM, Scalar>&", "potential", returnpolicy="reference_internal", doc="The last computed potential")
+    G = PYB11property("double", "G", doc="The gravitational constant")
+    softeningLength = PYB11property("Scalar", "softeningLength", "softeningLength", doc="The Plummer softening scale")
+    compatibleVelocityUpdate = PYB11property("bool", "compatibleVelocityUpdate", "compatibleVelocityUpdate", doc="Experimental compatible update for velocity")
