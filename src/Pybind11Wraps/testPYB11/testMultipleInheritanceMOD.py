7d5883589ef7c6f3e0161d6710bc662d3426ae9f
Mike Owen <mikeowen@llnl.gov>
2018-10-13 14:28:46 -0700

    Extending multiple inheritance test a bit.

--- src/Pybind11Wraps/testPYB11/testMultipleInheritanceMOD.py
+++ src/Pybind11Wraps/testPYB11/testMultipleInheritanceMOD.py
@@ -8,28 +8,32 @@ public:
   Base()                             { printf("Base()\\n"); }
   virtual ~Base()                    {}
   virtual void funcBase() const      { printf("Base.funcBase()\\n"); }
+  virtual void funcBaseOnly() const  { printf("Base.funcBaseOnly()\\n"); }
 };
 
 class A: public Base {
 public:
-  A()                                { printf("A()\\n"); }
-  virtual ~A()                       {}
-  virtual void funcA(int x) const    { printf("A.funcA(%d)\\n", x); }
+  A()                                 { printf("A()\\n"); }
+  virtual ~A()                        {}
+  virtual void funcA(int x) const     { printf("A.funcA(%d)\\n", x); }
+  virtual void funcAonly(int x) const { printf("A.funcAonly(%d)\\n", x); }
 };
 
 class B {
 public:
-  B()                                { printf("B()\\n"); }
-  virtual ~B()                       {}
-  virtual void funcB(double x) const { printf("B.funcB(%e)\\n", x); }
+  B()                                    { printf("B()\\n"); }
+  virtual ~B()                           {}
+  virtual void funcB(double x) const     { printf("B.funcB(%e)\\n", x); }
+  virtual void funcBonly(double x) const { printf("B.funcBonly(%e)\\n", x); }
 };
 
 class C: public A, public B {
 public:
-  C()                                { printf("C()"); }
-  virtual ~C()                       {}
-  virtual void funcA(int x) const    { printf("C.funcA(%d)\\n", -x); }
-  virtual void funcB(double x) const { printf("C.funcB(%e)\\n", -x); }
+  C()                                    { printf("C()"); }
+  virtual ~C()                           {}
+  virtual void funcA(int x) const        { printf("C.funcA(%d)\\n", -x); }
+  virtual void funcB(double x) const     { printf("C.funcB(%e)\\n", -x); }
+  virtual void funcConly(double x) const { printf("C.funcConly(%e)\\n", -x); }
 };
 """
 
@@ -43,6 +47,11 @@ class Base:
     def funcBase(self):
         return "void"
 
+    @PYB11virtual
+    @PYB11const
+    def funcBaseOnly(self):
+        return "void"
+
 #-------------------------------------------------------------------------------
 class A(Base):
     def pyinit(self):
@@ -53,6 +62,11 @@ class A(Base):
     def funcA(self, x="int"):
         return "void"
 
+    @PYB11virtual
+    @PYB11const
+    def funcAonly(self, x="int"):
+        return "void"
+
 #-------------------------------------------------------------------------------
 class B:
     def pyinit(self):
@@ -61,6 +75,11 @@ class B:
     @PYB11virtual
     @PYB11const
     def funcB(self, x="double"):
+        return "void"
+
+    @PYB11virtual
+    @PYB11const
+    def funcBonly(self, x="double"):
         return "void"
 
 #-------------------------------------------------------------------------------
