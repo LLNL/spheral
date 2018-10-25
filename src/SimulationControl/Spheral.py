e76cc7d24775786d56be96450d3933d4756ede70
Mike Owen <mikeowen@llnl.gov>
2018-10-24 21:50:30 -0700

    Adding PolyClipper to Spheral import.

--- src/SimulationControl/Spheral.py
+++ src/SimulationControl/Spheral.py
@@ -107,6 +107,11 @@ from SpheralController import *
 from SpheralOptionParser import commandLine
 
 # ------------------------------------------------------------------------------
+# Import PolyClipper
+# ------------------------------------------------------------------------------
+import SpheralPolyClipper as PolyClipper
+
+# ------------------------------------------------------------------------------
 # See if we can import the polytope bindings.
 # ------------------------------------------------------------------------------
 try:
