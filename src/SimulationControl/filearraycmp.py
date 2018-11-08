b10a677f302955e77f68c8966aff16ff59306023
Mike Owen <mikeowen@llnl.gov>
2018-11-07 16:19:42 -0800

    Changing the collisionless gravitating sphere collapse problem to use a profile fit to the gravitational potential rather than file reference data for pass/fail.

--- src/SimulationControl/filearraycmp.py
+++ src/SimulationControl/filearraycmp.py
@@ -8,5 +8,5 @@ def filearraycmp(filename1, filename2,
                  atol = 1.0e-8):
     array1 = np.loadtxt(filename1)
     array2 = np.loadtxt(filename2)
-    diff = np.absolute(array1 - array2)/np.clip(atol, 1e100, 0.5*(np.absolute(array1) + np.absolute(array2)))
+    #diff = np.absolute(array1 - array2)/np.clip(0.5*(np.absolute(array1) + np.absolute(array2)), atol, 1e100)
     return (np.absolute(array1 - array2) <= atol + rtol*0.5*(np.absolute(array1) + np.absolute(array2))).min()
