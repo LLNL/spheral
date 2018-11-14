127dd5d76842409322013c169d352a068ca666ca
Mike Owen <mikeowen@llnl.gov>
2018-11-14 14:10:03 -0800

    Restored basic restart backward-compatibility with pre-pybind11.

--- src/Pybind11Wraps/SpheralPickle.py
+++ src/Pybind11Wraps/SpheralPickle.py
@@ -9,71 +9,119 @@ from SpheralCompiledPackages import *
 #-------------------------------------------------------------------------------
 # Vector1d
 #-------------------------------------------------------------------------------
+def construct_Vector1d(x):
+    return Vector1d(x)
+
 def reduce_Vector1d(obj):
-    return Vector1d, (obj.x,)
-copy_reg.pickle(Vector1d, reduce_Vector1d)
+    return construct_Vector1d, (obj.x,)
+
+copy_reg.pickle(type(Vector1d()), reduce_Vector1d, construct_Vector1d)
 
 #-------------------------------------------------------------------------------
 # Vector2d
 #-------------------------------------------------------------------------------
+def construct_Vector2d(x, y):
+    return Vector2d(x, y)
+
 def reduce_Vector2d(obj):
-    return Vector2d, (obj.x, obj.y)
-copy_reg.pickle(Vector2d, reduce_Vector2d)
+    return construct_Vector2d, (obj.x, obj.y)
+
+copy_reg.pickle(type(Vector2d()), reduce_Vector2d, construct_Vector2d)
 
 #-------------------------------------------------------------------------------
 # Vector3d
 #-------------------------------------------------------------------------------
+def construct_Vector3d(x, y, z):
+    return Vector3d(x, y, z)
+
 def reduce_Vector3d(obj):
-    return Vector3d, (obj.x, obj.y, obj.z)
-copy_reg.pickle(Vector3d, reduce_Vector3d)
+    return construct_Vector3d, (obj.x, obj.y, obj.z)
+
+copy_reg.pickle(type(Vector3d()), reduce_Vector3d, construct_Vector3d)
 
 #-------------------------------------------------------------------------------
 # Tensor1d
 #-------------------------------------------------------------------------------
+def construct_Tensor1d(xx):
+    return Tensor1d(xx)
+
 def reduce_Tensor1d(obj):
-    return Tensor1d, (obj.xx,)
-copy_reg.pickle(Tensor1d, reduce_Tensor1d)
+    return construct_Tensor1d, (obj.xx,)
+
+copy_reg.pickle(type(Tensor1d()), reduce_Tensor1d, construct_Tensor1d)
 
 #-------------------------------------------------------------------------------
 # Tensor2d
 #-------------------------------------------------------------------------------
+def construct_Tensor2d(xx, xy,
+                       yx, yy):
+    return Tensor2d(xx, xy,
+                    yx, yy)
+
 def reduce_Tensor2d(obj):
-    return Tensor2d, (obj.xx, obj.xy,
-                      obj.yx, obj.yy)
-copy_reg.pickle(Tensor2d, reduce_Tensor2d)
+    return construct_Tensor2d, (obj.xx, obj.xy,
+                                obj.yx, obj.yy)
+
+copy_reg.pickle(type(Tensor2d()), reduce_Tensor2d, construct_Tensor2d)
 
 #-------------------------------------------------------------------------------
 # Tensor3d
 #-------------------------------------------------------------------------------
+def construct_Tensor3d(xx, xy, xz,
+                       yx, yy, yz,
+                       zx, zy, zz):
+    return Tensor3d(xx, xy, xz,
+                    yx, yy, yz,
+                    zx, zy, zz)
+
 def reduce_Tensor3d(obj):
-    return Tensor3d, (obj.xx, obj.xy, obj.xz,
-                      obj.yx, obj.yy, obj.yz,
-                      obj.zx, obj.zy, obj.zz)
-copy_reg.pickle(Tensor3d, reduce_Tensor3d)
+    return construct_Tensor3d, (obj.xx, obj.xy, obj.xz,
+                                obj.yx, obj.yy, obj.yz,
+                                obj.zx, obj.zy, obj.zz)
+
+copy_reg.pickle(type(Tensor3d()), reduce_Tensor3d, construct_Tensor3d)
 
 #-------------------------------------------------------------------------------
 # SymTensor1d
 #-------------------------------------------------------------------------------
+def construct_SymTensor1d(xx):
+    return SymTensor1d(xx)
+
 def reduce_SymTensor1d(obj):
-    return SymTensor1d, (obj.xx,)
-copy_reg.pickle(SymTensor1d, reduce_SymTensor1d)
+    return construct_SymTensor1d, (obj.xx,)
+
+copy_reg.pickle(type(SymTensor1d()), reduce_SymTensor1d, construct_SymTensor1d)
 
 #-------------------------------------------------------------------------------
 # SymTensor2d
 #-------------------------------------------------------------------------------
+def construct_SymTensor2d(xx, xy,
+                          yx, yy):
+    return SymTensor2d(xx, xy,
+                       yx, yy)
+
 def reduce_SymTensor2d(obj):
-    return SymTensor2d, (obj.xx, obj.xy,
-                         obj.yx, obj.yy)
-copy_reg.pickle(SymTensor2d, reduce_SymTensor2d)
+    return construct_SymTensor2d, (obj.xx, obj.xy,
+                                   obj.yx, obj.yy)
+
+copy_reg.pickle(type(SymTensor2d()), reduce_SymTensor2d, construct_SymTensor2d)
 
 #-------------------------------------------------------------------------------
 # SymTensor3d
 #-------------------------------------------------------------------------------
+def construct_SymTensor3d(xx, xy, xz,
+                          yx, yy, yz,
+                          zx, zy, zz):
+    return SymTensor3d(xx, xy, xz,
+                       yx, yy, yz,
+                       zx, zy, zz)
+
 def reduce_SymTensor3d(obj):
-    return SymTensor3d, (obj.xx, obj.xy, obj.xz,
-                         obj.yx, obj.yy, obj.yz,
-                         obj.zx, obj.zy, obj.zz)
-copy_reg.pickle(SymTensor3d, reduce_SymTensor3d)
+    return construct_SymTensor3d, (obj.xx, obj.xy, obj.xz,
+                                   obj.yx, obj.yy, obj.yz,
+                                   obj.zx, obj.zy, obj.zz)
+
+copy_reg.pickle(type(SymTensor3d()), reduce_SymTensor3d, construct_SymTensor3d)
 
 #-------------------------------------------------------------------------------
 # ThirdRankTensor1d
