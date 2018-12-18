84b287aad17ae00d40526b4150118d62b2ff4a22
Mike Owen <mikeowen@llnl.gov>
2018-12-17 22:05:45 -0800

    Splitting out STL bindings from Field module to reduce compilation memory usage.

--- src/Pybind11Wraps/Field/FieldMOD.py
+++ src/Pybind11Wraps/Field/FieldMOD.py
@@ -92,23 +92,3 @@ FieldListSet%(ndim)sd = PYB11TemplateClass(FieldListSet, template_parameters="Di
 ''' % {"ndim" : ndim,
        "value" : value,
        "label" : label})
-
-    #...........................................................................
-    # STL collections of Field types
-    for value, label in (("int", "Int"),
-                         ("double", "Scalar"),
-                         ("Dim<%i>::Vector" % ndim, "Vector"),
-                         ("Dim<%i>::Tensor" % ndim, "Tensor"),
-                         ("Dim<%i>::SymTensor" % ndim, "SymTensor")):
-        exec('''
-vector_of_%(label)sField%(ndim)id = PYB11_bind_vector("Field<%(Dimension)s, %(value)s>", opaque=True, local=False)
-vector_of_%(label)sFieldPtr%(ndim)id = PYB11_bind_vector("Field<%(Dimension)s, %(value)s>*", opaque=True, local=False)
-vector_of_vector_of_%(label)sField%(ndim)id = PYB11_bind_vector("std::vector<Field<%(Dimension)s, %(value)s>>", opaque=True, local=False)
-
-vector_of_%(label)sFieldList%(ndim)id = PYB11_bind_vector("FieldList<%(Dimension)s, %(value)s>", opaque=True, local=False)
-vector_of_%(label)sFieldListPtr%(ndim)id = PYB11_bind_vector("FieldList<%(Dimension)s, %(value)s>*", opaque=True, local=False)
-vector_of_vector_of_%(label)sFieldList%(ndim)id = PYB11_bind_vector("std::vector<FieldList<%(Dimension)s, %(value)s>>", opaque=True, local=False)
-''' % {"ndim": ndim,
-       "Dimension" : "Dim<%i>" % ndim,
-       "value" : value,
-       "label" : label})
