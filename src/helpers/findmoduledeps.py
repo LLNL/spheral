bb6ad82c7e3eac1cb05c58252af9ae73597a2381
Mike Owen <mikeowen@llnl.gov>
2018-12-19 10:31:47 -0800

    Checkpoint.

--- src/helpers/findmoduledeps.py
+++ src/helpers/findmoduledeps.py
@@ -2,9 +2,6 @@
 # For the given script, find the files it depends on from imports
 #-------------------------------------------------------------------------------
 import os, sys, modulefinder
-#from pydeps.pydeps import externals
-
-print sys.argv
 
 # Parse input for the file we're inspecting and any paths we want to exclude
 if len(sys.argv) == 2:
@@ -19,21 +16,19 @@ else:
 result = []
 def addModFile(mod):
     mfile = mod.__file__.replace(".pyc", ".py")
-    print " --> ", mod, mfile
-    if mfile:
+    if mfile and (not mfile in result) and (os.path.splitext(mfile)[1] == ".py"):
         ok = not max([False] + [mfile.startswith(x) for x in exclude_paths])
         if ok:
             result.append(mfile)
             f = modulefinder.ModuleFinder()
             f.run_script(mfile)
             for othername, othermod in f.modules.iteritems():
-                #print "Check : ", othermod.__file__
-                if othermod.__file__ and not max([False] + [othermod.__file__.startswith(x) for x in exclude_paths]):
+                if othermod.__file__ != mfile and othermod.__file__ and not max([False] + [othermod.__file__.startswith(x) for x in exclude_paths]):
                     addModFile(othermod)
 
 # Do the deed
-print " **> ", os.path.splitext(modfile)[0]
 exec("import %s as mod" % os.path.splitext(modfile)[0])
 addModFile(mod)
 
-print result
+for x in result:
+    print x
