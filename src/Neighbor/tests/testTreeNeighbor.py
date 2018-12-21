1ee1f5f9686b57b8cbf6a287e065260d3d720d4d
Mike Owen <mikeowen@llnl.gov>
2018-12-20 23:54:54 -0800

    fixing more of the Neighbor tests.

--- src/Neighbor/tests/testTreeNeighbor.py
+++ src/Neighbor/tests/testTreeNeighbor.py
@@ -12,23 +12,23 @@ from NeighborTestBase import *
 class TestTreeNeighborRandom1d(NeighborRandom1d):
     NeighborRandom1d._NeighborType = TreeNeighbor1d
 
-# #===============================================================================
-# # Radom node distribution -- 2-D.
-# #===============================================================================
-# class TestTreeNeighborRandom2d(NeighborRandom2d):
-#     NeighborRandom2d._NeighborType = TreeNeighbor2d
+#===============================================================================
+# Radom node distribution -- 2-D.
+#===============================================================================
+class TestTreeNeighborRandom2d(NeighborRandom2d):
+    NeighborRandom2d._NeighborType = TreeNeighbor2d
 
-# #===============================================================================
-# # Radom node distribution -- 3-D.
-# #===============================================================================
-# class TestTreeNeighborRandom3d(NeighborRandom3d):
-#     NeighborRandom3d._NeighborType = TreeNeighbor3d
+#===============================================================================
+# Radom node distribution -- 3-D.
+#===============================================================================
+class TestTreeNeighborRandom3d(NeighborRandom3d):
+    NeighborRandom3d._NeighborType = TreeNeighbor3d
 
-# #===============================================================================
-# # Cylindrical node distribution -- 2-D.
-# #===============================================================================
-# class TestTreeNeighborCylindrical2d(NeighborCylindrical2d):
-#     NeighborCylindrical2d._NeighborType = TreeNeighbor2d
+#===============================================================================
+# Cylindrical node distribution -- 2-D.
+#===============================================================================
+class TestTreeNeighborCylindrical2d(NeighborCylindrical2d):
+    NeighborCylindrical2d._NeighborType = TreeNeighbor2d
 
 #===============================================================================
 # Run the tests
