cfc51cdacc545a7410b710ba190183af6d24016f
Mike Owen <mikeowen@llnl.gov>
2018-12-21 11:08:52 -0800

    Getting rid of redundant tests.

--- src/Neighbor/tests/testNestedGridNeighbor.py
+++ src/Neighbor/tests/testNestedGridNeighbor.py
@@ -9,26 +9,22 @@ from NeighborTestBase import *
 #===============================================================================
 # Radom node distribution -- 1-D.
 #===============================================================================
-class TestNestedGridNeighborRandom1d(NeighborRandom1d):
-    NeighborRandom1d._NeighborType = NestedGridNeighbor1d
+NeighborRandom1d._NeighborType = NestedGridNeighbor1d
 
 #===============================================================================
 # Radom node distribution -- 2-D.
 #===============================================================================
-class TestNestedGridNeighborRandom2d(NeighborRandom2d):
-    NeighborRandom2d._NeighborType = NestedGridNeighbor2d
+NeighborRandom2d._NeighborType = NestedGridNeighbor2d
 
 #===============================================================================
 # Radom node distribution -- 3-D.
 #===============================================================================
-class TestNestedGridNeighborRandom3d(NeighborRandom3d):
-    NeighborRandom3d._NeighborType = NestedGridNeighbor3d
+NeighborRandom3d._NeighborType = NestedGridNeighbor3d
 
 #===============================================================================
 # Cylindrical node distribution -- 2-D.
 #===============================================================================
-class TestNestedGridNeighborCylindrical2d(NeighborCylindrical2d):
-    NeighborCylindrical2d._NeighborType = NestedGridNeighbor2d
+NeighborCylindrical2d._NeighborType = NestedGridNeighbor2d
 
 #===============================================================================
 # Run the tests
--- src/Neighbor/tests/testTreeNeighbor.py
+++ src/Neighbor/tests/testTreeNeighbor.py
@@ -9,26 +9,22 @@ from NeighborTestBase import *
 #===============================================================================
 # Radom node distribution -- 1-D.
 #===============================================================================
-class TestTreeNeighborRandom1d(NeighborRandom1d):
-    NeighborRandom1d._NeighborType = TreeNeighbor1d
+NeighborRandom1d._NeighborType = TreeNeighbor1d
 
 #===============================================================================
 # Radom node distribution -- 2-D.
 #===============================================================================
-class TestTreeNeighborRandom2d(NeighborRandom2d):
-    NeighborRandom2d._NeighborType = TreeNeighbor2d
+NeighborRandom2d._NeighborType = TreeNeighbor2d
 
 #===============================================================================
 # Radom node distribution -- 3-D.
 #===============================================================================
-class TestTreeNeighborRandom3d(NeighborRandom3d):
-    NeighborRandom3d._NeighborType = TreeNeighbor3d
+NeighborRandom3d._NeighborType = TreeNeighbor3d
 
 #===============================================================================
 # Cylindrical node distribution -- 2-D.
 #===============================================================================
-class TestTreeNeighborCylindrical2d(NeighborCylindrical2d):
-    NeighborCylindrical2d._NeighborType = TreeNeighbor2d
+NeighborCylindrical2d._NeighborType = TreeNeighbor2d
 
 #===============================================================================
 # Run the tests
