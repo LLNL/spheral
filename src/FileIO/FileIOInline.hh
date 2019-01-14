621c6e5e00a8f84f7a87948fdbaaaf9554ab4c7f
Mike Owen <mikeowen@llnl.gov>
2019-01-14 09:40:23 -0800

    Revert "Looking at how to better handle std::vector again."
    I meant this to be on an experimental branch, so pushing to pybind11 and reverting master.
    
    This reverts commit db0e226b9ba03321372cced52d9903185986ade3.

--- src/FileIO/FileIOInline.hh
+++ src/FileIO/FileIOInline.hh
@@ -6,13 +6,13 @@
 namespace Spheral {
 
 //------------------------------------------------------------------------------
-// Write a FieldList of arbitrary DataType (private)
+// Write a FieldList of arbitrary DataType.
 //------------------------------------------------------------------------------
 template<typename Dimension, typename DataType>
 inline
 void
-FileIO::writeFieldList(const FieldList<Dimension, DataType>& fieldList,
-                       const std::string pathName) {
+FileIO::write(const FieldList<Dimension, DataType>& fieldList,
+              const std::string pathName) {
 
   const std::string divider = "|";
 
@@ -48,13 +48,13 @@ FileIO::writeFieldList(const FieldList<Dimension, DataType>& fieldList,
 }
 
 //------------------------------------------------------------------------------
-// Read a FieldList of arbitrary DataType. (private)
+// Read a FieldList of arbitrary DataType.
 //------------------------------------------------------------------------------
 template<typename Dimension, typename DataType>
 inline
 void
-FileIO::readFieldList(FieldList<Dimension, DataType>& fieldList,
-                      const std::string pathName) const {
+FileIO::read(FieldList<Dimension, DataType>& fieldList,
+             const std::string pathName) const {
 
   const std::string divider = "|";
 
@@ -108,13 +108,13 @@ FileIO::readFieldList(FieldList<Dimension, DataType>& fieldList,
 }
 
 //------------------------------------------------------------------------------
-// Write a Field of std::vector<DataType>. (private)
+// Write a Field of std::vector<DataType>.
 //------------------------------------------------------------------------------
 template<typename Dimension, typename DataType>
 inline
 void
-FileIO::writeFieldVector(const Field<Dimension, std::vector<DataType> >& field,
-                         const std::string pathName) {
+FileIO::write(const Field<Dimension, std::vector<DataType> >& field,
+              const std::string pathName) {
 
   // Build an array with the number of elements per node, and count the total number of elements.
   std::vector<int> numElementsPerNode;
@@ -146,13 +146,13 @@ FileIO::writeFieldVector(const Field<Dimension, std::vector<DataType> >& field,
 }
 
 //------------------------------------------------------------------------------
-// Read a Field of std::vector<DataType>. (private)
+// Read a Field of std::vector<DataType>.
 //------------------------------------------------------------------------------
 template<typename Dimension, typename DataType>
 inline
 void
-FileIO::readFieldVector(Field<Dimension, std::vector<DataType> >& field,
-                        const std::string pathName) const {
+FileIO::read(Field<Dimension, std::vector<DataType> >& field,
+             const std::string pathName) const {
 
   // Read the serialized data back in.
   std::vector<int> numElementsPerNode;
