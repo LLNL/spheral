528da50d042aeac2885a2b20b267901ed271c52a
Mike Owen <mikeowen@llnl.gov>
2019-08-13 11:15:41 -0700

    Renaming.

--- src/CRKSPH/CellFaceFlag.hh
+++ src/CRKSPH/CellFaceFlag.hh
@@ -1,8 +1,8 @@
 //------------------------------------------------------------------------------
 // A struct for encoding cell face information returned by computeVoronoiVolume
 //------------------------------------------------------------------------------
-#ifndef __Spheral_CellFaceFlags__
-#define __Spheral_CellFaceFlags__
+#ifndef __Spheral_CellFaceFlag__
+#define __Spheral_CellFaceFlag__
 
 #include <vector>
 #include "Utilities/DataTypeTraits.hh"
@@ -10,7 +10,7 @@
 
 namespace Spheral {
 
-struct CellFaceFlags {
+struct CellFaceFlag {
   int cellFace;  // The index of the face in the cell.facets array
   int nodeListj; // The NodeList of the opposite node across the face
   int j;         // The index of the opposite node across the face
@@ -20,15 +20,15 @@ struct CellFaceFlags {
 // Communication/serialization traits
 //------------------------------------------------------------------------------
 template<>
-struct DataTypeTraits<CellFaceFlags> {
+struct DataTypeTraits<CellFaceFlag> {
   typedef int ElementType;
   static bool fixedSize() { return true; }
-  static int numElements(const CellFaceFlags& x) { return 3; }
-  static CellFaceFlags zero() { return CellFaceFlags({-1, -1, -1}); }
+  static int numElements(const CellFaceFlag& x) { return 3; }
+  static CellFaceFlag zero() { return CellFaceFlag({-1, -1, -1}); }
 };
 
 template<>
-void packElement<CellFaceFlags>(const CellFaceFlags& value,
+void packElement<CellFaceFlag>(const CellFaceFlag& value,
                                 std::vector<char>& buffer) {
   packElement(value.cellFace, buffer);
   packElement(value.nodeListj, buffer);
@@ -36,9 +36,9 @@ void packElement<CellFaceFlags>(const CellFaceFlags& value,
 }
 
 template<>
-void unpackElement<CellFaceFlags>(CellFaceFlags& value,
-                                  std::vector<char>::const_iterator& itr,
-                                  const std::vector<char>::const_iterator& endPackedVector) {
+void unpackElement<CellFaceFlag>(CellFaceFlag& value,
+                                 std::vector<char>::const_iterator& itr,
+                                 const std::vector<char>::const_iterator& endPackedVector) {
   unpackElement(value.cellFace, itr, endPackedVector);
   unpackElement(value.nodeListj, itr, endPackedVector);
   unpackElement(value.j, itr, endPackedVector);
