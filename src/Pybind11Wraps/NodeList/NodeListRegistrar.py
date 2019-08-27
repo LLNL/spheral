a3430116e44b6d5b0ff80ffc45121b282322c46d
Mike Owen <mikeowen@llnl.gov>
2019-08-27 10:38:45 -0700

    Fleshing out a few more methods.

--- src/Pybind11Wraps/NodeList/NodeListRegistrar.py
+++ src/Pybind11Wraps/NodeList/NodeListRegistrar.py
@@ -7,10 +7,6 @@ from PYB11Generator import *
 @PYB11singleton
 class NodeListRegistrar:
 
-    @PYB11const
-    def valid(self):
-        return "bool"
-
     # The instance attribute.  We expose this as a property of the class.
     @PYB11static
     @PYB11cppname("instance")
@@ -19,3 +15,14 @@ class NodeListRegistrar:
         return "NodeListRegistrar<%(Dimension)s>&"
     instance = property(getinstance, doc="The static NodeListRegistrar<%(Dimension)s> instance.")
 
+
+    # Attributes
+    numNodeLists = PYB11property(doc="The number of NodeLists that have been created")
+    numFluidNodeLists = PYB11property(doc="The number of FluidNodeLists that have been created")
+    registeredNames = PYB11property(doc="The set of names for all NodeLists")
+    registeredFluidNames = PYB11property(doc="The set of names for all FluidNodeLists")
+    valid = PYB11property(doc="Internal consistency check")
+    domainDecompositionIndependent = PYB11property("bool",
+                                                   getter="domainDecompositionIndependent",
+                                                   setter="domainDecompositionIndependent",
+                                                   doc="Flag to force domain decomposition independent calculations -- some runtime penalty involved!")
