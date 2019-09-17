84e867930dc1c292f89413d4d142a26c5ff2e0b5
Mike Owen <mikeowen@llnl.gov>
2019-09-17 05:03:06 -0700

    Checkpoint -- starting NodePair conversion of SPH derivatives.

--- src/SPH/SPHHydroBaseInst.cc.py
+++ src/SPH/SPHHydroBaseInst.cc.py
@@ -3,13 +3,14 @@ text = """
 // Explict instantiation.
 //------------------------------------------------------------------------------
 #include "SPHHydroBase.cc"
+#include "SPHEvaluateDerivatives.cc"
 #include "Geometry/Dimension.hh"
 
-#ifdef _OPENMP
-#include "SPHEvaluateDerivatives_OpenMP.cc"
-#else
-#include "SPHEvaluateDerivatives.cc"
-#endif
+// #ifdef _OPENMP
+// #include "SPHEvaluateDerivatives_OpenMP.cc"
+// #else
+// #include "SPHEvaluateDerivatives.cc"
+// #endif
 
 namespace Spheral {
   template class SPHHydroBase< Dim< %(ndim)s > >;
