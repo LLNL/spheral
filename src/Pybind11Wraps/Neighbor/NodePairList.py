3c7892296a14176a59db19f865f4bf5ccb933622
Mike Owen <mikeowen@llnl.gov>
2019-09-17 07:17:09 -0700

    Adding more methods and bindings to pair classes.

--- src/Pybind11Wraps/Neighbor/NodePairList.py
+++ src/Pybind11Wraps/Neighbor/NodePairList.py
@@ -1,18 +1,45 @@
 from PYB11Generator import *
 
+#-------------------------------------------------------------------------------
+# NodePairIdxType
+#-------------------------------------------------------------------------------
 class NodePairIdxType:
-  def pyinit(self,
-             i_n = "int",
-             i_l = "int",
-             j_n = "int",
-             j_l = "int"):
-    "Constructor"
+    def pyinit(self,
+               i_n = "int",
+               i_l = "int",
+               j_n = "int",
+               j_l = "int"):
+      "Constructor"
+      return
 
+    # Comparisons
+    def __eq__(self):
+      return
+    def __lt__(self):
+      return
+
+    # String representation
+    @PYB11implementation("""
+[](const NodePairIdxType& self) {
+  std::string result = "(" + 
+                       std::to_string(self.i_list) + " " +
+                       std::to_string(self.i_node) + " " +
+                       std::to_string(self.j_list) + " " +
+                       std::to_string(self.j_node) + ")";
+  return result;
+}""")
+    def __repr__(self):
+        return
+
+    # Attributes
     i_node = PYB11readwrite()
     j_node = PYB11readwrite()
     i_list = PYB11readwrite()
     j_list = PYB11readwrite()
 
+#-------------------------------------------------------------------------------
+# NodePairList
+#-------------------------------------------------------------------------------
 class NodePairList:
   def pyinit(self):
     "Default Constructor"
@@ -39,3 +66,28 @@ class NodePairList:
   @PYB11implementation('[](NodePairList& self, int i) { const int n = self.size(); return &self[(i %% n + n) %% n]; }')
   def __getitem__(self):
       return
+
+  @PYB11implementation("[](const NodePairList& self) { return py::make_iterator(self.begin(), self.end()); }, py::keep_alive<0,1>()")
+  def __iter__(self):
+    "Python iteration through a NodePairList."
+
+  @PYB11implementation("""[](const NodePairList& self, const NodePairIdxType val) { for (const auto& ele: self) {
+                                                                                      if (ele == val) return true;
+                                                                                    }
+                                                                                    return false;
+                                                                                  }""")
+  def __contains__(self,
+                   val = "NodePairIdxType"):
+    return "bool"
+
+  @PYB11implementation("""[](const NodePairList& self, const py::tuple val0) { if (val0.size() != 4) throw py::value_error("require a tuple of 4 integers");
+                                                                               NodePairIdxType val(val0[0].cast<int>(), val0[1].cast<int>(), val0[2].cast<int>(), val0[3].cast<int>());
+                                                                               for (const auto& ele: self) {
+                                                                                 if (ele == val) return true;
+                                                                               }
+                                                                               return false;
+                                                                             }""")
+  def __contains__(self,
+                   val = "py::tuple"):
+    return "bool"
+
