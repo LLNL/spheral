61e0389a28f391737cd81f93788a8b93312ee8b8
mdavis36 <davis291@llnl.gov>
2019-09-18 20:09:39 -0700

    typos

--- src/CMakeBasics.cmake
+++ src/CMakeBasics.cmake
@@ -9,22 +9,25 @@ function(instantiate _inst_var _source_var)
     list(APPEND _dims 3)
   endif()
 
-  set(_tmp_sources)
+  set(_tmp_sources )
   foreach(_inst ${${_inst_var}})
     if(ENABLE_INSTANTIATIONS)
       foreach(_dim ${_dims})
 	set(_inst_py ${CMAKE_CURRENT_SOURCE_DIR}/${_inst}Inst.cc.py)
 	set(_inst_file ${_inst}Inst.cc.${_dim}.cc)
 	#Run python script to generate instantiation file
-	execute_process(COMMAND ${PYTHON_EXECURTABLE} ${PROJECT_NAME}/src/helpers/InstantiationGenerator.py 
+	execute_process(COMMAND ${PYTHON_EXECUTABLE} ${PROJECT_NAME}/src/helpers/InstantiationGenerator.py 
 									  ${_inst_py} ${_inst_file} ${_dim}
 			WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
 			)
 	list(APPEND _tmp_sources ${CMAKE_CURRENT_BINARY_DIR}/${_inst_file})
       endforeach()
     else()
-      if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${_inst_file}.cc)
+      if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${_inst}.cc")
 	list(APPEND _tmp_source ${_inst}.cc)
+	message("Appending ${_inst}.cc")
+      else()
+	message("WARNING : Could not find ${CMAKE_CURRENT_SOURCE_DIR}/${_inst}.cc")
       endif()
     endif()      
   endforeach()
