35f441e0b6669d228b30bacb9e5c304c94d7f392
Mike Owen <mikeowen@llnl.gov>
2019-10-18 23:54:46 -0700

    Adding 2d inflow/outflow test.

--- src/Boundary/tests/testInflowOutflow.py
+++ src/Boundary/tests/testInflowOutflow.py
@@ -17,9 +17,9 @@ commandLine(geometry = "1d",    # (1d, 2d, 3d, RZ)
             x1 = 0.0,
             x2 = 1.0,
             y0 = 0.0,
-            y1 = 1.0,
+            y1 = 0.1,
             z0 = 0.0,
-            z1 = 1.0,
+            z1 = 0.1,
 
             nPerh = 4.0,
             hmin = 1e-10,
@@ -79,7 +79,26 @@ if geometry == "1d":
     distributeNodesInRange1d([(nodes1, nx1, rho1, (x0, x1)),
                               (nodes2, nx2, rho2, (x1, x2))],
                              nPerh = nPerh)
+elif geometry == "2d":
+    from GenerateNodeDistribution2d import GenerateNodeDistribution2d
+    from PeanoHilbertDistributeNodes import distributeNodes2d
+    ny1 = int((y1 - y0)/(x1 - x0)*nx1 + 0.5)
+    ny2 = int((y1 - y0)/(x2 - x1)*nx2 + 0.5)
+    gen1 = GenerateNodeDistribution2d(nx1, ny1, rho1, "lattice",
+                                      xmin = (x0, y0),
+                                      xmax = (x1, y1),
+                                      nNodePerh = nPerh,
+                                      SPH = True)
+    gen2 = GenerateNodeDistribution2d(nx2, ny2, rho2, "lattice",
+                                      xmin = (x1, y0),
+                                      xmax = (x2, y1),
+                                      nNodePerh = nPerh,
+                                      SPH = True)
+    distributeNodes2d((nodes1, gen1),
+                      (nodes2, gen2))
+
 output("nodes1.numNodes")
+output("nodes2.numNodes")
 
 # Set node velocities
 for nodes in nodeSet:
@@ -115,19 +134,22 @@ packages = [hydro]
 #-------------------------------------------------------------------------------
 # Create boundary conditions.
 #-------------------------------------------------------------------------------
-if geometry == "1d":
-    inplane = Plane(Vector(x0), Vector(1.0))
-    outplane = Plane(Vector(x2), Vector(-1.0))
-    inflow = InflowOutflowBoundary(db, inplane)
-    outflow = InflowOutflowBoundary(db, outplane)
+inplane = Plane(Vector(x0), Vector(1))
+outplane = Plane(Vector(x2), Vector(-1))
+inflow = InflowOutflowBoundary(db, inplane)
+outflow = InflowOutflowBoundary(db, outplane)
+bcs = [inflow, outflow]
+packages += bcs
 
-    bcs = [inflow, outflow]
+if geometry == "2d":
+    yplane0 = Plane(Vector(x0, y0), Vector(0,  1))
+    yplane1 = Plane(Vector(x0, y1), Vector(0, -1))
+    ybc = PeriodicBoundary(yplane0, yplane1)
+    bcs += [ybc]
 
 for p in packages:
     for bc in bcs:
         p.appendBoundary(bc)
-
-packages += bcs
 
 #-------------------------------------------------------------------------------
 # Construct an integrator.
@@ -171,3 +193,6 @@ else:
 if graphics:
     from SpheralMatplotlib import *
     rhoPlot, velPlot, epsPlot, PPlot, HPlot = plotState(db)
+
+    if geometry == "2d":
+        posplot = plotNodePositions2d(db, plotGhosts=True)
