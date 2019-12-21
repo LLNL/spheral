2bb27e7f98f01520e1d075eeac886d6940ca2c84
Mike Owen <mikeowen@llnl.gov>
2019-12-20 21:08:26 -0800

    Forgot to check this in.

---
+++ src/RK/hessianRKInst.cc.py
@@ -0,0 +1,30 @@
+text = """
+//------------------------------------------------------------------------------
+// Explicit instantiation.
+//------------------------------------------------------------------------------
+#include "RK/hessianRK.cc"
+#include "Geometry/Dimension.hh"
+#include "SPH/NodeCoupling.hh"
+
+namespace Spheral {
+"""
+
+for Value in ("Scalar", "Vector"):
+    text += """
+template 
+FieldList<Dim<%%(ndim)s>, MathTraits<Dim<%%(ndim)s>, Dim<%%(ndim)s>::%(Value)s>::HessianType> 
+hessianRK<Dim<%%(ndim)s>, Dim<%%(ndim)s>::%(Value)s>(const FieldList<Dim<%%(ndim)s>, Dim<%%(ndim)s>::%(Value)s>& fieldList,
+                                                     const FieldList<Dim<%%(ndim)s>, Dim<%%(ndim)s>::Vector>& position,
+                                                     const FieldList<Dim<%%(ndim)s>, Dim<%%(ndim)s>::Scalar>& weight,
+                                                     const FieldList<Dim<%%(ndim)s>, Dim<%%(ndim)s>::SymTensor>& H,
+                                                     const ConnectivityMap<Dim<%%(ndim)s>>& connectivityMap,
+                                                     const TableKernel<Dim<%%(ndim)s>>& W,
+                                                     const RKOrder correctionOrder,
+                                                     const FieldList<Dim<%%(ndim)s>, std::vector<double>>& corrections,
+                                                     const NodeCoupling& nodeCoupling);
+
+""" % {"Value" : Value}
+
+text += """
+}
+"""
