4befb9438e0f5445944b9832a4c9e0ad8bc53cb8
Mike Owen <mikeowen@llnl.gov>
2019-12-31 00:12:11 -0800

    Building and importing with removed CRKSPH utilities.

--- src/RK/RKFieldNames.cc
+++ src/RK/RKFieldNames.cc
@@ -2,8 +2,6 @@
 
 namespace Spheral {
 
-const std::string rkOrders = "rkOrders";
-static const std::string rkCorrections(const RKOrder order)     { return "rkCorrections_" + std::to_string(static_cast<int>(order)); }
-static const std::string reproducingKernel(const RKOrder order) { return "reproducingKernel_" + std::to_string(static_cast<int>(order)); }
+const std::string RKFieldNames::rkOrders = "rkOrders";
 
 }
--- src/RK/RKFieldNames.hh
+++ src/RK/RKFieldNames.hh
@@ -14,8 +14,8 @@ namespace Spheral {
 
 struct RKFieldNames {
   static const std::string rkOrders;
-  static const std::string rkCorrections(const RKOrder order);
-  static const std::string reproducingKernel(const RKOrder order);
+  static const std::string rkCorrections(const RKOrder order)     { return "rkCorrections_" + std::to_string(static_cast<int>(order)); }
+  static const std::string reproducingKernel(const RKOrder order) { return "reproducingKernel_" + std::to_string(static_cast<int>(order)); }
 };
 
 }
