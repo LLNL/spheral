8db80e3bb342ad1c88fedc070f6f3c9d251df1b0
Mike Owen <mikeowen@llnl.gov>
2020-01-12 21:57:00 -0800

    Interim checkpoint.

--- src/Boundary/ConstantBoundary.cc
+++ src/Boundary/ConstantBoundary.cc
@@ -4,14 +4,15 @@
 //----------------------------------------------------------------------------//
 #include "FileIO/FileIO.hh"
 #include "Boundary.hh"
-#include "mapPositionThroughPlanes.hh"
+#include "Boundary/mapPositionThroughPlanes.hh"
 #include "Field/FieldList.hh"
 #include "Field/Field.hh"
 #include "Field/FieldBase.hh"
 #include "Utilities/planarReflectingOperator.hh"
 #include "Utilities/DBC.hh"
 
-#include "ConstantBoundary.hh"
+#include "Boundary/ConstantBoundary.hh"
+#include "Boundary/ConstantBoundaryUtilities.hh"
 
 #include "boost/algorithm/string.hpp"
 
@@ -26,53 +27,6 @@ using std::max;
 using std::abs;
 
 namespace Spheral {
-
-namespace {
-
-//------------------------------------------------------------------------------
-// Store the Field values for the given NodeList.
-//------------------------------------------------------------------------------
-template<typename Dimension>
-void
-storeFieldValues(const NodeList<Dimension>& nodeList,
-                 const std::vector<int>& nodeIDs,
-                 std::map<std::string, std::vector<char>>& values) {
-  for (auto fieldItr = nodeList.registeredFieldsBegin();
-       fieldItr != nodeList.registeredFieldsEnd();
-       ++fieldItr) {
-    const auto buffer = (**fieldItr).packValues(nodeIDs);
-    const auto key = StateBase<Dimension>::key(**fieldItr);
-    CHECK2(values.find(key) == values.end(), key);
-    values[key] = buffer;
-    // cerr << "Stored " << vals.size() << " values for " << key << endl;
-  }
-}
-  
-//------------------------------------------------------------------------------
-// Set the Field values in the given field using the given map.
-//------------------------------------------------------------------------------
-template<typename Dimension>
-void
-resetValues(FieldBase<Dimension>& field,
-            const std::vector<int>& nodeIDs,
-            const std::map<std::string, std::vector<char>>& values,
-            const bool dieOnMissingField) {
-  const auto& nodeList = field.nodeList();
-
-  // Find this Field in the set of stored values.
-  const auto key = StateBase<Dimension>::key(field);
-  auto itr = values.find(key);
-  VERIFY2(itr != values.end() or not dieOnMissingField,
-          "ConstantBoundary error: " << key << " not found in stored field values.");
-
-  // Now set the values.
-  if (itr != values.end()) {
-    const auto& buffer = itr->second;
-    field.unpackValues(nodeIDs, buffer);
-  }
-}
-
-}
 
 //------------------------------------------------------------------------------
 // Construct with the given set of nodes.
