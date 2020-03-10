746a8c02691a9573cc2dd5637af2ef2b2f19a6aa
mdavis36 <davis291@llnl.gov>
2020-03-10 15:51:53 -0700

    Adding spheral_add_pybind11_library function.

--- cmake/Modules/SpheralCMake.cmake
+++ cmake/Modules/SpheralCMake.cmake
@@ -16,4 +16,26 @@ function(spheral_add_cxx_library package_name)
   endif()
 endfunction()
 
+function(spheral_add_pybind11_library package_name)
+  set(PYB11_MODULE_NAME ${package_name})
+  include(UsePYB11Generator)
+    PYB11_GENERATE_BINDINGS()
 
+  set(MODULE_NAME Spheral${PYB11_MODULE_NAME})
+  set(GENERATED_SOURCE Spheral${PYB11_GENERATED_SOURCE})
+
+  blt_add_library(
+    NAME         ${MODULE_NAME}
+    SOURCES      ${GENERATED_SOURCE} ${${package_name}_ADDITIONAL_SOURCES}
+    DEPENDS_ON   ${SPHERAL_PYTHON_DEPENDS} ${${package_name}_ADDITIONAL_DEPENDS} ${spheral_depends}
+    INCLUDES     ${${package_name}_ADDITIONAL_INCLUDES}
+    OUTPUT_NAME  ${MODULE_NAME}
+    CLEAR_PREFIX TRUE
+    SHARED       TRUE
+    )
+
+  install(
+    FILES ${CMAKE_BINARY_DIR}/lib/$<TARGET_FILE_NAME:${MODULE_NAME}>
+    DESTINATION .
+    )
+endfunction()
