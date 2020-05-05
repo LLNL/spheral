3fd24b0abe91d634b6f4cd2d4e0f6db4a4020909
mdavis36 <davis291@llnl.gov>
2020-05-05 15:52:38 -0700

    Fixing Gnuplot install

--- cmake/Modules/gnuplotInstall.cmake
+++ cmake/Modules/gnuplotInstall.cmake
@@ -4,18 +4,21 @@ set(GNUPLOT_URL http://downloads.sourceforge.net/gnuplot-py/gnuplot-py-1.8.tar.g
 set(GNUPLOT_HASH )
 set(GNUPLOT_CACHE ${CACHE_DIR}/${GNUPLOT_DIST})
 
+set(GNUPLOT_DOWNLOAD_CMD wget ${GNUPLOT_URL} -O ${CACHE_DIR}/gnuplot-py-1.8.tar.gz)
+
 if(EXISTS ${GNUPLOT_CACHE})
-  set(GNUPLOT_URL ${GNUPLOT_CACHE})
+  set(GNUPLOT_DOWNLOAD_CMD sleep 1)
 endif()
 
-ExternalProject_add(${lib_name}
-  URL ${GNUPLOT_URL} 
-  PREFIX ${GNUPLOT_PREFIX}
-  DOWNLOAD_DIR ${CACHE_DIR}
-  CONFIGURE_COMMAND ${PYTHON_EXE} ${GNUPLOT_PREFIX}/src/gnuplot/setup.py install
-  BUILD_COMMAND sleep 1
-  INSTALL_COMMAND sleep 1
-  DEPENDS python-install numpy-stl
-  LOG_DOWNLOAD ${OUT_PROTOCOL_EP}
-  LOG_CONFIGURE ${OUT_PROTOCOL_EP}
+add_custom_target(${lib_name}-unpack
+  COMMAND ${GNUPLOT_DOWNLOAD_CMD}
+  COMMAND tar -xvf ${CACHE_DIR}/gnuplot-py-1.8.tar.gz
+  COMMENT "Upacking gnuplot"
+)
+
+add_custom_target(${lib_name}
+  COMMAND ${PYTHON_EXE} setup.py install
+  WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/gnuplot-py-1.8
+  COMMENT "Installing gnuplot"
+  DEPENDS python-install numpy-stl ${lib_name}-unpack
 )
