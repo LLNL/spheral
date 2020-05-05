637b70ebd39bce10b64852dda260d35b972d5d20
mdavis36 <davis291@llnl.gov>
2020-05-05 10:25:33 -0700

    Using hdf5 shared lib

--- cmake/third-party-libs/hdf5Install.cmake
+++ cmake/third-party-libs/hdf5Install.cmake
@@ -2,7 +2,7 @@ set(HDF5_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/${lib_name})
 set(HDF5_CACHE "${CACHE_DIR}/hdf5-1.10.4.tar.bz2")
 set(HDF5_URL "https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.4/src/hdf5-1.10.4.tar.bz2")
 
-set(${lib_name}_libs libhdf5.a libhdf5.so libhdf5_hl.a)
+set(${lib_name}_libs libhdf5.so libhdf5_hl.a)
 
 if(${lib_name}_BUILD)
 
