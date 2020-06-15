eabcc75d573df78d3eb0f83205d8914aab6bb9c6
Cody Raskin <raskin1@llnl.gov>
2020-06-15 13:40:44 -0700

    now works in 3d, but need to restore regular lattice section

--- src/NodeGenerators/tests/latticeCyl.py
+++ src/NodeGenerators/tests/latticeCyl.py
@@ -1,17 +1,6 @@
 
 from math import *
-import matplotlib.pyplot as plt
-
-class Vector2d:
-    def __init__(self,x,y):
-        self.x = float(x)
-        self.y = float(y)
-        self.items = [x,y]
-    def __getitem__(self,i):
-        return items[i]
-    def magnitude(self):
-        return sqrt(self.x**2+self.y**2)\
-
+from vectormath import *
 
 def density(pos):
     x = pos.x
@@ -24,146 +13,166 @@ class constantDensity:
     def __call__(self,pos):
         return self.d
 
-def create(nx, ny, rho,
-            xmin = (0.0, 0.0),
-            xmax = (1.0, 1.0),
-            rmin = None,
-            rmax = None,
-            centroid = (0.0,0.0),
-            nNodePerh = 2.01):
-    if type(rho) == float:
-        rho = constantDensity(rho)
-    
-    centroid = Vector2d(centroid[0],centroid[1])
+class SquareCurve:
+    def __init__(self,sideLength):
+        self.sideLength = sideLength
+    def __call__(self,t):
+        a = t*4.0
+        b = a
+        if a < 1.0:
+            Q = Vector2d(-1.0,1.0)
+            P = Vector2d(1.0,1.0)
+        elif a < 2.0 and a >= 1.0:
+            b = a-1.0
+            Q = Vector2d(1.0,1.0)
+            P = Vector2d(1.0,-1.0)
+        elif a < 3.0 and a >= 2.0:
+            b = a-2.0
+            Q = Vector2d(1.0,-1.0)
+            P = Vector2d(-1.0,-1.0)
+        else:
+            b = a-3.0
+            Q = Vector2d(-1.0,-1.0)
+            P = Vector2d(-1.0,1.0)
+        P = P*self.sideLength
+        Q = Q*self.sideLength
+        return P*b+Q*(1.0-b)
 
-    k = 0
-    np = 0
-    if rmin is None:
-        rmin = 0
-    if rmax is None:
-        rmax = Vector2d(xmax[0],xmax[1]).magnitude()
-    deltar = rmax - rmin
+class CircleCurve:
+    def __init__(self,radius):
+        self.radius = radius
+    def __call__(self,t):
+        t = 1.0-t
+        t = t*2*3.14159
+        t = t - 3.14159 - 3.14159/4.0
+        return Vector2d(cos(t),sin(t))*self.radius
+
+def latticeDistribution(nx,ny, rho0,
+                            xmin,
+                            xmax,
+                            nNodePerh = 2.01):
+        
+    assert rho0 > 0
+
     dx = (xmax[0] - xmin[0])/nx
     dy = (xmax[1] - xmin[1])/ny
 
-    hx = 1.0/(nNodePerh*dx)
-    #hy = 1.0/(nNodePerh*dy)
-    #H0 = SymTensor2d(hx, 0.0, 0.0, hy)
+    m0 = dx*dy*rho0
 
     x = []
     y = []
     m = []
-    #H = []
-
-
-    ml = []
-    #Hl = []
-    xl = []
-    yl = []
-
-    xc = []
-    yc = []
-    mc = []
-    #Hc = []
 
     for j in range(ny):
         for i in range(nx):
             xx = xmin[0] + (i + 0.5)*dx
             yy = xmin[1] + (j + 0.5)*dy
-            r = sqrt(xx*xx + yy*yy)
-            m0 = dx*dy*rho(Vector2d(xx, yy))
-            if (r>=rmin*0.8):
-                xl.append(xx)
-                yl.append(yy)
-                ml.append(m0)
-                #Hl.append(H0)
-                k = k + 1
-            if (r>=rmax):
-                x.append(xx)
-                y.append(yy)
-                m.append(m0)
-                #H.append(H0)
-                np = np + 1
-
-    # Start at the outermost radius, and work our way inward.
-    theta = 2*3.14159
-    ri = rmax+2.0*nNodePerh/nx
-
-    #import random
-    #random.seed()
-
-    while ri > 0:
-        
-        # Get the nominal delta r, delta theta, number of nodes, and mass per
-        # node at this radius.
-        rhoi = rho(Vector2d(ri, 0.0))
-        dr = sqrt(m0/rhoi)
-        arclength = theta*ri
-        arcmass = arclength*dr*rhoi
-        nTheta = max(1, int(arcmass/m0))
-        dTheta = theta/nTheta
-        mi = arcmass/nTheta
-        #hi = nNodePerh*0.5*(dr + ri*dTheta)
-        #Hi = SymTensor2d(1.0/hi, 0.0,
-        #                    0.0, 1.0/hi)
-
-        # Now assign the nodes for this radius.
-        for i in range(nTheta):
-            thetai = (i + 0.5)*dTheta
-            xc.append(ri*cos(thetai))
-            yc.append(ri*sin(thetai))
-            mc.append(mi)
-            #Hc.append(Hi)
-            xi = ri*cos(thetai)
-            yi = ri*sin(thetai)
-            
-            if(ri < rmin):
-                x.append(xi)
-                y.append(yi)
-                m.append(mi)
-                #H.append(Hi)
-                np = np + 1
-            elif(ri>=rmin):
-                #eps = random.random()
-                #func = ((ri-rmin)/deltar)**2
-                func = 1-ri/(rmin-rmax) - rmax/(rmax-rmin)
-                if(func>1.0):
-                    func = 1.0
-                if(func<0.0):
-                    func = 0.0
-                #if (eps <= func):
-                #x.append(ri*cos(thetai))
-                #y.append(ri*sin(thetai))
-                #m.append(mi)
-                #H.append(Hi)
-                #else:
-                minddr = nx
-                mink = 2*k
-                for j in range(k):
-                    ddr = sqrt((xl[j]-xi)**2+(yl[j]-yi)**2)
-                    if (ddr < minddr):
-                        minddr = ddr
-                        mink = j
-                xi = xi+(xl[mink]-xi)*func
-                yi = yi+(yl[mink]-yi)*func
-                
-                minddr = nx
-                for j in range(np):
-                    ddr = sqrt((x[j]-xi)**2 + (y[j]-yi)**2)
-                    if (ddr < minddr):
-                        minddr = ddr
-                if(minddr > (1.0/hx)*0.5):
-                    x.append(xi+(xl[mink]-xi)*func)
-                    y.append(yi+(yl[mink]-yi)*func)
-                    m.append(ml[mink])
-                    #H.append(Hl[mink])
-
-
-        # Decrement to the next radial bin inward.
-        ri = max(0.0, ri - dr)
+            x.append(xx)
+            y.append(yy)
+            m.append(m0)
+    
     return x, y, m
 
-x,y,m = create(20,20,rho=1.0,xmin=(-1.0,-1.0),xmax=(1.0,1.0),rmax=0.8,rmin=0.5)
+def profileMethod(rad,maxrad):
+    ri = rad/maxrad
+    ri = 1.0+(1.0-ri)
+    return ri
 
-plt.plot(x,y,".")
+def latticeCylindrcalGenerator3d(nx,ny,nz,
+                                xmin = (-1.0, -1.0, -1.0),
+                                xmax = (1.0, 1.0, 1.0),
+                                centroid = (0.0, 0.0), # centroid remains a 2d vector in x-y plane
+                                rho0 = 1.0,
+                                nNodePerh = 2.01,
+                                refineMethod = None,
+                                rmax = 1.0,):
+
+    x = []
+    y = []
+    z = []
+    m = []
+
+    centroid = vectorfromtuple(centroid)
+    maxd = max((vectorfromtuple((xmax[0],xmax[1]))-centroid).magnitude(),(centroid-vectorfromtuple((xmin[0],xmin[1]))).magnitude())
+
+    # first fill the regular lattice positions  ------------------------
+    '''
+    nr = int(nx*0.5)
+    xl,yl,ml = latticeDistribution(nx,ny,rho0,xmin,xmax,nNodePerh)
+
+    for i in range(len(xl)):
+        xx = xl[i]
+        yy = yl[i]
+        if abs(xx-centroid[0])>rmax or abs(yy-centroid[1])>rmax:
+            x.append(xx)
+            y.append(yy)
+            z.append(0)
+    '''
+
+    # now fill the transfinite positions  -------------------------------
+    xxmin0 = max(centroid[0]-rmax,xmin[0])
+    xxmin1 = max(centroid[1]-rmax,xmin[1])
+    xxmin = (xxmin0,xxmin1,xmin[2])
+    xxmax0 = min(centroid[0]+rmax,xmax[0])
+    xxmax1 = min(centroid[1]+rmax,xmax[1])
+    xxmax = (xxmax0,xxmax1,xmax[2])
+
+    def interpT(rad,minrad,maxrad):
+        if rad > minrad and rad < maxrad:
+            return 1.0-(rad-minrad)/(maxrad-minrad)
+        elif rad >= maxrad:
+            return 0.0
+        else:
+            return 1.0
+
+    dx = (xmax[0] - xmin[0])/nx
+    xxmin = (xxmin[0]-dx*0.5,xxmin[1],xmin[2])
+
+    nx0 = int(nx*(2.0*rmax)/(xmax[0]-xmin[0]))
+    dx = (xmax[0] - xmin[0])/nx
+    dr = dx
+    dz = dr
+    r = rmax-dr*0.5
+    thcount = 1e5
+    while thcount > 1:
+        prevv = Vector2d(0,0)
+        dist = 0.0
+        sq = SquareCurve(r)
+        cir = CircleCurve(r)
+        ntot = nx0*4
+        thcount = max(int(ntot*(r/rmax)*refineMethod(r,rmax)),1)
+        ti = interpT(r,rmax*0.4,rmax*0.9) # linear interp from square to circle
+        for i in range(thcount):
+            th = 1.0/thcount*i
+            rs = sq(th)
+            rc = cir(th)
+            v = rs.lerp(rc,ti)+centroid
+            if prevv.magnitude() > 0.0:
+                dist += (v-prevv).magnitude()
+            xx = v[0]
+            yy = v[1]
+            if xx < xxmax[0] and yy < xxmax[1] and xx >= xxmin[0] and yy >= xxmin[1]:
+                nzi = int((xmax[2]-xmin[2])/dz)
+                zi = xmin[2]+dz/2.0
+                while zi < xmax[2]:
+                    x.append(xx)
+                    y.append(yy)
+                    z.append(zi)
+                    zi += dz
+            prevv = v
+        dist = dist / float(thcount)
+        dr = dist
+        dz = dr
+        r = r-dr
+    
+    return x,y,z,m
+
+x,y,z,m = latticeCylindrcalGenerator3d(nx=20,ny=20,nz=20,centroid=(0.5,0.5),refineMethod=profileMethod,rmax=0.8)
+print("Created %d particles"%len(x))
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(x, y,z, zdir='z', s=20, c=None, depthshade=True)
 plt.show()
--- src/NodeGenerators/tests/vectormath.py
+++ src/NodeGenerators/tests/vectormath.py
@@ -28,7 +28,59 @@ class Vector2d:
         return v2*t+self*(1.0-t)
 
 def vectorfromtuple(tup):
-    return Vector2d(tup[0],tup[1])
+    if len(tup) == 2:
+        return Vector2d(tup[0],tup[1])
+    elif len(tup) == 3:
+        return Vector3d(tup[0],tup[1],tup[2])
 
 def interpVector(v1,v2,t):
     return v2*t+v1*(1.0-t)
+
+class Vector3d:
+    def __init__(self,x,y=0,z=0):
+        if isinstance(x,int) or isinstance(x,float):
+            self.x = float(x)
+            self.y = float(y)
+            self.z = float(z)
+        elif isinstance(x,tuple) or isinstance(x,list) or isinstance(x,vector3d):
+            self.x = float(x[0])
+            self.y = float(x[1])
+            self.z = float(x[2])
+        self.magnitude = sqrt(self.x*self.x+self.y*self.y+self.z*self.z)
+    def __getitem__(self, key):
+        if( key == 0):
+            return self.x        
+        elif( key == 1):            
+            return self.y     
+        elif( key == 2):            
+            return self.z      
+        else:            
+            raise Exception("Invalid key to Point")
+    def __setitem__(self, key, value):        
+        if( key == 0):            
+            self.x = value        
+        elif( key == 1):            
+            self.y = value  
+        elif( key == 2):            
+            self.z = value      
+        else:            
+            raise Exception("Invalid key to Point")
+    def __add__(self,val):
+        return Vector3d(self.x+val.x,self.y+val.y,self.z+val.z)
+    def __sub__(self,val):
+        return Vector3d(self.x-val.x,self.y-val.y,self.z-val.z)
+    def __str__(self):
+        return "<"+str(self.x)+","+str(self.y)+","+str(self.z)+">"
+    def dot(self,val):
+        return self.x*val.x+self.y*val.y+self.z*val.z
+    def __mul__(self,val):
+        return Vector3d(self.x*val,self.y*val,self.z*val)
+    def __truediv__(self,val):
+        return Vector3d(self.x/val,self.y/val,self.z/val)
+    def cross(self,val):
+        x = self.y*val.z - self.z*val.y
+        y = self.z*val.x - self.x*val.z
+        z = self.x*val.y - self.y*val.x
+        return Vector3d(x,y,z)
+    def normal(self):
+        return Vector3d(self.x,self.y,self.z)/self.magnitude
