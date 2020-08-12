e9bc35f162c9445a8b2fd2c0ec93390d0001bc08
Mike Owen <mikeowen@llnl.gov>
2020-08-12 16:37:03 -0700

    Fixes for the new python source install function.

--- cmake/spheral/SpheralInstallPythonFiles.cmake
+++ cmake/spheral/SpheralInstallPythonFiles.cmake
@@ -2,15 +2,15 @@
 # spheral_install_python_files
 #     - Copies Python files to the Spheral install path, and byte compiles them
 #
-# pyfiles : a list of python files to install
+# The list of python files should be passed as the arguments
 #
 # Note, if ENABLE_CXXONLY is set, this function does nothing
 #-----------------------------------------------------------------------------------
 
-function(spheral_install_python_files pyfiles)
+function(spheral_install_python_files)
 
   if (NOT ENABLE_CXXONLY)
-    install(FILES ${pyfiles}
+    install(FILES ${ARGV}
             DESTINATION Spheral)
     install(CODE "execute_process( \
             COMMAND ${SPHERAL_INSTALL_DIR}/python/bin/python -m compileall Spheral \
