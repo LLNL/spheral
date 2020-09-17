bb3529d6a6a085290338ea8c54d2fbf50bae544d
Mikhail Zakharchanka <mishaz@quartz1532.llnl.gov>
2020-09-17 11:30:20 -0700

    working towards storing int Fields sidre test

--- src/Pybind11Wraps/Utilities/sidreDataCollection.py
+++ src/Pybind11Wraps/Utilities/sidreDataCollection.py
@@ -2,22 +2,24 @@
 # sidreDataCollection
 #-------------------------------------------------------------------------------
 from PYB11Generator import *
-from axom import sidre
-from Field import Field
+#from axom import sidre
+#from Field import Field
 
-class sidreDataCollection:
+class SidreDataCollection:
 
     #...........................................................................
     # Constructors
     def pyinit(self):
         "Default constructor"
 
-     #...........................................................................
+    #...........................................................................
     # Methods
     @PYB11template("Dimension", "DataType")
     def alloc_view(self, view_name="const std::string&", field="const Spheral::Field<%(Dimension)s, %(DataType)s>&"):
         "Create a sidre view containing the data in a Field"
         return "void"
+
+    alloc_viewIntField1D = PYB11TemplateMethod(alloc_view, ("Dim<1>", "int"), pyname="alloc_view")
     
     #...........................................................................
     # Attributes
