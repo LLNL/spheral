36e088f0449c99f57f494561b1ea7951e697dfb0
mdavis36 <davis291@llnl.gov>
2020-09-24 14:35:58 -0700

    target_compile_options needs individual options

--- cmake/spheral/SpheralAddLibs.cmake
+++ cmake/spheral/SpheralAddLibs.cmake
@@ -101,7 +101,7 @@ function(spheral_add_pybind11_library package_name)
                   SHARED       TRUE
                   )
   add_dependencies(${MODULE_NAME} ${spheral_py_depends} ${spheral_depends})
-  target_compile_options(${MODULE_NAME} PRIVATE "-Wno-unused-local-typedefs -Wno-self-assign-overloaded -Wno-overloaded-virtual")
+  target_compile_options(${MODULE_NAME} PRIVATE "-Wno-unused-local-typedefs" "-Wno-self-assign-overloaded" "-Wno-overloaded-virtual")
 
   install(TARGETS     ${MODULE_NAME}
           DESTINATION Spheral
