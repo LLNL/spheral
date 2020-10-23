0528a95fabb7dbc17b2c74681081247441a1bb39
Mike Owen <mikeowen@llnl.gov>
2020-10-23 16:51:15 -0700

    These methods should have been const.

--- src/Geometry/GeomFacet1d.hh
+++ src/Geometry/GeomFacet1d.hh
@@ -36,7 +36,7 @@ public:
   const Vector& normal() const;
 
   // For consistency with GeomFacet3d.
-  void decompose(std::vector<std::array<Vector, 1>>& subfacets);
+  void decompose(std::vector<std::array<Vector, 1>>& subfacets) const;
   
 private:
   //--------------------------- Private Interface ---------------------------//
