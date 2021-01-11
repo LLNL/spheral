032ac07cf8b615e7af24d87eb580ad0a3f4c8b61
Mike Owen <mikeowen@llnl.gov>
2021-01-11 13:26:04 -0800

    Adding a new argument for LC build scripts (-h) to specify the path to the host configs.

--- scripts/lc-builds/toss3_gcc8.3.1-debug-mpi-python.sh
+++ scripts/lc-builds/toss3_gcc8.3.1-debug-mpi-python.sh
@@ -16,7 +16,7 @@ cmake \
   ${SRC_DIR} \
   -DCMAKE_BUILD_TYPE=Debug \
   -DCMAKE_CXX_COMPILER=/usr/tce/packages/gcc/gcc-8.3.1/bin/g++ \
-  -C ${SRC_DIR}/host-configs/lc-builds/toss3/gccX_tpl.cmake \
+  -C ${HOST_CONFIGS_DIR}/lc-builds/toss3/gccX_tpl.cmake \
   -DENABLE_OPENMP=On \
   -DENABLE_MPI=On \
   -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
