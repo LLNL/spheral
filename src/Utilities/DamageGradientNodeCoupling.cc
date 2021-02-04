1f56cf189ee95ff50d2bc670eb23e37a293720bc
Mike Owen <mikeowen@llnl.gov>
2021-02-03 18:18:21 -0800

    Tuning damage gradient algorithm.

--- src/Utilities/DamageGradientNodeCoupling.cc
+++ src/Utilities/DamageGradientNodeCoupling.cc
@@ -79,19 +79,24 @@ DamageGradientNodeCoupling(const State<Dimension>& state,
   // as the f_couple parameter in the NodePairIdxType.
   TIME_DamageGradientCoupling_pairs.start();
   size_t i, il, j, jl;
-  Vector xji;
-#pragma omp parallel for private(i, il, j, jl, xji)
+  Scalar hij;
+  Vector xjihat;
+#pragma omp parallel for private(i, il, j, jl, hij, xjihat)
   for (auto k = 0u; k < npairs; ++k) {
     i = pairs[k].i_node;
     il = pairs[k].i_list;
     j = pairs[k].j_node;
     jl = pairs[k].j_list;
-    xji = position(jl, j) - position(il, i);
+    xjihat = (position(jl, j) - position(il, i)).unitVector();
     // di = (D(il, i)*xijhat).magnitude();
     // dj = (D(jl, j)*xijhat).magnitude();
     // CHECK(di >= 0.0 and di <= 1.0);
     // CHECK(dj >= 0.0 and dj <= 1.0);
-    pairs[k].f_couple = std::max(0.0, 1.0 + std::min(0.0, (gradD(il, i).dot(xji))*(gradD(jl, j).dot(xji))));
+    hij = 2.0/((H(il,i)*xjihat).magnitude() + (H(jl,j)*xjihat).magnitude());
+    pairs[k].f_couple = std::max(0.0, 1.0 + std::min(0.0, gradD(il, i).dot(gradD(jl, j))*4.0*hij));
+    // if (i == 47 and j == 49) {
+    //   std::cerr << " --> " << pairs[k] << " " << D(il,i) << " " << D(jl,j) << " " << gradD(il,i) << " " << gradD(jl,j) << " " << gradD(il,i).dot(gradD(jl,j)) << " " << hij << " : " << pairs[k].f_couple << std::endl;
+    // }
     CHECK(pairs[k].f_couple >= 0.0 and pairs[k].f_couple <= 1.0);
   }
   TIME_DamageGradientCoupling_pairs.stop();
