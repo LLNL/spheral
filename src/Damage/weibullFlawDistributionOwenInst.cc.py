63ba22a86e703f67e9cea7a5855c9fc60c9e1131
Mike Owen <mikeowen@llnl.gov>
2021-02-11 16:23:27 -0800

    Splitting up the Weibull genereration functions into their own files. It's just bad practice stuffing those functions in one header and so forth.

---
+++ src/Damage/weibullFlawDistributionOwenInst.cc.py
@@ -0,0 +1,16 @@
+text = """
+//------------------------------------------------------------------------------
+// Explicit instantiation.
+//------------------------------------------------------------------------------
+#include "Geometry/Dimension.hh"
+#include "Damage/weibullFlawDistributionOwen.cc"
+
+template Spheral::Field<Spheral::Dim< %(ndim)s >, std::vector<double> > 
+Spheral::weibullFlawDistributionOwen<Spheral::Dim< %(ndim)s > >(const unsigned,
+                                                                const double,
+                                                                const double,
+                                                                const Spheral::FluidNodeList<Spheral::Dim< %(ndim)s > >&,
+                                                                const int,
+                                                                const double,
+                                                                const Spheral::Field<Spheral::Dim<%(ndim)s>, int>&);
+"""
