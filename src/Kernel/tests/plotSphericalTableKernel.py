Fixed the kernel gradient error estimate.

src/Kernel/tests/plotSphericalTableKernel.py
@@ -103,8 +103,8 @@ print("Required %0.4f sec to construct SphericalTableKernel"% (t1 - t0))
 #-------------------------------------------------------------------------------
 # Return a useful r_j range for a given r_i
 #-------------------------------------------------------------------------------
-def rprange(r):
-    return np.arange(max(0.01, r - 2.0), r + 2.0, 0.05)
+def rprange(r, rstep=0.05):
+    return np.arange(max(0.01, r - 2.0), r + 2.0, rstep)
 
 #-------------------------------------------------------------------------------
 # Reproduce Fig 1 from Omang, M., Borve, S., & Trulsen, J. (2006)
@@ -226,11 +226,12 @@ ax.set_title("Numpy gradient")
 # Kernel gradient error
 ax = fig20.add_subplot(gs[1,:])
 for r in rvals:
-    rp = rprange(r)
-    yvals = np.array([W(Vector1d(rpi), Vector1d(r), 1.0) for rpi in rp])
+    rp = rprange(r, rstep=0.05)
+    rpfine = rprange(r, rstep=0.001)
     gyvals = np.array([W.grad(Vector1d(rpi), Vector1d(r), 1.0) for rpi in rp])
-    gyvals0 = np.gradient(yvals, rp)
-    errvals = np.array([abs(gyvals[i] - gyvals0[1])/max(1e-5, abs(gyvals0[i])) for i in xrange(len(gyvals))])
+    yvals = np.array([W(Vector1d(rpi), Vector1d(r), 1.0) for rpi in rpfine])
+    gyvals0 = np.gradient(yvals, rpfine)
+    errvals = np.array([abs(gyvals[i] - gyvals0[50*i])/max(1e-5, abs(gyvals0[50*i])) for i in xrange(len(gyvals))])
     ax.semilogy(rp - r, errvals, label = r"$r/h=%g$" % r)
 ax.set_xlabel(r"$(r^\prime - r)/h$")
 ax.set_ylabel(r"$|\langle \partial_r W_{3S1}(r^\prime, r, h) \rangle - \partial_r W_{3S1}(r^\prime, r, h)|/|\partial_r W_{3S1}(r^\prime, r, h)|$")