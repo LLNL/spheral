43409b4b9df7a1462378a0a63523c90a50080ce6
Mike Owen <mikeowen@llnl.gov>
2021-04-21 14:18:18 -0700

    Bugfix for local TPL builds.

--- cmake/tpl/util/Install_Python_distutils_library.cmake
+++ cmake/tpl/util/Install_Python_distutils_library.cmake
@@ -5,9 +5,9 @@ macro(Install_Python_distutils_library lib_name lib_file lib_url lib_dir)
     COMMAND wget ${lib_url} --no-check-certificate -O ${lib_file} > ${lib_file}-download.log
     )
   add_custom_target(${lib_name}
-    COMMAND echo "-- pip installing ${lib_list}"
-    COMMAND ${PYTHON_EXE} ${PIP_EXE} ${OUT_PROTOCOL_PIP} install ${${lib_list}} --no-index --find-links ${CACHE_DIR}
-    DEPENDS pip-install ${${lib_list}_DEPENDS} ${lib_file}
+    COMMAND echo "-- pip installing ${lib_file}"
+    COMMAND ${PYTHON_EXE} ${PIP_EXE} ${OUT_PROTOCOL_PIP} install ${${lib_file}} --no-index --find-links ${CACHE_DIR}
+    DEPENDS pip-install ${${lib_file}_DEPENDS} ${lib_file}
     )
   list(APPEND spheral_py_depends ${lib_name})
 endmacro()
