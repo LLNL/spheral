5671a6b04155095352cc2e3b41c2b6abdd800ac2
mdavis36 <davis291@llnl.gov>
2021-05-21 10:11:06 -0700

    Raw Cuda kernel test.

--- src/CXXTests/Spheral_CUDA_Test.cc
+++ src/CXXTests/Spheral_CUDA_Test.cc
@@ -1,7 +1,24 @@
 #include <iostream>
 
+
+__global__ void add(int a, int b, int *c)
+{
+  *c = a + b;
+}
+
 int main() {
   std::cout << "Hello World\n";
+
+  int a,b,c;
+  int *d_c;
+
+  a = 3; b = 4;
+  cudaMalloc((void**) &d_c, sizeof(int));
+  add<<<1,1>>>(a,b,d_c);
+  cudaMemcpy(&c, d_c, sizeof(int), cudaMemcpyDeviceToHost);
+
+  std::cout << "C : " << c << "\n";
+  cudaFree(d_c);
   
   return EXIT_SUCCESS;
 }
