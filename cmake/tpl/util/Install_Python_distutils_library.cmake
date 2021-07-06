caec37f3fab44550828581b36914605f996e38d8
mdavis36 <davis291@llnl.gov>
2021-07-06 16:13:31 -0700

    Fixing gnuplot & ats install issue, typo and adding pip modules dependencies.

--- cmake/tpl/util/Install_Python_distutils_library.cmake
+++ cmake/tpl/util/Install_Python_distutils_library.cmake
@@ -6,8 +6,8 @@ macro(Install_Python_distutils_library lib_name lib_file lib_url lib_dir)
     )
   add_custom_target(${lib_name}
     COMMAND echo "-- pip installing ${lib_file}"
-    COMMAND ${PIP_EXE} ${OUT_PROTOCOL_PIP} install ${${lib_file}} --no-index --find-links ${CACHE_DIR}
-    DEPENDS pip-install ${${lib_file}_DEPENDS} ${lib_file}
+    COMMAND ${PYTHON_EXE} ${PIP_EXE} ${OUT_PROTOCOL_PIP} install ${lib_file} --no-index --find-links ${CACHE_DIR}
+    DEPENDS pip-install pip-setup-modules pip-modules ${${lib_file}_DEPENDS} ${lib_file}
     )
   list(APPEND spheral_py_depends ${lib_name})
 endmacro()
