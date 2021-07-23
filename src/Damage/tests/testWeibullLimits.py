eb2f19b9776e028afe8ba74b58e1314ba6b384f0
Mike Owen <mikeowen@llnl.gov>
2021-07-23 10:10:04 -0700

    Updated our plots testing the min/max math for Weibull distributions. Looking good.

--- src/Damage/tests/testWeibullLimits.py
+++ src/Damage/tests/testWeibullLimits.py
@@ -14,29 +14,36 @@ import matplotlib.pyplot as plt
 commandLine(V = 100.0,          # volume
             k = 1e10,           # k Weibull constant (coefficient)
             m = 9.0,            # m Weibull constant (exponent)
-            Nflaws = 10000,     # number of flaws
-            Ntrials = 10000,    # number of trials
+            Nflaws = 5000,      # number of flaws
+            Ntrials = 5000,     # number of trials
             seed = 49489212)    # random number seed
+
+mInv = 1.0/m
+chi = (Nflaws/(k*V))**mInv
 
 #-------------------------------------------------------------------------------
 # Generate an example flaw distribution
 #-------------------------------------------------------------------------------
 random.seed(seed)
-minflaw, maxflaw = 2.0, -1.0
-for i in xrange(Nflaws):
-    fi = (Nflaws/(k*V)*random.random())**(1.0/m)
-    minflaw = min(minflaw, fi)
-    maxflaw = max(maxflaw, fi)
-print "Min/max flaws: [%g : %g]" % (minflaw, maxflaw)
+minflaws0, maxflaws0 = [2.0]*Ntrials, [-1.0]*Ntrials
+for itrial in xrange(Ntrials):
+    for i in xrange(Nflaws):
+        fi = chi*random.random()**mInv
+        minflaws0[itrial] = min(minflaws0[itrial], fi)
+        maxflaws0[itrial] = max(maxflaws0[itrial], fi)
+    #print "    %5i/%5i: Min/max flaws: [%g : %g]" % (itrial, Ntrials, minflaws0[itrial], maxflaws0[itrial])
+print "After creating flaw distributions the hard way:"
+print "  Range of minflaws: [%g : %g]" % (min(minflaws0), max(minflaws0))
+print "  Range of maxflaws: [%g : %g]" % (min(maxflaws0), max(maxflaws0))
 
 #-------------------------------------------------------------------------------
 # Generate distributions of the expected min/max flaws for comparison
 #-------------------------------------------------------------------------------
 minflaws = [(Nflaws/(k*V))**(1.0/m) * (1.0 - random.random()**(1.0/Nflaws))**(1.0/m) for i in xrange(Ntrials)]
-print "Range of minflaws: [%g : %g]" % (min(minflaws), max(minflaws))
-
 maxflaws = [(Nflaws/(k*V))**(1.0/m) * random.random()**(1.0/(m*Nflaws)) for i in xrange(Ntrials)]
-print "Range of maxflaws: [%g : %g]" % (min(maxflaws), max(maxflaws))
+print "Using math to estimate min/max flaw ranges:"
+print "  Range of minflaws: [%g : %g]" % (min(minflaws), max(minflaws))
+print "  Range of maxflaws: [%g : %g]" % (min(maxflaws), max(maxflaws))
 
 #-------------------------------------------------------------------------------
 # Now plot the results.
@@ -45,15 +52,32 @@ fig1 = plt.figure(1)
 fig2 = plt.figure(2)
 
 plt.figure(1)
+plt.hist(minflaws0,
+         bins = max(10, Ntrials/100),
+         color = "black",
+         label = "Randomly realized actual distribution")
 plt.hist(minflaws,
-         bins = max(10, Ntrials/100))
-plt.axvline(x=minflaw, color="red", linewidth=3)
+         bins = max(10, Ntrials/100),
+         color = "red",
+         alpha = 0.5,
+         label = "Predicted distribution")
+#plt.axvline(x=minflaw, color="red", linewidth=3)
 plt.xlabel(r"Min flaw activation energy ($\varepsilon_{\min}$)")
 plt.ylabel(r"N($\varepsilon_{\min}$)")
+plt.legend(loc="upper left")
 
 plt.figure(2)
+plt.hist(maxflaws0,
+         bins = max(10, Ntrials/100),
+         color = "black",
+         label = "Randomly realized actual distribution")
 plt.hist(maxflaws,
-         bins = max(10, Ntrials/100))
-plt.axvline(x=maxflaw, color="red", linewidth=3)
+         bins = max(10, Ntrials/100),
+         color = "red",
+         alpha = 0.5,
+         label = "Predicted distribution")
+#plt.axvline(x=maxflaw, color="red", linewidth=3)
+plt.yscale("log")
 plt.xlabel(r"Max flaw activation energy ($\varepsilon_{\max}$)")
 plt.ylabel(r"N($\varepsilon_{\max}$)")
+plt.legend(loc="upper left")
