2d96def26629000955fcd46114014d167a2ed7ce
jmpearl <pearl3@llnl.gov>
2021-08-04 10:24:36 -0700

    turned on Noh error test for FSISPH

--- tests/Hydro/Noh/Noh-planar-1d.py
+++ tests/Hydro/Noh/Noh-planar-1d.py
@@ -36,8 +36,8 @@
 #
 # Solid FSISPH
 #
-#ATS:t400 = test(        SELF, "--fsisph True --solid True --graphics None --clearDirectories True --checkError False --restartStep 20 --steps 40", label="Planar Noh problem with FSISPH -- 1-D (serial)")
-#ATS:t401 = testif(t400, SELF, "--fsisph True --solid True --graphics None --clearDirectories False --checkError False --restartStep 20 --restoreCycle 20 --steps 20 --checkRestart True", label="Planar Noh problem with FSISPH -- 1-D (serial) RESTART CHECK")
+#ATS:t400 = test(        SELF, "--fsisph True --solid True --graphics None --clearDirectories True --checkError True --restartStep 20 --steps 40", label="Planar Noh problem with FSISPH -- 1-D (serial)")
+#ATS:t401 = testif(t400, SELF, "--fsisph True --solid True --graphics None --clearDirectories False --checkError True --restartStep 20 --restoreCycle 20 --steps 20 --checkRestart True", label="Planar Noh problem with FSISPH -- 1-D (serial) RESTART CHECK")
 
 import os, shutil
 from SolidSpheral1d import *
