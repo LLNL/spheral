d1dc86aaea4f984f72992b610cf8ebe99a1a56dd
Mike Owen <mikeowen@llnl.gov>
2021-08-04 13:27:38 -0700

    Fixing a command line argument in the ATS test line.

--- tests/Strength/DiametralCompression/DiametralCompression.py
+++ tests/Strength/DiametralCompression/DiametralCompression.py
@@ -4,7 +4,7 @@
 #
 # Solid FSISPH
 #
-#ATS:t100 = test(        SELF, "--fsisph True  --clearDirectories True --checkError True --goalTime 5.0 --nrSpecimen 15 ", label="Diametral Compression Test FSISPH -- 2-D", np=1)
+#ATS:t100 = test(        SELF, "--clearDirectories True --checkError True --goalTime 5.0 --nrSpecimen 15 ", label="Diametral Compression Test FSISPH -- 2-D", np=1)
 
 from Spheral2d import *
 import sys, os
