e114a98438699137b9c0615ac1fe329a6cc2cd4d
Jason Pearl <pearl3@llnl.gov>
2021-09-21 12:35:55 -0700

    allowing explicit particle radius initialization in DEM constructor

--- src/DEM/DEM.py
+++ src/DEM/DEM.py
@@ -37,9 +37,12 @@ for dim in dims:
 #-------------------------------------------------------------------------------
 def DEM(dataBase,
         W,
+        particleRadius = None,
         cfl = 0.05,
         xmin = (-1e100, -1e100, -1e100),
         xmax = ( 1e100,  1e100,  1e100)):
+
+    assert dataBase.numDemNodeLists == dataBase.numNodeLists
 
     # We use the provided DataBase to sniff out what sort of NodeLists are being
     # used, and based on this determine which SPH object to build.
@@ -47,6 +50,21 @@ def DEM(dataBase,
 
     Constructor = eval("DEM%id" % ndim)
 
+    # if a particle radius is specified set it.
+    if particleRadius is not None: 
+        for DEMNodeList in dataBase.DEMNodeLists:
+            radii = DEMNodeList.particleRadius()
+            for i in range(DEMNodeList.numInternalNodes):
+               radii[i] = particleRadius
+
+    # do it based on nPerh and H
+    else:
+        for DEMNodeList in dataBase.DEMNodeLists:
+            radii = DEMNodeList.particleRadius()
+            H = DEMNodeList.HField()
+            nPerh = DEMNodeList.nPerh
+            for i in range(DEMNodeList.numInternalNodes):
+               radii[i] = 0.5/(nodesPerSmoothingScale*H[i].Determinant())
 
     # Build the constructor arguments
     xmin = (ndim,) + xmin
