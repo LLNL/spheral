e84fe5ccbea83119a6a54a4c0b84fd99c6eb77c2
Brody Richard Bassett <bassett4@llnl.gov>
2021-09-27 19:08:26 -0700

    Added missing header. Changed conduit to use static libraries as is done for axom.

--- cmake/tpl/conduit.cmake
+++ cmake/tpl/conduit.cmake
@@ -6,9 +6,9 @@ set(CONDUIT_CACHE "${CACHE_DIR}/${CONDUIT_DIST}")
 list(APPEND ${lib_name}_INCLUDES $<BUILD_INTERFACE:${${lib_name}_DIR}/include/${lib_name}>)
 
 set(${lib_name}_libs 
-    libconduit.so
-    libconduit_blueprint.so
-    libconduit_relay.so
+    libconduit.a
+    libconduit_blueprint.a
+    libconduit_relay.a
    )
 
 if(${lib_name}_BUILD)
