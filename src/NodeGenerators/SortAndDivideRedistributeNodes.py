6aa9ee60fa04d358f1ef0e3c749ab4846a2cf6a1
Mike Owen <mikeowen@llnl.gov>
2021-10-22 16:54:41 -0700

    Getting bit-perfect same results as reference data requires using the same node distributor.

--- src/NodeGenerators/SortAndDivideRedistributeNodes.py
+++ src/NodeGenerators/SortAndDivideRedistributeNodes.py
@@ -3,4 +3,4 @@ from spheralDimensions import spheralDimensions
 dims = spheralDimensions()
 
 for dim in dims:
-    exec("import Spheral{dim}d; distributeNodes{dim}d = makeDistributeNodesMethod('{distributor}', Spheral{dim}d)".format(dim=dim, distributor="SortAndDivideRedistribute"))
+    exec("import Spheral{dim}d; distributeNodes{dim}d = makeDistributeNodesMethod('{distributor}', Spheral{dim}d)".format(dim=dim, distributor="SortAndDivideRedistributeNodes"))
