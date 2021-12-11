96941cb0f09b27bc8bc554206932b575ed318e8d
mdavis36 <davis291@llnl.gov>
2021-12-10 18:35:36 -0800

    Pass at gitlab script with mirror.

--- scripts/gitlab/build_and_test.sh
+++ scripts/gitlab/build_and_test.sh
@@ -24,31 +24,49 @@ then
 
     upstream_opt=""
     prefix_opt=""
-    if [[ "${option}" != "--tpl-only" ]]
-    then
-        #prefix="/dev/shm/${hostname}"
-        prefix="build/${hostname}"
-        if [[ -z ${job_unique_id} ]]; then
+
+
+    prefix="../${hostname}"
+    if [[ -z ${job_unique_id} ]]; then
+      job_unique_id=manual_job_$(date +%s)
+      while [[ -d ${prefix}/${job_unique_id} ]] ; do
+          sleep 1
           job_unique_id=manual_job_$(date +%s)
-          while [[ -d ${prefix}/${job_unique_id} ]] ; do
-              sleep 1
-              job_unique_id=manual_job_$(date +%s)
-          done
-        fi
-
-        prefix="${prefix}/${job_unique_id}"
-        #prefix="/usr/WS2/davis291/SPHERAL/spheral-pr/shm/rzgenie47/manual_job_1637216151"
-        mkdir -p ${prefix}
-        prefix_opt="--prefix=${prefix}"
-
-        #upstream_opt="--upstream=/usr/WS2/davis291/SPHERAL/lc_uberenv_tpl2/spack/opt/spack"
-        upstream_opt="--upstream=/usr/workspace/wsb/davis291/SPHERAL/spheral-spack-tpls/uberenv_libs/spack/opt/spack/__spack_path_placeholder__/__spack_path_placeholder__"
+      done
     fi
 
-    python3 scripts/uberenv/uberenv.py --spec="${spec}" --reuse=True ${upstream_opt} ${prefix_opt}
+    prefix="${prefix}/${job_unique_id}"
+    spack_env_dir="${prefix}/spack-env"
+    mirror_dir="/usr/WS2/davis291/SPHERAL/spheral-tpl/mirror"
+    src_dir=pwd
 
-    echo "Activating spack instance."
+    #prefix="/usr/WS2/davis291/SPHERAL/spheral-pr/shm/rzgenie47/manual_job_1637216151"
+
+    mkdir -p ${prefix}
+    mkdir -p ${spack_env_dir}
+    prefix_opt="--prefix=${prefix}"
+
+    #upstream_opt="--upstream=/usr/WS2/davis291/SPHERAL/lc_uberenv_tpl2/spack/opt/spack"
+    #upstream_opt="--upstream=/usr/workspace/wsb/davis291/SPHERAL/spheral-spack-tpls/uberenv_libs/spack/opt/spack/__spack_path_placeholder__/__spack_path_placeholder__"
+    #fi
+
+    #python3 scripts/uberenv/uberenv.py --spec="${spec}" --reuse=True ${upstream_opt} ${prefix_opt}
+    python3 scripts/uberenv/uberenv.py --setup-only ${prefix_opt}
+
+    echo "Activating spack instance..."
     . ${prefix}/spack/share/spack/setup-env.sh
+
+    echo "Create and activate Spack environment..."
+    cd ${spack_env_dir}
+    spack env create -d .
+    spacktivate .
+
+    echo "Adding Spheral-tpl mirror..."
+    spack mirror add spheral-tpl ${mirror_dir}
+    spack gpg trust `find ${mirror_dir} -name "*.pub"`
+
+    echo "spack dev-build spheral@develop${spec}"
+    spack dev-build -d ${pwd} spheral@develop${spec}
 
     spheral_spack_hash=`spack find -L spheral | grep spheral | cut -d ' ' -f1`
     spheral_install_prefix=`spack find -p spheral | grep spheral | cut -d ' ' -f3`
@@ -61,8 +79,8 @@ then
     echo "Spheral import test..."
     ./spheral -c "import Spheral"
 
-    echo "Spheral ats test..."
-    ./spheral-atstest tests/integration.ats
+    #echo "Spheral ats test..."
+    #./spheral-atstest tests/integration.ats
 
     ## TODO inject debug and non mpi filters when appropriate ...
     #echo "Running spheral-atstest from ${spheral_install_prefix} ..."
