f4d9bd937a0ef259f7f36ba760b6c471a52d40f4
mdavis36 <davis291@llnl.gov>
2021-12-13 18:50:18 -0800

    build_and_test script pass; Adding upstreams back in.

--- scripts/gitlab/build_and_test.sh
+++ scripts/gitlab/build_and_test.sh
@@ -26,7 +26,8 @@ then
     prefix_opt=""
 
 
-    prefix="../${hostname}"
+    src_dir=$(pwd)
+    prefix="${src_dir}/../${hostname}"
     if [[ -z ${job_unique_id} ]]; then
       job_unique_id=manual_job_$(date +%s)
       while [[ -d ${prefix}/${job_unique_id} ]] ; do
@@ -36,17 +37,14 @@ then
     fi
 
     prefix="${prefix}/${job_unique_id}"
-    spack_env_dir="${prefix}/spack-env"
-    mirror_dir="/usr/WS2/davis291/SPHERAL/spheral-tpl/mirror"
-    src_dir=$(pwd)
-
-    #prefix="/usr/WS2/davis291/SPHERAL/spheral-pr/shm/rzgenie47/manual_job_1637216151"
+    #spack_env_dir="${prefix}/spack-env"
+    mirror_dir="/usr/WS2/davis291/SPHERAL/uberenv-tpl/mirror"
+    #mirror_dir="/usr/WS2/davis291/SPHERAL/spheral-tpl/mirror"
 
     mkdir -p ${prefix}
-    mkdir -p ${spack_env_dir}
+    #mkdir -p ${spack_env_dir}
     prefix_opt="--prefix=${prefix}"
 
-    #upstream_opt="--upstream=/usr/WS2/davis291/SPHERAL/lc_uberenv_tpl2/spack/opt/spack"
     #upstream_opt="--upstream=/usr/workspace/wsb/davis291/SPHERAL/spheral-spack-tpls/uberenv_libs/spack/opt/spack/__spack_path_placeholder__/__spack_path_placeholder__"
     #fi
 
@@ -56,23 +54,22 @@ then
     echo "Activating spack instance..."
     . ${prefix}/spack/share/spack/setup-env.sh
 
-    echo "Create and activate Spack environment..."
-    cd ${spack_env_dir}
-    spack env create -d .
-    spack env activate .
 
-    echo "Adding Spheral-tpl mirror..."
-    spack mirror add spheral-tpl ${mirror_dir}
+    if spack mirror list | grep spheral-tpl; then
+      echo "Spheral-tpl mirror found."
+    else
+      echo "Adding Spheral-tpl mirror..."
+      spack mirror add spheral-tpl ${mirror_dir}
+    fi
+
     spack gpg trust `find ${mirror_dir} -name "*.pub"`
 
     echo "spack dev-build spheral@develop${spec}"
-    spack dev-build -d ${src_dir} spheral@develop${spec}
+    spack spec -I spheral@develop${spec}
+    spack dev-build -d ${src_dir} spheral@develop${spec} 2>&1 | tee -a dev-build-out.txt
 
-    echo "Activating spack instance..."
-    . ${prefix}/spack/share/spack/setup-env.sh
-
-    spheral_spack_hash=`spack find -L spheral | grep spheral | cut -d ' ' -f1`
-    spheral_install_prefix=`spack find -p spheral | grep spheral | cut -d ' ' -f3`
+    spheral_spack_hash=$(tail -1 dev-build-out.txt | grep "\[+\]" | rev | cut -d"-" -f 1 | rev)
+    spheral_install_prefix=$(tail -1 dev-build-out.txt | grep "\[+\]" | cut -d" " -f 2)
 
     echo "Loading spheral ${spheral_spack_hash} ..."
     spack load /${spheral_spack_hash}
