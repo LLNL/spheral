383be71df7972b5f289671634b4fc12d8fe1b981
Benjamin T. Liu <liu15@llnl.gov>
2022-06-06 10:35:27 -0700

    Build fix for spheral (undoing hack)

--- cmake/tpl/qhull.cmake
+++ cmake/tpl/qhull.cmake
@@ -1,15 +1,11 @@
-# Setting this to just the release library until we support TPL debug builds on LC
 set(${lib_name}_libs libqhull.so)
-#if(ENABLE_STATIC_TPL)
-#  set(${lib_name}_libs libqhullstatic.a)
-#endif()
-#set(QHULL_BUILD_TYPE Release)
+# Default to just the release library until we support TPL debug builds on LC
+set(QHULL_BUILD_TYPE Release CACHE STRING "qhull build type")
 if(ENABLE_STATIC_TPL)
-  if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+  if (QHULL_BUILD_TYPE STREQUAL "Debug")
     set(${lib_name}_libs libqhullstatic_d.a)
   else()
     set(${lib_name}_libs libqhullstatic.a)
   endif()
 endif()
-set(QHULL_BUILD_TYPE ${CMAKE_BUILD_TYPE})
 
