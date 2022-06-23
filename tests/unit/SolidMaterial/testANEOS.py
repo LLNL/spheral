5642bbce53bdb42ae132c61c964eaae515d145da
Mike Owen <mikeowen@llnl.gov>
2022-06-23 14:19:45 -0700

    Fixed convenient material name constructors for ANEOS, and adding a unit test to catch this problem.

---
+++ tests/unit/SolidMaterial/testANEOS.py
@@ -0,0 +1,40 @@
+#ATS:test(SELF, label="ANEOS unit tests.")
+# Unit tests of the ANEOS equation of state.  This is a silly test, just checking
+# that our convenient constructors using the provided inputs build properly.
+import unittest
+from Spheral1d import *
+
+#===============================================================================
+# Unit tests.
+#===============================================================================
+class TestANEOS(unittest.TestCase):
+
+    #===========================================================================
+    # quartz
+    #===========================================================================
+    def test_quartz(self):
+        units = CGS()
+        eos = ANEOS(material = "quartz",
+                    constants = units)
+
+    #===========================================================================
+    # dunite
+    #===========================================================================
+    def test_dunite(self):
+        units = CGS()
+        eos = ANEOS(material = "dunite",
+                    constants = units)
+
+    #===========================================================================
+    # serpentine
+    #===========================================================================
+    def test_serpentine(self):
+        units = CGS()
+        eos = ANEOS(material = "serpentine",
+                    constants = units)
+
+#===============================================================================
+# Run the suckers.
+#===============================================================================
+if __name__ == "__main__":
+    unittest.main()
