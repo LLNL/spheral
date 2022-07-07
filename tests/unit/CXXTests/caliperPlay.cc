7bd12b3600c63b9cd308150b0e1b5e281af4dfce
MishaZakharchanka <mishaz@rzgenie5.llnl.gov>
2022-07-06 19:30:27 -0700

    Caliper working in build

--- tests/unit/CXXTests/caliperPlay.cc
+++ tests/unit/CXXTests/caliperPlay.cc
@@ -7,5 +7,15 @@ int main()
   CALI_MARK_BEGIN("area");
   std::cout << "Hello World!" << std::endl;
   CALI_MARK_END("area");
+
+  int a = 12398745;
+
+  CALI_CXX_MARK_LOOP_BEGIN(mainloop_id, "mainloop");
+  for (int i = 0; i < 100; ++i)
+  {
+    CALI_CXX_MARK_LOOP_ITERATION(mainloop_id, i);
+    a += 13;
+  }
+  CALI_CXX_MARK_LOOP_END(mainloop_id);
   return 0;
 }
