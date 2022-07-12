8b941da1528dfe1b294a0f2d623d150ecc475b27
jmpearl <pearl3@llnl.gov>
2022-07-12 12:45:37 -0700

    update ATS label for DEM cohesion test

--- tests/functional/DEM/LinearSpringDEM/twoParticleCollision-2d.py
+++ tests/functional/DEM/LinearSpringDEM/twoParticleCollision-2d.py
@@ -1,7 +1,7 @@
 #ATS:DEM2d0 = test(          SELF, "--clearDirectories True  --checkError True --checkConservation True  --restartStep 10 --steps 100", label="DEM individual particle collision restitution 0.55 -- 2-D (serial)")
 #ATS:DEM2d1 = testif(DEM2d0, SELF, "--clearDirectories False --checkError False  --restartStep 100 --restoreCycle 10 --steps 10 --checkRestart True", label="DEM individual particle collision restitution 0.55  -- 2-D (serial) RESTART CHECK")
 #ATS:DEM2d2 = test(          SELF, "--clearDirectories True  --checkError True --checkConservation True  --normalRestitutionCoefficient 1.0 --steps 100", label="DEM individual particle collision restitution 1.00 -- 2-D (serial)")
-#ATS:DEM2d3 = test(          SELF, "--clearDirectories True  --checkError True --checkConservation True  --normalRestitutionCoefficient 0.60 --cohesiveTensileStrength 1000.0 --steps 100", label="DEM individual particle collision restitution 1.00 -- 2-D (serial)")
+#ATS:DEM2d3 = test(          SELF, "--clearDirectories True  --checkError True --checkConservation True  --normalRestitutionCoefficient 0.60 --cohesiveTensileStrength 1000.0 --steps 100", label="DEM individual particle collision restitution 0.60 with cohesion -- 2-D (serial)")
 
 import os, sys, shutil, mpi
 from math import *
