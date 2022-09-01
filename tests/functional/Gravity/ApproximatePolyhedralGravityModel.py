43ef557cd73cdb7a16d0e030a8684d0600cc4cc5
Jason Pearl <jasonmpearl91@gmail.com>
2022-08-31 18:19:28 -0700

    ats test for approx polyhedral grav model

---
+++ tests/functional/Gravity/ApproximatePolyhedralGravityModel.py
@@ -0,0 +1,47 @@
+#-------------------------------------------------------------------------------
+# Set up a pair of equal mass N-body points in a simple circular orbit of each
+# other.
+#-------------------------------------------------------------------------------
+from Spheral3d import *
+from SpheralTestUtilities import *
+from SpheralGnuPlotUtilities import *
+from NodeHistory import *
+from PolyhedronFileUtilities import *
+from SpheralVisitDump import dumpPhysicsState
+from math import *
+import os, sys
+
+print "Test ApproximatePolyhedralGravityModel"
+
+#-------------------------------------------------------------------------------
+# Generic problem parameters
+#-------------------------------------------------------------------------------
+commandLine(fileName = "data/Bennu_996.obj",  # bennu surface definition
+            M = 7.8e10,                        # mass of bennu
+            G = 6.67e-11,
+
+            pos = Vector(500.0,0.0,0.0),
+            nominalAcceleration = Vector(-0.0000214555067869693,   0.0000000066680564648,  -0.0000000589458328628), 
+            errorThreshold = 1e-4,
+    )
+
+
+G = 6.67e-11
+
+# read our mesh
+polyBennu = readPolyhedronOBJ(fileName)
+
+# create the gravity model
+GravModel = ApproximatePolyhedralGravityModel(polyBennu,M,G)
+
+a = GravModel.acceleration(pos)
+
+error = 100*(a - nominalAcceleration).magnitude()/nominalAcceleration.magnitude()
+
+
+print "error acceleration field precentage : %0.15f" % error
+
+if error > errorThreshold:
+    raise ValueError, "error bounds exceeded"
+
+
