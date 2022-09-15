05cef59fc8a895a4f7043f20ed50d1a2eaae1152
mdavis36 <davis291@llnl.gov>
2022-09-15 15:26:03 -0700

    Removing old python cmake tpl files.

--- cmake/tpl/decorator.cmake
+++
@@ -1,5 +0,0 @@
-set(${lib_name}_NO_INCLUDES On)
-include(${SPHERAL_ROOT_DIR}/cmake/tpl/util/Install_Python_distutils_library.cmake)
-set(decorator_IMPORT PYB11Generator PARENT_SCOPE)
-set(decorator_DEPENDS setuptools PARENT_SCOPE)
-set(decorator_NO_INCLUDES On)
--- cmake/tpl/setuptools.cmake
+++
@@ -1,4 +0,0 @@
-set(${lib_name}_NO_INCLUDES On)
-include(${SPHERAL_ROOT_DIR}/cmake/tpl/util/Install_Python_distutils_library.cmake)
-set(setuptools_IMPORT setuptools PARENT_SCOPE)
-set(setuptools_NO_INCLUDES On)
