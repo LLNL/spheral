5c217aba04d104b5887f22969efdaa6fa6c999d2
mdavis36 <davis291@llnl.gov>
2022-10-03 06:37:55 -0700

    Don't need full node for drm.

--- scripts/gitlab/clean_spheral_builds.sh
+++ scripts/gitlab/clean_spheral_builds.sh
@@ -4,7 +4,7 @@ ls -ltd */ | sed "1,$1d" | rev | cut -d ' ' -f1 | rev &> dir_list
 if [ -s dir_list ]; then
   echo "Removing:"
   cat dir_list
-  srun -N 1 -n 20 drm `< dir_list`
+  srun -n 20 drm `< dir_list`
 else
   echo "No directories to remove at this time."
 fi
