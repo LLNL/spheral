2bb07f4f3207ad3e86b260a98f7e71941883a907
MishaZakharchanka <mishaz@rzgenie14.llnl.gov>
2022-10-12 11:51:12 -0700

    removing python from dependancies for C++ build

--- cmake/spheral/SpheralAddLibs.cmake
+++ cmake/spheral/SpheralAddLibs.cmake
@@ -34,7 +34,7 @@ function(spheral_add_obj_library
   blt_add_library(NAME        Spheral_${package_name}
                   HEADERS     ${${package_name}_headers}
                   SOURCES     ${${package_name}_sources}
-                  DEPENDS_ON  ${spheral_blt_depends} ${spheral_blt_cxx_depends} blt_python ${${package_name}_ADDITIONAL_DEPENDS} ${SPHERAL_CXX_DEPENDS}
+                  DEPENDS_ON  ${spheral_blt_depends} ${spheral_blt_cxx_depends} ${${package_name}_ADDITIONAL_DEPENDS} ${SPHERAL_CXX_DEPENDS}
                   OBJECT TRUE
                   )
 
