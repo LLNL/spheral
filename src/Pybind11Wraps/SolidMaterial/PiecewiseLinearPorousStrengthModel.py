a84c0a3cda76b93aadf05dd7f9bcc3428f579ab3
jmpearl <pearl3@llnl.gov>
2022-12-09 10:06:25 -0800

    allow the user to specify piecewise linear shear and yield with porosity

---
+++ src/Pybind11Wraps/SolidMaterial/PiecewiseLinearPorousStrengthModel.py
@@ -0,0 +1,40 @@
+#-------------------------------------------------------------------------------
+# PorousStrengthModel
+#-------------------------------------------------------------------------------
+from PYB11Generator import *
+from PorousStrengthModel import *
+
+@PYB11template("Dimension")
+@PYB11module("SpheralSolidMaterial")
+class PiecewiseLinearPorousStrengthModel(PorousStrengthModel):
+    """PorousStrengthModel
+
+    The shear modulus and yield strength ratios are treated as piecewise
+    linear functions of the porosity. Here ratio means the porous value
+    divided by the value from the solidStrength model."""
+
+    PYB11typedefs = """
+    using Scalar = typename %(Dimension)s::Scalar;
+    using SymTensor = typename %(Dimension)s::SymTensor;
+    using ScalarField = Field<%(Dimension)s, Scalar>;
+"""
+
+    #...........................................................................
+    # Constructors
+    def pyinit(self,
+               solidStrength = "const StrengthModel<%(Dimension)s>&",
+               porosityAbscissa = "const vector<Scalar>",
+               shearModulusRatios = "const vector<Scalar>",
+               yieldStrengthRatios = "const vector<Scalar>",):
+        "Construct with the strength model we're modifying"
+
+
+
+    #...........................................................................
+    # Properties
+    porosityAbscissa = PYB11property("vector<Scalar>&","porosityAbscissa", returnpolicy="reference_internal")
+    shearModulusRatios = PYB11property("vector<Scalar>&","shearModulusRatios", returnpolicy="reference_internal")
+    yieldStrengthRatios = PYB11property("vector<Scalar>&","yieldStrengthRatios", returnpolicy="reference_internal")
+
+
+
