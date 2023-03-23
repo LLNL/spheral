b5bc92b56f4c25c4298f8cde7e6021fb05f6b821
Mike Owen <mikeowen@llnl.gov>
2023-03-23 16:30:51 -0700

    Checkpoint looking at polytope python problems

--- scripts/spack/packages/polytope/package.py
+++ scripts/spack/packages/polytope/package.py
@@ -14,7 +14,8 @@ class Polytope(CMakePackage):
     #version('0.7.0', sha256='e7be5a5d06a95309b9100f02ab80c2f3401c11ac6304a3204fb1f25052efd77e')
 
     git = "https://github.com/pbtoast/polytope.git"
-    version('0.7.2', tag='0.7.2', submodules=True)
+    #version('0.7.2', tag='0.7.2', submodules=True)
+    version("2023-03-23", commit="77c0f29", submodules=True)
 
     variant('python', default=True, description='Enable Python Support.')
 
