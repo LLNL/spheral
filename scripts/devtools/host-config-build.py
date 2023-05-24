41d65b1afc63579b3485ab147413d991231355cd
mdavis36 <davis291@llnl.gov>
2023-05-24 09:50:33 -0700

    python3

--- scripts/devtools/host-config-build.py
+++ scripts/devtools/host-config-build.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import sys
@@ -123,12 +123,12 @@ def main():
       print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
       sys.exit(1)
 
-    ## Did we build the python interface?
-    #if sexe("grep -i ENABLE_CXXONLY:BOOL=Off {0}/CMakeCache.txt".format(build_dir)) == 0:
-    #  # Try to import Spheral for a basic sanity test.
-    #  smoke_test = sexe("{0} {1}/spheral -c \"import Spheral\"".format("module load gcc/10.3.1 &&", install_dir))
-    #  if smoke_test != 0:
-    #    sys.exit(1)
+    # Did we build the python interface?
+    if sexe("grep -i ENABLE_CXXONLY:BOOL=Off {0}/CMakeCache.txt".format(build_dir)) == 0:
+      # Try to import Spheral for a basic sanity test.
+      smoke_test = sexe("{0} {1}/spheral -c \"import Spheral\"".format("module load gcc/10.3.1 &&", install_dir))
+      if smoke_test != 0:
+        sys.exit(1)
 
 
 if __name__ == "__main__":
