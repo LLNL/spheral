12d77f678ff26ecbfbaa3d24d05bee4a29c6345c
mdavis36 <davis291@llnl.gov>
2023-06-16 15:34:03 -0700

    Pulling giltab-ci changes from develop and spack configs for toss4; other changes to get 2023.03.0 tpls building on TOSS4.

--- scripts/spack/packages/spheral/package.py
+++ scripts/spack/packages/spheral/package.py
@@ -38,7 +38,7 @@ class Spheral(CachedCMakePackage, CudaPackage):
 
     depends_on('zlib@1.2.11 -shared +pic', type='build')
 
-    depends_on('boost@1.74.0 -atomic -container -coroutine -chrono -context -date_time -exception -fiber -graph -iostreams -locale -log -math -mpi -program_options -python -random -regex -test -thread -timer -wave +pic', type='build')
+    depends_on('boost@1.74.0 +system +filesystem -atomic -container -coroutine -chrono -context -date_time -exception -fiber -graph -iostreams -locale -log -math -mpi -program_options -python -random -regex -test -thread -timer -wave +pic', type='build')
 
     depends_on('qhull@2020.1 +pic', type='build')
     depends_on('m-aneos')
@@ -77,7 +77,7 @@ class Spheral(CachedCMakePackage, CudaPackage):
     depends_on('py-pillow@6.2.2', type='build')
     depends_on('py-decorator@4.4.2', type='build')
     depends_on('py-h5py@2.10.0', type='build')
-    depends_on('py-docutils@0.17.1', type='build')
+    depends_on('py-docutils@0.16', type='build')
     depends_on('py-cython@0.29.21', type='build')
     depends_on('py-scipy@1.2.3', type='build')
     depends_on('py-pipreqs@0.4.10', type='build')
