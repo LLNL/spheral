6dda2037a2947e8b7e4998c85d29822a2619aa66
Arlie Capps <capps2@llnl.gov>
2023-07-26 13:28:27 -0700

    Remove blt_patch_target (it crashes the build on rzwhippet)

--- cmake/InstallTPLs.cmake
+++ cmake/InstallTPLs.cmake
@@ -64,7 +64,6 @@ foreach(_tgt umpire RAJA mfem conduit
              axom::sidre axom::mint axom::quest axom)
   if(TARGET ${_tgt})
     list(APPEND spheral_blt_cxx_depends ${_tgt})
-    blt_patch_target(NAME ${_tgt} TREAT_INCLUDES_AS_SYSTEM On)
   else()
     message(WARNING "${_tgt} **IS NOT** a target.")
   endif()
