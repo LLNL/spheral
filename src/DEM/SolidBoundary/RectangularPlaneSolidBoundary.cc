38e8fe11957467da83d15db7f04c9066e07eaa1f
jmpearl <pearl3@llnl.gov>
2023-10-31 19:52:33 -0700

    reworking RectangularPlane distance method

--- src/DEM/SolidBoundary/RectangularPlaneSolidBoundary.cc
+++ src/DEM/SolidBoundary/RectangularPlaneSolidBoundary.cc
@@ -8,7 +8,6 @@
 #include "DataBase/DataBase.hh"
 #include "DataBase/State.hh"
 #include "DataBase/StateDerivatives.hh"
-
 #include "DEM/SolidBoundary/RectangularPlaneSolidBoundary.hh"
 
 namespace Spheral {
@@ -34,13 +33,8 @@ typename Dimension::Vector
 RectangularPlaneSolidBoundary<Dimension>::
 distance(const Vector& position) const { 
   const auto q = mBasis*(position-mPoint);
-  const double signX = (q.x() > 0.0 ? 1. : -1.);
-  const double signY = (q.y() > 0.0 ? 1. : -1.);
-  const double signZ = (q.z() > 0.0 ? 1. : -1.);
-  const auto signedExtent = Vector(signX*std::min(signX*q.x(),mExtent.x()),
-                                   signY*std::min(signY*q.y(),mExtent.y()),
-                                   signZ*std::min(signZ*q.z(),mExtent.z()));
-  return mBasis.Transpose()*(q-signedExtent);
+  const auto q0 = elementWiseMax(elementWiseMin(q,mExtent),-mExtent);
+  return mBasis.Transpose()*(q-q0);
 }
 
 template<typename Dimension>
