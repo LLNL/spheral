e5c9917d48be25daf7c81f5ee0c61534b452bdfd
Mike Owen <mikeowen@llnl.gov>
2024-03-19 11:16:37 -0700

    More testing of the new ASPH ideas

--- tests/unit/Kernel/testHadaptation.py
+++ tests/unit/Kernel/testHadaptation.py
@@ -9,51 +9,61 @@ from SpheralMatplotlib import *
 #-------------------------------------------------------------------------------
 commandLine(Kernel = WendlandC4Kernel,
             nPerh = 4.01,
-            rotation = 0.0,
-            xscale = 1.0,
-            yscale = 1.0,
+            fscale = 1.0, 
+            fscaleAngle = 0.0,  # degrees
             iterations = 10,
             startCorrect = False,
 )
+assert fscale <= 1.0
+fscaleAngle *= pi/180.0
 
 #-------------------------------------------------------------------------------
 # Make the kernel and the ASPH update method
 #-------------------------------------------------------------------------------
 WT = TableKernel(Kernel())
+etamax = WT.kernelExtent
 asph = ASPHSmoothingScale(WT, targetNperh = nPerh)
 
 #-------------------------------------------------------------------------------
 # Generate our test point positions
 #-------------------------------------------------------------------------------
-fxscale = max(1.0, yscale/xscale)
-fyscale = max(1.0, xscale/yscale)
-nx = int(4.0*nPerh * fxscale)
-ny = int(4.0*nPerh * fyscale)
-
-# Make sure we have a point at (0, 0)
+# First generate the points in the reference frame (eta space)
+nx = int(4.0*etamax*nPerh/fscale)
 if nx % 2 == 0:
     nx += 1
-if ny % 2 == 0:
-    ny += 1
 
-dx = 2.0/(nx - 1)
-dy = 2.0/(ny - 1)
+xcoords = 2.0/fscale*np.linspace(-etamax, etamax, nx)
+ycoords = 2.0/fscale*np.linspace(-etamax, etamax, nx)
+X, Y = np.meshgrid(xcoords, ycoords)
+eta_coords = np.column_stack((X.ravel(), Y.ravel()))
 
-xcoords = np.linspace(-1.0, 1.0, nx)
-ycoords = np.linspace(-1.0, 1.0, ny)
-assert xcoords[(nx - 1)//2] == 0.0
-assert ycoords[(ny - 1)//2] == 0.0
+# Apply the inverse of the H transformation to map these eta coordinates to the
+# lab frame
+HCinv = SymTensor(fscale, 0.0,
+                  0.0,    1.0)
+R = rotationMatrix(Vector(cos(fscaleAngle), sin(fscaleAngle))).Transpose()
+HCinv.rotationalTransform(R)
+coords = np.copy(eta_coords)
+for i in range(len(coords)):
+    eta_coords[i] = R*Vector(eta_coords[i][0],eta_coords[i][1])
+    coords[i] = HCinv * Vector(coords[i][0], coords[i][1])
+HC = HCinv.Inverse()
 
 #-------------------------------------------------------------------------------
 # Function for plotting the current H tensor
 #-------------------------------------------------------------------------------
-def plotH(H, plot, style="k-"):
-    Hinv = WT.kernelExtent * H.Inverse()
+def plotH(H, plot,
+          style = "k-",
+          etaSpace = False):
+    etamax = WT.kernelExtent
+    Hinv = H.Inverse()
+    if etaSpace:
+        Hinv = HC*Hinv
     t = np.linspace(0, 2.0*pi, 180)
     x = np.cos(t)
     y = np.sin(t)
     for i in range(len(x)):
-        etav = Hinv*Vector(x[i], y[i])
+        etav = Hinv*Vector(x[i], y[i]) * etamax
         x[i] = etav.x
         y[i] = etav.y
     plot.plot(x, y, style)
@@ -62,20 +72,17 @@ def plotH(H, plot, style="k-"):
 #-------------------------------------------------------------------------------
 # Function to measure the second moment tensor psi
 #-------------------------------------------------------------------------------
-def computePsi(x, y, H, WT, nPerh):
-    nx = len(x)
-    ny = len(y)
+def computePsi(coords, H, WT, nPerh):
     Wsum = 0.0
     psiLab = SymTensor()
     psiEta = SymTensor()
-    for j in range(ny):
-        for i in range(nx):
-            rji = Vector(x[i], y[j])
-            eta = H*rji
-            Wsum += WT.kernelValueSPH(eta.magnitude())
-            Wi = WT.kernelValueASPH(eta.magnitude(), nPerh)
-            psiLab += Wi * rji.selfdyad()
-            psiEta += Wi * eta.selfdyad()
+    for vals in coords:
+        rji = Vector(*vals)
+        eta = H*rji
+        Wsum += WT.kernelValueSPH(eta.magnitude())
+        Wi = WT.kernelValueASPH(eta.magnitude(), nPerh)
+        psiLab += Wi * rji.selfdyad()
+        psiEta += Wi * eta.selfdyad()
     return Wsum, psiLab, psiEta
 
 #-------------------------------------------------------------------------------
@@ -117,31 +124,48 @@ def newH(H0, Wsum, psiLab, psiEta, WT, asph, nPerh):
 # Plot the initial point distribution and H
 #-------------------------------------------------------------------------------
 if startCorrect:
-    H = SymTensor(1.0/(nPerh*dx), 0.0,
-                  0.0, 1.0/(nPerh*dy))
+    H = SymTensor(HC)
 else:
-    H = SymTensor(1.0/(nPerh*dx*fxscale), 0.0,
-                  0.0, 1.0/(nPerh*dy*fyscale))
+    H = SymTensor(1.0, 0.0,
+                  0.0, 1.0)
     H *= 2.0   # Make it too small to start
 print("Initial H tensor (inverse): ", H.Inverse())
 
-# Plot the initial point distribution
-plot = newFigure()
-plot.set_box_aspect(1.0)
-X, Y = np.meshgrid(xcoords, ycoords)
-plot.plot(X, Y, "ro")
-plotH(H, plot, "k-")
+# Plot the initial point distribution in lab coordinates
+plotLab = newFigure()
+plotLab.set_box_aspect(1.0)
+plotLab.plot([x[0] for x in coords], [x[1] for x in coords], "ro")
+plotH(H, plotLab, "k-")
+plim = max(abs(np.min(coords)), np.max(coords))
+plotLab.set_xlim(-plim, plim)
+plotLab.set_ylim(-plim, plim)
+plotLab.set_xlabel(r"$x$")
+plotLab.set_ylabel(r"$y$")
+plotLab.set_title("Lab frame")
+
+# Plot in eta space
+plotEta = newFigure()
+plotEta.set_box_aspect(1.0)
+plotEta.plot([x[0] for x in eta_coords], [x[1] for x in eta_coords], "ro")
+plotH(H, plotEta, "k-", True)
+plim = max(abs(np.min(eta_coords)), np.max(eta_coords))
+plotEta.set_xlim(-plim, plim)
+plotEta.set_ylim(-plim, plim)
+plotEta.set_xlabel(r"$\eta_x$")
+plotEta.set_ylabel(r"$\eta_y$")
+plotEta.set_title("$\eta$ frame")
 
 #-------------------------------------------------------------------------------
 # Iterate on relaxing H
 #-------------------------------------------------------------------------------
 for iter in range(iterations):
     print("Iteration ", iter)
-    Wsum, psiLab, psiEta = computePsi(xcoords, ycoords, H, WT, nPerh)
+    Wsum, psiLab, psiEta = computePsi(coords, H, WT, nPerh)
     print("     Wsum, psiLab, psiEta: ", Wsum, psiLab, psiEta)
     #H = asph.idealSmoothingScale(H, Vector(0,0), 0.0, psi, WT, 1e-10, 1e10, 1e-10, nPerh, ConnectivityMap(), 0, 0)
     H = newH(H, Wsum, psiLab, psiEta, WT, asph, nPerh)
     evals = H.eigenValues()
     aspectRatio = evals.maxElement()/evals.minElement()
     output("     H.Inverse(), aspectRatio")
-    plotH(H, plot, "b-")
+    plotH(H, plotLab, "b-")
+    plotH(H, plotEta, "b-", True)
