ab25e9946df267c5fc25e02f56f73cd50b4398ce
Landon Owen <lndnwn9@gmail.com>
2024-05-23 14:34:05 -0700

    Fix scipy to ubuntu build version as well

--- scripts/spack/packages/spheral/package.py
+++ scripts/spack/packages/spheral/package.py
@@ -70,7 +70,7 @@ class Spheral(CachedCMakePackage, CudaPackage):
     depends_on('py-matplotlib@3.7.4 backend=tkagg +fonts', type='build')
     depends_on('py-h5py@3.9.0', type='build')
     depends_on('py-docutils@0.18.1', type='build')
-    depends_on('py-scipy@1.13.0', type='build')
+    depends_on('py-scipy@1.12.0', type='build')
     depends_on('py-ats@exit', type='build')
     depends_on('py-mpi4py@3.1.5', type='build', when='+mpi')
 
