4db574c55cd01a53383ddc320c5f72c4e6849ef7
Landon Owen <lndnwn9@gmail.com>
2024-05-28 09:38:06 -0700

    Updated the upstream location

--- scripts/devtools/tpl-manager.py
+++ scripts/devtools/tpl-manager.py
@@ -11,7 +11,7 @@ import json
 project_dir=os.path.abspath(os.path.join(os.path.realpath(__file__), "../../../"))
 
 default_spheral_spack_dir=os.path.join(os.getcwd(), "../spheral-spack-tpls")
-default_upstream_dir="/usr/WS2/wciuser/Spheral/spheral-spack-tpls/spack/opt/spack/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_p/"
+default_upstream_dir="/usr/WS2/sduser/Spheral/0.22/spack/opt/spack/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/__/"
 
 uberenv_path = os.path.join(project_dir, "scripts/devtools/uberenv/uberenv.py")
 uberenv_project_json = os.path.join(os.getcwd(), ".uberenv_config.json")
