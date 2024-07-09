1df8ec86207a35a782936298d745cede9b834634
Landon Owen <lndnwn9@gmail.com>
2024-07-09 13:19:12 -0700

    Added nproc check to allreduce in generateCyl routine

--- src/NodeGenerators/generateCylDistributionFromRZ.cc
+++ src/NodeGenerators/generateCylDistributionFromRZ.cc
@@ -139,7 +139,10 @@ generateCylDistributionFromRZ(vector<double>& x,
          (int)globalIDs.size() == ndomain and
          (int)H.size() == ndomain);
   for (int ikey = 0; ikey != nextra; ++ikey) VERIFY((int)extraFields[ikey].size() == ndomain);
-  int nglobal = allReduce(x.size(), SPHERAL_OP_SUM);
+  int nglobal = x.size();
+  if (nProcs > 1) {
+    nglobal = allReduce(x.size(), SPHERAL_OP_SUM);
+  }
   VERIFY(nglobal == ntot);
 
 }
