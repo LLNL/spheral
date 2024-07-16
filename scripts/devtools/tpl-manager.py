0b1c9a95ce7bdcba0a8a8f9a672476bbe42c6c72
mdavis36 <davis291@llnl.gov>
2024-07-16 16:28:30 -0700

    Allow us to return the output as a string and print output from the command in real-time.

--- scripts/devtools/tpl-manager.py
+++ scripts/devtools/tpl-manager.py
@@ -70,13 +70,38 @@ def sexe(cmd,ret_output=False,echo=True):
     """ Helper for executing shell commands. """
     if echo:
       print("[exe: {0}]".format(cmd))
-    p = subprocess.run(cmd, shell=True,
-                       capture_output=ret_output,
-                       check=True, text=True)
+
+    # If we want to return the output as string a print to stdout
+    # in real-time we need to let subprocess print as normal to 
+    # PIPE and STDOUT. We then need to read it back ourselves and 
+    # append to an ouput string of our own making. There is no way
+    # to do this with subprocess currently.
     if ret_output:
+      p = subprocess.Popen(cmd,
+                           shell=True,
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.STDOUT,
+                           encoding='utf8')
+      out = "";
+      while True:
+        realtime_output = p.stdout.readline()
+
+        if realtime_output == '' and p.poll() is not None:
+          break
+
+        if realtime_output:
+          print(realtime_output.strip(), flush=True)
+          out += realtime_output
+
       if echo:
-        print(p.stdout)
-      return p.stdout
+        print(out)
+      return out
+    
+    # If we do not need to return the output as a string, run() 
+    # will suffice.
+    else:
+      p = subprocess.run(cmd, shell=True,
+                         check=True, text=True)
 
 
 # Parse the json formatted spec list...
