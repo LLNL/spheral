23e9825041007f48c816678adaebc0500396118b
Mike Owen <mikeowen@llnl.gov>
2024-07-24 16:42:39 -0700

    Working on our targeted test for hourglass filtering (1D)

--- tests/functional/Hydro/Hourglass/Hourglass-1d.py
+++ tests/functional/Hydro/Hourglass/Hourglass-1d.py
@@ -4,40 +4,33 @@
 #-------------------------------------------------------------------------------
 from Spheral1d import *
 from SpheralTestUtilities import *
+from NodeHistory import *
 
 title("1-D planar hourglassing test")
 
 #-------------------------------------------------------------------------------
 # Generic problem parameters
 #-------------------------------------------------------------------------------
-commandLine(nx1 = 100,
+commandLine(nx1 = 20,
             rho1 = 1.0,
             eps1 = 1.0,
             x0 = 0.0,
             x1 = 1.0,
-            nPerh = 2.01,
+            nPerh = 4.01,
             
+            gamma = 5.0/3.0,
+            mu = 1.0,
+
             wavelength = 0.05,
             amplitude = 0.25,
 
-            a0 = Vector(1.0),
-
-            SVPH = False,
-            CRKSPH = False,
+            hydroType = "SPH",
+            fhourglass = 0.0,
             filter = 0.0,
-            gamma = 5.0/3.0,
-            mu = 1.0,
-            Qconstructor = MonaghanGingoldViscosity,
-            Cl = 1.0,
-            Cq = 1.0,
-            Qlimiter = False,
-            epsilon2 = 1e-2,
-            negligibleSoundSpeed = 1e-5,
-            csMultiplier = 1e-4,
-            energyMultiplier = 0.1,
+
             hmin = 0.0001, 
             hmax = 100.0,
-            cfl = 0.5,
+            cfl = 0.25,
             XSPH = False,
             epsilonTensile = 0.0,
             nTensile = 8,
@@ -52,6 +45,8 @@ commandLine(nx1 = 100,
             statsStep = 1,
             smoothIters = 0,
             HUpdate = IdealH,
+            useVelocityMagnitudeForDt = False,
+            evolveTotalEnergy = False,         # Only for SPH variants -- evolve total rather than specific energy
             densityUpdate = RigorousSumDensity, # VolumeScaledDensity,
             compatibleEnergy = True,
             gradhCorrection = False,
@@ -65,12 +60,6 @@ commandLine(nx1 = 100,
             )
 
 #-------------------------------------------------------------------------------
-# CRKSPH Switches to ensure consistency
-#-------------------------------------------------------------------------------
-if CRKSPH:
-    Qconstructor = LimitedMonaghanGingoldViscosity
-
-#-------------------------------------------------------------------------------
 # Material properties.
 #-------------------------------------------------------------------------------
 eos = GammaLawGasMKS(gamma, mu)
@@ -78,10 +67,7 @@ eos = GammaLawGasMKS(gamma, mu)
 #-------------------------------------------------------------------------------
 # Interpolation kernels.
 #-------------------------------------------------------------------------------
-WT = TableKernel(NBSplineKernel(5), 1000)
-Wfbase = NBSplineKernel(7)
-WTf = TableKernel(Wfbase, 1000, hmult = 1.0/(nPerh*Wfbase.kernelExtent))
-kernelExtent = WT.kernelExtent
+WT = TableKernel(WendlandC4Kernel(), 100)
 output("WT")
 
 #-------------------------------------------------------------------------------
@@ -101,13 +87,12 @@ output("nodes1.nodesPerSmoothingScale")
 #-------------------------------------------------------------------------------
 from DistributeNodes import distributeNodesInRange1d
 distributeNodesInRange1d([(nodes1, nx1, rho1, (x0, x1))])
-nNodesThisDomain1 = nodes1.numInternalNodes
 output("nodes1.numNodes")
 
 # Set node specific thermal energies
 nodes1.specificThermalEnergy(ScalarField("tmp", nodes1, eps1))
 
-# Displace the nodes in a pattern that looks like the tensile instability clumping.
+# Displace the nodes in a pattern that looks like the tensile instability clumping
 dx = (x1 - x0)/nx1
 for i in range(nodes1.numInternalNodes):
     delta = amplitude*((-1.0)**(i % 2))*dx # amplitude*sin(2.0*pi*nodes1.positions()[i].x/wavelength)
@@ -123,87 +108,144 @@ output("db.numNodeLists")
 output("db.numFluidNodeLists")
 
 #-------------------------------------------------------------------------------
-# Construct the artificial viscosity.
-#-------------------------------------------------------------------------------
-q = Qconstructor(Cl, Cq)
-q.limiter = Qlimiter
-q.epsilon2 = epsilon2
-q.negligibleSoundSpeed = negligibleSoundSpeed
-q.csMultiplier = csMultiplier
-q.energyMultiplier = energyMultiplier
-output("q")
-output("q.Cl")
-output("q.Cq")
-output("q.limiter")
-output("q.epsilon2")
-output("q.negligibleSoundSpeed")
-output("q.csMultiplier")
-output("q.energyMultiplier")
-
-#-------------------------------------------------------------------------------
 # Construct the hydro physics object.
 #-------------------------------------------------------------------------------
-if SVPH:
-    hydro = SVPHFacetedHydro(W = WT, 
-                             Q = q,
-                             cfl = cfl,
-                             compatibleEnergyEvolution = compatibleEnergy,
-                             densityUpdate = densityUpdate,
-                             XSVPH = XSPH,
-                             linearConsistent = linearConsistent,
-                             generateVoid = False,
-                             HUpdate = HUpdate,
-                             fcentroidal = fcentroidal,
-                             fcellPressure = fcellPressure,
-                             xmin = Vector(-100.0),
-                             xmax = Vector( 100.0))
-elif CRKSPH:
-    hydro = CRKSPHHydro(W = WT, 
-                        Q = q, 
-                        filter = filter,
-                        cfl = cfl,
-                        compatibleEnergyEvolution = compatibleEnergy,
-                        XSPH = XSPH,
-                        densityUpdate = densityUpdate,
-                        HUpdate = HUpdate)
+if hydroType == "SVPH":
+    hydro = SVPH(dataBase = db,
+                 W = WT,
+                 cfl = cfl,
+                 useVelocityMagnitudeForDt = useVelocityMagnitudeForDt,
+                 compatibleEnergyEvolution = compatibleEnergy,
+                 densityUpdate = densityUpdate,
+                 XSVPH = XSPH,
+                 linearConsistent = linearConsistent,
+                 generateVoid = False,
+                 HUpdate = HUpdate,
+                 fcentroidal = fcentroidal,
+                 fcellPressure = fcellPressure,
+                 xmin = Vector(-100.0),
+                 xmax = Vector( 100.0))
+elif hydroType == "CRKSPH":
+    hydro = CRKSPH(dataBase = db,
+                   W = WT,
+                   order = correctionOrder,
+                   filter = filter,
+                   cfl = cfl,
+                   useVelocityMagnitudeForDt = useVelocityMagnitudeForDt,
+                   compatibleEnergyEvolution = compatibleEnergy,
+                   evolveTotalEnergy = evolveTotalEnergy,
+                   XSPH = XSPH,
+                   densityUpdate = densityUpdate,
+                   HUpdate = HUpdate,
+                   crktype = crktype)
+elif hydroType == "PSPH":
+    hydro = PSPH(dataBase = db,
+                 W = WT,
+                 filter = filter,
+                 cfl = cfl,
+                 useVelocityMagnitudeForDt = useVelocityMagnitudeForDt,
+                 compatibleEnergyEvolution = compatibleEnergy,
+                 evolveTotalEnergy = evolveTotalEnergy,
+                 densityUpdate = densityUpdate,
+                 HUpdate = HUpdate,
+                 XSPH = XSPH)
+
+elif hydroType == "FSISPH":
+    hydro = FSISPH(dataBase = db,
+                   W = WT,
+                   cfl = cfl,
+                   interfaceMethod = HLLCInterface,
+                   sumDensityNodeLists=[nodes1],                       
+                   densityStabilizationCoefficient = 0.00,
+                   useVelocityMagnitudeForDt = useVelocityMagnitudeForDt,
+                   compatibleEnergyEvolution = compatibleEnergy,
+                   evolveTotalEnergy = evolveTotalEnergy,
+                   HUpdate = HUpdate)
+elif hydroType == "GSPH":
+    limiter = VanLeerLimiter()
+    waveSpeed = DavisWaveSpeed()
+    solver = HLLC(limiter,
+                  waveSpeed,
+                  True)
+    hydro = GSPH(dataBase = db,
+                riemannSolver = solver,
+                W = WT,
+                cfl=cfl,
+                useVelocityMagnitudeForDt = useVelocityMagnitudeForDt,
+                compatibleEnergyEvolution = compatibleEnergy,
+                evolveTotalEnergy = evolveTotalEnergy,
+                XSPH = XSPH,
+                gradientType = gsphReconstructionGradient,
+                densityUpdate=densityUpdate,
+                HUpdate = IdealH,
+                epsTensile = epsilonTensile,
+                nTensile = nTensile)
+elif hydroType == "MFM":
+    limiter = VanLeerLimiter()
+    waveSpeed = DavisWaveSpeed()
+    solver = HLLC(limiter,
+                  waveSpeed,
+                  True)
+    hydro = MFM(dataBase = db,
+                riemannSolver = solver,
+                W = WT,
+                cfl=cfl,
+                useVelocityMagnitudeForDt = useVelocityMagnitudeForDt,
+                compatibleEnergyEvolution = compatibleEnergy,
+                evolveTotalEnergy = evolveTotalEnergy,
+                XSPH = XSPH,
+                gradientType = gsphReconstructionGradient,
+                densityUpdate=densityUpdate,
+                HUpdate = IdealH,
+                epsTensile = epsilonTensile,
+                nTensile = nTensile)
 else:
-    hydro = SPHHydro(W = WT, 
-                     Q = q,
-                     cfl = cfl,
-                     compatibleEnergyEvolution = compatibleEnergy,
-                     gradhCorrection = gradhCorrection,
-                     densityUpdate = densityUpdate,
-                     HUpdate = HUpdate,
-                     XSPH = XSPH,
-                     epsTensile = epsilonTensile,
-                     nTensile = nTensile)
+    assert hydroType == "SPH"
+    hydro = SPH(dataBase = db,
+                W = WT,
+                filter = filter,
+                cfl = cfl,
+                useVelocityMagnitudeForDt = useVelocityMagnitudeForDt,
+                compatibleEnergyEvolution = compatibleEnergy,
+                evolveTotalEnergy = evolveTotalEnergy,
+                gradhCorrection = gradhCorrection,
+                densityUpdate = densityUpdate,
+                HUpdate = HUpdate,
+                XSPH = XSPH,
+                epsTensile = epsilonTensile,
+                nTensile = nTensile)
 output("hydro")
-output("hydro.kernel()")
-output("hydro.PiKernel()")
+try:
+    output("hydro.kernel")
+    output("hydro.PiKernel")
+    output("hydro.XSPH")
+except:
+    pass
 output("hydro.cfl")
 output("hydro.compatibleEnergyEvolution")
 output("hydro.densityUpdate")
-output("hydro.HEvolution")
 
 packages = [hydro]
 
-# #-------------------------------------------------------------------------------
-# # Construct a constant acceleration package.
-# #-------------------------------------------------------------------------------
-# indices = vector_of_int()
-# for i in xrange(nodes1.numInternalNodes):
-#     indices.append(i)
-# accel = ConstantAcceleration(a0, nodes1, indices)
-# packages.append(accel)
+#-------------------------------------------------------------------------------
+# Optionally construct an hourglass control object.
+#-------------------------------------------------------------------------------
+if fhourglass > 0.0:
+    hg = SubPointPressureHourglassControl(fhourglass)
+    output("hg")
+    output("hg.fHG")
+    packages.append(hg)
 
 #-------------------------------------------------------------------------------
 # Create boundary conditions.
 #-------------------------------------------------------------------------------
 xPlane0 = Plane(Vector(x0), Vector( 1.0))
 xPlane1 = Plane(Vector(x1), Vector(-1.0))
-xbc0 = PeriodicBoundary(xPlane0, xPlane1)
+xbc0 = ReflectingBoundary(xPlane0)
+xbc1 = ReflectingBoundary(xPlane1)
 for p in packages:
     p.appendBoundary(xbc0)
+    p.appendBoundary(xbc1)
 
 #-------------------------------------------------------------------------------
 # Construct a predictor corrector integrator, and add the one physics package.
@@ -224,20 +266,26 @@ integrator.dtGrowth = dtGrowth
 output("integrator.dtGrowth")
 
 #-------------------------------------------------------------------------------
+# Track the history of the motion of select points
+#-------------------------------------------------------------------------------
+def samplefunc(nodes, indices):
+    i = indices[0]
+    pos = nodes.positions()
+    vel = nodes.velocity()
+    DvDt = hydro.DvDt
+    return pos[i].x, vel[i].x, DvDt(0,i).x
+
+histories = [NodeHistory(nodes1, [i], samplefunc, "/dev/null") for i in range(nx1)]
+
+#-------------------------------------------------------------------------------
 # Make the problem controller.
 #-------------------------------------------------------------------------------
 control = SpheralController(integrator, WT,
                             statsStep = statsStep,
                             restartStep = restartStep,
-                            restartBaseName = restartBaseName)
+                            restartBaseName = restartBaseName,
+                            periodicWork = [(hist, 1) for hist in histories])
 output("control")
-
-# Smooth the initial conditions.
-if restoreCycle is not None:
-    control.loadRestartFile(restoreCycle)
-else:
-    control.iterateIdealH()
-    control.smoothState(smoothIters)
 
 #-------------------------------------------------------------------------------
 # Advance to the end time.
@@ -252,18 +300,60 @@ else:
 #-------------------------------------------------------------------------------
 # Plot the final state.
 #-------------------------------------------------------------------------------
-import Gnuplot
-from SpheralGnuPlotUtilities import *
+from SpheralMatplotlib import *
 rhoPlot, velPlot, epsPlot, PPlot, HPlot = plotState(db)
-## Eplot = plotEHistory(control.conserve)
-## xplot = plotFieldList(db.fluidPosition,
-##                       yFunction = "%s.x",
-##                       plotStyle = "points",
-##                       winTitle = "Position (x)")
-
-a = hydro.DvDt()
+EPlot = plotEHistory(control.conserve)
+a = hydro.DvDt
 aplot = plotFieldList(a,
                       yFunction = "%s.x",
-                      plotStyle = "linespoints",
                       winTitle = "Acceleration")
 
+def computeNearestNeighborDistance():
+    result = ScalarField("nearest neighbor distance", nodes1, 1e10)
+    db.updateConnectivityMap()
+    cm = db.connectivityMap()
+    pairs = cm.nodePairList
+    pos = nodes1.positions()
+    for pair in pairs:
+        i, j = pair.i_node, pair.j_node
+        rij = (pos(i) - pos(j)).magnitude()
+        result[i] = min(result[i], rij)
+        result[j] = min(result[j], rij)
+    return result
+
+def plotit(x, y,
+           style = "ro",
+           title = None,
+           xlabel = None,
+           ylabel = None):
+    fig = newFigure()
+    plt.plot(x, y, style)
+    if title:  plt.title(title)
+    if xlabel: plt.xlabel(xlabel)
+    if ylabel: plt.ylabel(ylabel)
+    return fig
+
+nearestPlot = plotField(computeNearestNeighborDistance(),
+                        winTitle = "Nearest neighbor distance",
+                        xlabel = "x",
+                        ylabel = "min(d)")
+
+x0hist = plotit(histories[0].timeHistory, [s[0] for s in histories[0].sampleHistory],
+                title = "Node 0 position",
+                xlabel = "time",
+                ylabel = "x")
+v0hist = plotit(histories[0].timeHistory, [s[1] for s in histories[0].sampleHistory],
+                title = "Node 0 velocity",
+                xlabel = "time",
+                ylabel = "vel")
+a0hist = plotit(histories[0].timeHistory, [s[2] for s in histories[0].sampleHistory],
+                title = "Node 0 acceleration",
+                xlabel = "time",
+                ylabel = "accel")
+
+plots = [(rhoPlot, "Hourglass-1d-rho.png"),
+         (velPlot, "Hourglass-1d-vel.png"),
+         (epsPlot, "Hourglass-1d-eps.png"),
+         (PPlot, "Hourglass-1d-P.png"),
+         (HPlot, "Hourglass-1d-h.png"),
+         (aplot, "Hourglass-1d-accel.png")]
