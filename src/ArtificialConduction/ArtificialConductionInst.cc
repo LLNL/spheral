7a3f292c6c1dddb1003ff49e3c6732e81c579687
Alan Dayton <dayton8@llnl.gov>
2024-08-16 08:57:48 -0700

    Refactor explicit instantiations in ArtificialConduction

---
+++ src/ArtificialConduction/ArtificialConductionInst.cc
@@ -0,0 +1,21 @@
+//------------------------------------------------------------------------------
+// Explict instantiations.
+//------------------------------------------------------------------------------
+
+#include "config.hh"
+#include "ArtificialConduction/ArtificialConduction.cc"
+#include "Geometry/Dimension.hh"
+
+namespace Spheral {
+#if defined(SPHERAL_ENABLE_1D)
+  template class ArtificialConduction< Dim< 1 > >;
+#endif
+
+#if defined(SPHERAL_ENABLE_2D)
+  template class ArtificialConduction< Dim< 2 > >;
+#endif
+
+#if defined(SPHERAL_ENABLE_3D)
+  template class ArtificialConduction< Dim< 3 > >;
+#endif
+}
