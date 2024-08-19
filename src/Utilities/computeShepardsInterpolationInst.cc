d473fd0cbbb54cc94e3db1095194721b7d9222cc
Alan Dayton <dayton8@llnl.gov>
2024-08-19 16:09:28 -0700

    Last instantiations

--- src/Utilities/computeShepardsInterpolationInst.cc
+++ src/Utilities/computeShepardsInterpolationInst.cc
@@ -8,71 +8,116 @@
 
 namespace Spheral {
 #if defined(SPHERAL_ENABLE_1D)
-"""
-
-for DT in ("Dim<1>::Scalar",
-           "Dim<1>::Vector",
-           "Dim<1>::Tensor",
-           "Dim<1>::SymTensor"):
-    text += """
+template
+FieldList<Dim<1>, Dim<1>::Scalar>
+computeShepardsInterpolation(const FieldList<Dim<1>, Dim<1>::Scalar>& fieldList,
+                             const ConnectivityMap<Dim<1> >&,
+                             const TableKernel<Dim<1> >&,
+                             const FieldList<Dim<1>, Dim<1>::Vector>& position,
+                             const FieldList<Dim<1>, Dim<1>::SymTensor>& H,
+                             const FieldList<Dim<1>, Dim<1>::Scalar>& weight);
 
 template
-FieldList<Dim< %1 >, %(DT)s>
-computeShepardsInterpolation(const FieldList<Dim< %1 >, %(DT)s>& fieldList,
-                             const ConnectivityMap<Dim< %1 > >&,
-                             const TableKernel<Dim< %1 > >&,
-                             const FieldList<Dim< %1 >, Dim< %1 >::Vector>& position,
-                             const FieldList<Dim< %1 >, Dim< %1 >::SymTensor>& H,
-                             const FieldList<Dim< %1 >, Dim< %1 >::Scalar>& weight);
+FieldList<Dim<1>, Dim<1>::Vector>
+computeShepardsInterpolation(const FieldList<Dim<1>, Dim<1>::Vector>& fieldList,
+                             const ConnectivityMap<Dim<1> >&,
+                             const TableKernel<Dim<1> >&,
+                             const FieldList<Dim<1>, Dim<1>::Vector>& position,
+                             const FieldList<Dim<1>, Dim<1>::SymTensor>& H,
+                             const FieldList<Dim<1>, Dim<1>::Scalar>& weight);
 
-""" % {"DT" : DT}
+template
+FieldList<Dim<1>, Dim<1>::Tensor>
+computeShepardsInterpolation(const FieldList<Dim<1>, Dim<1>::Tensor>& fieldList,
+                             const ConnectivityMap<Dim<1> >&,
+                             const TableKernel<Dim<1> >&,
+                             const FieldList<Dim<1>, Dim<1>::Vector>& position,
+                             const FieldList<Dim<1>, Dim<1>::SymTensor>& H,
+                             const FieldList<Dim<1>, Dim<1>::Scalar>& weight);
 
-text += """
+template
+FieldList<Dim<1>, Dim<1>::SymTensor>
+computeShepardsInterpolation(const FieldList<Dim<1>, Dim<1>::SymTensor>& fieldList,
+                             const ConnectivityMap<Dim<1> >&,
+                             const TableKernel<Dim<1> >&,
+                             const FieldList<Dim<1>, Dim<1>::Vector>& position,
+                             const FieldList<Dim<1>, Dim<1>::SymTensor>& H,
+                             const FieldList<Dim<1>, Dim<1>::Scalar>& weight);
 #endif
 
 #if defined(SPHERAL_ENABLE_2D)
-"""
-
-for DT in ("Dim<2>::Scalar",
-           "Dim<2>::Vector",
-           "Dim<2>::Tensor",
-           "Dim<2>::SymTensor"):
-    text += """
+template
+FieldList<Dim<2>, Dim<2>::Scalar>
+computeShepardsInterpolation(const FieldList<Dim<2>, Dim<2>::Scalar>& fieldList,
+                             const ConnectivityMap<Dim<2> >&,
+                             const TableKernel<Dim<2> >&,
+                             const FieldList<Dim<2>, Dim<2>::Vector>& position,
+                             const FieldList<Dim<2>, Dim<2>::SymTensor>& H,
+                             const FieldList<Dim<2>, Dim<2>::Scalar>& weight);
 
 template
-FieldList<Dim< %2 >, %(DT)s>
-computeShepardsInterpolation(const FieldList<Dim< %2 >, %(DT)s>& fieldList,
-                             const ConnectivityMap<Dim< %2 > >&,
-                             const TableKernel<Dim< %2 > >&,
-                             const FieldList<Dim< %2 >, Dim< %2 >::Vector>& position,
-                             const FieldList<Dim< %2 >, Dim< %2 >::SymTensor>& H,
-                             const FieldList<Dim< %2 >, Dim< %2 >::Scalar>& weight);
+FieldList<Dim<2>, Dim<2>::Vector>
+computeShepardsInterpolation(const FieldList<Dim<2>, Dim<2>::Vector>& fieldList,
+                             const ConnectivityMap<Dim<2> >&,
+                             const TableKernel<Dim<2> >&,
+                             const FieldList<Dim<2>, Dim<2>::Vector>& position,
+                             const FieldList<Dim<2>, Dim<2>::SymTensor>& H,
+                             const FieldList<Dim<2>, Dim<2>::Scalar>& weight);
 
-""" % {"DT" : DT}
+template
+FieldList<Dim<2>, Dim<2>::Tensor>
+computeShepardsInterpolation(const FieldList<Dim<2>, Dim<2>::Tensor>& fieldList,
+                             const ConnectivityMap<Dim<2> >&,
+                             const TableKernel<Dim<2> >&,
+                             const FieldList<Dim<2>, Dim<2>::Vector>& position,
+                             const FieldList<Dim<2>, Dim<2>::SymTensor>& H,
+                             const FieldList<Dim<2>, Dim<2>::Scalar>& weight);
 
-text += """
+template
+FieldList<Dim<2>, Dim<2>::SymTensor>
+computeShepardsInterpolation(const FieldList<Dim<2>, Dim<2>::SymTensor>& fieldList,
+                             const ConnectivityMap<Dim<2> >&,
+                             const TableKernel<Dim<2> >&,
+                             const FieldList<Dim<2>, Dim<2>::Vector>& position,
+                             const FieldList<Dim<2>, Dim<2>::SymTensor>& H,
+                             const FieldList<Dim<2>, Dim<2>::Scalar>& weight);
 #endif
 
 #if defined(SPHERAL_ENABLE_3D)
-"""
-
-for DT in ("Dim<3>::Scalar",
-           "Dim<3>::Vector",
-           "Dim<3>::Tensor",
-           "Dim<3>::SymTensor"):
-    text += """
+template
+FieldList<Dim<3>, Dim<3>::Scalar>
+computeShepardsInterpolation(const FieldList<Dim<3>, Dim<3>::Scalar>& fieldList,
+                             const ConnectivityMap<Dim<3> >&,
+                             const TableKernel<Dim<3> >&,
+                             const FieldList<Dim<3>, Dim<3>::Vector>& position,
+                             const FieldList<Dim<3>, Dim<3>::SymTensor>& H,
+                             const FieldList<Dim<3>, Dim<3>::Scalar>& weight);
 
 template
-FieldList<Dim< %3 >, %(DT)s>
-computeShepardsInterpolation(const FieldList<Dim< %3 >, %(DT)s>& fieldList,
-                             const ConnectivityMap<Dim< %3 > >&,
-                             const TableKernel<Dim< %3 > >&,
-                             const FieldList<Dim< %3 >, Dim< %3 >::Vector>& position,
-                             const FieldList<Dim< %3 >, Dim< %3 >::SymTensor>& H,
-                             const FieldList<Dim< %3 >, Dim< %3 >::Scalar>& weight);
+FieldList<Dim<3>, Dim<3>::Vector>
+computeShepardsInterpolation(const FieldList<Dim<3>, Dim<3>::Vector>& fieldList,
+                             const ConnectivityMap<Dim<3> >&,
+                             const TableKernel<Dim<3> >&,
+                             const FieldList<Dim<3>, Dim<3>::Vector>& position,
+                             const FieldList<Dim<3>, Dim<3>::SymTensor>& H,
+                             const FieldList<Dim<3>, Dim<3>::Scalar>& weight);
 
-""" % {"DT" : DT}
+template
+FieldList<Dim<3>, Dim<3>::Tensor>
+computeShepardsInterpolation(const FieldList<Dim<3>, Dim<3>::Tensor>& fieldList,
+                             const ConnectivityMap<Dim<3> >&,
+                             const TableKernel<Dim<3> >&,
+                             const FieldList<Dim<3>, Dim<3>::Vector>& position,
+                             const FieldList<Dim<3>, Dim<3>::SymTensor>& H,
+                             const FieldList<Dim<3>, Dim<3>::Scalar>& weight);
 
-text += """
+template
+FieldList<Dim<3>, Dim<3>::SymTensor>
+computeShepardsInterpolation(const FieldList<Dim<3>, Dim<3>::SymTensor>& fieldList,
+                             const ConnectivityMap<Dim<3> >&,
+                             const TableKernel<Dim<3> >&,
+                             const FieldList<Dim<3>, Dim<3>::Vector>& position,
+                             const FieldList<Dim<3>, Dim<3>::SymTensor>& H,
+                             const FieldList<Dim<3>, Dim<3>::Scalar>& weight);
 #endif
 }
