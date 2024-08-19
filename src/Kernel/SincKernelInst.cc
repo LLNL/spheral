3370c5708c3d7056e49a7981663c6849cb7711a8
Alan Dayton <dayton8@llnl.gov>
2024-08-19 13:21:18 -0700

    More clean up

--- src/Kernel/SincKernelInst.cc
+++ src/Kernel/SincKernelInst.cc
@@ -4,18 +4,18 @@
 
 #include "config.hh"
 #include "Kernel/SincKernel.hh"
-#include <math.h>
+#include "Geometry/Dimension.hh"
 
 namespace Spheral {
 #if defined(SPHERAL_ENABLE_1D)
-  template class SincKernel< Dim<1>  >;
+  template class SincKernel< Dim<1> >;
 #endif
 
 #if defined(SPHERAL_ENABLE_2D)
-  template class SincKernel< Dim<2>  >;
+  template class SincKernel< Dim<2> >;
 #endif
 
 #if defined(SPHERAL_ENABLE_3D)
-  template class SincKernel< Dim<3>  >;
+  template class SincKernel< Dim<3> >;
 #endif
 }
