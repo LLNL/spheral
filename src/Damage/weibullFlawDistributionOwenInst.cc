01e9257b6ba7195eecf9ae97c6a119cbc6fc62cb
Alan Dayton <dayton8@llnl.gov>
2024-08-19 12:21:28 -0700

    Fix another bad transformation

--- src/Damage/weibullFlawDistributionOwenInst.cc
+++ src/Damage/weibullFlawDistributionOwenInst.cc
@@ -8,14 +8,38 @@
 
 namespace Spheral {
 #if defined(SPHERAL_ENABLE_1D)
-
+template Field<Dim<1>, std::vector<double> >
+weibullFlawDistributionOwen<Dim<1> >(const unsigned,
+                                     const double,
+                                     const double,
+                                     const FluidNodeList<Dim<1> >&,
+                                     const State<Dim<1> >&,
+                                     const int,
+                                     const double,
+                                     const Field<Dim<1>, int>&);
 #endif
 
 #if defined(SPHERAL_ENABLE_2D)
-
+template Field<Dim<2>, std::vector<double> >
+weibullFlawDistributionOwen<Dim<2> >(const unsigned,
+                                     const double,
+                                     const double,
+                                     const FluidNodeList<Dim<2> >&,
+                                     const State<Dim<2> >&,
+                                     const int,
+                                     const double,
+                                     const Field<Dim<2>, int>&);
 #endif
 
 #if defined(SPHERAL_ENABLE_3D)
-
+template Field<Dim<3>, std::vector<double> >
+weibullFlawDistributionOwen<Dim<3> >(const unsigned,
+                                     const double,
+                                     const double,
+                                     const FluidNodeList<Dim<3> >&,
+                                     const State<Dim<3> >&,
+                                     const int,
+                                     const double,
+                                     const Field<Dim<3>, int>&);
 #endif
 }
