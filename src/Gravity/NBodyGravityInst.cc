4b0d615fc51e7c0323e613810814ac0536b7ea91
Alan Dayton <dayton8@llnl.gov>
2024-08-19 11:23:55 -0700

    Fix bad instantiations

--- src/Gravity/NBodyGravityInst.cc
+++ src/Gravity/NBodyGravityInst.cc
@@ -8,29 +8,14 @@
 
 namespace Spheral {
 #if defined(SPHERAL_ENABLE_1D)
-
   template class NBodyGravity<Dim<1> >;
-
-} // end namespace Spheral
-
-"""
 #endif
 
 #if defined(SPHERAL_ENABLE_2D)
-
   template class NBodyGravity<Dim<2> >;
-
-} // end namespace Spheral
-
-"""
 #endif
 
 #if defined(SPHERAL_ENABLE_3D)
-
   template class NBodyGravity<Dim<3> >;
-
-} // end namespace Spheral
-
-"""
 #endif
 }
