2f7c7fccd2e8621dc0954b7314b2d874384c9404
Alan Dayton <dayton8@llnl.gov>
2024-08-19 15:38:58 -0700

    More clean up

--- src/Neighbor/GridCellIndexInst.cc
+++ src/Neighbor/GridCellIndexInst.cc
@@ -5,40 +5,22 @@
 #include "config.hh"
 #include "Neighbor/GridCellIndex.cc"
 
+#define INDEXMAX 1048576 - 1 // 2^20 - 1
+#define INDEXMIN -INDEXMAX - 1 // -2^20
+
 namespace Spheral {
 #if defined(SPHERAL_ENABLE_1D)
-
-  // 2^20
-#define INDEXMAX 1048576 - 1
-#define INDEXMIN -INDEXMAX - 1
-#define XMULTIPLIER (INDEXMAX - INDEXMIN)
-
-  template<> const int GridCellIndex< Dim<1> >::mIndexMax = INDEXMAX; // 2^20 - 1
+  template<> const int GridCellIndex< Dim<1> >::mIndexMax = INDEXMAX;
   template<> const int GridCellIndex< Dim<1> >::mIndexMin = INDEXMIN;
-
 #endif
 
 #if defined(SPHERAL_ENABLE_2D)
-
-  // 2^20
-#define INDEXMAX 1048576 - 1
-#define INDEXMIN -INDEXMAX - 1
-#define XMULTIPLIER (INDEXMAX - INDEXMIN)
-
-  template<> const int GridCellIndex< Dim<2> >::mIndexMax = INDEXMAX; // 2^20 - 1
+  template<> const int GridCellIndex< Dim<2> >::mIndexMax = INDEXMAX;
   template<> const int GridCellIndex< Dim<2> >::mIndexMin = INDEXMIN;
-
 #endif
 
 #if defined(SPHERAL_ENABLE_3D)
-
-  // 2^20
-#define INDEXMAX 1048576 - 1
-#define INDEXMIN -INDEXMAX - 1
-#define XMULTIPLIER (INDEXMAX - INDEXMIN)
-
-  template<> const int GridCellIndex< Dim<3> >::mIndexMax = INDEXMAX; // 2^20 - 1
+  template<> const int GridCellIndex< Dim<3> >::mIndexMax = INDEXMAX;
   template<> const int GridCellIndex< Dim<3> >::mIndexMin = INDEXMIN;
-
 #endif
 }
