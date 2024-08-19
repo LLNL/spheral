dbabb7f90f48bea384ca456fef4bcb53927d168f
Alan Dayton <dayton8@llnl.gov>
2024-08-19 16:05:50 -0700

    More fixes

--- src/Utilities/overlayRemapFieldsInst.cc
+++ src/Utilities/overlayRemapFieldsInst.cc
@@ -7,19 +7,6 @@
 #include "Geometry/Dimension.hh"
 
 namespace Spheral {
-#if defined(SPHERAL_ENABLE_1D)
-template void
-overlayRemapFields(const std::vector<Boundary<Dim<1>>*>& boundaries,
-                   const std::vector<Field<Dim<1>, Dim<1>::Scalar>*>& scalarDonorFields,
-                   const std::vector<Field<Dim<1>, Dim<1>::Vector>*>& vectorDonorFields,
-                   const std::vector<Field<Dim<1>, Dim<1>::Tensor>*>& tensorDonorFields,
-                   const std::vector<Field<Dim<1>, Dim<1>::SymTensor>*>& symTensorDonorFields,
-                   std::vector<Field<Dim<1>, Dim<1>::Scalar>*>& scalarAcceptorFields,
-                   std::vector<Field<Dim<1>, Dim<1>::Vector>*>& vectorAcceptorFields,
-                   std::vector<Field<Dim<1>, Dim<1>::Tensor>*>& tensorAcceptorFields,
-                   std::vector<Field<Dim<1>, Dim<1>::SymTensor>*>& symTensorAcceptorFields);
-#endif
-
 #if defined(SPHERAL_ENABLE_2D)
 template void
 overlayRemapFields(const std::vector<Boundary<Dim<2>>*>& boundaries,
