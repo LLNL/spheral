2bf98cbd3420360a70b3b1fa2c6b4ae781ec1c62
Alan Dayton <dayton8@llnl.gov>
2024-08-19 12:26:09 -0700

    Fix bad transformation

--- src/Distributed/SortAndDivideRedistributeNodesInst.cc
+++ src/Distributed/SortAndDivideRedistributeNodesInst.cc
@@ -8,14 +8,14 @@
 
 namespace Spheral {
 #if defined(SPHERAL_ENABLE_1D)
-
+  template class SortAndDivideRedistributeNodes< Dim<1> >;
 #endif
 
 #if defined(SPHERAL_ENABLE_2D)
-
+  template class SortAndDivideRedistributeNodes< Dim<2> >;
 #endif
 
 #if defined(SPHERAL_ENABLE_3D)
-
+  template class SortAndDivideRedistributeNodes< Dim<3> >;
 #endif
 }
