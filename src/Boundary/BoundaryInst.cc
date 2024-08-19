8b40831488d243d14286736b84e1f6e5b6c859a6
Alan Dayton <dayton8@llnl.gov>
2024-08-19 11:20:06 -0700

    Build fixes

--- src/Boundary/BoundaryInst.cc
+++ src/Boundary/BoundaryInst.cc
@@ -8,14 +8,14 @@
 
 namespace Spheral {
 #if defined(SPHERAL_ENABLE_1D)
-template class Boundary< Dim<1> >;
+  template class Boundary< Dim<1> >;
 #endif
 
 #if defined(SPHERAL_ENABLE_2D)
-template class Boundary< Dim<2> >;
+  template class Boundary< Dim<2> >;
 #endif
 
 #if defined(SPHERAL_ENABLE_3D)
-template class Boundary< Dim<3> >;
+  template class Boundary< Dim<3> >;
 #endif
 }
