909bacbd2b6b760e8204a65f0adf2e56ca52e4fa
Mike Owen <mikeowen@llnl.gov>
2024-12-04 11:59:08 -0800

    Moved how we build the State to be robust when not running a full problem using the SpheralController

--- src/Utilities/iterateIdealH.cc
+++ src/Utilities/iterateIdealH.cc
@@ -118,24 +118,12 @@ iterateIdealH(DataBase<Dimension>& dataBase,
   // Build a list of flags to indicate which nodes have been completed.
   auto flagNodeDone = dataBase.newFluidFieldList(0, "node completed");
 
-  // Prepare the state and derivatives
-  State<Dimension> state(dataBase, packages);
-  StateDerivatives<Dimension> derivs(dataBase, packages);
-
   // Since we don't have a hydro there are a few other fields we need registered.
   auto zerothMoment = dataBase.newFluidFieldList(0.0, HydroFieldNames::massZerothMoment);
   auto firstMoment = dataBase.newFluidFieldList(Vector::zero, HydroFieldNames::massFirstMoment);
   auto DvDx = dataBase.newFluidFieldList(Tensor::zero, HydroFieldNames::velocityGradient);
   auto DHDt = dataBase.newFluidFieldList(SymTensor::zero, IncrementBoundedState<Dimension, SymTensor>::prefix() + HydroFieldNames::H);
   auto H1 = dataBase.newFluidFieldList(SymTensor::zero, ReplaceBoundedState<Dimension, SymTensor>::prefix() + HydroFieldNames::H);
-  state.enroll(pos);
-  state.enroll(m);
-  state.enroll(rho);
-  derivs.enroll(zerothMoment);
-  derivs.enroll(firstMoment);
-  derivs.enroll(DvDx);
-  derivs.enroll(DHDt);
-  derivs.enroll(H1);
 
   // Iterate until we either hit the max iterations or the H's achieve convergence.
   auto maxDeltaH = 2.0*tolerance;
@@ -162,7 +150,19 @@ iterateIdealH(DataBase<Dimension>& dataBase,
 
     // Update connectivity
     dataBase.updateConnectivityMap(false, false, false);
+
+    // Prepare the state and derivatives
+    State<Dimension> state(dataBase, packages);
     state.enrollConnectivityMap(dataBase.connectivityMapPtr(false, false, false));
+    StateDerivatives<Dimension> derivs(dataBase, packages);
+    state.enroll(pos);
+    state.enroll(m);
+    state.enroll(rho);
+    derivs.enroll(zerothMoment);
+    derivs.enroll(firstMoment);
+    derivs.enroll(DvDx);
+    derivs.enroll(DHDt);
+    derivs.enroll(H1);
 
     // Some methods (ASPH) update both Hideal and H in the finalize, so we make a copy of the state
     // to give the methods
