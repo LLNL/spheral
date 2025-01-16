7225834bf7ff1398de84993eb9c863f2e68dd1da
Landon Owen <lndnwn9@gmail.com>
2025-01-16 11:09:36 -0800

    Fix incorrect proc count on ruby in performance test, fix bug in performance_analysis.py

--- tests/performance.py
+++ tests/performance.py
@@ -41,7 +41,7 @@ spheral_install_config = SpheralConfigs.config()
 # Retrieve the host name and remove any numbers
 temp_uname = os.uname()
 hostname = "".join([i for i in temp_uname[1] if not i.isdigit()])
-mac_procs = {"rzhound": 112, "rzwhippet": 112, "ruby": 112,
+mac_procs = {"rzhound": 112, "rzwhippet": 112, "ruby": 56,
              "rzadams": 84, "rzvernal": 64, "tioga": 64,
              "rzansel": 40, "lassen": 40, "rzgenie": 36}
 # Find out how many nodes our allocation has grabbed
