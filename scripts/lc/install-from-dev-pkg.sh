15ed0aa7327ca6424f958966960c15ef00922df7
Landon Owen <lndnwn9@gmail.com>
2025-03-13 10:18:03 -0700

    Make the production install script fails as soon as it hits an issue and delete tar file at the end incase it hits a network issue

--- scripts/lc/install-from-dev-pkg.sh
+++ scripts/lc/install-from-dev-pkg.sh
@@ -1,3 +1,4 @@
+set -Eeuo pipefail
 trap 'echo "# $BASH_COMMAND"' DEBUG
 
 SPACK_PKG_NAME=${SPACK_PKG_NAME:-'spheral'}
@@ -23,8 +24,11 @@ echo $INSTALL_DIR
 echo $SCRIPT_DIR
 echo $BUILD_ALLOC
 
-rm -rf $INSTALL_DIR
-mkdir -p $INSTALL_DIR
+DEV_TAR_FILE=$DEV_PKG_SPEC.tar.gz
+
+# Delete the install directory but not the tar file, this way we can rerun this stage if a failure happens
+# like with the network
+find $INSTALL_DIR -type f ! -name '$DEV_TAR_FILE' -delete
 
 cp -a $PWD/resources/pip_cache/. $SPHERAL_PIP_CACHE_DIR
 
@@ -44,3 +48,6 @@ $BUILD_ALLOC ./$SCRIPT_DIR/devtools/tpl-manager.py --no-upstream --spack-dir $IN
 
 HOST_CONFIG_FILE=$(ls -t | grep -E "*\.cmake" | head -1)
 $BUILD_ALLOC ./$SCRIPT_DIR/devtools/host-config-build.py --host-config $HOST_CONFIG_FILE -i $INSTALL_DIR --build --no-clean -DSPHERAL_PIP_CACHE_DIR=$SPHERAL_PIP_CACHE_DIR -DSPHERAL_NETWORK_CONNECTED=Off
+
+# Now delete the tar file
+rm -rf $INSTALL_DIR/$DEV_TAR_FILE
