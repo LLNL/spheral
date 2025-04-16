7c584516e19808f19ca855410238719fc4fe6d85
mdavis36 <davis291@llnl.gov>
2025-04-15 18:20:21 -0700

    free ManagedVectors in tests.

--- tests/cpp/Utilities/managed_vector_tests.cc
+++ tests/cpp/Utilities/managed_vector_tests.cc
@@ -18,356 +18,383 @@ TYPED_TEST_CASE(ManagedVectorTypedTest, EXEC_TYPES);
 GPU_TYPED_TEST(ManagedVectorTypedTest, DefaultConstructor) {
   using WORK_EXEC_POLICY = TypeParam;
 
-  EXEC_IN_SPACE_BEGIN(WORK_EXEC_POLICY)
-  MVDouble array;
+  MVDouble arr;
 
-  SPHERAL_ASSERT_EQ(array.size(), 0);
-  SPHERAL_ASSERT_EQ(array.capacity(), 0);
-  EXEC_IN_SPACE_END();
+  SPHERAL_ASSERT_EQ(arr.size(), 0);
+  SPHERAL_ASSERT_EQ(arr.capacity(), 0);
+
+  arr.free();
 }
 
 TEST(ManagedVectorTest, SizeConstructor) {
   // Size Constructor will allocate initial capacity if elements
   // are below initial_capacity
-  MVDouble array(6);
-  SPHERAL_ASSERT_EQ(array.size(), 6u);
-  SPHERAL_ASSERT_EQ(array.capacity(), MVDouble::initial_capacity);
+  MVDouble arr(6);
+  SPHERAL_ASSERT_EQ(arr.size(), 6u);
+  SPHERAL_ASSERT_EQ(arr.capacity(), MVDouble::initial_capacity);
 
   // Size Constructor will allocate n elements if elements
   // are above initial_capacity
-  MVDouble array2(MVDouble::initial_capacity + 1);
-  SPHERAL_ASSERT_EQ(array2.size(), MVDouble::initial_capacity + 1);
-  SPHERAL_ASSERT_EQ(array2.capacity(), MVDouble::initial_capacity * 2);
+  MVDouble arr2(MVDouble::initial_capacity + 1);
+  SPHERAL_ASSERT_EQ(arr2.size(), MVDouble::initial_capacity + 1);
+  SPHERAL_ASSERT_EQ(arr2.capacity(), MVDouble::initial_capacity * 2);
 
-  array.free();
-  array2.free();
+  arr.free();
+  arr2.free();
 }
 
 TEST(ManagedVectorTest, IdentityConstructor) {
-  MVDouble array(6, 5);
-  SPHERAL_ASSERT_EQ(array.size(), 6u);
-  SPHERAL_ASSERT_EQ(array.capacity(), MVDouble::initial_capacity);
+  MVDouble arr(6, 5);
+  SPHERAL_ASSERT_EQ(arr.size(), 6u);
+  SPHERAL_ASSERT_EQ(arr.capacity(), MVDouble::initial_capacity);
 
-  for (auto &elem : array) {
+  for (auto &elem : arr) {
     SPHERAL_ASSERT_EQ(elem, 5);
   }
-  array.free();
+  arr.free();
 }
 
 GPU_TYPED_TEST(ManagedVectorTypedTest, IdentityConstructor) {
 
   using WORK_EXEC_POLICY = TypeParam;
 
-  MVDouble array(6, 5);
-  MVDouble array2(6);
-  SPHERAL_ASSERT_EQ(array.size(), 6u);
-  SPHERAL_ASSERT_EQ(array.capacity(), MVDouble::initial_capacity);
+  MVDouble arr(6, 5);
+  MVDouble arr2(6);
+  SPHERAL_ASSERT_EQ(arr.size(), 6u);
+  SPHERAL_ASSERT_EQ(arr.capacity(), MVDouble::initial_capacity);
 
   RAJA::forall<WORK_EXEC_POLICY>(
       TRS_UINT(0, 6),
-      [=] RAJA_HOST_DEVICE(unsigned idx) { array2[idx] = array[idx]; });
+      [=] RAJA_HOST_DEVICE(unsigned idx) { arr2[idx] = arr[idx]; });
   RAJA::forall<LOOP_EXEC_POLICY>(TRS_UINT(0, 6), [=] RAJA_HOST(unsigned idx) {
-    SPHERAL_ASSERT_EQ(array2[idx], 5);
+    SPHERAL_ASSERT_EQ(arr2[idx], 5);
   });
 
-  array.free();
-  array2.free();
+  arr.free();
+  arr2.free();
 }
 
 GPU_TYPED_TEST(ManagedVectorTypedTest, CopyConstructor) {
   using WORK_EXEC_POLICY = TypeParam;
 
-  MVDouble array(4);
-  MVDouble copy_array(array);
-  // MVDouble copy_array = array.slice(0, array.size());
+  MVDouble arr(4);
+  MVDouble copy_arr(arr);
+  // MVDouble copy_arr = arr.slice(0, arr.size());
 
-  array.resize(6);
+  arr.resize(6);
 
-  SPHERAL_ASSERT_EQ(&array[0], &copy_array[0]);
-  SPHERAL_ASSERT_EQ(array.capacity(), copy_array.capacity());
+  SPHERAL_ASSERT_EQ(&arr[0], &copy_arr[0]);
+  SPHERAL_ASSERT_EQ(arr.capacity(), copy_arr.capacity());
 
   RAJA::forall<WORK_EXEC_POLICY>(
-      TRS_UINT(0, 6), [=] RAJA_HOST_DEVICE(unsigned i) { array[i] = i; });
+      TRS_UINT(0, 6), [=] RAJA_HOST_DEVICE(unsigned i) { arr[i] = i; });
 
   RAJA::forall<LOOP_EXEC_POLICY>(TRS_UINT(0, 6), [=] RAJA_HOST(unsigned i) {
-    SPHERAL_ASSERT_EQ(copy_array[i], i);
+    SPHERAL_ASSERT_EQ(copy_arr[i], i);
   });
 
-  SPHERAL_ASSERT_EQ(&array[0], &copy_array[0]);
+  SPHERAL_ASSERT_EQ(&arr[0], &copy_arr[0]);
 
-  array.resize(20);
+  arr.resize(20);
 
-  SPHERAL_ASSERT_EQ(&array[0], &copy_array[0]);
+  SPHERAL_ASSERT_EQ(&arr[0], &copy_arr[0]);
+
+  copy_arr.free();
 }
 
 GPU_TYPED_TEST(ManagedVectorTypedTest, AssignmentOperator) {
   using WORK_EXEC_POLICY = TypeParam;
 
-  MVDouble array(6, 5);
+  MVDouble arr(6, 5);
 
-  MVDouble copy_array = array;
+  MVDouble copy_arr = arr;
 
   RAJA::forall<WORK_EXEC_POLICY>(
-      TRS_UINT(0, 6), [=] RAJA_HOST_DEVICE(unsigned i) { array[i] = i; });
+      TRS_UINT(0, 6), [=] RAJA_HOST_DEVICE(unsigned i) { arr[i] = i; });
 
   RAJA::forall<LOOP_EXEC_POLICY>(TRS_UINT(0, 6), [=] RAJA_HOST(unsigned i) {
-    SPHERAL_ASSERT_EQ(copy_array[i], i);
+    SPHERAL_ASSERT_EQ(copy_arr[i], i);
   });
 
-  SPHERAL_ASSERT_EQ(&array[0], &copy_array[0]);
+  SPHERAL_ASSERT_EQ(&arr[0], &copy_arr[0]);
+
+  copy_arr.free();
 }
 
 GPU_TYPED_TEST(ManagedVectorTypedTest, Equivalence) {
   using WORK_EXEC_POLICY = TypeParam;
 
-  MVDouble array(6, 5);
-  MVDouble array2(6, 3);
+  MVDouble arr(6, 5);
+  MVDouble arr2(6, 3);
 
-  MVDouble copy_array = array;
+  MVDouble copy_arr = arr;
 
   EXEC_IN_SPACE_BEGIN(WORK_EXEC_POLICY)
 
   for (size_t i = 0; i < 6u; i++) {
-    array[i] = i;
-    array2[i] = i;
+    arr[i] = i;
+    arr2[i] = i;
   }
-  SPHERAL_ASSERT_TRUE(copy_array == array);
-  SPHERAL_ASSERT_FALSE(copy_array == array2);
+  SPHERAL_ASSERT_TRUE(copy_arr == arr);
+  SPHERAL_ASSERT_FALSE(copy_arr == arr2);
   EXEC_IN_SPACE_END()
 
   EXEC_IN_SPACE_BEGIN(LOOP_EXEC_POLICY)
-  ASSERT_TRUE(copy_array == array);
-  ASSERT_FALSE(copy_array == array2);
+  ASSERT_TRUE(copy_arr == arr);
+  ASSERT_FALSE(copy_arr == arr2);
   EXEC_IN_SPACE_END()
+
+  arr2.free();
+  copy_arr.free();
 }
 
 GPU_TYPED_TEST(ManagedVectorTypedTest, PushBackDefault) {
   using WORK_EXEC_POLICY = TypeParam;
 
-  MVDouble array;
+  MVDouble arr;
   double val = 5;
 
-  array.push_back(val);
+  arr.push_back(val);
 
-  SPHERAL_ASSERT_EQ(array.size(), 1u);
-  SPHERAL_ASSERT_EQ(array.capacity(), MVDouble::initial_capacity);
-  SPHERAL_ASSERT_EQ(array[0], val);
+  SPHERAL_ASSERT_EQ(arr.size(), 1u);
+  SPHERAL_ASSERT_EQ(arr.capacity(), MVDouble::initial_capacity);
+  SPHERAL_ASSERT_EQ(arr[0], val);
 
-  MVDouble array2;
+  MVDouble arr2;
 
   for (size_t i = 0; i < 10; i++)
-    array2.push_back(i);
+    arr2.push_back(i);
 
-  for (size_t i = 0; i < array2.size(); i++)
-    SPHERAL_ASSERT_EQ(array2[i], i);
+  for (size_t i = 0; i < arr2.size(); i++)
+    SPHERAL_ASSERT_EQ(arr2[i], i);
 
   RAJA::forall<WORK_EXEC_POLICY>(TRS_UINT(0, 10),
                                  [=] RAJA_HOST_DEVICE(unsigned idx) {
-                                   SPHERAL_ASSERT_EQ(array2.size(), 10u);
-                                   SPHERAL_ASSERT_EQ(array2[idx], idx);
-                                   array2[idx] *= 2;
+                                   SPHERAL_ASSERT_EQ(arr2.size(), 10u);
+                                   SPHERAL_ASSERT_EQ(arr2[idx], idx);
+                                   arr2[idx] *= 2;
                                  });
 
   RAJA::forall<LOOP_EXEC_POLICY>(TRS_UINT(0, 6), [=] RAJA_HOST(unsigned idx) {
-    SPHERAL_ASSERT_EQ(array2[idx], idx * 2);
+    SPHERAL_ASSERT_EQ(arr2[idx], idx * 2);
   });
 
-  SPHERAL_ASSERT_EQ(array2.capacity(), MVDouble::initial_capacity * 2);
+  SPHERAL_ASSERT_EQ(arr2.capacity(), MVDouble::initial_capacity * 2);
+
+  arr.free();
+  arr2.free();
 }
 
 TEST(ManagedVectorTest, PushBackMove) {
-  MVDouble array;
+  MVDouble arr;
 
-  array.push_back(std::move(5));
+  arr.push_back(std::move(5));
 
-  SPHERAL_ASSERT_EQ(array.size(), 1u);
-  SPHERAL_ASSERT_EQ(array.capacity(), MVDouble::initial_capacity);
-  SPHERAL_ASSERT_EQ(array[0], 5);
+  SPHERAL_ASSERT_EQ(arr.size(), 1u);
+  SPHERAL_ASSERT_EQ(arr.capacity(), MVDouble::initial_capacity);
+  SPHERAL_ASSERT_EQ(arr[0], 5);
 
-  MVDouble array2;
+  MVDouble arr2;
 
   for (size_t i = 0; i < 10; i++)
-    array2.push_back(std::move(i));
+    arr2.push_back(std::move(i));
 
-  for (size_t i = 0; i < array2.size(); i++)
-    SPHERAL_ASSERT_EQ(array2[i], i);
+  for (size_t i = 0; i < arr2.size(); i++)
+    SPHERAL_ASSERT_EQ(arr2[i], i);
 
-  SPHERAL_ASSERT_EQ(array2.capacity(), MVDouble::initial_capacity * 2);
+  SPHERAL_ASSERT_EQ(arr2.capacity(), MVDouble::initial_capacity * 2);
+
+  arr.free();
+  arr2.free();
 }
 
 GPU_TYPED_TEST(ManagedVectorTypedTest, ResizeLargerNoRealloc) {
   using WORK_EXEC_POLICY = TypeParam;
 
-  MVDouble array;
+  MVDouble arr;
 
   EXEC_IN_SPACE_BEGIN(WORK_EXEC_POLICY)
-  SPHERAL_ASSERT_EQ(array.size(), 0);
-  SPHERAL_ASSERT_EQ(array.capacity(), 0);
+  SPHERAL_ASSERT_EQ(arr.size(), 0);
+  SPHERAL_ASSERT_EQ(arr.capacity(), 0);
   EXEC_IN_SPACE_END()
 
-  array.move(chai::CPU);
-  array.resize(10);
+  arr.move(chai::CPU);
+  arr.resize(10);
 
   EXEC_IN_SPACE_BEGIN(WORK_EXEC_POLICY)
-  SPHERAL_ASSERT_EQ(array.size(), 10);
-  SPHERAL_ASSERT_EQ(array.capacity(), 16);
+  SPHERAL_ASSERT_EQ(arr.size(), 10);
+  SPHERAL_ASSERT_EQ(arr.capacity(), 16);
   EXEC_IN_SPACE_END()
 
-  MVDouble array2(4);
-  MVValType *pre_resize_ptr = &(array2[0]);
+  MVDouble arr2(4);
+  MVValType *pre_resize_ptr = &(arr2[0]);
 
   EXEC_IN_SPACE_BEGIN(WORK_EXEC_POLICY)
-  SPHERAL_ASSERT_EQ(array2.size(), 4);
-  // SPHERAL_ASSERT_EQ(array2.capacity(), MVDouble::initial_capacity);
+  SPHERAL_ASSERT_EQ(arr2.size(), 4);
+  // SPHERAL_ASSERT_EQ(arr2.capacity(), MVDouble::initial_capacity);
   EXEC_IN_SPACE_END()
 
-  array.move(chai::CPU);
-  array2.resize(6);
-  MVValType *post_resize_ptr = &(array2[0]);
+  arr.move(chai::CPU);
+  arr2.resize(6);
+  MVValType *post_resize_ptr = &(arr2[0]);
 
   EXEC_IN_SPACE_BEGIN(WORK_EXEC_POLICY)
-  SPHERAL_ASSERT_EQ(array2.size(), 6);
-  // SPHERAL_ASSERT_EQ(array2.capacity(), MVDouble::initial_capacity);
+  SPHERAL_ASSERT_EQ(arr2.size(), 6);
+  // SPHERAL_ASSERT_EQ(arr2.capacity(), MVDouble::initial_capacity);
   EXEC_IN_SPACE_END()
 
   // No reallocation has been performed the data should be in the same location.
   ASSERT_EQ(pre_resize_ptr, post_resize_ptr);
+
+  arr.free();
+  arr2.free();
 }
 
 GPU_TYPED_TEST(ManagedVectorTypedTest, ResizeLargerRealloc) {
   using WORK_EXEC_POLICY = TypeParam;
 
-  MVDouble array(4);
-  MVValType *pre_resize_ptr = &(array[0]);
+  MVDouble arr(4);
+  MVValType *pre_resize_ptr = &(arr[0]);
 
   EXEC_IN_SPACE_BEGIN(WORK_EXEC_POLICY)
-  SPHERAL_ASSERT_EQ(array.size(), 4);
-  // SPHERAL_ASSERT_EQ(array.capacity(), 0);
+  SPHERAL_ASSERT_EQ(arr.size(), 4);
+  // SPHERAL_ASSERT_EQ(arr.capacity(), 0);
   EXEC_IN_SPACE_END()
 
-  array.move(chai::CPU);
-  array.resize(12);
-  MVValType *post_resize_ptr = &(array[0]);
+  arr.move(chai::CPU);
+  arr.resize(12);
+  MVValType *post_resize_ptr = &(arr[0]);
 
   EXEC_IN_SPACE_BEGIN(WORK_EXEC_POLICY)
-  SPHERAL_ASSERT_EQ(array.size(), 12);
-  SPHERAL_ASSERT_EQ(array.capacity(), 16);
+  SPHERAL_ASSERT_EQ(arr.size(), 12);
+  SPHERAL_ASSERT_EQ(arr.capacity(), 16);
   EXEC_IN_SPACE_END()
 
   // Reallocation has been performed the data should NOT be in the same
   // location.
   ASSERT_NE(pre_resize_ptr, post_resize_ptr);
+
+  arr.free();
 }
 
 GPU_TYPED_TEST(ManagedVectorTypedTest, ResizeSmaller) {
   using WORK_EXEC_POLICY = TypeParam;
 
-  MVDouble array(4);
-  MVValType *pre_resize_ptr = &(array[0]);
+  MVDouble arr(4);
+  MVValType *pre_resize_ptr = &(arr[0]);
 
   EXEC_IN_SPACE_BEGIN(WORK_EXEC_POLICY)
-  SPHERAL_ASSERT_EQ(array.size(), 4);
-  SPHERAL_ASSERT_EQ(array.capacity(), MVDouble::initial_capacity);
+  SPHERAL_ASSERT_EQ(arr.size(), 4);
+  SPHERAL_ASSERT_EQ(arr.capacity(), MVDouble::initial_capacity);
   EXEC_IN_SPACE_END()
 
-  array.resize(2);
-  MVValType *post_resize_ptr = &(array[0]);
+  arr.resize(2);
+  MVValType *post_resize_ptr = &(arr[0]);
 
   EXEC_IN_SPACE_BEGIN(WORK_EXEC_POLICY)
-  SPHERAL_ASSERT_EQ(array.size(), 2);
-  SPHERAL_ASSERT_EQ(array.capacity(), MVDouble::initial_capacity);
+  SPHERAL_ASSERT_EQ(arr.size(), 2);
+  SPHERAL_ASSERT_EQ(arr.capacity(), MVDouble::initial_capacity);
   EXEC_IN_SPACE_END()
 
   // No reallocation has been performed the data should be in the same location.
   ASSERT_EQ(pre_resize_ptr, post_resize_ptr);
+
+  arr.free();
 }
 
 TEST(ManagedVectorTest, Erase) {
-  MVDouble array;
+  MVDouble arr;
   std::vector<double> check = {0, 1, 2, 3, 4, 5};
 
   for (size_t i = 0; i < 6; i++) {
-    array.push_back(check[i]);
+    arr.push_back(check[i]);
   }
-  SPHERAL_ASSERT_EQ(array.size(), check.size());
+  SPHERAL_ASSERT_EQ(arr.size(), check.size());
 
   // Erase the last element
-  array.erase(array.end() - 1);
+  arr.erase(arr.end() - 1);
 
   std::vector<double> check2 = {0, 1, 2, 3, 4};
-  SPHERAL_ASSERT_EQ(array.size(), check2.size());
-  for (size_t i = 0; i < array.size(); i++)
-    SPHERAL_ASSERT_EQ(array[i], check2[i]);
+  SPHERAL_ASSERT_EQ(arr.size(), check2.size());
+  for (size_t i = 0; i < arr.size(); i++)
+    SPHERAL_ASSERT_EQ(arr[i], check2[i]);
 
   // Erase the 3rd element
-  array.erase(array.begin() + 2);
+  arr.erase(arr.begin() + 2);
 
   std::vector<double> check3 = {0, 1, 3, 4};
-  SPHERAL_ASSERT_EQ(array.size(), check3.size());
-  for (size_t i = 0; i < array.size(); i++)
-    SPHERAL_ASSERT_EQ(array[i], check3[i]);
+  SPHERAL_ASSERT_EQ(arr.size(), check3.size());
+  for (size_t i = 0; i < arr.size(); i++)
+    SPHERAL_ASSERT_EQ(arr[i], check3[i]);
 
   // Erase the first element
-  array.erase(array.begin());
+  arr.erase(arr.begin());
 
   std::vector<double> check4 = {1, 3, 4};
-  SPHERAL_ASSERT_EQ(array.size(), check4.size());
-  for (size_t i = 0; i < array.size(); i++)
-    SPHERAL_ASSERT_EQ(array[i], check4[i]);
+  SPHERAL_ASSERT_EQ(arr.size(), check4.size());
+  for (size_t i = 0; i < arr.size(); i++)
+    SPHERAL_ASSERT_EQ(arr[i], check4[i]);
+
+  arr.free();
 }
 
 TEST(ManagedVectorTest, Insert) {
-  MVDouble array;
+  MVDouble arr;
   std::vector<double> check = {0, 1, 2, 3, 4, 5};
 
   for (size_t i = 0; i < 6; i++) {
-    array.insert(array.begin() + i, check[i]);
+    arr.insert(arr.begin() + i, check[i]);
   }
-  SPHERAL_ASSERT_EQ(array.size(), check.size());
-  for (size_t i = 0; i < array.size(); i++)
-    SPHERAL_ASSERT_EQ(array[i], check[i]);
+  SPHERAL_ASSERT_EQ(arr.size(), check.size());
+  for (size_t i = 0; i < arr.size(); i++)
+    SPHERAL_ASSERT_EQ(arr[i], check[i]);
 
   // Insert element at end
-  array.insert(array.end(), 6);
+  arr.insert(arr.end(), 6);
 
   std::vector<double> check2 = {0, 1, 2, 3, 4, 5, 6};
-  SPHERAL_ASSERT_EQ(array.size(), check2.size());
-  for (size_t i = 0; i < array.size(); i++)
-    SPHERAL_ASSERT_EQ(array[i], check2[i]);
+  SPHERAL_ASSERT_EQ(arr.size(), check2.size());
+  for (size_t i = 0; i < arr.size(); i++)
+    SPHERAL_ASSERT_EQ(arr[i], check2[i]);
 
   // Erase the 3rd element
-  array.insert(array.begin() + 2, 7);
+  arr.insert(arr.begin() + 2, 7);
 
   std::vector<double> check3 = {0, 1, 7, 2, 3, 4, 5, 6};
-  SPHERAL_ASSERT_EQ(array.size(), check3.size());
-  for (size_t i = 0; i < array.size(); i++)
-    SPHERAL_ASSERT_EQ(array[i], check3[i]);
+  SPHERAL_ASSERT_EQ(arr.size(), check3.size());
+  for (size_t i = 0; i < arr.size(); i++)
+    SPHERAL_ASSERT_EQ(arr[i], check3[i]);
 
   // Erase the first element
-  array.insert(array.begin(), -1);
+  arr.insert(arr.begin(), -1);
 
   std::vector<double> check4 = {-1, 0, 1, 7, 2, 3, 4, 5, 6};
-  SPHERAL_ASSERT_EQ(array.size(), check4.size());
-  for (size_t i = 0; i < array.size(); i++)
-    SPHERAL_ASSERT_EQ(array[i], check4[i]);
+  SPHERAL_ASSERT_EQ(arr.size(), check4.size());
+  for (size_t i = 0; i < arr.size(); i++)
+    SPHERAL_ASSERT_EQ(arr[i], check4[i]);
+
+  arr.free();
 }
 
 TEST(ManagedVectorTest, DeepCopy) {
   double init_val = 5;
-  MVDouble array(6, init_val);
+  MVDouble arr(6, init_val);
 
-  MVDouble copy_array(array);
-  MVDouble deep_copy_array(deepCopy(array));
+  MVDouble copy_arr(arr);
+  MVDouble deep_copy_arr(deepCopy(arr));
 
   for (size_t i = 0; i < 6u; i++) {
-    array[i] = 4;
+    arr[i] = 4;
   }
 
   for (size_t i = 0; i < 6u; i++) {
-    ASSERT_NE(array[i], deep_copy_array[i]);
-    SPHERAL_ASSERT_EQ(deep_copy_array[i], init_val);
+    ASSERT_NE(arr[i], deep_copy_arr[i]);
+    SPHERAL_ASSERT_EQ(deep_copy_arr[i], init_val);
   }
 
-  ASSERT_FALSE(array == deep_copy_array);
-  ASSERT_NE(&array[0], &deep_copy_array[0]);
+  ASSERT_FALSE(arr == deep_copy_arr);
+  ASSERT_NE(&arr[0], &deep_copy_arr[0]);
+
+  copy_arr.free();
+  deep_copy_arr.free();
 }
