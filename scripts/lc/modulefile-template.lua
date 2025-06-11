582565d380be56de574f806310c95cd971e02a33
mdavis36 <davis291@llnl.gov>
2025-06-11 10:10:49 -0700

    calling executable scripts from bin/.

--- scripts/lc/modulefile-template.lua
+++ scripts/lc/modulefile-template.lua
@@ -1,14 +1,14 @@
 -- -*- lua -*-
-help( [[
+help([[
 Loads Spheral/YYYY.MM.p release
  Commands: spheral
  Modifies: Add /usr/gapps/Spheral/$SYS_TYPE/YYYY.MM.p to the path in order to use the YYYY.MM.p release version of the Spheral modeling tools.
-]] )
+]])
 whatis("simulation")
 whatis("Spheral release YYYY.MM.p")
-whatis( [[ Loads Spheral/YYYY.MM.p
+whatis([[ Loads Spheral/YYYY.MM.p
  Commands: spheral
  Modifies: Add /usr/gapps/Spheral/$SYS_TYPE/YYYY.MM.p to the path in order to use the YYYY.MM.p release version of the Spheral modeling tools.
-]] )
+]])
 
-prepend_path("PATH", "/usr/gapps/Spheral/" .. os.getenv("SYS_TYPE") .. "/YYYY.MM.p")
+prepend_path("PATH", "/usr/gapps/Spheral/" .. os.getenv("SYS_TYPE") .. "/vYYYY.MM.p/bin")
