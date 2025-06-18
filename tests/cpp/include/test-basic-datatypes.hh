30914722f013302d6d439c8eb8948e0e6df98ccd
mdavis36 <davis291@llnl.gov>
2025-06-17 17:03:04 -0700

    Generate at compile time tests templated on datatypes to be used in the field_tests.

---
+++ tests/cpp/include/test-basic-datatypes.hh
@@ -0,0 +1,25 @@
+#ifndef SPHERAL_BASIC_DATATYPES_HH
+#define SPHERAL_BASIC_DATATYPES_HH
+
+#include "Utilities/DataTypeTraits.hh"
+#include "config.hh"
+
+// clang-format off
+using TEST_FIELD_DATATYPES = camp::list<
+  bool
+  ,char
+  ,int
+  ,size_t
+  ,float
+  ,double
+  ,Spheral::Dim<1>::Vector
+  ,Spheral::Dim<1>::Vector3d
+  ,Spheral::Dim<1>::Tensor
+  //Dim<1>::SymTensor,
+  //Dim<1>::ThirdRankTensor,
+  //Dim<1>::FourthRankTensor,
+  //Dim<1>::FifthRankTensor
+>;
+// clang-format on
+
+#endif // SPHERAL_BASIC_DATATYPES_HH
