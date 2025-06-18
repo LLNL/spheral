ec60a56a3bb36e797bb6a1bb4129e80e058a53fa
mdavis36 <davis291@llnl.gov>
2025-06-17 17:02:08 -0700

    Field/FieldView using a basic MA type under the hood; Enforcing contract all types T MUST be trivially copyable.

--- src/Field/FieldView.hh
+++ src/Field/FieldView.hh
@@ -1,17 +1,31 @@
 #ifndef __Spheral_FieldView_hh__
 #define __Spheral_FieldView_hh__
 
+#include "chai/ManagedArray.hpp"
 #include "config.hh"
 
 namespace Spheral {
 
 template <typename Dimension, typename DataType> class FieldView {
-  DataType *m_data;
-  size_t m_size;
+
+  // Ensure the datatype is trivially copyable.
+  static_assert(std::is_trivially_copyable<DataType>::value,
+                "Error: The template type T must be trivially copyable.");
+
+  using ContainerType = typename chai::ManagedArray<DataType>;
+
+  ContainerType m_data = chai::ArrayManager::s_null_record;
 
 public:
+  SPHERAL_HOST_DEVICE FieldView() {}
+
+  SPHERAL_HOST
+  FieldView(ContainerType const &d) : m_data(d) {}
+
   SPHERAL_HOST_DEVICE
-  FieldView(DataType *d, size_t s) : m_data(d), m_size(s) {}
+  FieldView(FieldView const &) = default;
+  SPHERAL_HOST_DEVICE
+  FieldView(FieldView &&) = default;
 
   // Element access.
   SPHERAL_HOST_DEVICE
@@ -27,10 +41,10 @@ public:
   DataType &operator[](const size_t index) const { return m_data[index]; }
 
   SPHERAL_HOST_DEVICE
-  size_t size() const { return m_size; }
+  size_t size() const { return m_data.size(); }
 
   SPHERAL_HOST_DEVICE
-  DataType *data() const { return m_data; }
+  DataType *data() const { return m_data.getActivePointer(); }
 };
 
 } // namespace Spheral
