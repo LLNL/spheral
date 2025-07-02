4cd1d19dafac205bf792733bf2237812795fc292
mdavis36 <davis291@llnl.gov>
2025-07-01 19:57:50 -0700

    Generalized hash function for std::pair<>.

--- src/Utilities/Hashes.hh
+++ src/Utilities/Hashes.hh
@@ -10,10 +10,10 @@
 
 namespace std {
 
-template<>
-struct hash<std::pair<size_t, size_t>> {
-  size_t operator()(const std::pair<size_t, size_t>& p) const {
-    return hash<size_t>()(p.first) ^ hash<size_t>()(p.second);
+template<typename first, typename second>
+struct hash<std::pair<first, second>> {
+  size_t operator()(const std::pair<first, second>& p) const {
+    return hash<first>()(p.first) ^ hash<second>()(p.second);
   }
 };
 
