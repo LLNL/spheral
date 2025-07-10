bcb7fbb3a38b8e1adcfe700030f8e5184a65b076
mdavis36 <davis291@llnl.gov>
2025-07-10 14:49:45 -0700

    Decorating geom3vector w/ H/D attributes.

--- src/Geometry/Geom3Vector.hh
+++ src/Geometry/Geom3Vector.hh
@@ -30,76 +30,76 @@ class Geom3Vector
   static const Geom3Vector zero;
 
   // Constructors.
-  explicit Geom3Vector(const double x = 0.0,
+  SPHERAL_HOST_DEVICE explicit Geom3Vector(const double x = 0.0,
                        const double y = 0.0,
                        const double z = 0.0);
 
-  Geom3Vector(const Geom3Vector& vec);
+  SPHERAL_HOST_DEVICE Geom3Vector(const Geom3Vector& vec);
 
   // Implicit conversion from vectors that happen to be 3-D.
-  Geom3Vector(const GeomVector<3>& vec);
+  SPHERAL_HOST_DEVICE Geom3Vector(const GeomVector<3>& vec);
 
   // Destructor.
-  ~Geom3Vector();
+  SPHERAL_HOST_DEVICE ~Geom3Vector();
 
   // Assignment operators.
-  Geom3Vector& operator=(const Geom3Vector& rhs);
+  SPHERAL_HOST_DEVICE Geom3Vector& operator=(const Geom3Vector& rhs);
 
   // Access elements by indicies.
-  double operator()(size_type index) const;
-  double& operator()(size_type index);
+  SPHERAL_HOST_DEVICE double operator()(size_type index) const;
+  SPHERAL_HOST_DEVICE double& operator()(size_type index);
 
   // Access the individual elements by (x, y, z) notation.
-  double x() const;
-  double y() const;
-  double z() const;
-  void x(const double val);
-  void y(const double val);
-  void z(const double val);
+  SPHERAL_HOST_DEVICE double x() const;
+  SPHERAL_HOST_DEVICE double y() const;
+  SPHERAL_HOST_DEVICE double z() const;
+  SPHERAL_HOST_DEVICE void x(const double val);
+  SPHERAL_HOST_DEVICE void y(const double val);
+  SPHERAL_HOST_DEVICE void z(const double val);
 
   // Iterator access to the raw data.
-  iterator begin() { return mGeomVector.begin(); }
-  iterator end() { return mGeomVector.end(); }
+  SPHERAL_HOST_DEVICE iterator begin() { return mGeomVector.begin(); }
+  SPHERAL_HOST_DEVICE iterator end() { return mGeomVector.end(); }
 
-  const_iterator begin() const { return mGeomVector.begin(); }
-  const_iterator end() const { return mGeomVector.end(); }
+  SPHERAL_HOST_DEVICE const_iterator begin() const { return mGeomVector.begin(); }
+  SPHERAL_HOST_DEVICE const_iterator end() const { return mGeomVector.end(); }
 
   // Zero the vector.
-  void Zero() { mGeomVector.Zero(); }
+  SPHERAL_HOST_DEVICE void Zero() { mGeomVector.Zero(); }
 
   // Mathematical operators.
-  Geom3Vector operator-() const { return Geom3Vector(-mGeomVector); }
+  SPHERAL_HOST_DEVICE Geom3Vector operator-() const { return Geom3Vector(-mGeomVector); }
 
-  Geom3Vector operator+(const Geom3Vector& vec) const { return Geom3Vector(mGeomVector + vec.mGeomVector); }
-  Geom3Vector operator-(const Geom3Vector& vec) const { return Geom3Vector(mGeomVector - vec.mGeomVector); }
-  Geom3Vector operator*(const double val) const { return Geom3Vector(mGeomVector * val); }
-  Geom3Vector operator/(const double val) const { return Geom3Vector(mGeomVector / val); }
+  SPHERAL_HOST_DEVICE Geom3Vector operator+(const Geom3Vector& vec) const { return Geom3Vector(mGeomVector + vec.mGeomVector); }
+  SPHERAL_HOST_DEVICE Geom3Vector operator-(const Geom3Vector& vec) const { return Geom3Vector(mGeomVector - vec.mGeomVector); }
+  SPHERAL_HOST_DEVICE Geom3Vector operator*(const double val) const { return Geom3Vector(mGeomVector * val); }
+  SPHERAL_HOST_DEVICE Geom3Vector operator/(const double val) const { return Geom3Vector(mGeomVector / val); }
 
-  Geom3Vector& operator+=(const Geom3Vector& vec) { mGeomVector += vec.mGeomVector; return *this; }
-  Geom3Vector& operator-=(const Geom3Vector& vec) { mGeomVector -= vec.mGeomVector; return *this; }
-  Geom3Vector& operator*=(const double val) { mGeomVector *= val; return *this; }
-  Geom3Vector& operator/=(const double val) { mGeomVector /= val; return *this; }
+  SPHERAL_HOST_DEVICE Geom3Vector& operator+=(const Geom3Vector& vec) { mGeomVector += vec.mGeomVector; return *this; }
+  SPHERAL_HOST_DEVICE Geom3Vector& operator-=(const Geom3Vector& vec) { mGeomVector -= vec.mGeomVector; return *this; }
+  SPHERAL_HOST_DEVICE Geom3Vector& operator*=(const double val) { mGeomVector *= val; return *this; }
+  SPHERAL_HOST_DEVICE Geom3Vector& operator/=(const double val) { mGeomVector /= val; return *this; }
 
-  bool operator==(const Geom3Vector& vec) const { return mGeomVector == vec.mGeomVector; }
-  bool operator!=(const Geom3Vector& vec) const { return mGeomVector != vec.mGeomVector; }
-  bool operator<(const Geom3Vector& vec) const { return mGeomVector < vec.mGeomVector; }
-  bool operator>(const Geom3Vector& vec) const { return mGeomVector > vec.mGeomVector; }
-  bool operator<=(const Geom3Vector& vec) const { return mGeomVector <= vec.mGeomVector; }
-  bool operator>=(const Geom3Vector& vec) const { return mGeomVector >= vec.mGeomVector; }
+  SPHERAL_HOST_DEVICE bool operator==(const Geom3Vector& vec) const { return mGeomVector == vec.mGeomVector; }
+  SPHERAL_HOST_DEVICE bool operator!=(const Geom3Vector& vec) const { return mGeomVector != vec.mGeomVector; }
+  SPHERAL_HOST_DEVICE bool operator<(const Geom3Vector& vec) const { return mGeomVector < vec.mGeomVector; }
+  SPHERAL_HOST_DEVICE bool operator>(const Geom3Vector& vec) const { return mGeomVector > vec.mGeomVector; }
+  SPHERAL_HOST_DEVICE bool operator<=(const Geom3Vector& vec) const { return mGeomVector <= vec.mGeomVector; }
+  SPHERAL_HOST_DEVICE bool operator>=(const Geom3Vector& vec) const { return mGeomVector >= vec.mGeomVector; }
 
-  double dot(const Geom3Vector& rhs) const { return mGeomVector.dot(rhs.mGeomVector); }
-  Geom3Vector cross(const Geom3Vector& rhs) const { return Geom3Vector(mGeomVector.cross(rhs.mGeomVector)); }
-  GeomTensor<3> dyad(const Geom3Vector& rhs) const;
-  GeomSymmetricTensor<3> selfdyad() const;
-  GeomTensor<3> operator*(const Geom3Vector& rhs) const;
+  SPHERAL_HOST_DEVICE double dot(const Geom3Vector& rhs) const { return mGeomVector.dot(rhs.mGeomVector); }
+  SPHERAL_HOST_DEVICE Geom3Vector cross(const Geom3Vector& rhs) const { return Geom3Vector(mGeomVector.cross(rhs.mGeomVector)); }
+  SPHERAL_HOST_DEVICE GeomTensor<3> dyad(const Geom3Vector& rhs) const;
+  SPHERAL_HOST_DEVICE GeomSymmetricTensor<3> selfdyad() const;
+  SPHERAL_HOST_DEVICE GeomTensor<3> operator*(const Geom3Vector& rhs) const;
 
-  Geom3Vector unitVector() const { return Geom3Vector(mGeomVector.unitVector()); }
+  SPHERAL_HOST_DEVICE Geom3Vector unitVector() const { return Geom3Vector(mGeomVector.unitVector()); }
 
-  double magnitude() const { return mGeomVector.magnitude(); }
-  double magnitude2() const { return mGeomVector.magnitude2(); }
-  double minElement() const { return mGeomVector.minElement(); }
-  double maxElement() const { return mGeomVector.maxElement(); }
-  double sumElements() const { return mGeomVector.sumElements(); }
+  SPHERAL_HOST_DEVICE double magnitude() const { return mGeomVector.magnitude(); }
+  SPHERAL_HOST_DEVICE double magnitude2() const { return mGeomVector.magnitude2(); }
+  SPHERAL_HOST_DEVICE double minElement() const { return mGeomVector.minElement(); }
+  SPHERAL_HOST_DEVICE double maxElement() const { return mGeomVector.maxElement(); }
+  SPHERAL_HOST_DEVICE double sumElements() const { return mGeomVector.sumElements(); }
 
 private:
   GeomVector<3> mGeomVector;
@@ -110,6 +110,7 @@ std::istream& operator>>(std::istream& is, Geom3Vector& vec);
 std::ostream& operator<<(std::ostream& os, const Geom3Vector& vec);
 
 }
+
 
 #ifndef __GCCXML__
 #include "Geom3VectorInline.hh"
