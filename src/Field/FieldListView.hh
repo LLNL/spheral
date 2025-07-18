2c587106c6cb93e2001714ce1563700696322bf6
Landon Owen <lndnwn9@gmail.com>
2025-07-18 09:37:44 -0700

    First attempt at creating a FieldListView
    Forgot to include the FieldListView.hh

---
+++ src/Field/FieldListView.hh
@@ -0,0 +1,39 @@
+#ifndef __Spheral_FieldListView_hh__
+#define __Spheral_FieldListView_hh__
+
+#include "chai/ManagedArray.hpp"
+#include "config.hh"
+
+namespace Spheral {
+
+template <typename Dimension, typename DataType>
+class FieldListView {
+
+  using ElementType = FieldView<Dimension, DataType>;
+  using ContainerType = chai::ManagedArray<ElementType>;
+  ContainerType mData;
+
+public:
+
+  FieldListView(const ContainerType& d) : mData(d) {}
+  SPHERAL_HOST_DEVICE DataType& operator()(const size_t fieldIndex,
+                                           const size_t nodeIndx) {
+    return mData[fieldIndex][nodeIndx];
+  }
+  SPHERAL_HOST_DEVICE DataType& operator()(const size_t fieldIndex,
+                                           const size_t nodeIndx) const {
+    return mData[fieldIndex][nodeIndx];
+  }
+  SPHERAL_HOST_DEVICE ElementType operator[](const size_t index) {
+    return mData[index];
+  }
+  SPHERAL_HOST_DEVICE ElementType operator[](const size_t index) const {
+    return mData[index];
+  }
+  void move(chai::ExecutionSpace space) { mData.move(space); }
+  SPHERAL_HOST_DEVICE size_t size() const { return mData.size(); }
+  SPHERAL_HOST_DEVICE ElementType* data() const { return mData.data(); }
+  //SPHERAL_HOST ElementType* data(chai::ExecutionSpace space, bool do_move = true) const { return mData.data(space, do_move); }
+};
+} // namespace Spheral
+#endif // __Spheral_FieldListView_hh__
---
+++ tests/cpp/Field/fieldlistview_tests.cc
@@ -0,0 +1,60 @@
+#include "chai/ExecutionSpaces.hpp"
+#include "test-basic-exec-policies.hh"
+#include "test-utilities.hh"
+
+#include "Field/Field.hh"
+#include "NodeList/NodeList.hh"
+
+#include "Field/FieldList.hh"
+#include "Field/FieldView.hh"
+#include "Field/FieldListView.hh"
+
+using DIM3 = Spheral::Dim<3>;
+using FieldBase = Spheral::FieldBase<DIM3>;
+using FieldDouble = Spheral::Field<DIM3, double>;
+using FieldViewDouble = Spheral::FieldView<DIM3, double>;
+using FieldListDouble = Spheral::FieldList<DIM3, double>;
+using NodeList_t = Spheral::NodeList<DIM3>;
+
+class FieldListViewTest : public ::testing::Test {
+public:
+  NodeList_t createNodeList(size_t count) {
+    return NodeList_t("DataNodeList", count, 0);
+  }
+  int someInt = 5;
+
+  void SetUp() override {}
+};
+
+// Setting up G Test for Field
+TYPED_TEST_SUITE_P(FieldListViewTypedTest);
+template <typename T> class FieldListViewTypedTest : public FieldListViewTest {};
+
+GPU_TYPED_TEST_P(FieldListViewTypedTest, DefaultConstructor) {
+  const int N = 10;
+  const double val = 4.;
+  std::string field_name = "Field::NodeListValCtor";
+  NodeList_t nl = gpu_this->createNodeList(N);
+  FieldDouble field(field_name, nl, val);
+  FieldListDouble field_list;
+  field_list.appendField(field);
+  auto fl_v = field_list.toView();
+  printf("cpu fv %p\n", (void*)(fl_v.data()));
+  auto f_v = field_list[0].toView();
+  RAJA::forall<TypeParam>(
+       TRS_UINT(0, N),
+       [=] SPHERAL_HOST_DEVICE(size_t i) {
+         printf("gpu %p\n", (void*)(fl_v.data()));
+         //printf("%zu %zu\n", i, fl_v[0].size());
+         SPHERAL_ASSERT_EQ(fl_v[0].size(), N);
+       });
+  // EXEC_IN_SPACE_BEGIN(TypeParam)
+  //   SPHERAL_ASSERT_EQ(fl_v(0, 0), 4.0);
+  // EXEC_IN_SPACE_END()
+  // fl_v.move(chai::CPU);
+}
+
+REGISTER_TYPED_TEST_SUITE_P(FieldListViewTypedTest, DefaultConstructor);
+
+INSTANTIATE_TYPED_TEST_SUITE_P(FieldListView, FieldListViewTypedTest,
+                               typename Spheral::Test<EXEC_TYPES>::Types, );
