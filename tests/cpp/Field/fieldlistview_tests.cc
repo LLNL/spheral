c0ed02f9035168eb6499ba3918705677744053b8
mdavis36 <davis291@llnl.gov>
2025-07-19 10:39:47 -0700

    counter struct and callback are managed by the test class.

--- tests/cpp/Field/fieldlistview_tests.cc
+++ tests/cpp/Field/fieldlistview_tests.cc
@@ -1,3 +1,7 @@
+// Debug log printing can be quickly enabled for this unit test by uncommenting the
+// definition below even if Spheral was not configured w/ SPHERAL_ENABLE_LOGGER=On.
+// #define SPHERAL_ENABLE_LOGGER
+
 #include "chai/ExecutionSpaces.hpp"
 #include "test-basic-exec-policies.hh"
 #include "test-utilities.hh"
@@ -16,28 +20,28 @@ using FieldViewDouble = Spheral::FieldView<DIM3, double>;
 using FieldListDouble = Spheral::FieldList<DIM3, double>;
 using NodeList_t = Spheral::NodeList<DIM3>;
 
-static GPUCounters gcounts;
-
-// Increment variables for each action and space
-static auto callback = [](const chai::PointerRecord *,
-                          chai::Action action,
-                          chai::ExecutionSpace space) {
-      if (action == chai::ACTION_MOVE) {
-        (space == chai::CPU) ?
-          gcounts.DToHCopies++ : gcounts.HToDCopies++;
-      } else if (action == chai::ACTION_ALLOC) {
-        (space == chai::CPU) ?
-          gcounts.HNumAlloc++ : gcounts.DNumAlloc++;
-      } else if (action == chai::ACTION_FREE) {
-        (space == chai::CPU) ?
-          gcounts.HNumFree++ : gcounts.DNumFree++;
-      }
-};
+// Default Testing Size.
+static constexpr int N = 100;
 
 class FieldListViewTest : public ::testing::Test {
 public:
+  GPUCounters gcounts;
+
   NodeList_t createNodeList(size_t count) {
     return NodeList_t("DataNodeList", count, 0);
+  }
+
+  // Increment variables for each action and space
+  auto callback() {
+    return [&](const chai::PointerRecord *, chai::Action action,
+                            chai::ExecutionSpace space) {
+    if (action == chai::ACTION_MOVE)
+      (space == chai::CPU) ? gcounts.DToHCopies++ : gcounts.HToDCopies++;
+    if (action == chai::ACTION_ALLOC)
+      (space == chai::CPU) ? gcounts.HNumAlloc++ : gcounts.DNumAlloc++;
+    if (action == chai::ACTION_FREE)
+      (space == chai::CPU) ? gcounts.HNumFree++ : gcounts.DNumFree++;
+    };
   }
 };
 
@@ -46,32 +50,28 @@ TYPED_TEST_SUITE_P(FieldListViewTypedTest);
 template <typename T> class FieldListViewTypedTest : public FieldListViewTest {};
 
 GPU_TYPED_TEST_P(FieldListViewTypedTest, DefaultConstructor) {
-  gcounts.resetCounters();
-  const int N = 10;
+
   const double val = 4.;
-  std::string field_name = "Field::NodeListValCtor";
   NodeList_t nl = gpu_this->createNodeList(N);
   {
-    FieldDouble field(field_name, nl, val);
-    FieldDouble field2("Field2", nl, val);
+    FieldDouble field("TestField1", nl, val);
+    FieldDouble field2("TestField2", nl, val);
     FieldListDouble field_list;
 
     field_list.appendField(field);
     field_list.appendField(field2);
 
     const size_t numFields = field_list.size() ;
-    auto fl_v = field_list.toView(callback);
+    auto fl_v = field_list.toView(gpu_this->callback());
 
-    RAJA::forall<TypeParam>
-      (TRS_UINT(0, N),
-       [=] SPHERAL_HOST_DEVICE(size_t i) {
-         SPHERAL_ASSERT_EQ(fl_v.size(), numFields);
-         for (size_t l = 0; l < numFields; ++l) {
-           SPHERAL_ASSERT_EQ(fl_v[l].size(), N);
-         }
-       });
+    RAJA::forall<TypeParam>(TRS_UINT(0, numFields),
+      [=] SPHERAL_HOST_DEVICE (size_t i) {
+        SPHERAL_ASSERT_EQ(fl_v.size(), numFields);
+        SPHERAL_ASSERT_EQ(fl_v[i].size(), N);
+      });
     fl_v.touch(chai::CPU, true);
   }
+
   GPUCounters ref_count;
   if (typeid(RAJA::seq_exec) != typeid(TypeParam)) {
     ref_count.HToDCopies = 1;
@@ -81,7 +81,7 @@ GPU_TYPED_TEST_P(FieldListViewTypedTest, DefaultConstructor) {
   } else {
     ref_count.HNumFree = 1;
   }
-  COMP_COUNTERS(gcounts, ref_count);
+  COMP_COUNTERS(gpu_this->gcounts, ref_count);
 }
 
 // TODO: Add test for having multiple FL contain the same field
