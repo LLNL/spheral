4f7a2f18280cefdd31222541657cf11d5d8aadb5
mdavis36 <davis291@llnl.gov>
2025-07-19 12:12:29 -0700

    Ensure allocations and frees are being checked for in fieldlist_tests.

--- tests/cpp/Field/fieldlistview_tests.cc
+++ tests/cpp/Field/fieldlistview_tests.cc
@@ -3,6 +3,7 @@
 // #define SPHERAL_ENABLE_LOGGER
 
 #include "chai/ExecutionSpaces.hpp"
+#include "chai/Types.hpp"
 #include "test-basic-exec-policies.hh"
 #include "test-utilities.hh"
 
@@ -12,6 +13,7 @@
 #include "Field/FieldList.hh"
 #include "Field/FieldView.hh"
 #include "Field/FieldListView.hh"
+#include <Utilities/Logger.hh>
 
 using DIM3 = Spheral::Dim<3>;
 using FieldBase = Spheral::FieldBase<DIM3>;
@@ -49,7 +51,7 @@ public:
 TYPED_TEST_SUITE_P(FieldListViewTypedTest);
 template <typename T> class FieldListViewTypedTest : public FieldListViewTest {};
 
-GPU_TYPED_TEST_P(FieldListViewTypedTest, DefaultConstructor) {
+GPU_TYPED_TEST_P(FieldListViewTypedTest, BasicCapture) {
 
   const double val = 4.;
   NodeList_t nl = gpu_this->createNodeList(N);
@@ -69,25 +71,136 @@ GPU_TYPED_TEST_P(FieldListViewTypedTest, DefaultConstructor) {
         SPHERAL_ASSERT_EQ(fl_v.size(), numFields);
         SPHERAL_ASSERT_EQ(fl_v[i].size(), N);
       });
-    fl_v.touch(chai::CPU, true);
   }
 
   GPUCounters ref_count;
   if (typeid(RAJA::seq_exec) != typeid(TypeParam)) {
     ref_count.HToDCopies = 1;
+    ref_count.HNumAlloc = 1;
     ref_count.DNumAlloc = 1;
     ref_count.HNumFree = 1;
     ref_count.DNumFree = 1;
   } else {
+    ref_count.HNumAlloc = 1;
     ref_count.HNumFree = 1;
   }
   COMP_COUNTERS(gpu_this->gcounts, ref_count);
 }
 
 // TODO: Add test for having multiple FL contain the same field
-// TODO: Add test where toView is called in multiple scopes for the same FL
+
+GPU_TYPED_TEST_P(FieldListViewTypedTest, MultiScopeAndTouch) {
+
+  const double val = 4.;
+  NodeList_t nl = gpu_this->createNodeList(N);
+  {
+    FieldDouble field("TestField1", nl, val);
+    FieldDouble field2("TestField2", nl, val);
+    FieldListDouble field_list;
+
+    field_list.appendField(field);
+    field_list.appendField(field2);
+
+    const size_t numFields = field_list.size() ;
+
+    { // Scope 1
+    auto fl_v = field_list.toView(gpu_this->callback());
+
+    DEBUG_LOG << "Start Kernel 1";
+    RAJA::forall<TypeParam>(TRS_UINT(0, numFields),
+      [=] SPHERAL_HOST_DEVICE (size_t i) {
+        SPHERAL_ASSERT_EQ(fl_v.size(), numFields);
+        SPHERAL_ASSERT_EQ(fl_v[i].size(), N);
+      });
+    DEBUG_LOG << "Stop Kernel 1";
+
+    fl_v.touch(chai::CPU, true);
+    } // Scope 1
+
+    { // Scope 2
+    auto fl_v = field_list.toView(gpu_this->callback());
+
+    DEBUG_LOG << "Start Kernel 2";
+    RAJA::forall<TypeParam>(TRS_UINT(0, numFields),
+      [=] SPHERAL_HOST_DEVICE (size_t i) {
+        SPHERAL_ASSERT_EQ(fl_v.size(), numFields);
+        SPHERAL_ASSERT_EQ(fl_v[i].size(), N);
+      });
+    DEBUG_LOG << "Stop Kernel 2";
+    } // Scope 2
+  }
+
+  GPUCounters ref_count;
+  if (typeid(RAJA::seq_exec) != typeid(TypeParam)) {
+    ref_count.HToDCopies = 2;
+    ref_count.HNumAlloc = 2;
+    ref_count.DNumAlloc = 2;
+    ref_count.HNumFree = 2;
+    ref_count.DNumFree = 2;
+  } else {
+    ref_count.HNumAlloc = 2;
+    ref_count.HNumFree = 2;
+  }
+
+  COMP_COUNTERS(gpu_this->gcounts, ref_count);
+}
+
+GPU_TYPED_TEST_P(FieldListViewTypedTest, MultiScopeNoTouch) {
+
+  const double val = 4.;
+  NodeList_t nl = gpu_this->createNodeList(N);
+  {
+    FieldDouble field("TestField1", nl, val);
+    FieldDouble field2("TestField2", nl, val);
+    FieldListDouble field_list;
+
+    field_list.appendField(field);
+    field_list.appendField(field2);
+
+    const size_t numFields = field_list.size() ;
+
+    { // Scope 1
+    auto fl_v = field_list.toView(gpu_this->callback());
+
+    DEBUG_LOG << "Start Kernel 1";
+    RAJA::forall<TypeParam>(TRS_UINT(0, numFields),
+      [=] SPHERAL_HOST_DEVICE (size_t i) {
+        SPHERAL_ASSERT_EQ(fl_v.size(), numFields);
+        SPHERAL_ASSERT_EQ(fl_v[i].size(), N);
+      });
+    DEBUG_LOG << "Stop Kernel 1";
+    } // Scope 1
+
+    { // Scope 2
+    auto fl_v = field_list.toView(gpu_this->callback());
+
+    DEBUG_LOG << "Start Kernel 2";
+    RAJA::forall<TypeParam>(TRS_UINT(0, numFields),
+      [=] SPHERAL_HOST_DEVICE (size_t i) {
+        SPHERAL_ASSERT_EQ(fl_v.size(), numFields);
+        SPHERAL_ASSERT_EQ(fl_v[i].size(), N);
+      });
+    DEBUG_LOG << "Stop Kernel 2";
+    } // Scope 2
+  }
+
+  GPUCounters ref_count;
+  if (typeid(RAJA::seq_exec) != typeid(TypeParam)) {
+    ref_count.HToDCopies = 2;
+    ref_count.HNumAlloc = 2;
+    ref_count.DNumAlloc = 2;
+    ref_count.HNumFree = 2;
+    ref_count.DNumFree = 2;
+  } else {
+    ref_count.HNumAlloc = 2;
+    ref_count.HNumFree = 2;
+  }
+
+  COMP_COUNTERS(gpu_this->gcounts, ref_count);
+}
+
 // TODO: Add test where 
-REGISTER_TYPED_TEST_SUITE_P(FieldListViewTypedTest, DefaultConstructor);
+REGISTER_TYPED_TEST_SUITE_P(FieldListViewTypedTest, BasicCapture, MultiScopeAndTouch, MultiScopeNoTouch);
 
 INSTANTIATE_TYPED_TEST_SUITE_P(FieldListView, FieldListViewTypedTest,
                                typename Spheral::Test<EXEC_TYPES>::Types, );
